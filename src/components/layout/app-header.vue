79b3fc80da8d3b7823f51f6ad4825c49bee25985
Jason Behik <jason@aloware.com>
2021-03-10 02:36:46 +0800

    Fixes

--- src/components/layout/app-header.vue
+++ src/components/layout/app-header.vue
@@ -7,9 +7,11 @@
              dense
              icon="img:app-icons/header/nav_icon-burger.svg"
              class="mobile-menu d-none d-sm-block d-lg-none mr-2" />
-      <img :src="pageIcon"
-           class="page-icon pr-1 d-none d-sm-block d-lg-none"/>
-      <div class="page-title font-weight-bold">
+      <q-img :src="pageIcon"
+             height="15px"
+             width="20px"
+             class="page-icon d-flex mr-1 d-none d-sm-block d-lg-none" />
+      <div class="page-title font-weight-bold ">
         {{ $route.name }}
       </div>
     </div>
