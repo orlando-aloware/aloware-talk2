8c0bb4a96437ab1bba1ef131bce1166fd5d705cf
John Dave Decano <johndavedecano@gmail.com>
2021-03-13 02:42:05 +0800

    initial commit

---
+++ .storybook/config.js
@@ -0,0 +1,27 @@
+import { configure } from '@storybook/vue'
+
+import Vue from 'vue'
+
+import Quasar from 'quasar'
+
+import 'bootstrap/scss/bootstrap.scss';
+// import 'bootstrap-vue/src/index.scss';
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'quasar/dist/quasar.min.css'
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+
+import './../src/css/breakpoints.scss'
+import './../src/css/quasar.variables.scss'
+import './../src/css/mixins.scss'
+import './../src/css/fonts.scss'
+import './../src/css/variables.scss'
+
+Vue.use(Quasar)
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+configure(require.context('../src', true, /\.stories\.js$/), module)
