65ad94e1f1e3ca980ac89fb409193642577a8caf
John Dave Decano <johndavedecano@gmail.com>
2021-03-18 06:02:53 +0800

    hover

--- src/components/inbox/dialer/dialer.stories.js
+++ src/components/inbox/dialer/dialer.stories.js
@@ -1,6 +1,7 @@
 import DialerSearch from './dialer-search.vue'
 import DialerHeader from './dialer-header.vue'
 import DialerSelect from './dialer-select.vue'
+import DialerItem from './dialer-item.vue'
 
 import { storiesOf } from '@storybook/vue'
 
@@ -24,7 +25,22 @@ const select = () => ({
   template: '<dialer-select />'
 })
 
+const items = () => ({
+  components: { DialerItem },
+  template: `
+    <div class="d-flex flex-column">
+      <dialer-item avatar="https://uifaces.co/our-content/donated/ukegoVAy.jpg" initial="AL" name="Andy Lim" number="(000) 111-2222" />
+      <dialer-item avatar="https://uifaces.co/our-content/donated/ukegoVAy.jpg" initial="AL" name="Andy Lim" number="(000) 111-2222" />
+      <dialer-item avatar="https://uifaces.co/our-content/donated/ukegoVAy.jpg" initial="AL" name="Andy Lim" number="(000) 111-2222" />
+      <dialer-item avatar="https://uifaces.co/our-content/donated/ukegoVAy.jpg" initial="AL" name="Andy Lim" number="(000) 111-2222" />
+      <dialer-item avatar="https://uifaces.co/our-content/donated/ukegoVAy.jpg" initial="AL" name="Andy Lim" number="(000) 111-2222" />
+      <dialer-item avatar="https://uifaces.co/our-content/donated/ukegoVAy.jpg" initial="AL" name="Andy Lim" number="(000) 111-2222" />
+    </div>
+  `
+})
+
 storiesOf('Inbox - Dialer', module)
   .add('Search', search)
   .add('Header', header)
   .add('Select', select)
+  .add('items', items)
