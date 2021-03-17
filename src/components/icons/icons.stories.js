c9b0439a40ed150143166bc0dc8ffb8c55e220f3
John Dave Decano <johndavedecano@gmail.com>
2021-03-18 01:25:58 +0800

    added small icons

--- src/components/icons/icons.stories.js
+++ src/components/icons/icons.stories.js
@@ -1,5 +1,6 @@
 import InboxIconsView from './inbox-icons-view.vue'
 import CallsIconsView from './calls-icons-view.vue'
+import CallsSmIconsView from './calls-sm/calls-sm-icons-view'
 
 import { storiesOf } from '@storybook/vue'
 
@@ -13,6 +14,12 @@ const callsIcons = () => ({
   template: '<calls-icons-view />'
 })
 
+const callsIconsSm = () => ({
+  components: { CallsSmIconsView },
+  template: '<calls-sm-icons-view />'
+})
+
 storiesOf('Icons', module)
   .add('Inbox', inboxIcons)
   .add('Calls', callsIcons)
+  .add('Calls - Small', callsIconsSm)
