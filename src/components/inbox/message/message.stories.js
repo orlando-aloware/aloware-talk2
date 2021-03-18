dd2110ad5a534d10db8bc0e32b71a3735ddb6cae
John Dave Decano <johndavedecano@gmail.com>
2021-03-19 04:12:20 +0800

    inbox text box

--- src/components/inbox/message/message.stories.js
+++ src/components/inbox/message/message.stories.js
@@ -1,5 +1,6 @@
 import { storiesOf } from '@storybook/vue'
 
+import MessageBox from './message-box'
 import MessageHeader from './message-header'
 
 const header = () => ({
@@ -18,5 +19,21 @@ const header = () => ({
   template: '<message-header name="John Dave Decano" status="Inbound..." @answer="answer" @decline="decline" @info="info" /> '
 })
 
+const messagebox = () => ({
+  components: { MessageBox },
+  methods: {
+    decline () {
+      console.log('decline')
+    },
+    answer () {
+      console.log('answer1')
+    },
+    info () {
+      console.log('info')
+    }
+  },
+  template: '<message-box /> '
+})
 storiesOf('Inbox - Message', module)
   .add('Header', header)
+  .add('Message Box', messagebox)
