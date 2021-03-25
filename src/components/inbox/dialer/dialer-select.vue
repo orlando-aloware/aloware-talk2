8d9472bde2b88b7b84dbffe983102b63859dd4b0
John Dave Decano <johndavedecano@gmail.com>
2021-03-26 04:05:45 +0800

    inbox responsive fixes

--- src/components/inbox/dialer/dialer-select.vue
+++ src/components/inbox/dialer/dialer-select.vue
@@ -3,43 +3,45 @@
     <div class="dialer-select__label">
       Call using:
     </div>
-    <div class="dialer-select__value">
-      <span class="dialer-select__item" @click="toggle">Aloware Main (000) 123-4567</span>
-      <div class="dialer-select__icon" ref="show" @click="toggle"><i class="gg-chevron-down"></i></div>
+    <div class="dialer-select__value" @click="toggle">
+      <span class="dialer-select__item">Aloware Main (000) 123-4567</span>
+      <div class="dialer-select__icon" ref="show"><i class="gg-chevron-down"></i></div>
     </div>
-    <div ref="drop"
-         class="dialer-select__dropdown"
-         :class="{'dialer-select__dropdown--show animate__animated animate__fadeIn': showing, 'animate__animated animate__fadeOut': !showing}">
-      <div class="dialer-select__dropdown__search">
-        <input type="text" class="form-control search-control" placeholder="Search...">
+    <portal to="app">
+      <div ref="drop"
+           class="dialer-select__dropdown"
+           :class="{'dialer-select__dropdown--show animate__animated animate__fadeIn': showing, 'animate__animated animate__fadeOut': !showing}">
+        <div class="dialer-select__dropdown__search">
+          <input type="text" class="form-control search-control" placeholder="Search...">
+        </div>
+        <div class="dialer-select__dropdown__items" @click="toggle">
+          <div class="dialer-select__dropdown__item">
+            Aloware Main (000) 123-4567
+          </div>
+          <div class="dialer-select__dropdown__item">
+            Aloware Main (000) 123-4567
+          </div>
+          <div class="dialer-select__dropdown__item">
+            Aloware Main (000) 123-4567
+          </div>
+          <div class="dialer-select__dropdown__item">
+            Aloware Main (000) 123-4567
+          </div>
+          <div class="dialer-select__dropdown__item">
+            Aloware Main (000) 123-4567
+          </div>
+          <div class="dialer-select__dropdown__item">
+            Aloware Main (000) 123-4567
+          </div>
+          <div class="dialer-select__dropdown__item">
+            Aloware Main (000) 123-4567
+          </div>
+          <div class="dialer-select__dropdown__item">
+            Aloware Main (000) 123-4567
+          </div>
+        </div>
       </div>
-      <div class="dialer-select__dropdown__items" @click="toggle">
-        <div class="dialer-select__dropdown__item">
-          Aloware Main (000) 123-4567
-        </div>
-        <div class="dialer-select__dropdown__item">
-          Aloware Main (000) 123-4567
-        </div>
-        <div class="dialer-select__dropdown__item">
-          Aloware Main (000) 123-4567
-        </div>
-        <div class="dialer-select__dropdown__item">
-          Aloware Main (000) 123-4567
-        </div>
-        <div class="dialer-select__dropdown__item">
-          Aloware Main (000) 123-4567
-        </div>
-        <div class="dialer-select__dropdown__item">
-          Aloware Main (000) 123-4567
-        </div>
-        <div class="dialer-select__dropdown__item">
-          Aloware Main (000) 123-4567
-        </div>
-        <div class="dialer-select__dropdown__item">
-          Aloware Main (000) 123-4567
-        </div>
-      </div>
-    </div>
+    </portal>
   </div>
 </template>
 
@@ -60,6 +62,9 @@ export default {
       this.showing = !this.showing
       console.log(this.showing)
       popper.forceUpdate()
+    },
+    onWindowResize () {
+      this.showing = false
     }
   },
   mounted () {
@@ -76,11 +81,13 @@ export default {
         ]
       })
     })
+    window.addEventListener('resize', this.onWindowResize)
   },
   beforeDestroy () {
     if (popper && typeof popper.destroy === 'function') {
       popper.destroy()
     }
+    window.removeEventListener('resize', this.onWindowResize)
   }
 }
 </script>
@@ -126,7 +133,7 @@ export default {
   flex-direction: column;
 
   &__dropdown {
-    width: 102%;
+    min-width: 250px;
     min-height: 100px;
     max-height: 200px;
     background-color: $white;
@@ -136,7 +143,8 @@ export default {
     visibility: hidden;
     overflow: hidden;
     border: solid 1px $grey-light3;
-    @include border-radius(10px);
+    @include border-radius(5px);
+    z-index: -1;
 
     &__items {
       padding-left: 10px;
@@ -161,6 +169,9 @@ export default {
       padding-left: 10px;
       padding-right: 10px;
       transition: background-color 100ms ease-in;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      overflow: hidden;
 
       &:hover {
         background-color: $grey-light2;
@@ -169,16 +180,18 @@ export default {
 
     &__search {
       padding: 10px;
+      box-shadow: 0 0 10px 0 rgb(0 0 0 / 10%);
     }
 
     &--show {
       visibility: visible;
+      z-index: 3;
     }
   }
 
   &__label {
     font-size: 12px;
-    color: $grey-light7;
+    color: $grey-light5;
     line-height: 16px;
     opacity: 0.9;
   }
--- src/components/inbox/message/message-box.vue
+++ src/components/inbox/message/message-box.vue
@@ -3,29 +3,92 @@
     <div class="message-box__input">
       <input type="text" :placeholder="placeholder" @focus="handleFocus" @blur="handleBlur"/>
     </div>
+
     <input type="file" ref="attachment" class="message-box__file" @change="handleFileChanged"/>
-    <button class="message-box__attach" @click="attach">
-      <attach-icon/>
+
+    <button class="message-box__action" ref="actionbtn" @click="toggleAction">
+      <setting-icon icon-color="grey-mid"/>
     </button>
+
     <button class="message-box__btn">
       <send-icon/>
     </button>
+    <portal to="app">
+      <div ref="actiondropdown" class="message-actions"
+           :class="{
+            'message-actions--show animate__animated animate__fadeIn': actionShowing,
+            'message-actions--show animate__animated animate__fadeOut': !actionShowing}">
+        <a class="message-actions__item">
+          <div class="message-actions__item__icon">
+            <attach-icon></attach-icon>
+          </div>
+          <div class="message-actions__item__label">
+            Templates
+          </div>
+        </a>
+        <a class="message-actions__item">
+          <div class="message-actions__item__icon">
+            <attach-icon></attach-icon>
+          </div>
+          <div class="message-actions__item__label">
+            Send Media File
+          </div>
+        </a>
+        <a class="message-actions__item">
+          <div class="message-actions__item__icon">
+            <attach-icon></attach-icon>
+          </div>
+          <div class="message-actions__item__label">
+            Send GIF
+          </div>
+        </a>
+        <a class="message-actions__item">
+          <div class="message-actions__item__icon">
+            <attach-icon></attach-icon>
+          </div>
+          <div class="message-actions__item__label">
+            Send Fax
+          </div>
+        </a>
+      </div>
+    </portal>
   </div>
 </template>
 
 <script>
+import { createPopper } from '@popperjs/core'
+
 import SendIcon from 'components/icons/send-icon'
+import SettingIcon from 'components/icons/setting-icon'
 import AttachIcon from 'components/icons/attach-icon'
+
+let actionPopper
 
 export default {
   name: 'message-box.vue',
-  components: { AttachIcon, SendIcon },
+  components: { AttachIcon, SettingIcon, SendIcon },
   data () {
     return {
-      focused: false
+      focused: false,
+      actionShowing: false
     }
   },
+  mounted () {
+    this.$nextTick(() => {
+      this.createActionPopper()
+    })
+  },
   methods: {
+    createActionPopper () {
+      actionPopper = createPopper(this.$refs['actionbtn'], this.$refs['actiondropdown'], {
+        placement: 'top-end'
+      })
+    },
+    toggleAction () {
+      this.actionShowing = !this.actionShowing
+      console.log(this.actionShowing)
+      actionPopper.forceUpdate()
+    },
     handleFocus () {
       this.focused = true
     },
@@ -54,6 +117,52 @@ export default {
 <style lang="scss" scoped>
 @import 'src/css/mixins.scss';
 @import 'src/css/variables.scss';
+@import 'src/css/breakpoints.scss';
+
+.message-actions {
+  min-width: 200px;
+  min-height: 100px;
+  max-height: 200px;
+  background-color: $white;
+  position: absolute;
+  box-shadow: 0 0 10px 0 rgb(0 0 0 / 10%);
+  display: flex;
+  flex-direction: column;
+  overflow-x: hidden;
+  overflow-y: auto;
+  visibility: hidden;
+  border: solid 1px $grey-light3;
+  @include border-radius(10px);
+  z-index: -1;
+
+  &--show {
+    visibility: visible;
+    z-index: 3;
+  }
+
+  &__item {
+    display: flex;
+    height: 40px;
+    align-items: center;
+    text-decoration: none;
+    border-bottom: solid 1px $grey-light;
+    cursor: pointer;
+    transition: background-color 100ms ease-in;
+    &:hover {
+      background-color: $grey-light2;
+    }
+
+    &__icon {
+      padding-right: 5px;
+      padding-left: 10px;
+    }
+    &__label {
+      font-size: 12px;
+      color: $black;
+      flex-grow: 1;
+    }
+  }
+}
 
 .message-box {
   display: flex;
@@ -74,7 +183,7 @@ export default {
     right: -3000px;
   }
 
-  &__attach {
+  &__action {
     background-color: $white;
     border: 0;
     height: 38px;
