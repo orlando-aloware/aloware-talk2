0a868f6ac7ee3083558bbf8aad1ca4e2d6f3c8c2
John Dave Decano <johndavedecano@gmail.com>
2021-05-22 01:13:55 +0800

    fix folders max height

--- src/pages/contacts/_components/contacts-folders.vue
+++ src/pages/contacts/_components/contacts-folders.vue
@@ -147,7 +147,7 @@ export default {
   flex-direction: column;
   flex-grow: 1;
   min-height: 500px;
-  max-height: calc(100vh - 385px);
+  max-height: calc(100vh - 500px);
   &__header {
     min-height: 40px;
     font-size: 10px;
