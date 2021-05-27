5948b8a1a0c4c92982742cd5f62d187a713c41d3
John Dave Decano <johndavedecano@gmail.com>
2021-05-28 04:10:48 +0800

    fix sidebar max height

--- src/pages/contacts/_components/contacts-folders.vue
+++ src/pages/contacts/_components/contacts-folders.vue
@@ -141,7 +141,7 @@ export default {
   flex-direction: column;
   flex-grow: 1;
   min-height: 500px;
-  max-height: calc(100vh - 150px);
+  max-height: calc(100vh - 350px);
   &__header {
     min-height: 40px;
     font-size: 10px;
