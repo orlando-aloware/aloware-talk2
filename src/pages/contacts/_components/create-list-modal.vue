c5c680b9341c481ebe4efccd020bdf950cdf570a
John Dave Decano <johndavedecano@gmail.com>
2021-05-28 12:15:13 +0800

    fix redirection

--- src/pages/contacts/_components/create-list-modal.vue
+++ src/pages/contacts/_components/create-list-modal.vue
@@ -131,7 +131,7 @@ export default {
           const data = response.data.data
           const message = response.data.message
 
-          this.$router.push(`/contacts/list/${data.id}`)
+          this.$router.push(`/contacts/list/${data.id}/add`)
 
           this.createListClose()
 
