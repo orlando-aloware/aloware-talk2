b46711b271b650239b36a53554f131f0dbe00c03
Jason Behik <jason@aloware.com>
2021-06-04 06:02:36 +0800

    Fixed merge issues

--- src/pages/contacts/_components/contacts-filters.vue
+++ src/pages/contacts/_components/contacts-filters.vue
@@ -6,70 +6,85 @@
       rounded="sm"
       style="width: 100%"
     >
-      <div class="filter-contents">
-        <div class="p-4">
-          <!-- Using slots -->
-          <div class="pt-2"
-               v-if="step == 1">
-            <compact-btn
-              variant="primary"
-              customClass="px-4 add-filters"
-              :onClick="toAddFiltersStep"
-            >
-              <i class="material-icons mr-1 add-icon">add</i> Add a Filter
-            </compact-btn>
-          </div>
-          <div v-else-if="step == 2">
-            <div class="mb-3">
-              <contacts-table-search placeholder="Search"
-                                     searchOnKeyup
-                                     @search="searchFilter"/>
+      <b-card header="Primary"
+              header-bg-variant="primary"
+              header-text-variant="white">
+        <template #header >
+          <div class="d-flex justify-content-between">
+            <div class="d-inline-flex">
+              <b-button variant="outline-primary header-buttons" size="sm"><i class="fa fa-arrow-left"></i> </b-button>
+              <h6 class="mb-0">Filters</h6>
             </div>
-            <b-list-group class="filter-list">
-              <b-list-group-item class="filter-divider">
-                All properties
-              </b-list-group-item>
-              <b-list-group-item
-                class="filter-list-item"
-                v-for="filter in filtersFiltered"
-                :key="filter.key"
-                @click="selectFilter(filter)"
-                >
-                {{ filter.label }}
-              </b-list-group-item>
-            </b-list-group>
+
+            <b-button variant="outline-primary header-buttons btn-close-filter" size="sm" @click="onCloseFilter"><i class="fa fa-times"></i> </b-button>
           </div>
-          <div class="step-3"
-               v-else-if="step == 3">
-            <span class="filter-label">{{ selectedFilter.label }}</span>
-            <div v-if="selectedFilter.type == 'string'">
-              <div v-for="operator in selectedFilter.operators"
-                      :key="(selectedFilter.key + '-' + operator.value)"
+        </template>
+        <div class="filter-contents">
+          <div class="p-4">
+            <!-- Using slots -->
+            <div class="pt-2"
+                 v-if="step == 1">
+              <compact-btn
+                variant="primary"
+                customClass="px-4 add-filters"
+                :onClick="toAddFiltersStep"
               >
-                <q-radio class="m-0"
-                         :val="operator.value"
-                         :label="operator.label"
-                         v-model="filterOperator"
+                <i class="material-icons mr-1 add-icon">add</i> Add a Filter
+              </compact-btn>
+            </div>
+            <div v-else-if="step == 2">
+              <div class="mb-3">
+                <h6 class="contact-prop-label">Contact properties</h6>
+                <contacts-table-search placeholder="Search"
+                                       searchOnKeyup
+                                       @search="searchFilter"/>
+              </div>
+              <b-list-group class="filter-list">
+                <b-list-group-item class="filter-divider">
+                  All properties
+                </b-list-group-item>
+                <b-list-group-item
+                  class="filter-list-item"
+                  v-for="filter in filtersFiltered"
+                  :key="filter.key"
+                  @click="selectFilter(filter)"
+                  >
+                  {{ filter.label }}
+                </b-list-group-item>
+              </b-list-group>
+            </div>
+            <div class="step-3"
+                 v-else-if="step == 3">
+              <span class="filter-label">{{ selectedFilter.label }}</span>
+              <div v-if="selectedFilter.type == 'string'">
+                <div v-for="operator in selectedFilter.operators"
+                        :key="(selectedFilter.key + '-' + operator.value)"
                 >
-                </q-radio>
-                <q-select
-                  class="filter-operation border"
-                  borderless
-                  dense
-                  use-input
-                  use-chips
-                  multiple
-                  input-debounce="0"
-                  v-if="operator.value == filterOperator && hasValue"
-                  v-model="filterOperatorValue"
-                  :options="filterOptions"
-                  @new-value="createValue"
-                />
+                  <q-radio class="m-0"
+                           :val="operator.value"
+                           :label="operator.label"
+                           v-model="filterOperator"
+                  >
+                  </q-radio>
+                  <q-select
+                    class="filter-operation border"
+                    borderless
+                    dense
+                    use-input
+                    use-chips
+                    multiple
+                    input-debounce="0"
+                    v-if="operator.value == filterOperator && hasValue"
+                    v-model="filterOperatorValue"
+                    :options="filterOptions"
+                    @new-value="createValue"
+                  />
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </b-card>
     </b-overlay>
   </div>
 </template>
@@ -83,7 +98,7 @@ export default {
   data () {
     return {
       items: Array.from(new Array(10)),
-      show: true,
+      show: false,
       filterSearch: '',
       step: 1,
       selectedFilter: null,
