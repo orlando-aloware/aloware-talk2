03d6ecff6ce5639c34eb5e6f51260e559b26f89a
Jason Behik <jason@aloware.com>
2021-06-19 05:38:50 +0800

    Added mixins and constant

--- src/store/index.js
+++ src/store/index.js
@@ -33,6 +33,7 @@ export default function (/* { ssrContext } */) {
       changelogs: [],
       dispositionStatuses: [],
       callDispositions: [],
+      broadcasts: [],
       filters: [],
       firstLogin: false,
       userStatus: false,
