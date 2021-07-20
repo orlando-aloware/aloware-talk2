04aa91fc5bb069c3c3eca233109ec38fd31d1861
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2021-07-20 10:52:13 -0700

    Updated npm packages
    - @quasar/extras 1.9.15 => 1.10.8
    - @quasar/quasar-app-extension-dotenv 1.0.5 => 1.1.0
    - @sentry/vue 6.0.3 => 6.9.0
    - countries-and-timezones 2.4.0 => 2.5.0
    - electron 9.4.2 => 13.1.7
    - electron-devtools-installer 3.1.1 => 3.2.0
    - electron-log 4.3.1 => 4.3.5
    - electron-packager 15.2.0 => 15.3.0
    - electron-updater 4.3.5 => 4.3.9
    - eslint-plugin-import 2.22.1 => 2.23.4
    - eslint-plugin-promise 4.2.1 => 5.1.0
    - google-libphonenumber 3.2.16 => 3.2.22
    - humanize-duration 3.25.1 => 3.27.0
    - moment-timezone 0.5.32 => 0.5.33
    - vue-custom-scrollbar 1.4.0 => 1.4.1

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -8,20 +8,20 @@ window.axios.defaults.baseURL = process.env.API_URL
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 
-if (Platform.is.electron) {
-  window.axios.defaults.headers.common['Requested-From'] = 'talk2-desktop'
-}
-
-if (Platform.within.iframe) {
-  window.axios.defaults.headers.common['Requested-From'] = 'talk2-iframe'
-}
-
 if (Platform.is.desktop) {
   window.axios.defaults.headers.common['Requested-From'] = 'talk2-web'
 }
 
 if (Platform.is.mobile) {
   window.axios.defaults.headers.common['Requested-From'] = 'mobile-web'
+}
+
+if (Platform.is.electron) {
+  window.axios.defaults.headers.common['Requested-From'] = 'talk2-desktop'
+}
+
+if (Platform.within.iframe) {
+  window.axios.defaults.headers.common['Requested-From'] = 'talk2-iframe'
 }
 
 let apiToken = localStorage.getItem('api_token')
