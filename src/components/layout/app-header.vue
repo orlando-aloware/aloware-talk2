31f7c4218d4a7c1417ab0e751708a786e47fe2e7
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2021-07-20 11:35:58 -0700

    Removed call active componenet and references and cleaned up header

--- src/components/layout/app-header.vue
+++ src/components/layout/app-header.vue
@@ -1,101 +1,18 @@
 <template>
   <q-toolbar class="page-header">
     <div class="d-flex h-100 align-items-center">
-      <q-btn flat
-             @click="toggleSidebar"
-             round
-             dense
-             icon="img:app-icons/header/nav_icon-burger.svg"
-             class="mobile-menu d-none d-sm-block d-lg-none mr-2" />
-      <q-img :src="pageIcon"
-             height="20px"
-             width="20px"
-             class="page-icon mr-1 d-none d-sm-block d-lg-none" />
       <div class="page-title font-weight-bold">
         {{ $route.name }}
       </div>
     </div>
     <div class="ml-auto d-none d-lg-block">
       <div class="d-flex h-100 align-items-center">
-<!--        <q-btn flat-->
-<!--               @click="toggleSidebar"-->
-<!--               round-->
-<!--               dense-->
-<!--               icon="img:app-icons/header/nav_icon-dialer.svg"-->
-<!--               class="mr-2 ml-auto"-->
-<!--               style="color: #202125;" />-->
-        <call-active name="May Kerr" time="00:00" />
-        <q-btn flat
-               @click="toggleSidebar"
-               round
-               dense
-               icon="img:app-icons/header/nav_icon-notification.svg"
-               class="mr-2 ml-auto"
-               style="color: #202125;" />
-        <div v-if="user.profile"
-             class="d-none d-lg-block mr-2 ml-auto">
-          <span>
-            <div class="small-text">
-              <strong>{{ user.profile.name }}</strong>
-              <template v-if="phoneNumber">
-                | {{ phoneNumber | fixPhone }}
-              </template>
-            </div>
-            <div class="xs-text inactive status-label w-100 text-right">{{ statusLabel }}</div>
-          </span>
-        </div>
-        <q-btn-dropdown avatar
-                        flat
-                        round
-                        dropdown-icon="img:app-icons/header/arrow-down.svg"
-                        class="profile-menu ml-auto d-none d-lg-block"
-                        content-style="{ padding: '0' }">
-          <template v-if="user.profile"
-                    v-slot:label>
-            <div class="items-center no-wrap">
-              <div class="text-center">
-                <span class="d-flex justify-content-center align-items-center w-40 avatar agent-avatar grey-300"
-                      v-bind:style="avatarStyle(user.profile.name)">
-                    <span>{{ user.profile.name | initials }}</span>
-                    <i class="b-white bottom"
-                       :class="[ $options.filters.agentStatusClass(user.profile.agent_status) ]">
-                    </i>
-                </span>
-              </div>
-            </div>
-          </template>
-          <q-list class="list-drp">
-            <q-item class="pl-3 pr-3"
-                    v-close-popup
-                    clickable>
-              <q-item-section>
-                Test 1
-              </q-item-section>
-            </q-item>
-            <q-item class="pl-3 pr-3"
-                    v-close-popup
-                    clickable>
-              Test 2
-            </q-item>
-          </q-list>
-        </q-btn-dropdown>
+
       </div>
     </div>
     <div class="ml-auto d-block d-sm-none">
       <div class="d-flex h-100 align-items-center">
-        <q-btn flat
-               round
-               dense
-               icon="img:app-icons/header/search.svg"
-               class="mr-2 ml-auto"
-               style="color: #202125;" />
-        <q-btn flat
-               @click="toggleSidebar"
-               round
-               dense
-               icon="img:app-icons/header/inactive/notification.svg"
-               class="mr-2 ml-auto"
-               style="color: #202125;" />
+
       </div>
     </div>
   </q-toolbar>
@@ -106,11 +23,8 @@ import * as AgentStatus from '../../constants/agent-status'
 import { avatarMixin } from '../../boot/mixins'
 import { mapGetters, mapState } from 'vuex'
 
-import CallActive from 'components/inbox/call-active/call-active'
-
 export default {
   name: 'app-header',
-  components: { CallActive },
   mixins: [
     avatarMixin
   ],
