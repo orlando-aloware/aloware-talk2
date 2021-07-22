1c56559d6cfaf7d35207cabdf5a4cb95a1da5091
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2021-07-22 08:46:47 -0700

    Fixes

--- src/components/layout/app-header.vue
+++ src/components/layout/app-header.vue
@@ -95,6 +95,33 @@
                  align="center"
                  flat
                  @click="toggleDialer">
+            <q-menu>
+              <div class="row no-wrap q-pa-md">
+                <div class="column">
+                  <div class="text-h6 q-mb-md">Settings</div>
+                  <q-toggle v-model="mobileData" label="Use Mobile Data" />
+                  <q-toggle v-model="bluetooth" label="Bluetooth" />
+                </div>
+
+                <q-separator vertical inset class="q-mx-lg" />
+
+                <div class="column items-center">
+                  <q-avatar size="72px">
+                    <img src="https://cdn.quasar.dev/img/avatar4.jpg">
+                  </q-avatar>
+
+                  <div class="text-subtitle1 q-mt-md q-mb-xs">John Doe</div>
+
+                  <q-btn
+                    color="primary"
+                    label="Logout"
+                    push
+                    size="sm"
+                    v-close-popup
+                  />
+                </div>
+              </div>
+            </q-menu>
           </q-btn>
         </q-item>
       </div>
