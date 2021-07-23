2c7d9eec228f72dbb1dc29025750a122a3a2e380
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2021-07-23 09:01:19 -0700

    Refactoring

---
+++ src/components/dialer/dialer.vue
@@ -0,0 +1,182 @@
+<template>
+  <div class="row no-wrap q-pa-md width-290">
+    <div class="col no-padding max-width-266">
+      <line-selector v-model="campaignId"></line-selector>
+
+      <div class="tab-links d-inline-flex w-100">
+        <b-link href="#"
+                :class="{ active : mode === 'call' }"
+                @click="setMode('call')">
+          Call
+        </b-link>
+        <b-link href="#"
+                :class="{ active : mode === 'text' }"
+                @click="setMode('text')">
+          Text
+        </b-link>
+      </div>
+      <div class="d-inline-flex align-items-end justify-content-between dialer w-100 pb-2">
+        <b-form-group :label="label"
+                      :invalid-feedback="invalidPhoneNumber"
+                      :state="validPhoneNumber"
+                      class="mt-2 mb-0">
+          <contact-phone-number-search v-model="phoneNumber"
+                                       @change="changePhoneNumber">
+          </contact-phone-number-search>
+        </b-form-group>
+        <q-btn :ripple="true"
+               :disable="!validPhoneNumber || !phoneNumber.length"
+               v-show="mode == 'call'"
+               icon="img:app-icons/dialer/call_btn_small.svg"
+               size="36px"
+               class="icon-btn auto-size height-36"
+               align="right"
+               padding="none"
+               rounded
+               flat>
+        </q-btn>
+        <q-btn :ripple="true"
+               :disable="!validPhoneNumber || !phoneNumber.length"
+               v-show="mode == 'text'"
+               icon="img:app-icons/dialer/text_btn_small.svg"
+               size="36px"
+               class="icon-btn auto-size height-36"
+               align="right"
+               padding="none"
+               rounded
+               flat>
+        </q-btn>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapState } from 'vuex'
+import ContactPhoneNumberSearch from 'components/dialer/contact-phone-number-search'
+import LineSelector from 'components/dialer/line-selector'
+import * as UserOutboundCallingModes from 'src/constants/user-outbound-calling-modes'
+
+export default {
+  name: 'dialer.vue',
+
+  components: { ContactPhoneNumberSearch, LineSelector },
+
+  props: {
+    value: {
+      type: Boolean,
+      required: false
+    }
+  },
+
+  data () {
+    return {
+      label: 'Call a number',
+      mode: 'call',
+      defaultOutboundCampaignId: null,
+      campaignId: null,
+      phoneNumber: ''
+    }
+  },
+
+  computed: {
+    ...mapState(['currentCompany']),
+    ...mapGetters('auth', ['user']),
+
+    validPhoneNumber () {
+      return this.$options.filters.fixPhone(this.phoneNumber) !== false
+    },
+
+    invalidPhoneNumber () {
+      return 'Please enter a valid phone number'
+    }
+  },
+
+  mounted () {
+    if (this.value) {
+      this.showDialer()
+    } else {
+      this.hideDialer()
+    }
+  },
+
+  methods: {
+    showDialer () {
+      // find default outbound campaign
+      this.findDefaultOutboundCampaign()
+      this.$emit('show')
+    },
+
+    hideDialer () {
+      this.resetForm()
+      this.$emit('hide')
+    },
+
+    resetForm () {
+      this.phoneNumber = ''
+      this.defaultOutboundCampaignId = null
+      this.campaignId = null
+      this.label = 'Call a number'
+      this.mode = 'call'
+    },
+
+    changePhoneNumber (phoneNumber) {
+      this.phoneNumber = phoneNumber
+    },
+
+    findDefaultOutboundCampaign () {
+      this.campaignId = null
+      this.defaultOutboundCampaignId = null
+
+      // force outbound line on all users
+      if (this.currentCompany && this.currentCompany.default_outbound_campaign_id && this.currentCompany.force_outbound_line) {
+        this.defaultOutboundCampaignId = this.currentCompany.default_outbound_campaign_id
+        this.campaignId = this.defaultOutboundCampaignId
+        return
+      }
+
+      // outbound line is set to use account default and account has a default
+      if (this.currentCompany && this.currentCompany.default_outbound_campaign_id && this.user.profile && this.user.profile.outbound_calling_mode === UserOutboundCallingModes.OUTBOUND_CALLING_MODE_DEFAULT && !this.user.profile.default_outbound_campaign_id) {
+        this.defaultOutboundCampaignId = this.currentCompany.default_outbound_campaign_id
+        this.campaignId = this.defaultOutboundCampaignId
+        return
+      }
+
+      // user has a default outbound line
+      if (this.user.profile && this.user.profile.default_outbound_campaign_id && this.user.profile.outbound_calling_mode === UserOutboundCallingModes.OUTBOUND_CALLING_MODE_DEFAULT) {
+        this.defaultOutboundCampaignId = this.user.profile.default_outbound_campaign_id
+        this.campaignId = this.defaultOutboundCampaignId
+        return
+      }
+
+      // user has to choose outbound line every time
+      if (this.user.profile && this.user.profile.outbound_calling_mode === UserOutboundCallingModes.OUTBOUND_CALLING_MODE_ALWAYS_ASK) {
+        this.defaultOutboundCampaignId = null
+        this.campaignId = null
+      }
+    },
+
+    setMode (mode) {
+      this.mode = mode
+      switch (mode) {
+        case 'call':
+          this.label = 'Call a number'
+          break
+        case 'text':
+          this.label = 'Text a number'
+          break
+      }
+    }
+  },
+
+  watch: {
+    value () {
+      if (this.value) {
+        this.showDialer()
+      } else {
+        this.hideDialer()
+      }
+    }
+  }
+}
+</script>
--- src/components/layout/app-header.vue
+++ src/components/layout/app-header.vue
@@ -99,56 +99,7 @@
                     self="top right"
                     @before-show="showDialer"
                     @before-hide="hideDialer">
-              <div class="row no-wrap q-pa-md width-290">
-                <div class="col no-padding max-width-266">
-                  <line-selector v-model="campaignId"></line-selector>
-
-                  <div class="tab-links d-inline-flex w-100">
-                    <b-link href="#"
-                            :class="{ active : mode === 'call' }"
-                            @click="setMode('call')">
-                      Call
-                    </b-link>
-                    <b-link href="#"
-                            :class="{ active : mode === 'text' }"
-                            @click="setMode('text')">
-                      Text
-                    </b-link>
-                  </div>
-                  <div class="d-inline-flex align-items-end justify-content-between dialer w-100 pb-2">
-                    <b-form-group :label="label"
-                                  :invalid-feedback="invalidPhoneNumber"
-                                  :state="validPhoneNumber"
-                                  class="mt-2 mb-0">
-                      <contact-phone-number-search v-model="phoneNumber"
-                                                   @change="changePhoneNumber">
-                      </contact-phone-number-search>
-                    </b-form-group>
-                    <q-btn :ripple="true"
-                           :disable="!validPhoneNumber"
-                           v-show="mode == 'call'"
-                           icon="img:app-icons/dialer/call_btn_small.svg"
-                           size="36px"
-                           class="icon-btn auto-size height-36"
-                           align="right"
-                           padding="none"
-                           rounded
-                           flat>
-                    </q-btn>
-                    <q-btn :ripple="true"
-                           :disable="!validPhoneNumber"
-                           v-show="mode == 'text'"
-                           icon="img:app-icons/dialer/text_btn_small.svg"
-                           size="36px"
-                           class="icon-btn auto-size height-36"
-                           align="right"
-                           padding="none"
-                           rounded
-                           flat>
-                    </q-btn>
-                  </div>
-                </div>
-              </div>
+              <dialer v-model="dialerStatus"></dialer>
             </q-menu>
           </q-btn>
         </q-item>
@@ -164,26 +115,21 @@
 
 <script>
 import { mapGetters, mapState } from 'vuex'
-import * as AgentStatus from '../../constants/agent-status'
 import { aclMixin, agentMixin, avatarMixin } from 'src/plugins/mixins'
-import LineSelector from 'components/dialer/line-selector'
-import * as UserOutboundCallingModes from 'src/constants/user-outbound-calling-modes'
-import ContactPhoneNumberSearch from 'components/dialer/contact-phone-number-search'
+import Dialer from 'components/dialer/dialer'
+import * as AgentStatus from '../../constants/agent-status'
 
 export default {
   name: 'app-header',
-  components: { ContactPhoneNumberSearch, LineSelector },
+
+  components: { Dialer },
+
   mixins: [aclMixin, avatarMixin, agentMixin],
 
   data () {
     return {
       dialerIcon: 'img:app-icons/header/dialer_gray.svg',
       dialerStatus: false,
-      label: 'Call a number',
-      mode: 'call',
-      defaultOutboundCampaignId: null,
-      campaignId: null,
-      phoneNumber: '',
       AgentStatus
     }
   },
@@ -212,14 +158,6 @@ export default {
       }
     },
 
-    validPhoneNumber () {
-      return this.$options.filters.fixPhone(this.phoneNumber) !== false
-    },
-
-    invalidPhoneNumber () {
-      return 'Please enter a valid phone number'
-    },
-
     personalPhoneNumber () {
       let found = this.campaigns.find(campaign => campaign.id === this.user.profile.campaign_id)
       if (found && found.incoming_numbers.length) {
@@ -235,73 +173,18 @@ export default {
     },
 
     showDialer () {
-      // find default outbound campaign
-      this.findDefaultOutboundCampaign()
-
-      this.dialerStatus = true
       this.dialerIcon = 'img:app-icons/header/dialer_active.svg'
-      this.$emit('showDialer')
+      this.dialerStatus = true
     },
 
     hideDialer () {
-      this.phoneNumber = ''
-
-      this.dialerStatus = false
       this.dialerIcon = 'img:app-icons/header/dialer_gray.svg'
-      this.$emit('hideDialer')
+      this.dialerStatus = false
     },
 
     changeStatus (status) {
       this.changeAgentStatus(status)
       this.$refs.menu.hide()
-    },
-
-    changePhoneNumber (phoneNumber) {
-      this.phoneNumber = phoneNumber
-    },
-
-    findDefaultOutboundCampaign () {
-      this.campaignId = null
-      this.defaultOutboundCampaignId = null
-
-      // force outbound line on all users
-      if (this.currentCompany && this.currentCompany.default_outbound_campaign_id && this.currentCompany.force_outbound_line) {
-        this.defaultOutboundCampaignId = this.currentCompany.default_outbound_campaign_id
-        this.campaignId = this.defaultOutboundCampaignId
-        return
-      }
-
-      // outbound line is set to use account default and account has a default
-      if (this.currentCompany && this.currentCompany.default_outbound_campaign_id && this.user.profile && this.user.profile.outbound_calling_mode === UserOutboundCallingModes.OUTBOUND_CALLING_MODE_DEFAULT && !this.user.profile.default_outbound_campaign_id) {
-        this.defaultOutboundCampaignId = this.currentCompany.default_outbound_campaign_id
-        this.campaignId = this.defaultOutboundCampaignId
-        return
-      }
-
-      // user has a default outbound line
-      if (this.user.profile && this.user.profile.default_outbound_campaign_id && this.user.profile.outbound_calling_mode === UserOutboundCallingModes.OUTBOUND_CALLING_MODE_DEFAULT) {
-        this.defaultOutboundCampaignId = this.user.profile.default_outbound_campaign_id
-        this.campaignId = this.defaultOutboundCampaignId
-        return
-      }
-
-      // user has to choose outbound line every time
-      if (this.user.profile && this.user.profile.outbound_calling_mode === UserOutboundCallingModes.OUTBOUND_CALLING_MODE_ALWAYS_ASK) {
-        this.defaultOutboundCampaignId = null
-        this.campaignId = null
-      }
-    },
-
-    setMode (mode) {
-      this.mode = mode
-      switch (mode) {
-        case 'call':
-          this.label = 'Call a number'
-          break
-        case 'text':
-          this.label = 'Text a number'
-          break
-      }
     }
   }
 }
