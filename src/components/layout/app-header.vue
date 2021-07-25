974416e0a559dec43f9a755adbeace1e7f97b156
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2021-07-25 16:44:36 -0700

    Code refactoring

--- src/components/layout/app-header.vue
+++ src/components/layout/app-header.vue
@@ -5,83 +5,7 @@
     </div>
     <div class="ml-auto d-none d-lg-block h-100">
       <div class="d-flex h-100 align-items-center">
-        <q-item>
-          <q-item-section>
-            <q-item-label class="text-regular _500">{{ profile.first_name }}</q-item-label>
-          </q-item-section>
-          <q-item-section class="profile-menu"
-                          avatar>
-            <q-btn-dropdown :ripple="false"
-                            :disabled="loadingAgentStatus || ['RECEIVED_CALL_INVITE', 'CALL_CONNECTED'].includes(dialer.currentStatus)"
-                            :menu-offset="[4, 16]"
-                            class="tab-dropdown"
-                            ref="menu"
-                            auto-close
-                            flat>
-              <template v-slot:label>
-                <q-avatar size="34px"
-                          v-if="profile"
-                          :style="avatarStyle(profile.name)">
-                  {{ profile.name | fixName | initials }}
-                  <q-badge :color="color(profile.agent_status)"
-                           class="availability-status"
-                           floating>
-                  </q-badge>
-                </q-avatar>
-              </template>
-
-              <q-list class="tab-dropdown-list no-select">
-                <q-item @click="changeStatus(AgentStatus.AGENT_STATUS_OFFLINE)"
-                        :class="[profile.agent_status === AgentStatus.AGENT_STATUS_OFFLINE ? 'text-primary _500' : '']"
-                        dense
-                        clickable>
-                  <div class="d-flex align-items-center">
-                    <q-badge :color="color(AgentStatus.AGENT_STATUS_OFFLINE)"
-                             class="rounded-badge q-mr-sm">
-                    </q-badge>
-                    Offline
-                  </div>
-                </q-item>
-
-                <q-item @click="changeStatus(AgentStatus.AGENT_STATUS_ACCEPTING_CALLS)"
-                        :class="[profile.agent_status === AgentStatus.AGENT_STATUS_ACCEPTING_CALLS ? 'text-primary _500' : '']"
-                        dense
-                        clickable>
-                  <div class="d-flex align-items-center">
-                    <q-badge :color="color(AgentStatus.AGENT_STATUS_ACCEPTING_CALLS)"
-                             class="rounded-badge q-mr-sm">
-                    </q-badge>
-                    Available
-                  </div>
-                </q-item>
-
-                <q-item @click="changeStatus(AgentStatus.AGENT_STATUS_NOT_ACCEPTING_CALLS)"
-                        :class="[profile.agent_status === AgentStatus.AGENT_STATUS_NOT_ACCEPTING_CALLS ? 'text-primary _500' : '']"
-                        dense
-                        clickable>
-                  <div class="d-flex align-items-center">
-                    <q-badge :color="color(AgentStatus.AGENT_STATUS_NOT_ACCEPTING_CALLS)"
-                             class="rounded-badge q-mr-sm">
-                    </q-badge>
-                    Busy
-                  </div>
-                </q-item>
-
-                <q-item @click="changeStatus(AgentStatus.AGENT_STATUS_ON_BREAK)"
-                        :class="[profile.agent_status === AgentStatus.AGENT_STATUS_ON_BREAK ? 'text-primary _500' : '']"
-                        dense
-                        clickable>
-                  <div class="d-flex align-items-center">
-                    <q-badge :color="color(AgentStatus.AGENT_STATUS_ON_BREAK)"
-                             class="rounded-badge q-mr-sm">
-                    </q-badge>
-                    On-break
-                  </div>
-                </q-item>
-              </q-list>
-            </q-btn-dropdown>
-          </q-item-section>
-        </q-item>
+        <profile></profile>
 
         <q-separator class="height-28 ml-3 mr-3 margin-auto position-relative"
                      vertical>
@@ -119,57 +43,19 @@
 </template>
 
 <script>
-import { mapGetters, mapState } from 'vuex'
-import { aclMixin, agentMixin, avatarMixin } from 'src/plugins/mixins'
 import DialerForm from 'components/dialer/dialer-form'
 import ActiveCall from 'components/dialer/active-call'
-import * as AgentStatus from '../../constants/agent-status'
+import Profile from 'components/profile'
 
 export default {
   name: 'app-header',
 
-  components: { ActiveCall, DialerForm },
-
-  mixins: [aclMixin, avatarMixin, agentMixin],
+  components: { ActiveCall, DialerForm, Profile },
 
   data () {
     return {
       dialerIcon: 'img:app-icons/header/dialer_gray.svg',
-      dialerStatus: false,
-      AgentStatus
-    }
-  },
-
-  computed: {
-    ...mapState(['currentCompany', 'dialer', 'campaigns']),
-    ...mapGetters('auth', ['profile']),
-
-    statusLabel () {
-      switch (this.profile.agent_status) {
-        case AgentStatus.AGENT_STATUS_OFFLINE:
-          return 'Offline'
-        case AgentStatus.AGENT_STATUS_ACCEPTING_CALLS:
-          return 'Available'
-        case AgentStatus.AGENT_STATUS_ON_BREAK:
-          return 'On-break'
-        case AgentStatus.AGENT_STATUS_NOT_ACCEPTING_CALLS:
-        case AgentStatus.AGENT_STATUS_ON_CALL:
-        case AgentStatus.AGENT_STATUS_ON_WRAP_UP:
-        case AgentStatus.AGENT_STATUS_RINGING:
-        case AgentStatus.AGENT_STATUS_AUTO_DIAL:
-        case AgentStatus.AGENT_STATUS_SENTRY:
-          return 'Busy'
-        default:
-          return 'Offline'
-      }
-    },
-
-    personalPhoneNumber () {
-      let found = this.campaigns.find(campaign => campaign.id === this.profile.campaign_id)
-      if (found && found.incoming_numbers.length) {
-        return found.incoming_numbers[0].phone_number
-      }
-      return ''
+      dialerStatus: false
     }
   },
 
@@ -195,11 +81,6 @@ export default {
     hideDialer () {
       this.dialerIcon = 'img:app-icons/header/dialer_gray.svg'
       this.dialerStatus = false
-    },
-
-    changeStatus (status) {
-      this.changeAgentStatus(status)
-      this.$refs.menu.hide()
     }
   }
 }
