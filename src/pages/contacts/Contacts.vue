462c1be587c00afe63ddd8b3314b4ed95b87746a
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2021-07-31 20:36:28 -0700

    Cleanup

--- src/pages/contacts/Contacts.vue
+++ src/pages/contacts/Contacts.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="row mx-0 content-row d-flex overflow-hidden">
-    <div class="col-lg-2 px-0 pl-lg-2 pr-lg-3 mb-3"
+    <div class="col-lg-2 px-0 pl-lg-2 pr-1 mb-3"
          v-if="$route.name === 'Contacts'">
       <contacts-sidebar v-if="auth.authenticated"></contacts-sidebar>
     </div>
-    <div class="col-lg-10 px-0 pr-lg-3 mb-3 main"
+    <div class="col-lg-10 px-0 pr-1 mb-3 main"
          :class="[ $route.name === 'Contacts' ? 'col-lg-10' : 'col-lg-12' ]">
       <router-view v-if="auth.authenticated"></router-view>
     </div>
