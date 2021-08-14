d16040cd07365be9f115c8dbbe6e15a828661766
Jason Behik <jason@aloware.com>
2021-08-14 09:32:46 +0800

    Fix

--- src/components/generic-selectors/contact-tags.vue
+++ src/components/generic-selectors/contact-tags.vue
@@ -1,7 +1,7 @@
 <template>
   <b-card class="border-0 tags-wrapper"
           v-if="hasPermissionTo('tag contact')">
-    <generic-multi-select :="Tags"
+    <generic-multi-select label="Tags"
                           :values="contact.tag_ids"
                           :options="combinedTags"
                           :canEdit="hasPermissionTo(['list tag', 'view tag'])"
