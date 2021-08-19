1f596806b5cd3699d560fe6cdaeebe058518e994
reggie <reggie@aloware.com>
2021-08-19 23:46:41 +0800

    Fixed scrolling issue when changing inbox task status.

--- src/components/inbox/inbox-tab.vue
+++ src/components/inbox/inbox-tab.vue
@@ -60,8 +60,12 @@
             </template>
           </q-btn-toggle>
         </div>
-        <div class="h-100 w-100 flex-grow-1 scroll-y" @scroll="handleScroll">
-          <inbox-task-list :contacts="contacts" @onItemSelected="onItemSelected"></inbox-task-list>
+        <div class="h-100 w-100 flex-grow-1 scroll-y"
+             ref="taskListScroller"
+             @scroll="handleScroll">
+          <inbox-task-list :contacts="contacts"
+                           @onItemSelected="onItemSelected">
+          </inbox-task-list>
         </div>
       </div>
       <template #overlay>
@@ -87,13 +91,16 @@ import Vue from 'vue'
 let scrollTimeout
 export default {
   name: 'inbox-tab',
+
   components: { InboxTaskList, CallsHeader },
+
   props: {
     searchText: {
       type: String,
       default: ''
     }
   },
+
   computed: {
     ...mapState('inbox', ['taskCounts', 'contacts', 'selectedContact']),
     nextPage () {
@@ -111,6 +118,7 @@ export default {
       }
     }
   },
+
   data () {
     return {
       currentTask: ContactTaskStatus.STATUS_OPEN,
@@ -147,7 +155,7 @@ export default {
       isFetchingContacts: false,
       hasMore: false,
       isLoadingMore: false,
-      isLoaded: true,
+      isLoaded: false,
       currentPage: 0,
       page: 1,
       perPage: 20
@@ -169,6 +177,7 @@ export default {
     },
     loadMoreContactTasks () {
       this.isFetchingContacts = true
+      this.isLoaded = false
       this.getContactsByTaskStatus(this.currentTask).then(response => {
         this.setContacts([...this.contacts, ...response.data.data])
         this.isFetchingContacts = false
@@ -232,6 +241,7 @@ export default {
     },
     resetList () {
       this.page = 1
+      this.isLoaded = false
       this.loadContactTasks()
       if (!this.$route.params.id) {
         this.setSelectedContact({})
@@ -250,7 +260,11 @@ export default {
           this.currentTask = ContactTaskStatus.STATUS_OPEN
       }
     },
-    onToggleStatus (value) {
+    onToggleStatus () {
+      this.$nextTick(() => {
+        this.$refs.taskListScroller.scrollTop = 0
+      })
+
       this.$router.push({
         name: 'Inbox Channel Task Status',
         params: {
@@ -275,7 +289,9 @@ export default {
       })
     }
   },
+
   created () {
+    this.setContacts([])
     this.setStatus()
 
     this.loadContactTasks()
@@ -319,15 +335,17 @@ export default {
       })
     })
 
-    this.$VueEvent.listen('contact_task_status_updated', (contact) => {
+    this.$VueEvent.listen('contact_task_status_updated', () => {
       this.loadContactTasks()
     })
   },
+
   mounted () {
     if (['Inbox Channel', 'Inbox'].includes(this.$route.name)) {
       this.setSelectedContact({})
     }
   },
+
   watch: {
     'sorting.order': function () {
       this.loadContactTasks()
@@ -342,6 +360,11 @@ export default {
       if (['Inbox'].includes(value)) {
         this.currentTask = ContactTaskStatus.STATUS_OPEN
         this.resetList()
+      }
+    },
+    '$route.params.id': function (value) {
+      if (!value) {
+        this.setSelectedContact({})
       }
     }
   }
