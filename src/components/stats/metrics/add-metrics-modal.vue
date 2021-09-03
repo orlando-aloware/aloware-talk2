90a621097932a7ad568fd738c4f1a36e6d55f8e9
Karl Fabe <karl@aloware.com>
2021-09-04 06:00:36 +0800

    Fixed add metrics bug

--- src/components/stats/metrics/add-metrics-modal.vue
+++ src/components/stats/metrics/add-metrics-modal.vue
@@ -12,33 +12,66 @@
           style="width: 300px;">
           <q-card-section class="pt-4">
             <div class="text-center text-h6 pt-3 pb-4">Add Metric</div>
-            <b-form-select
+            <q-select
+              @popup-show="onShowMetricsMenu"
+              outlined dense emit-value
               v-model="metrics"
               :options="metricOptions"
-              icon="exclamation-triangle"
-              class="my-3">
-              <template #first>
-                <b-form-select-option :value="null" disabled>Add Metrics</b-form-select-option>
+              option-value="text"
+              option-label="text"
+              ref="statsSelectMetrics"
+              :popup-content-style="`width: ${selectWidth}px; word-break: break-all;`">
+              <template v-slot:selected>
+                <template v-if="metrics">
+                  {{ metrics }}
+                </template>
+                <template v-else>
+                  Add Metrics
+                </template>
               </template>
-            </b-form-select>
+              <template v-slot:option="scope">
+                <q-item v-bind="scope.itemProps" v-on="scope.itemEvents">
+                  <q-item-section v-if="!scope.opt.disable" avatar></q-item-section>
+                  <q-item-section>
+                    <q-item-label
+                      v-if="scope.opt.disable"
+                      class="text-subtitle2 font-weight-medium"
+                      disabled label
+                      v-html="scope.opt.text" />
+                    <q-item-label
+                      v-else
+                      v-html="scope.opt.text" />
+                  </q-item-section>
+                </q-item>
+              </template>
+            </q-select>
             <q-select
+              @popup-show="onShowColorMenu"
               outlined dense
               v-model="color"
               :options="colors"
               option-value="text"
-              option-label="text">
-              <!-- <template v-slot:selected>
+              option-label="text"
+              ref="statsSelectTextColor"
+              :popup-content-style="`width: ${selectWidth}px; word-break: break-all;`">
+              <template v-slot:selected>
                 <template v-if="color">
+                  <q-icon
+                    :color="color.value"
+                    name="font_download"
+                    class="pr-2" />
                   {{ color.text }}
                 </template>
                 <template v-else>
                   Select Color
                 </template>
-              </template> -->
+              </template>
               <template v-slot:option="scope">
-                <q-item v-bind="scope.itemProps">
+                <q-item v-bind="scope.itemProps" v-on="scope.itemEvents">
                   <q-item-section avatar>
-                    <q-icon :color="scope.opt.value" name="font_download" />
+                    <q-icon
+                      :color="scope.opt.value"
+                      name="font_download" />
                   </q-item-section>
                   <q-item-section>
                     <q-item-label v-html="scope.opt.text" />
@@ -63,13 +96,12 @@
 
 <script>
 
+// import ContactDispositionSelector from 'components/generic-selectors/contact-disposition-selector'
 import {
-  METRIC_OPTIONS,
   METRIC_OPTIONS_2
 } from 'src/constants/stats'
 
-const stats = { METRIC_OPTIONS }
-const stats2 = { METRIC_OPTIONS_2 }
+const stats = { METRIC_OPTIONS_2 }
 
 export default {
   name: 'AddMetricsModal',
@@ -83,20 +115,10 @@ export default {
       default: ''
     }
   },
-  components: {
-    // VueMultiselect,
-    // PredefinedTimeSelector
-  },
   computed: {
     metricOptions () {
       if (stats) {
-        return stats.METRIC_OPTIONS
-      }
-      return []
-    },
-    options () {
-      if (stats) {
-        return stats2.METRIC_OPTIONS_2
+        return stats.METRIC_OPTIONS_2
       }
       return []
     },
@@ -106,6 +128,31 @@ export default {
         { value: 'red', text: 'Warning' },
         { value: 'blue', text: 'Success' }
       ]
+    },
+    times () {
+      const times = []
+      for (let hour = 6; hour < 24; hour++) {
+        times.push({
+          label: window.moment({ hour }).format('h:mm A'),
+          value: window.moment({ hour }).format('HH:mm')
+        })
+        times.push({
+          label: window.moment({ hour, minute: 15 }).format('h:mm A'),
+          value: window.moment({ hour, minute: 15 }).format('HH:mm')
+        }
+        )
+        times.push({
+          label: window.moment({ hour, minute: 30 }).format('h:mm A'),
+          value: window.moment({ hour, minute: 30 }).format('HH:mm')
+        }
+        )
+        times.push({
+          label: window.moment({ hour, minute: 45 }).format('h:mm A'),
+          value: window.moment({ hour, minute: 45 }).format('HH:mm')
+        }
+        )
+      }
+      return times
     }
   },
   data () {
@@ -113,7 +160,8 @@ export default {
       modal: false,
       metrics: null,
       model: '',
-      color: ''
+      color: '',
+      selectWidth: 0
     }
   },
   watch: {
@@ -136,6 +184,12 @@ export default {
     },
     resetData () {
       this.metrics = ''
+    },
+    onShowColorMenu () {
+      this.selectWidth = this.$refs.statsSelectTextColor.$el.offsetWidth
+    },
+    onShowMetricsMenu () {
+      this.selectWidth = this.$refs.statsSelectMetrics.$el.offsetWidth
     }
   }
 }
