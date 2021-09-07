4d0602e0da6b5135ece0d23b648b0bac51b9e325
Jason Behik <jason@aloware.com>
2021-09-08 04:42:17 +0800

    Fixes on filters

--- src/components/contacts/contacts-filters.vue
+++ src/components/contacts/contacts-filters.vue
@@ -323,23 +323,34 @@ export default {
           filterGroups.splice(groupIndex, 1)
           continue
         }
+
         for (let filterIndex in filterGroups[groupIndex].filters) {
           if (filterIndex === 'search') {
             continue
           }
+
           const found = this.filters.find(filter => filter.key === filterIndex)
           const operators = found ? _.get(found, 'operators', null) : null
+
           if (operators) {
             const operator = found.operators.find(operator => operator.value === filterGroups[groupIndex].filters[filterIndex].operator)
             const options = operator ? _.get(operator, 'options', null) : null
             const option = options ? options.find(option => option.value === filterGroups[groupIndex].filters[filterIndex].value) : null
-            const trueValue = option ? [option.label] : filterGroups[groupIndex].filters[filterIndex].value
+            let trueValue = filterGroups[groupIndex].filters[filterIndex].value
+
+            if (option) {
+              trueValue = [option.label]
+            } else if (typeof filterGroups[groupIndex].filters[filterIndex].value === 'string') {
+              trueValue = filterGroups[groupIndex].filters[filterIndex].value.split(',')
+            }
+
+            const newValue = [trueValue.join(' and ')]
             filterGroups[groupIndex].filters[filterIndex] = {
               key: filterIndex,
               label: found.label,
               operator: operator.label,
               trueValue: trueValue,
-              value: JSON.stringify(trueValue)
+              value: JSON.stringify(newValue)
             }
           } else {
             filterGroups[groupIndex].filters[filterIndex] = {
