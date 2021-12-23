db20d8b1cb3d14cfa70ce61b57e20b1446da191f
reg-aloware <reggie@aloware.com>
2021-12-24 06:01:06 +0800

    Changed put to patch.

--- src/plugins/api/api.js
+++ src/plugins/api/api.js
@@ -184,7 +184,7 @@ export default {
         return window.axios.get(`${suffixV1}user/get-users-with-access-to-contact?contact_id=${contactId}`)
       },
       setDefaultLogin (userId, params) {
-        return window.axios.put(`${suffixV1}user/${userId}/set-default-app`, params)
+        return window.axios.patch(`${suffixV1}user/${userId}/set-default-app`, params)
       }
     },
     reports: {
