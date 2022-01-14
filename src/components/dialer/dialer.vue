22c6cfbce85d5dbcbb3192c7f9fdc7ccb3de0aed
Jason Behik <jason@aloware.com>
2022-01-15 00:19:27 +0800

    Fixed call fishing hangup and connect

--- src/components/dialer/dialer.vue
+++ src/components/dialer/dialer.vue
@@ -677,6 +677,7 @@ export default {
 
     unparkCommunication (parkedCallData) {
       this.loadingUnpark = true
+      this.setDialerParkedCall()
       let data = {
         currentNumber: 'unhold:' + parkedCallData.id,
         outboundCampaignId: parkedCallData.campaign_id,
