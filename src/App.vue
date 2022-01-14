eb335c2c53fd757dce12a7a554e37b106579ea4d
Jason Behik <jason@aloware.com>
2022-01-15 06:51:28 +0800

    Bridge fixes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="q-app">
-    <router-view />
+    <router-view :key="routerKey" />
     <portal-target name="app"
                    multiple>
     </portal-target>
@@ -17,8 +17,93 @@
 </template>
 <script>
 import ActionNotification from 'components/action-notification'
+import { mapActions, mapState } from 'vuex'
 export default {
   name: 'App',
-  components: { ActionNotification }
+  components: { ActionNotification },
+  data () {
+    return {
+      routerKey: 0,
+      sharedCookie: null
+    }
+  },
+  computed: {
+    ...mapState('auth', ['profile', 'authenticated'])
+  },
+  created () {
+    // proceed to cookie validation if account is talk allowed access
+    this.getSharedCookie().then(sharedCookie => {
+      this.sharedCookie = sharedCookie
+
+      // localStorage.getItem('shared_cookie') !== this.sharedCookie &&
+      if (this.$route.name !== 'Login') {
+        this.validateCookieUser()
+      }
+    })
+  },
+  mounter () {
+    // if account is not allowed to access talk, we need to logout
+    if (this.profile && !this.profile.company.talk_enabled) {
+      this.logout()
+    } else {
+      // proceed to cookie validation if account is talk allowed access
+      this.getSharedCookie().then(sharedCookie => {
+        this.sharedCookie = sharedCookie
+
+        if (localStorage.getItem('shared_cookie') !== this.sharedCookie && this.$route.name !== 'Login') {
+          this.validateCookieUser()
+        }
+      })
+    }
+  },
+  methods: {
+    async validateCookieUser () {
+      if (this.sharedCookie) {
+        this.getCookieUser().then(response => {
+          this.cookieUserValidated(response)
+        }).catch(() => {
+          this.logout()
+        })
+      }
+    },
+    async cookieUserValidated ({ data: { data } }) {
+      const { usage, company } = data
+      this.resetVuex()
+      this.setCurrentCompany(company)
+      this.setUsage(usage)
+      this.routerKey += 1
+
+      localStorage.setItem('shared_cookie', this.sharedCookie)
+      localStorage.setItem('company_id', company.id)
+
+      const urlParams = new URLSearchParams(window.location.search)
+      const fromClassic = Number(urlParams.get('from_classic'))
+
+      // we need to redirect and reload if coming from classic instead of simply router push
+      if (fromClassic) {
+        location.href = '/'
+      }
+    },
+    logout () {
+      this.logoutUser().then((res) => {
+        this.response = res.data
+        this.$router.push({ name: 'Login' }).catch((err) => {
+          console.log(err)
+        })
+      }).catch((err) => {
+        console.log(err)
+      })
+    },
+    ...mapActions('auth', {
+      logoutUser: 'logout',
+      getCookieUser: 'getCookieUser',
+      getSharedCookie: 'getSharedCookie'
+    }),
+    ...mapActions([
+      'resetVuex',
+      'setUsage',
+      'setCurrentCompany'
+    ])
+  }
 }
 </script>
