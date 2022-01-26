a1a6d77ba137b2e8e2b8dbfd983c3392699db30d
Karl Fabe <karl@aloware.com>
2022-01-27 00:22:37 +0800

    Added class

--- src/components/generic-selectors/script-selector.vue
+++ src/components/generic-selectors/script-selector.vue
@@ -4,7 +4,7 @@
             :loading="loadingScripts"
             :disable="disable || loadingScripts"
             :class="[ prepend ? 'with-prepend' : '' ]"
-            class="generic-selector"
+            class="padded-container generic-selector"
             v-model="scriptId"
             options-selected-class="text-primary"
             color="primary"
