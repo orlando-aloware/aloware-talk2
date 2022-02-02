51de721fe7b23a2ff257ab664102612991e79687
Karl Fabe <karl@aloware.com>
2022-02-03 06:48:33 +0800

    Metrics should be available on PD

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1698,6 +1698,11 @@ export default {
         this.metricsDataLoaded = true
       }
 
+      if (to.name === 'Power Dialer') {
+        this.getAvailableMetrics()
+        this.getMetricGroups()
+      }
+
       if (!this.isMobile) {
         this.setShowContactsHeader(true)
       }
