e1c9f87402bcbd6af363f9d0775d8dc0b7ec6173
reg-aloware <reggie@aloware.com>
2022-02-25 08:40:54 +0800

    Update value on contact disposition status.

--- src/components/contacts/contact-information.vue
+++ src/components/contacts/contact-information.vue
@@ -27,7 +27,7 @@
                                       :use-chips="false"
                                       :outlined="false"
                                       :show-placeholder="false"
-                                      :emit-value="false"
+                                      :emit-value="true"
                                       custom-class="inline-select"
                                       v-model="contact.disposition_status_id"
                                       @change="(eventPayload) => onUpdateFields(eventPayload, 'disposition_status_id')">
