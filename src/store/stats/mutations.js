cd85b018a941fcd0578b68b7a40cf5aca5d995c4
Jason Behik <jason@aloware.com>
2022-03-26 00:31:58 +0800

    Fixed metric value representation issue

--- src/store/stats/mutations.js
+++ src/store/stats/mutations.js
@@ -152,7 +152,7 @@ export default {
       index.data = Object.keys(state.metricGroups[metricGroupIndex].agent_metrics).length
     }
 
-    const metric = state.availableMetrics.find(metric => metric.metric_id === data.data.metric_id)
+    const metric = state.availableMetrics.find(item => item.metric_id === parseInt(data.data.metric_id))
 
     if (metric) {
       data.data.category = metric.category
