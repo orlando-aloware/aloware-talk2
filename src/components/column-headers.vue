5d6fc9edb77c3daae5cbf877dc932055e8e9804a
Karl Fabe <karl@aloware.com>
2022-04-19 22:48:21 +0800

    Temporary implementation -- relations only fetch on Contacts

--- src/components/column-headers.vue
+++ src/components/column-headers.vue
@@ -265,9 +265,8 @@ export default {
 
       // we need to reload contacts data to include relations data
       const relations = this.currentColumns.filter(item => ALL_RELATIONS.includes(item.name))
-      console.log('relations :>> ', relations)
-      console.log('101 :>> ', relations.length && this.hasAddedRelation)
-      if (relations.length && this.hasAddedRelation) {
+
+      if (relations.length && this.hasAddedRelation && this.isContactsRoute) {
         this.$VueEvent.fire('clearContacts')
         this.$VueEvent.fire('fetchContacts')
       }
