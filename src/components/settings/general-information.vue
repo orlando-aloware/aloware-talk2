2ecea7c9e890550e5b23be6bb302b87bba809f7b
alberto-aloware <alberto@aloware.com>
2022-04-27 17:45:53 -0300

    Adds skeleton to loading ring group name on settings

--- src/components/settings/general-information.vue
+++ src/components/settings/general-information.vue
@@ -46,9 +46,10 @@
               <li v-for="ringGroupId in profile.ring_group_ids"
                   :key="ringGroupId"
                   class="pb-0">
-                  <span class="text-grey-90 _400 fs-12">
+                  <span class="text-grey-90 _400 fs-12" v-if="ringGroups.length > 0">
                       {{ getRingGroupName(ringGroupId) || 'Ring group data not available' }}
                   </span>
+                  <q-skeleton type="text" animation="fade" height="20px" v-else />
               </li>
             </ul>
 
