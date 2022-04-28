376e047d48ebc5208352e8b54c7c59da21689515
Jonathan Marmitt <jmarmitt.br@gmail.com>
2022-04-28 15:38:14 -0300

    Enroll to Sequence and Power Dialer buttons hidden

--- src/components/bulk-action-menu.vue
+++ src/components/bulk-action-menu.vue
@@ -4,24 +4,18 @@
       <div class="items">
         <span>{{ getSelectedCount }} selected</span>
       </div>
-      <div class="items">
-        <a disabled>
-           <q-tooltip anchor="top middle"
-                      self="bottom middle"
-                      max-width="150px">
-            In development
-          </q-tooltip>
+      <!-- <div class="items">
+        <a href="#" disabled>
           <i class="fa fa-layer-group"></i>
           Enroll in Sequence
         </a>
       </div>
       <div class="items">
-        <a href=""
-           @click.prevent="onAddToPowerDialer">
+        <a href="#" disabled>
           <i class="fa fa-crosshairs"></i>
           Power Dialer
         </a>
-      </div>
+      </div> -->
       <div class="items">
         <a href=""
            @click="onAddToStaticList">
@@ -50,7 +44,6 @@
 
 import { mapActions, mapGetters } from 'vuex'
 import { FROM_BULK_MENU } from 'src/constants/contacts-list-create-mode'
-import extractErrorMessage from 'src/plugins/helpers/extract-error-message'
 
 export default {
   name: 'bulk-action-menu',
@@ -67,7 +60,7 @@ export default {
     }
   },
   methods: {
-    ...mapActions('contacts', ['removeContactOpen', 'setBulkDelete', 'createListOpen', 'selectListOpen', 'setSelectedStaticList', 'setShouldUpdateSelectedListContactCount']),
+    ...mapActions('contacts', ['removeContactOpen', 'setBulkDelete', 'createListOpen', 'selectListOpen', 'setSelectedStaticList']),
     onDelete (e) {
       this.setBulkDelete(true)
       this.$bvModal.show('remove-contact-dialog')
@@ -87,26 +80,6 @@ export default {
       })
       this.setSelectedStaticList({ id: null, name: '', type: null })
       e.preventDefault()
-    },
-    onAddToPowerDialer (e) {
-      const contactIds = this.selectedContacts.all.map(contact => contact.id)
-      this.isLoading = true
-
-      return this.$axios
-        .post('api/v2/power-dialer-list-items', { contact_ids: contactIds })
-        .then(() => {
-          this.setShouldUpdateSelectedListContactCount(true)
-          this.$router.push(`/power-dialer`)
-          this.$generalNotification('Selected contacts were successfully added')
-        })
-        .catch((err) => {
-          const { message, html } = extractErrorMessage(err)
-          console.log(html)
-          this.$generalNotification(message, 'error')
-        })
-        .finally(() => {
-          this.isLoading = false
-        })
     }
   }
 }
