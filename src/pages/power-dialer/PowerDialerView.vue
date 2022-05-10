d8b046be029b75a130dc2fd37e7ab5e7b1255917
Karl Fabe <karl@aloware.com>
2022-05-10 08:03:36 +0800

    Style adjustments on filters UI

--- src/pages/power-dialer/PowerDialerView.vue
+++ src/pages/power-dialer/PowerDialerView.vue
@@ -23,9 +23,9 @@
         <b-container fluid class="bv-example-row m-0 p-0 pb-0 border-bottom">
           <b-row
             class="pr-2 pt-4 pb-3"
-            v-if="$q.screen.name !== 'lg'">
+            v-if="$q.screen.lt.lg">
             <b-col cols="12">
-              <div class="d-flex">
+              <div class="d-flex">11111111
                 <PowerDialerFilter
                   :list-data="fixedContactsData"
                   :id="selectedListId"
@@ -47,8 +47,8 @@
             </b-col>
             <b-col
               cols="8"
-              v-if="$q.screen.name === 'lg'">
-              <div class="d-flex">
+              v-if="$q.screen.gt.md">
+              <div class="d-flex">22222222
                 <PowerDialerFilter
                   :list-data="fixedContactsData"
                   :id="selectedListId"
