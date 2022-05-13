0efa7dee81a32e17bc2ef5b2611b731ae8691118
Jonathan M <jonathan@aloware.com>
2022-05-13 16:44:24 -0300

    Function removed

--- src/pages/power-dialer/PowerDialer.vue
+++ src/pages/power-dialer/PowerDialer.vue
@@ -255,7 +255,6 @@ export default {
         }).finally(() => {
           this.contactsToDelete = null
           this.isBusy = false
-          this.removeContactClose()
         })
     },
     onRemove () {
