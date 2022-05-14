c72709a867f9fb70a271dd6ade6d7b76c3703560
Karl Fabe <karl@aloware.com>
2022-05-14 10:57:38 +0800

    Major update -- Session engine - in-progress

--- src/plugins/mixins/sessions-call-status.js
+++ src/plugins/mixins/sessions-call-status.js
@@ -82,19 +82,12 @@ export default {
       this.$VueEvent.fire('hangupCall')
     },
 
-    // async fetchNextContact () {
-    //   this.taskToCall = this.powerDialerTasks.in_queue[0]
-    //   if (this.taskToCall?.id) {
-    //     await this.fetchContact(this.taskToCall?.id)
-    //   }
-    // },
-
-    async fetchContact (taskId = '') {
+    async fetchContact (taskId = null) {
       this.TOGGLE_SESSION_LOADER(true)
       await this.getContact({ id: taskId })
-      if (!this.flagged) {
+      if (!this.isSessionRunning) {
         // this.activeTask = res
-        this.flagged = true
+        this.isSessionRunning = true
       }
     },
 
