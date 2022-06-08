ff438e4efd59ed60af201d8d3577c3caf0c6fe59
alberto-aloware <alberto@aloware.com>
2022-06-08 12:21:30 -0300

    Adds information for dev mode, but disabled

--- src/boot/fullstory.js
+++ src/boot/fullstory.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import * as FullStory from '@fullstory/browser'
 
-FullStory.init({ orgId: process.env.FULLSTORY_ORG_ID })
+FullStory.init({
+  orgId: process.env.FULLSTORY_ORG_ID
+  // devMode: process.env.APP_ENV === 'local'
+})
 
 Vue.prototype.$FullStory = FullStory
