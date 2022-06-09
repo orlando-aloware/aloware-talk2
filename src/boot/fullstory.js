df00ed9cf356f361bec3bf80236f6fac02bfd63c
alberto-aloware <alberto@aloware.com>
2022-06-09 15:18:16 -0300

    devmode if not org id

--- src/boot/fullstory.js
+++ src/boot/fullstory.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import * as FullStory from '@fullstory/browser'
 
 FullStory.init({
-  orgId: process.env.FULLSTORY_ORG_ID
+  orgId: process.env.FULLSTORY_ORG_ID,
+  devMode: !process.env.FULLSTORY_ORG_ID
   // devMode: process.env.APP_ENV === 'local'
 })
 
