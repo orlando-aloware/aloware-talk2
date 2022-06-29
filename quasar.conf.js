116227bdf964a9e7827861941e92f309a8de4fd1
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2022-06-29 15:33:38 -0700

    Fixed electron-builder issues

--- quasar.conf.js
+++ quasar.conf.js
@@ -205,11 +205,11 @@ module.exports = function (/* ctx */) {
         linux: {
           target: 'AppImage'
         },
-        publish: {
-          provider: 'github',
-          token: process.env.GH_TOKEN,
-          publishAutoUpdate: true
-        },
+        // publish: {
+        //   provider: 'github',
+        //   token: process.env.GH_TOKEN,
+        //   publishAutoUpdate: true
+        // },
         protocols: {
           name: 'Aloware Talk',
           schemes: ['alowaretalk', 'tel', 'callto']
