5357aaca6d10149ea7639660f409122c2f840117
Jonathan M <jonathan@aloware.com>
2022-07-05 18:25:12 -0300

    Pushing activeTask to queue if current task is queued

--- src/plugins/mixins/sessions-engine.mixin.js
+++ src/plugins/mixins/sessions-engine.mixin.js
@@ -77,10 +77,8 @@ export default {
       }
 
       // Status Queued
-      let taskToMove = this.powerDialerTasks.in_queue.find(lst => lst.contact_list_item_id === task.id)
-      this.powerDialerTasks.in_queue = this.powerDialerTasks.in_queue.filter(lst => lst.contact_list_item_id !== task.id)
-      this.powerDialerTasks.in_queue.push(taskToMove)
-      window.VueEvent.fire('initiate_session', task)
+      this.powerDialerTasks.in_queue.push(this.activeTask)
+      window.VueEvent.fire('initiate_session')
     },
     onStatusScheduled (task) {
       // Status Scheduled
