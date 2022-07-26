6a8ef6c10698648b4b1668694697870a8cc4eacd
Jonathan M <jonathan@aloware.com>
2022-07-26 16:44:37 -0300

    Using props instead of fixed values

--- src/components/icons/inbox/all-communications-icon.vue
+++ src/components/icons/inbox/all-communications-icon.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg :width="width" :height="height" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M10.5 8.62476H16.5" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
     <path d="M10.5 11.9985H16.5" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
     <path d="M10.5 15.3286H16.5" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
