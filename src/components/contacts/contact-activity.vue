03b84d1bb743733ed45a48db177222cf704b3fa9
Eskie M <eskie@aloware.com>
2022-08-12 21:22:06 +0800

    more strict condition

--- src/components/contacts/contact-activity.vue
+++ src/components/contacts/contact-activity.vue
@@ -846,7 +846,7 @@ export default {
     },
 
     showAuthor (audit) {
-      return audit.property != 'text_authorized' && audit.property != 'is_opted_out'
+      return audit.property !== 'text_authorized' && audit.property !== 'is_opted_out'
     }
   }
 }
