4989b94d9594657bda8e994e200037d9e77e7833
alberto-aloware <alberto@aloware.com>
2022-09-13 16:12:36 -0300

    Change place of method

--- src/plugins/mixins/inbox.mixin.js
+++ src/plugins/mixins/inbox.mixin.js
@@ -264,21 +264,21 @@ export default {
     },
     setContacts (contacts) {
       this.contacts = contacts
-    }
-  },
-  fetchTaskCounts () {
-    this.setLoadingPendingTaskCount(true)
-    this.getContactsCountByTaskStatus(ContactTaskStatus.STATUS_PENDING)
-    this.setLoadingOpenTaskCount(true)
-    this.getContactsCountByTaskStatus(ContactTaskStatus.STATUS_OPEN)
-    return talk2Api.V2.contacts.inboxCounts().then(res => {
-      this.setTaskCount({
-        new: res.data.open,
-        open: res.data.open,
-        pending: res.data.pending,
-        closed: res.data.closed
+    },
+    fetchTaskCounts () {
+      this.setLoadingPendingTaskCount(true)
+      this.getContactsCountByTaskStatus(ContactTaskStatus.STATUS_PENDING)
+      this.setLoadingOpenTaskCount(true)
+      this.getContactsCountByTaskStatus(ContactTaskStatus.STATUS_OPEN)
+      return talk2Api.V2.contacts.inboxCounts().then(res => {
+        this.setTaskCount({
+          new: res.data.open,
+          open: res.data.open,
+          pending: res.data.pending,
+          closed: res.data.closed
+        })
       })
-    })
+    }
   },
 
   created () {
