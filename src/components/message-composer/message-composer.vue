d2cf5c9c2318724ac8dc3cafa71cab9b545973c5
alberto-aloware <alberto@aloware.com>
2022-09-16 12:17:08 -0300

    Process quote

--- src/components/message-composer/message-composer.vue
+++ src/components/message-composer/message-composer.vue
@@ -42,10 +42,14 @@
       </div>
       <div>
         <message-composer-sms :is-disabled="isSmsDisabled"
-                              v-if="messageComposer.mode === 'sms'"/>
-        <message-composer-fax v-if="messageComposer.mode === 'fax'" />
-        <message-composer-email v-if="messageComposer.mode === 'email' && contact.email" />
-        <message-composer-note v-if="messageComposer.mode === 'note'" />
+                              v-if="messageComposer.mode === 'sms'"
+                              @message-sent="onMessageSent"/>
+        <message-composer-fax v-if="messageComposer.mode === 'fax'"
+                              @message-sent="onMessageSent"/>
+        <message-composer-email v-if="messageComposer.mode === 'email' && contact.email"
+                                @message-sent="onMessageSent"/>
+        <message-composer-note v-if="messageComposer.mode === 'note'"
+                               @message-sent="onMessageSent"/>
       </div>
     </div>
     <div class="composer-footer d-flex justify-content-between pt-1">
@@ -146,6 +150,15 @@ export default {
     },
     onLineChange (line) {
       this.setSelectedLine(line)
+    },
+    onMessageSent (message) {
+      console.log({ message })
+
+      let sendingMessage = {
+        ...message,
+        user_id: this.profile.id
+      }
+      this.$emit('message-sent', sendingMessage)
     }
   },
 
