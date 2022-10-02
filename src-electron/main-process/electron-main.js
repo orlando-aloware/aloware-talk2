97c0e3b1f8cf8083497f00c93e2affda16020d35
alberto-aloware <alberto@aloware.com>
2022-10-02 11:06:01 -0300

    Everything on one file

--- src-electron/main-process/electron-main.js
+++ src-electron/main-process/electron-main.js
@@ -1,8 +1,21 @@
-import { app, BrowserWindow, ipcMain, Menu, nativeTheme, shell, Tray } from 'electron'
+import { app, BrowserWindow, ipcMain, Menu, nativeTheme, shell, Tray, dialog } from 'electron'
 import { autoUpdater } from 'electron-updater'
 import path from 'path'
 import { Registry } from 'rage-edit'
-import checkForUpdates from 'check-for-updates.js'
+import { clone } from 'ramda'
+
+// Application menu
+const menuTemplate = [
+  {
+    label: 'Updates',
+    submenu: [
+      { label: 'Check for updates', enabled: false, click: () => checkForUpdates({ silent: false }) }
+    ]
+  }
+]
+
+const menu = Menu.buildFromTemplate(menuTemplate)
+Menu.setApplicationMenu(menu)
 
 // register for tel: links in windows
 if (process.platform === 'win32') {
@@ -25,19 +38,6 @@ if (process.platform === 'win32') {
     await Registry.set('HKCU\\Software\\RegisteredApplications', 'Aloware Talk', 'Software\\Aloware Talk\\Capabilities')
   })()
 }
-
-// Application menu
-const applicationMenu = [
-  {
-    label: 'Updates',
-    submenu: [
-      { label: 'Check for updates', enabled: false, click: () => checkForUpdates({ silent: false }) }
-    ]
-  }
-]
-
-const menu = Menu.buildFromTemplate(applicationMenu)
-Menu.setApplicationMenu(menu)
 
 const log = require('electron-log')
 log.transports.file.level = 'info'
@@ -185,7 +185,7 @@ if (gotTheLock) {
 
       if (process.env.NODE_ENV === 'production') {
         log.info('Setup check for updates and notify')
-        autoUpdater.checkForUpdatesAndNotify()
+        checkForUpdates({ silent: true })
       }
     } else {
       mainWindow.show()
@@ -195,7 +195,7 @@ if (gotTheLock) {
 
       if (process.env.NODE_ENV === 'production') {
         log.info('Setup check for updates and notify')
-        autoUpdater.checkForUpdatesAndNotify()
+        checkForUpdates({ silent: true })
       }
     }
   })
@@ -208,7 +208,7 @@ if (gotTheLock) {
 
     if (process.env.NODE_ENV === 'production') {
       log.info('Setup check for updates and notify')
-      autoUpdater.checkForUpdatesAndNotify()
+      checkForUpdates({ silent: true })
     }
   })
 } else {
@@ -431,6 +431,103 @@ try {
     require('fs').unlinkSync(require('path').join(app.getPath('userData'), 'DevTools Extensions'))
   }
 } catch (_) {
+}
+
+autoUpdater.autoDownload = false
+let isSilent
+let updateDownloaded = false
+
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...')
+})
+
+autoUpdater.on('error', (error) => {
+  sendStatusToWindow(`Error in autoUpdater. ${error}`)
+  changeUpdaterMenu({ label: 'Check for updates', enabled: true })
+  if (isSilent) return
+  dialog.showErrorBox('Error during the update', `Application couldn't be updated. Please try again or contact the support team.`)
+})
+
+autoUpdater.on('update-available', () => {
+  sendStatusToWindow('Update available.')
+  if (isSilent) {
+    autoUpdater.downloadUpdate()
+    return
+  }
+  dialog.showMessageBox({
+    type: 'info',
+    title: 'Found Updates',
+    message: 'New updates are available, do you want update now?',
+    defaultId: 0,
+    cancelId: 1,
+    buttons: ['Yes', 'No']
+  }, (buttonIndex) => {
+    if (buttonIndex === 0) {
+      autoUpdater.downloadUpdate()
+    } else {
+      changeUpdaterMenu({ label: 'Check for updates', enabled: true })
+    }
+  })
+})
+
+autoUpdater.on('update-not-available', () => {
+  sendStatusToWindow('Update not available.')
+  changeUpdaterMenu({ label: 'Check for updates', enabled: true })
+  if (isSilent) return
+  dialog.showMessageBox({
+    title: 'No Updates',
+    message: 'Current version is up-to-date.'
+  })
+})
+
+autoUpdater.on('update-downloaded', () => {
+  sendStatusToWindow('Update downloaded.')
+  updateDownloaded = true
+  changeUpdaterMenu({ label: 'Updates available', enabled: true })
+  if (isSilent) return
+  dialog.showMessageBox({
+    title: 'Install Updates',
+    message: 'Updates are ready to be installed.',
+    defaultId: 0,
+    cancelId: 1,
+    buttons: ['Install and restart', 'Close']
+  }, (buttonIndex) => {
+    if (buttonIndex === 0) {
+      setImmediate(() => autoUpdater.quitAndInstall())
+    } else {
+      changeUpdaterMenu({ label: 'Updates available', enabled: true })
+    }
+  })
+})
+
+function checkForUpdates ({ silent }) {
+  isSilent = silent
+  changeUpdaterMenu({ label: 'Checking for updates...', enabled: false })
+  if (updateDownloaded) {
+    dialog.showMessageBox({
+      title: 'Available Updates',
+      message: 'New updates are available and ready to be installed.',
+      defaultId: 0,
+      cancelId: 1,
+      buttons: ['Install and restart', 'Close']
+    }, (buttonIndex) => {
+      if (buttonIndex === 0) {
+        setImmediate(() => autoUpdater.quitAndInstall())
+      } else {
+        changeUpdaterMenu({ label: 'Updates available', enabled: true })
+      }
+    })
+  } else {
+    autoUpdater.checkForUpdates()
+  }
+}
+
+const changeUpdaterMenu = ({ label, enabled }) => {
+  const newTemplate = clone(menuTemplate)
+  newTemplate[0].submenu[2].label = label
+  newTemplate[0].submenu[2].enabled = enabled
+  const menu = Menu.buildFromTemplate(newTemplate)
+  Menu.setApplicationMenu(menu)
 }
 
 process.on('uncaughtException', (err) => {
