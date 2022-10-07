64501357c69f85e0fbafacb9d1ed2c51c8ca2348
alberto-aloware <alberto@aloware.com>
2022-10-07 12:43:25 -0300

    Verify if it's electron

--- src/components/guest/login-form.vue
+++ src/components/guest/login-form.vue
@@ -85,6 +85,7 @@ import { mapActions, mapState } from 'vuex'
 import * as AppDefaultLogin from 'src/constants/user-default-login'
 import { aclMixin } from 'src/plugins/mixins'
 import * as storage from 'src/plugins/helpers/storage'
+import { useQuasar } from 'quasar'
 
 export default {
   mixins: [aclMixin],
@@ -108,8 +109,12 @@ export default {
       loading: false,
       sb: null,
       deviceInfo: null,
-      isPwd: true
+      isPwd: true,
+      $q: null
     }
+  },
+  created () {
+    this.$q = useQuasar()
   },
   methods: {
     getLoginParams () {
@@ -159,7 +164,7 @@ export default {
       storage.local.setItem('company_id', company.id)
 
       // redirect to Alo classic for agents
-      if (this.shouldRedirectToClassic) {
+      if (this.shouldRedirectToClassic && !this.$q.platform.is.electron) {
         location.href = process.env.API_URL + '?from_talk_2=1&token=' + storage.local.getItem('shared_cookie')
       } else {
         const redirectPath = (this.$route.query.redirect === '/suspended' ? '' : this.$route.query.redirect) || '/'
