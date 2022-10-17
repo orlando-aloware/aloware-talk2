05e7e51ed1aa44ca9d1563ba5cfced4a19b2d6f2
Eskie M <eskie@aloware.com>
2022-10-17 23:21:30 +0800

    change duplicate error message

--- src/plugins/mixins/communication-info.mixin.js
+++ src/plugins/mixins/communication-info.mixin.js
@@ -242,7 +242,7 @@ export default {
         case CommunicationRejectionReasons.REJECTION_REASON_COMPANY_SUSPENDED:
           return 'Company was suspended.'
         case CommunicationRejectionReasons.REJECTION_REASON_LINE_IS_SPAMMING:
-          return 'Line was sending the same message to the same contact.'
+          return 'SPAM Detected: Sending the same message multiple times has been stopped.'
       }
     },
 
