c36cd328bf159a7f02d3e17cb9680249a397fa11
Jonathan M <jonathan@aloware.com>
2022-10-27 12:04:55 -0300

    Changed from user to profile

--- src/components/power-dialer/power-dialer-list.vue
+++ src/components/power-dialer/power-dialer-list.vue
@@ -35,14 +35,14 @@ export default {
   },
 
   computed: {
-    ...mapGetters('auth', ['user'])
+    ...mapGetters('auth', ['profile'])
   },
 
   mounted () {
     this.$VueEvent.stop('contact_list_import_hubspot')
     this.$VueEvent.listen('contact_list_import_hubspot', event => {
       // return if event is for another user
-      if (event.user_id !== this.user.profile.id) {
+      if (event.user_id !== this.profile.id) {
         return
       }
 
