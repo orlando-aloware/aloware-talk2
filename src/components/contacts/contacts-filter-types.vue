bea275928cc8b31f662c83e0c536d6e56f4445c3
alberto-aloware <alberto@aloware.com>
2022-11-01 11:08:48 -0300

    Readjust vue component

--- src/components/contacts/contacts-filter-types.vue
+++ src/components/contacts/contacts-filter-types.vue
@@ -101,6 +101,24 @@
                       v-model="filterOperatorValue">
         </q-btn-toggle>
       </template>
+      <template v-if="filter.type === 'selection'">
+        <q-select
+          ref="filterOperation"
+          class="filter-operation border"
+          :options="options"
+          option-disable="disabled"
+          input-debounce="0"
+          borderless
+          dense
+          map-options
+          emit-value
+          use-input
+          v-model="filterOperatorValue"
+          v-if="operator.value === filterOperator && hasValue"
+          @input="onInput"
+          @filter="filterFn"
+        />
+      </template>
     </div>
     <compact-btn
       class="mr-2 mt-3 p-3"
