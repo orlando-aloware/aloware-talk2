3cc0514173215341bbbfdf2020feee79875f25e2
Eskie M <eskie@aloware.com>
2022-11-18 06:13:38 +0800

    add modal

---
+++ src/components/modal.vue
@@ -0,0 +1,41 @@
+<template>
+  <b-modal
+    :id="id"
+    :dialog-class="className"
+    :centered="centered"
+    :no-close-on-backdrop="noCloseOnBackdrop"
+    :no-close-on-esc="noCloseOnEsc"
+  >
+    <template #modal-title>
+      <slot name="title" />
+    </template>
+    <slot name="body" />
+    <template #modal-footer>
+      <slot name="footer" />
+    </template>
+  </b-modal>
+</template>
+
+<script>
+export default {
+  props: {
+    id: String,
+    className: {
+      type: String,
+      default: 'modal-pd-add'
+    },
+    centered: {
+      type: Boolean,
+      default: true
+    },
+    noCloseOnBackdrop: {
+      type: Boolean,
+      default: true
+    },
+    noCloseOnEsc: {
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>
