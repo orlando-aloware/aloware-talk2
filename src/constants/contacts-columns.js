9a8ffbaf993f5aa6074a777c1f355abcd9f89d02
alberto-aloware <alberto@aloware.com>
2022-12-02 19:07:47 -0300

    Revert

--- src/constants/contacts-columns.js
+++ src/constants/contacts-columns.js
@@ -446,16 +446,6 @@ export const ALL_COLUMNS = [
     default: true,
     maxWidth: 130,
     minWidth: 130
-  },
-  {
-    name: 'line_type',
-    label: 'Line Type',
-    category: 0,
-    order: 41,
-    sortable: false,
-    draggable: true,
-    resizable: false,
-    default: false
   }
 ]
 
