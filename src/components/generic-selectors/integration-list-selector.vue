94b2b049c15861291c8bea52115f6b947c990385
alberto-aloware <alberto@aloware.com>
2022-12-08 15:49:22 -0300

    Move hubspot to component

--- src/components/generic-selectors/integration-list-selector.vue
+++ src/components/generic-selectors/integration-list-selector.vue
@@ -1,85 +1,12 @@
 <template>
   <div>
     <template v-if="isIntegrationEnabled('hubspot')">
-      <q-select ref="hubspotListSelector"
-                options-selected-class="text-primary"
-                class="q-basic-selector"
-                color="primary"
-                option-value="id"
-                option-label="name"
-                input-debounce="0"
-                style="word-break: break-all;"
-                use-input
-                emit-value
-                map-options
-                outlined
-                dense
-                v-model="selectedId"
-                :options="options"
-                :placeholder="placeholder"
-                :multiple="multiple"
-                :disable="disable"
-                :class="[ prepend ? 'with-prepend' : '', genericStyling ? 'generic-selector' : '', highlighted ? highlightedClass : '']"
-                :use-chips="useChips"
-                :clearable="clearable"
-                :popup-content-style="`width: ${selectWidth}px; word-break: break-all;`"
-                :loading="isLoading"
-                @popup-show="onShowMenu"
-                @focus="onFocus"
-                @blur="onBlur"
-                @input="onInput"
-                @filter="filterFn">
-        <template v-slot:prepend
-                  v-if="prepend">
-          <span class="text-size-xs text-grey-80">{{ prepend }}</span>
-        </template>
-
-        <template v-slot:no-option>
-          <q-item>
-            <q-item-section class="no-results text-grey">
-              No results
-            </q-item-section>
-          </q-item>
-        </template>
-
-        <template v-slot:option="scope">
-          <q-item v-if="!scope.opt.group"
-                  v-bind="scope.itemProps"
-                  v-on="scope.itemEvents">
-            <q-item-section>
-              <q-item-label>
-                <q-item-label v-html="scope.opt.name" ></q-item-label>
-              </q-item-label>
-            </q-item-section>
-          </q-item>
-          <q-item v-if="scope.opt.group"
-                  v-bind="scope.itemProps"
-                  v-on="scope.itemEvents">
-            <q-item-label header
-                          class="text-size-xs">
-              {{ scope.opt.group }}
-            </q-item-label>
-          </q-item>
-        </template>
-
-        <template v-slot:selected-item="scope"
-                  v-if="useChips">
-          <q-chip dense
-                  :tabindex="scope.tabindex"
-                  color="white"
-                  class="tag-selected-chip"
-                  text-color="secondary">
-            <i class="fa fa-circle position-absolute"
-               :style="`color: ${scope.opt.color}; font-size: 50%; left: 4px; top: 40%; margin-right: 10px;`"></i>
-            <span class="ml-3 mr-3 pr-1 pl-1">{{ scope.opt.name }}</span>
-            <div role="button"
-                 class="custom__remove d-flex align-items-center position-absolute r-0"
-                 @click="scope.removeAtIndex(scope.index)">
-              <remove-tag-icon class="ml-1 remove-tag-icon"/>
-            </div>
-          </q-chip>
-        </template>
-      </q-select>
+      <q-form ref="add_hubspot_list"
+              :model="hubspotList"
+              :rules="rulesHubspotList"
+              @submit.prevent.native="addHubspotList">
+        <hubspot-list-selector />
+      </q-form>
     </template>
     <template v-else-if="isIntegrationEnabled('zoho')">
       <q-form ref="add_zoho_view"
@@ -88,8 +15,7 @@
               @submit.prevent.native="addZohoView">
         <zoho-view-selector v-model="zohoView.view_id"
                             :value="zohoView.view_id"
-                            @change="zohoViewChanged">
-        </zoho-view-selector>
+                            @change="zohoViewChanged"/>
         <div class="row no-gutter centered-content">
           <button class="btn btn-block greenish"
                   :loading="loadingAddZohoView"
@@ -109,8 +35,7 @@
               @submit.prevent.native="addPipedriveFilter">
         <pipedrive-filter-selector v-model="pipedriveFilter.filter_id"
                                   :value="pipedriveFilter.filter_id"
-                                  @selectedFilter="pipedriveFilterChanged">
-        </pipedrive-filter-selector>
+                                  @selectedFilter="pipedriveFilterChanged"/>
         <div class="row no-gutter centered-content">
           <button class="btn btn-block greenish"
                   :loading="loadingAddPipedriveFilter"
@@ -128,9 +53,6 @@
 
 <script>
 import { mapState } from 'vuex'
-import RemoveTagIcon from 'components/icons/contact-activity/remove-tag-icon'
-import { selectorMixin } from 'src/plugins/mixins'
-import talk2Api from 'src/plugins/api/api'
 import PipedriveFilterSelector from 'src/components/integrations/pipedrive-filter-selector.vue'
 import ZohoViewSelector from 'src/components/integrations/zoho-view-selector.vue'
 import axios from 'axios'
@@ -138,12 +60,7 @@ import axios from 'axios'
 export default {
   name: 'integration-list-selector',
 
-  mixins: [
-    selectorMixin
-  ],
-
   components: {
-    RemoveTagIcon,
     PipedriveFilterSelector,
     ZohoViewSelector
   },
@@ -268,39 +185,6 @@ export default {
   },
 
   methods: {
-    filterFn (val, update) {
-      if (val === '') {
-        update(() => {
-          this.options = this.sorted
-        })
-        return
-      }
-
-      update(() => {
-        const needle = val.toLowerCase()
-        this.options = this.sorted.filter((item) => item.name && item.name.toLowerCase().indexOf(needle) > -1)
-      })
-    },
-    getLists (offset = 0) {
-      this.isLoading = true
-      talk2Api.V1.integrations.hubspot.getList({
-        params: {
-          offset: offset
-        }
-      }).then(response => {
-        const result = response.data
-        this.lists.push(...result.lists)
-        if (result.has_more) {
-          return this.getLists(result.offset)
-        } else {
-          this.isLoading = false
-        }
-      }).catch((err) => {
-        this.isLoading = false
-        this.$handleErrors(err.response)
-        console.log(err)
-      })
-    },
     isIntegrationEnabled (name) {
       if (!name) {
         return false
