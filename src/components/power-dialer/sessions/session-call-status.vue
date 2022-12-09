13b184482ee2c28eae99b688ffaafad67b476278
Eskie M <eskie@aloware.com>
2022-12-10 00:20:07 +0800

    move logic

--- src/components/power-dialer/sessions/session-call-status.vue
+++ src/components/power-dialer/sessions/session-call-status.vue
@@ -1041,11 +1041,12 @@ export default {
       this.onPhoneExpansionReset()
       this.taskToCall = cloneDeep(this.powerDialerTasks.in_queue[0])
       this.redialTask(this.activeTask).then(() => {
-        this.powerDialerTasks.in_queue.push(this.activeTask)
-        this.activeTask = this.taskToCall
         if (this.dialer.currentStatus === 'CALL_CONNECTED') {
           this.$VueEvent.fire('hangupCall')
         }
+
+        this.powerDialerTasks.in_queue.push(this.activeTask)
+        this.activeTask = this.taskToCall
 
         // when there is wrap up, manually move the queue
         if (this.wrapUpSeconds !== -1) {
