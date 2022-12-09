c1aafeb1e0f4b707278bd3ba503e178d1e189158
alberto-aloware <alberto@aloware.com>
2022-12-09 11:18:45 -0300

    Fix call

--- src/components/generic-selectors/integration-list-selector.vue
+++ src/components/generic-selectors/integration-list-selector.vue
@@ -239,7 +239,7 @@ export default {
     getZohoViews () {
       this.zoho_views = []
       this.loading_zoho_views = true
-      talk2Api.V1.integrations.zoho.getViews.then(response => {
+      talk2Api.V1.integrations.zoho.getViews().then(response => {
         this.isLoading = false
         this.lists.push(...response.data)
       }).catch((err) => {
