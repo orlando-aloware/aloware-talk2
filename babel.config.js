da95fee76c3df4cb8e2a14366d1902e95b9ce5af
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2022-12-21 12:02:50 -0800

    babel fixes

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,9 @@
 module.exports = {
   presets: [
     '@quasar/babel-preset-app'
+  ],
+  plugins: [
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-optional-chaining'
   ]
 }
