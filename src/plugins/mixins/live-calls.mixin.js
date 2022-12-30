8cb051a6c6833f90713bfee3f403432c3fe72bc9
Amir <amir@aloware.com>
2022-12-30 15:37:52 +0300

    Again logs

--- src/plugins/mixins/live-calls.mixin.js
+++ src/plugins/mixins/live-calls.mixin.js
@@ -120,6 +120,7 @@ export default {
       }
 
       // make sure that comms has the correct status and is already included in live contacts
+      console.log('isConnectedCall', this.communication.current_status2, this.liveContacts)
       return [CommunicationCurrentStatus.CURRENT_STATUS_INPROGRESS_NEW].includes(this.communication.current_status2) &&
         this.liveContacts.findIndex(item => item.id === this.contact.id) >= 0
     },
