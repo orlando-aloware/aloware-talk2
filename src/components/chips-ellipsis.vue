162551ae24527f8311a41a82393a5894ce87f178
Jason Behik <jason@aloware.com>
2023-01-26 18:37:44 +0800

    Fixes on the power dialer session's call and contact disposition selector

--- src/components/chips-ellipsis.vue
+++ src/components/chips-ellipsis.vue
@@ -1,56 +1,72 @@
 <template>
-  <div class="chip-ellipsis">
-
-    <template v-if="hasContent">
-
-      <template
-        v-for="(chip, key) in filteredListItems">
-        <q-chip
-          :class="`${chip.id === selectedItemId ? 'active' : ''} p-0 mx-2`"
-          :style="`border:${chip.id === selectedItemId ? '#256eff' : chip.color || '#ebebeb'} solid 1px;`"
-          :key="`${identity}-${chip.name}-${key}`"
-          :color="`grey-5`"
-          outline clickable
-          square
-          v-if="key < displayCount"
-          @click="$emit('on-selected-item', chip)">
-
-          <div
-            :class="`chip-label text-capitalize ${identity === 'contact-disposition' ? 'text-grey-7' : 'text-black'}`">
-            {{ chip.name }}
-          </div>
-
-        </q-chip>
+  <div class="position-relative">
+    <b-overlay class="h-100 w-100 position-absolute"
+               rounded="sm"
+               :show="true"
+               v-show="loading">
+      <template #overlay>
+        <q-spinner-bars color="primary"
+                        size="40px" />
       </template>
-
-      <b-dropdown
-        v-if="!hasReachedLimit"
-        text="..."
-        no-caret
-        right size="sm"
-        variant="white"
-        :class="`scrollable-dropdown-list m-1 b-compact-dropdown-button text-bold dropdown-white ${hasSelectedObject ? 'dropdown-active' : ''} contacts-options-dropdown`">
-        <template #button-content>
-          <i :class="`fa fa-ellipsis-h ${hasSelectedObject ? 'text-primary' : ''}`"></i>
+    </b-overlay>
+    <div :class="headerClass"
+         v-if="headerLabel">
+      <div class="header__header__title font-weight-bold text-grey-8 pl-3 flex-grow-1">
+        {{ headerLabel }}
+      </div>
+    </div>
+    <div class="d-flex t-menu__content over-flow px-3">
+      <div class="chip-ellipsis">
+        <template v-if="hasContent">
+          <template
+            v-for="(chip, key) in filteredListItems">
+            <q-chip
+              outline
+              clickable
+              square
+              :class="`${chip.id === selectedItemId ? 'active' : ''} p-0 mx-2`"
+              :style="`border:${chip.id === selectedItemId ? '#256eff' : chip.color || '#ebebeb'} solid 1px;`"
+              :key="`1-${key}`"
+              :color="`grey-5`"
+              :enabled="!loading"
+              v-if="key < displayCount"
+              @click="onClick(chip)">
+              <div
+                :class="`chip-label text-capitalize ${identity === 'contact-disposition' ? 'text-grey-7' : 'text-black'}`">
+                {{ chip.name }}
+              </div>
+            </q-chip>
+          </template>
+          <b-dropdown
+            v-if="!hasReachedLimit"
+            text="..."
+            no-caret
+            right size="sm"
+            variant="white"
+            :class="`scrollable-dropdown-list m-1 b-compact-dropdown-button text-bold dropdown-white ${hasSelectedObject ? 'dropdown-active' : ''} contacts-options-dropdown`">
+            <template #button-content>
+              <i :class="`fa fa-ellipsis-h ${hasSelectedObject ? 'text-primary' : ''}`"></i>
+            </template>
+            <template
+              v-for="(chip, key) in filteredListItems">
+              <b-dropdown-item
+                href="#"
+                :key="`2-${key}`"
+                :class="`${chip.id === selectedItemId ? 'active' : ''}`"
+                :enabled="!loading"
+                v-if="key >= displayCount"
+                @click="onClick(chip)">
+                {{ chip.name }}
+              </b-dropdown-item>
+            </template>
+          </b-dropdown>
         </template>
-        <template
-          v-for="(chip, key) in filteredListItems">
-          <b-dropdown-item
-            v-if="key >= displayCount"
-            :key="`${identity}-${chip.name}-${key}`"
-            @click="$emit('on-selected-item', chip)"
-            :class="`${chip.id === selectedItemId ? 'active' : ''}`"
-            href="#">
-            {{ chip.name }}
-          </b-dropdown-item>
-        </template>
-      </b-dropdown>
-
-    </template>
-    <div
-      v-else
-      class="text-caption text-grey-6 text-weight-bold">
-      {{ defaultLabel }}
+        <div
+          v-else
+          class="text-caption text-grey-6 text-weight-bold">
+          {{ defaultLabel }}
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -79,11 +95,26 @@ export default {
     defaultLabel: {
       type: String,
       default: 'No record found'
+    },
+    headerLabel: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    headerClass: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      loading: false
     }
   },
   computed: {
     filteredListItems () {
-      const listItems = [ ...this.listItems ]
+      const listItems = [...this.listItems]
       const flag = { data: null }
       if (this.selectedItem) {
         listItems.forEach((list, key) => {
@@ -121,6 +152,18 @@ export default {
     },
     hasReachedLimit () {
       return this.listItems.length < this.displayCount
+    }
+  },
+  methods: {
+    onClick (chip) {
+      this.$emit('on-selected-item', chip)
+      this.loading = true
+    },
+    showLoading () {
+      this.loading = true
+    },
+    hideLoading () {
+      this.loading = false
     }
   }
 }
--- src/components/power-dialer/sessions/session-call-disposition.vue
+++ src/components/power-dialer/sessions/session-call-disposition.vue
@@ -8,35 +8,26 @@
       Session Paused
     </div>
     <div v-else class="t-menu pb-2">
-      <div class="t-menu__header no-border t-dense d-flex align-items-center">
-        <div class="header__header__title font-weight-bold text-grey-8 pl-3 flex-grow-1">
-          CALL DISPOSITION
-        </div>
-      </div>
-      <div class="d-flex t-menu__content over-flow px-3">
-        <ChipsEllipsis
-          @on-selected-item="onSelectedCallDisposition"
-          :list-items="filteredCallDispositions"
-          :selected-item="callDisposition"
-          :display-count="4"
-          identity="call-disposition"
-          default-label="No Call Dispositions" />
-      </div>
-
-      <div class="t-menu__header t-dense d-flex align-items-center no-border pt-0">
-        <div class="header__header__title font-weight-bold text-grey-8 pl-3 flex-grow-1">
-          CONTACT DISPOSITION
-        </div>
-      </div>
-      <div class="d-flex t-menu__content over-flow px-3">
-        <ChipsEllipsis
-          @on-selected-item="onSelectedContactDisposition"
-          :list-items="filteredContactDispositions"
-          :selected-item="contactDisposition"
-          :display-count="6"
-          identity="contact-disposition"
-          default-label="No Contact Dispositions" />
-      </div>
+      <ChipsEllipsis
+        headerLabel="CONTACT DISPOSITION"
+        headerClass="t-menu__header no-border t-dense d-flex align-items-center"
+        ref="callDispositionSelector"
+        identity="call-disposition"
+        default-label="No Call Dispositions"
+        :list-items="filteredCallDispositions"
+        :selected-item="callDisposition"
+        :display-count="4"
+        @on-selected-item="onSelectedCallDisposition" />
+      <ChipsEllipsis
+        headerLabel="CONTACT DISPOSITION"
+        headerClass="t-menu__header t-dense d-flex align-items-center no-border pt-0"
+        ref="contactDispositionSelector"
+        identity="contact-disposition"
+        default-label="No Contact Dispositions"
+        :list-items="filteredContactDispositions"
+        :selected-item="contactDisposition"
+        :display-count="6"
+        @on-selected-item="onSelectedContactDisposition" />
 
       <!--div class="t-menu__header t-dense d-flex align-items-center no-border pt-0">
         <div class="header__header__title font-weight-bold text-grey-8 pl-3 flex-grow-1">
@@ -63,6 +54,7 @@
 import { mapFields } from 'vuex-map-fields'
 import { mapState, mapGetters, mapActions } from 'vuex'
 import ChipsEllipsis from 'components/chips-ellipsis'
+import { get, isEmpty } from 'lodash'
 
 export default {
   name: 'SessionCallDisposition',
@@ -71,6 +63,10 @@ export default {
   },
   mounted () {
     this.sessionPaused = false
+
+    if (!this.dialer.communication) {
+      this.$refs.callDispositionSelector.showLoading()
+    }
   },
   computed: {
     ...mapFields('powerDialer', [
@@ -78,7 +74,8 @@ export default {
     ]),
     ...mapState([
       'callDispositions',
-      'dispositionStatuses'
+      'dispositionStatuses',
+      'dialer'
     ]),
     ...mapGetters('contacts', [
       'contact'
@@ -106,10 +103,10 @@ export default {
       return this.dispositionStatuses
     },
     contactDisposition () {
-      return this.contact_disposition || this.contact?.disposition_status_id
+      return this.selectedContactDisposition || this.contact?.disposition_status_id
     },
     callDisposition () {
-      return this.call_disposition || this.contact?.last_communication?.call_disposition_id
+      return this.selectedCallDisposition || this.dialer?.communication?.id
     }
   },
   methods: {
@@ -118,36 +115,80 @@ export default {
       'updateCallDisposition'
     ]),
     async onSelectedCallDisposition (data) {
-      const response = await this.updateCallDisposition({
-        id: this.contact?.last_communication?.id,
+      const communicationId = get(this.dialer, 'communication.id', null)
+
+      if (!communicationId) {
+        return
+      }
+
+      this.updateCallDisposition({
+        id: communicationId,
         params: {
           call_disposition_id: data.id
         }
+      }).then(res => {
+        if (res?.id) {
+          this.selectedCallDisposition = res?.call_disposition_id
+        }
+
+        this.$refs.callDispositionSelector.hideLoading()
+      }).catch((err) => {
+        console.log(err)
+        this.$handleErrors(err.response)
+        this.$refs.callDispositionSelector.hideLoading()
       })
-      if (response?.id) {
-        this.call_disposition = response?.call_disposition_id
-      }
     },
     async onSelectedContactDisposition (data) {
-      const response = await this.updateContactDisposition({
-        id: this.contact.id,
+      const contactId = get(this.contact, 'id', null)
+
+      if (!contactId) {
+        return
+      }
+
+      this.updateContactDisposition({
+        id: contactId,
         params: {
           disposition_status: data.id
         }
+      }).then(res => {
+        if (res?.id) {
+          const status = this.dispositionStatuses.find(ds => {
+            return ds.id === res?.disposition_status_id
+          })
+          this.selectedContactDisposition = status.id
+        }
+
+        this.$refs.contactDispositionSelector.hideLoading()
+      }).catch((err) => {
+        console.log(err)
+        this.$handleErrors(err.response)
+        this.$refs.contactDispositionSelector.hideLoading()
       })
-      if (response?.id) {
-        const status = this.dispositionStatuses.find(ds => {
-          return ds.id === response?.disposition_status_id
-        })
-        this.contact_disposition = status.id
-      }
     }
   },
   data () {
     return {
-      contact_disposition: null,
-      call_disposition: null,
+      selectedContactDisposition: null,
+      selectedCallDisposition: null,
       voicemail: []
+    }
+  },
+  watch: {
+    'contact.id': function () {
+      this.selectedContactDisposition = null
+      this.selectedCallDisposition = null
+
+      if (!this.dialer.communication) {
+        this.$refs.callDispositionSelector.showLoading()
+      }
+    },
+    'dialer.communication': function (value) {
+      if (isEmpty(value)) {
+        this.$refs.callDispositionSelector.showLoading()
+        return
+      }
+
+      this.$refs.callDispositionSelector.hideLoading()
     }
   }
 }
