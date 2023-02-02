eb590b107e69f9aff7ecd1c2df18654d2369d967
Jason Behik <jason@aloware.com>
2023-02-03 02:53:47 +0800

    Revert "Forced counts API requests to reporting.aloware.io"
    This reverts commit c617f7c45662a83e2237c9d2bd98b2fc6d03a5de.

--- src/components/power-dialer/power-dialer-add-modal.vue
+++ src/components/power-dialer/power-dialer-add-modal.vue
@@ -383,7 +383,7 @@ export default {
     },
     getListCount (id) {
       return this.$axios
-        .get(`https://reporting.aloware.io/api/v2/power-dialer-lists/${id}/count`)
+        .get(`/api/v2/power-dialer-lists/${id}/count`)
     },
     async checkHubspotList () {
       this.loading++
