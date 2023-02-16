b53b96a37fba843d75caf1c04906d68a75105a08
Eleonor Somosot <eleonor@aloware.com>
2023-02-17 03:35:05 +0800

    Implemented PD custom CRM view iframe

--- src/components/power-dialer/sessions/pages/session-contact-page-crm.vue
+++ src/components/power-dialer/sessions/pages/session-contact-page-crm.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="row full-height">
     <div class="col-12 p-0">
-      <div class="hubspot-iframe-container">
+      <div v-if="!isReferrizer"
+           class="hubspot-iframe-container">
         <iframe class="hubspot-crm-iframe"
                 :src="hubspotLink"
                 frameborder="0"
                 id="hubspot-crm"
                 v-if="hubspotLink">
+        </iframe>
+      </div>
+
+      <div v-if="isReferrizer"
+           class="referrizer-iframe-container">
+        <iframe class="referrizer-crm-iframe"
+                :src="referrizerLink"
+                :key="contact.id"
+                frameborder="0"
+                id="referrizer-crm"
+                v-if="referrizerLink">
         </iframe>
       </div>
     </div>
@@ -15,7 +27,7 @@
 
 <script>
 
-import { mapState } from 'vuex'
+import { mapGetters, mapState } from 'vuex'
 import { hubspotIntegrationMixin } from 'src/plugins/mixins'
 
 export default {
@@ -26,10 +38,36 @@ export default {
   ],
 
   computed: {
+    ...mapGetters('auth', ['profile']),
     ...mapState('contacts', ['contact']),
 
     hubspotLink () {
       return this.getHubspotContactLink(this.contact)
+    },
+
+    referrizerLink () {
+      if (!this.isReferrizer) {
+        return null
+      }
+
+      const agentName = this.profile.name
+      const leadNumber = this.contact.phone_number
+      const firstName = this.contact.first_name
+      const address = this.contact.address
+      const companyName = this.contact.company_name
+      const city = this.contact.cnam_city
+      const state = this.contact.cnam_state
+      const zipcode = this.contact.cnam_zipcode
+      const email = this.contact.email
+      const website = this.contact.website
+      const customUrl = this.currentCompany?.crm_view_custom_url
+
+      return `${customUrl}&utm_source=Aloware&utm_medium=${agentName}&feneroleadid=${leadNumber}&fenerousername=${agentName}&phone=${leadNumber}&firstname=${firstName}&address1=${address}&title=${companyName}&city=${city}&state=${state}&zip=${zipcode}&email=${email}&website=${website}`
+    },
+
+    isReferrizer () {
+      // return this.profile.company.id === 2140
+      return true
     },
 
     test () {
