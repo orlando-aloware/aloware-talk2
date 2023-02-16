3617fab3d8afcb59dfab13589e3ff820397db491
Eleonor Somosot <eleonor@aloware.com>
2023-02-17 07:22:06 +0800

    Better dynamic and more maintainable approach

--- src/components/power-dialer/sessions/pages/session-contact-page-crm.vue
+++ src/components/power-dialer/sessions/pages/session-contact-page-crm.vue
@@ -50,19 +50,17 @@ export default {
         return null
       }
 
-      const agentName = this.profile.name
-      const leadNumber = this.contact.phone_number
-      const firstName = this.contact.first_name
-      const address = this.contact.address
-      const companyName = this.contact.company_name
-      const city = this.contact.cnam_city
-      const state = this.contact.cnam_state
-      const zipcode = this.contact.cnam_zipcode
-      const email = this.contact.email
-      const website = this.contact.website
       const customUrl = this.currentCompany?.crm_view_custom_url
 
-      return `${customUrl}&utm_source=Aloware&utm_medium=${agentName}&feneroleadid=${leadNumber}&fenerousername=${agentName}&phone=${leadNumber}&firstname=${firstName}&address1=${address}&title=${companyName}&city=${city}&state=${state}&zip=${zipcode}&email=${email}&website=${website}`
+      if (!customUrl) {
+        return null
+      }
+
+      const url = new URL(customUrl)
+      const params = new URLSearchParams(url.search)
+      const mappedParams = this.getMappedUrlParams(params)
+
+      return `${url.origin}?` + mappedParams.join('&')
     },
 
     isReferrizer () {
@@ -73,6 +71,75 @@ export default {
     test () {
       const iframe = document.getElementById('hubspot-crm')
       return iframe.contentWindow
+    }
+  },
+
+  methods: {
+    getMappedUrlParams (params) {
+      const agentName = this.profile.name
+      const leadNumber = this.contact.phone_number
+      const firstName = this.contact.first_name
+      const address = this.contact.address
+      const companyName = this.contact.company_name
+      const city = this.contact.cnam_city
+      const state = this.contact.cnam_state
+      const zipcode = this.contact.cnam_zipcode
+      const email = this.contact.email
+      const website = this.contact.website
+      let mappedParams = []
+
+      for (const entry of params.entries()) {
+        let [key, value] = entry
+
+        // map and convert to its equivalent actual value
+        if (/\[*\]/.test(value)) {
+          switch (value) {
+            case '[AgentName]':
+              value = agentName
+              break
+
+            case '[LeadNumber]':
+              value = leadNumber
+              break
+
+            case '[FirstName]':
+              value = firstName
+              break
+
+            case '[Address]':
+              value = address
+              break
+
+            case '[CompanyName]':
+              value = companyName
+              break
+
+            case '[FullState]':
+              value = city
+              break
+
+            case '[City]':
+              value = state
+              break
+
+            case '[ZipCode]':
+              value = zipcode
+              break
+
+            case '[Email]':
+              value = email
+              break
+
+            case '[Website]':
+              value = website
+              break
+          }
+        }
+
+        mappedParams.push(`${key}=${value}`)
+      }
+
+      return mappedParams
     }
   }
 }
