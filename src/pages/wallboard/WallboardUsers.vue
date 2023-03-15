b23c1efaa116db26e23aaf81dd7bd55739d9fef1
Jonathan M <jonathan@aloware.com>
2023-03-15 09:44:02 -0300

    Added skeleton for filter and pagination in Wallboard Users

--- src/pages/wallboard/WallboardUsers.vue
+++ src/pages/wallboard/WallboardUsers.vue
@@ -11,8 +11,60 @@
                      :is-empty="users.length === 0"
                      :paginated="true"
                      :show-pagination="true"
-                     :total-rows="users.length">
-            <!-- rows -->
+                     :total-rows="users.length"
+                     :current-page="pagination.page"
+                     :last-page="lastPage"
+                     @paginated="onPaginated"
+                     @sort="onSort">
+            <template slot="tbody">
+              <tr class="datatable-row"
+                  :key="`${index}`"
+                  v-for="(user, index) in paginatedUsers">
+                <template v-for="(column, colIndex) in columns">
+                  <!-- id -->
+                  <td :key="`col-${colIndex}`"
+                      v-if="column.name === 'id'">
+                    {{ user.id }}
+                  </td>
+
+                  <!-- agent name -->
+                  <td :key="`col-${colIndex}`"
+                      v-if="column.name === 'name'">
+                    {{ user.name }}
+                  </td>
+
+                  <!-- status -->
+                  <td :key="`col-${colIndex}`"
+                      v-if="column.name === 'status'">
+                    status
+                  </td>
+
+                  <!-- status duration -->
+                  <td :key="`col-${colIndex}`"
+                      v-if="column.name === 'status-duration'">
+                    status-duration
+                  </td>
+
+                  <!-- ring groups -->
+                  <td :key="`col-${colIndex}`"
+                      v-if="column.name === 'ring-groups'">
+                    ring groups
+                  </td>
+
+                  <!-- last login -->
+                  <td :key="`col-${colIndex}`"
+                      v-if="column.name === 'last-login'">
+                    last login
+                  </td>
+
+                  <!-- last updated -->
+                  <td :key="`col-${colIndex}`"
+                      v-if="column.name === 'last-updated'">
+                    last updated
+                  </td>
+                </template>
+              </tr>
+            </template>
           </datatable>
         </div>
         <template #overlay>
@@ -59,7 +111,7 @@ export default {
           sortable: true
         },
         {
-          name: 'agents',
+          name: 'name',
           label: 'Agents',
           order: 1,
           sortable: true
@@ -95,8 +147,66 @@ export default {
           sortable: true
         }
       ]
+    },
+
+    paginatedUsers () {
+      let from = this.pagination.perPage * (this.pagination.page - 1)
+      let to = from + this.pagination.perPage
+      let users = this.orderedUsers
+
+      // use reversed list if order is desc
+      if (this.sort.order === 'desc') {
+        users.slice().reverse()
+      }
+
+      return users.filter((user, index) => index >= from && index < to)
+    },
+
+    orderedUsers () {
+      let users = this.filteredUsers
+
+      return users.sort((a, b) => {
+        let condition = null
+
+        switch (this.sort.orderBy) {
+          case 'id':
+            condition = a.id > b.id
+            break
+          case 'name':
+            condition = a.name > b.name
+            break
+          // status
+          // status duration
+          // ring groups
+          // last login
+          // last updated
+        }
+
+        return this.sort.order === 'asc'
+          ? condition > 0 ? 1 : -1
+          : condition > 0 ? -1 : 1
+      })
+    },
+
+    filteredUsers () {
+      return this.users
+    },
+
+    lastPage () {
+      return Math.ceil(this.users.length / this.pagination.perPage)
     }
   },
+
+  data: () => ({
+    pagination: {
+      page: 1,
+      perPage: 25
+    },
+    sort: {
+      orderBy: 'id',
+      order: 'asc'
+    }
+  }),
 
   mounted () {
     this.fetchUsers()
@@ -105,7 +215,18 @@ export default {
   methods: {
     ...mapActions('wallboard', [
       'fetchUsers'
-    ])
+    ]),
+
+    onPaginated (pageData) {
+      this.pagination = {
+        page: pageData.page,
+        perPage: pageData.per_page
+      }
+    },
+
+    onSort (sortData) {
+      this.sort = sortData
+    }
   }
 }
 </script>
