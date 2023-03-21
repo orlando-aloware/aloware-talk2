520e68e3e8855130a13f9dcfba37b8de81691738
Eskie M <eskie@aloware.com>
2023-03-22 06:14:54 +0800

    fix

--- src/components/dialer/dialer.vue
+++ src/components/dialer/dialer.vue
@@ -538,7 +538,7 @@ export default {
         this.dialerCallPrep(call)
         this.startCallTimer()
         this.setDialerCurrentStatus('CALL_CONNECTED')
-        this.getCommunication(this.dialer.call.callSid, this.dialer.currentNumber)
+        this.getCommunication(call.callSid, call.from)
           // .finally(() => {
           //   this.$router.push({ name: 'Call' }).catch(err => {
           //     console.log(err)
