cda2c778bcd00acef6dd7e7ae6e390f4f8a0f676
Eskie M <eskie@aloware.com>
2023-03-22 22:37:17 +0800

    fix connection

--- src/components/dialer/dialer.vue
+++ src/components/dialer/dialer.vue
@@ -158,7 +158,7 @@ export default {
     })
 
     this.device.on(WebrtcEvents.INCOMING, (call) => {
-      this.connection = this.device._createConnection(call, true)
+      this.connection = this.device._createConnection(call._connection, true)
       this.initConnectionEvents()
       console.log('Received call invite', call)
       this.dialerCallPrep(call._connection)
