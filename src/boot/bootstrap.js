5b6ad0ad39921817fc9cc945a2731684f0eff45f
Jason Behik <jason@aloware.com>
2023-03-28 04:20:23 +0800

    Applied refactoring

--- src/boot/bootstrap.js
+++ src/boot/bootstrap.js
@@ -516,22 +516,26 @@ Vue.prototype.$jsonClone = (value) => {
 }
 
 Vue.prototype.$copyToClipboard = (value) => {
-  if (value) {
-    const el = document.createElement('textarea')
-    el.value = value
-    el.setAttribute('readonly', '')
-    el.style.position = 'absolute'
-    el.style.left = '-9999px'
-    document.body.appendChild(el)
-    const selected = document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false
-    el.select()
-    document.execCommand('copy')
-    document.body.removeChild(el)
+  if (!value) {
+    return
+  }
 
-    if (selected) {
-      document.getSelection().removeAllRanges()
-      document.getSelection().addRange(selected)
-    }
+  const el = document.createElement('textarea')
+  el.value = value
+  el.setAttribute('readonly', '')
+  el.style.position = 'absolute'
+  el.style.left = '-9999px'
+  document.body.appendChild(el)
+
+  const selected = document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false
+
+  el.select()
+  document.execCommand('copy')
+  document.body.removeChild(el)
+
+  if (selected) {
+    document.getSelection().removeAllRanges()
+    document.getSelection().addRange(selected)
   }
 }
 
