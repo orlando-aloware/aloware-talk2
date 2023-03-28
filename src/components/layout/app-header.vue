5cdb533672235feb4820b2a3fa358dae676ca694
Amir <amir@aloware.com>
2023-03-28 16:16:50 +0300

    Fixed support link

--- src/components/layout/app-header.vue
+++ src/components/layout/app-header.vue
@@ -125,7 +125,7 @@
                      placement="bottomleft"
                      v-if="dialer.error.code">
             <template #title>Connection timeout</template>
-            Click here to reset or please check the <a href="https://support.aloware.com/" target="_blank">troubleshooting guide here</a>. Error Code: [{{ dialer.error.code }}]
+            Click here to reset or please check the <a href="https://support.aloware.com/en/articles/6958138-common-dialer-errors" target="_blank">troubleshooting guide here</a>. Error Code: [{{ dialer.error.code }}]
             <br>
             <q-btn
               v-if="[31000, 31003, 31009, 31201, 53405, 31402].includes(dialer.error.code)"
