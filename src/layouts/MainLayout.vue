aff12cdf7d33e7a3390d3adb6015c82d8f71b9c1
Amir <amir@aloware.com>
2023-03-28 19:13:14 +0300

    Manually reverted extra stuff

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1290,18 +1290,10 @@ export default {
       }
 
       this.getTimezones()
-
-      const companyId = _.get(this.currentCompany, 'id', null)
-
-      // get statics if company id is already available
-      if (companyId !== null) {
-        this.getStatics()
-        fetchingStatics = true
-      }
+      this.getStatics()
 
       this.initAccount().then(() => {
         this.loading = false
-
         if (this.profile && this.profile.live_calls === 0 && this.dialer.call) {
           if (!this.profile.go_to_available_after_login) {
             this.changeAgentStatus(AgentStatus.AGENT_STATUS_OFFLINE)
@@ -1310,11 +1302,6 @@ export default {
 
         if (this.profile && this.profile.go_to_available_after_login && !this.dialer.call) {
           this.changeAgentStatus(AgentStatus.AGENT_STATUS_ACCEPTING_CALLS)
-        }
-
-        // company id should be available by now so fetch statics if it's not yet fetched
-        if (!fetchingStatics) {
-          this.getStatics()
         }
 
         this.broadcastInit()
