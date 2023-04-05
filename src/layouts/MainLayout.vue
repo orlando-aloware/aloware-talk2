970dbf0bc29072bea178f9105a289aa618dc67df
Jason Behik <jason@aloware.com>
2023-04-06 00:04:00 +0800

    Reverted the fixes for get statics and page title.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1349,6 +1349,7 @@ export default {
     },
 
     initAuth () {
+      let fetchingStatics = false
       this.loading = true
       this.setCampaignsIsLoading(true)
       this.setTagsFullyLoaded(true)
@@ -1358,10 +1359,18 @@ export default {
       }
 
       this.getTimezones()
-      this.getStatics()
+
+      const companyId = _.get(this.currentCompany, 'id', null)
+
+      // get statics if company id is already available
+      if (companyId !== null) {
+        this.getStatics()
+        fetchingStatics = true
+      }
 
       this.initAccount().then(() => {
         this.loading = false
+
         if (this.profile && this.profile.live_calls === 0 && this.dialer.call) {
           if (!this.profile.go_to_available_after_login) {
             this.changeAgentStatus(AgentStatus.AGENT_STATUS_OFFLINE)
@@ -1370,6 +1379,11 @@ export default {
 
         if (this.profile && this.profile.go_to_available_after_login && !this.dialer.call) {
           this.changeAgentStatus(AgentStatus.AGENT_STATUS_ACCEPTING_CALLS)
+        }
+
+        // company id should be available by now so fetch statics if it's not yet fetched
+        if (!fetchingStatics) {
+          this.getStatics()
         }
 
         this.broadcastInit()
