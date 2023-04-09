7acf5157ac8d0ebd58c515edf25d6b7e76733204
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2023-04-08 23:24:29 -0700

    Removed thread_status

--- src/constants/contact-thread-status-types.js
+++
@@ -1,8 +0,0 @@
-// agent responded to the contact
-export const THREAD_STATUS_OPEN = 0
-// new contact
-export const THREAD_STATUS_PENDING = 1
-// archived contact
-export const THREAD_STATUS_CLOSED = 2
-// contact responded
-export const THREAD_STATUS_LIVE = 3
