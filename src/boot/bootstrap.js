3941cfdc668386ef29c12a4db829c7b4ee6803df
Eleonor Somosot <eleonor@aloware.com>
2023-04-11 01:09:55 +0800

    Sentry DSN

--- src/boot/bootstrap.js
+++ src/boot/bootstrap.js
@@ -6,6 +6,7 @@ import 'moment-timezone'
 import momentDurationFormatSetup from 'moment-duration-format'
 import Bowser from 'bowser'
 import * as Sentry from '@sentry/vue'
+import { Integrations } from '@sentry/tracing'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import PortalVue from 'portal-vue'
 import 'vue-popperjs/dist/vue-popper.css'
@@ -141,11 +142,15 @@ if (
   process.env.APP_ENV !== 'local'
 ) {
   Sentry.init({
-    Vue: Vue,
+    Vue,
+    dsn: storage.local.getItem('sentry_dsn_public'),
+    integrations: [new Integrations.BrowserTracing()],
     tracingOptions: {
       trackComponents: true
     },
-    dsn: storage.local.getItem('sentry_dsn_public')
+    attachProps: true,
+    trackComponents: true,
+    tracesSampleRate: process.env.MIX_SENTRY_TRACES_SAMPLE_RATE
   })
 
   Sentry.configureScope((scope) => {
