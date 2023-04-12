aa713036d1d3dd206088da79d8b32157bea233d4
Eleonor Somosot <eleonor@aloware.com>
2023-04-13 06:49:14 +0800

    Updates

--- src/boot/sentry-webpack.js
+++ src/boot/sentry-webpack.js
@@ -1,15 +1,13 @@
 const SentryWebpackPlugin = require('@sentry/webpack-plugin')
 
 module.exports = {
-  // ... other config above ...
   devtool: 'source-map', // Source map generation must be turned on
   plugins: [
     new SentryWebpackPlugin({
       authToken: process.env.SENTRY_AUTH_TOKEN,
       org: process.env.SENTRY_ORGANIZATION,
       project: process.env.SENTRY_PROJECT,
-      include: './dist',
-      configFile: 'sentry.properties'
+      include: './dist'
       // release: process.env.SENTRY_RELEASE
     })
   ]
