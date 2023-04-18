2e924fa3ad1f4616b3b981480cd470c2b4ca0fa0
Eleonor Somosot <eleonor@aloware.com>
2023-04-19 01:44:32 +0800

    Multiple source paths

--- src/boot/sentry-webpack.js
+++ src/boot/sentry-webpack.js
@@ -7,7 +7,7 @@ module.exports = {
       authToken: process.env.SENTRY_AUTH_TOKEN,
       org: process.env.SENTRY_ORGANIZATION,
       project: process.env.SENTRY_PROJECT,
-      include: '../dist'
+      include: ['../../dist', '../../src-electron/build', '..']
       // release: process.env.SENTRY_RELEASE
     })
   ]
