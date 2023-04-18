cc80f73ef8f32b8f922c37dc4eab75842c0399ca
Eleonor Somosot <eleonor@aloware.com>
2023-04-19 06:39:23 +0800

    Try

--- quasar.conf.js
+++ quasar.conf.js
@@ -52,6 +52,7 @@ module.exports = function (/* ctx */) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'history',
+      sourcemap: true,
 
       // transpile: false,
 
