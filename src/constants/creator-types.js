1045fb4a08734400c6982490a8b14ffe0d069bd1
Jonathan M <jonathan@aloware.com>
2023-04-24 09:26:52 -0300

    Added creator types constant file

---
+++ src/constants/creator-types.js
@@ -0,0 +1,57 @@
+// Communication that initiated manually
+export const CREATOR_TYPE_MANUAL = 1
+// Communication that initiated from API
+export const CREATOR_TYPE_API = 2
+// Communication that initiated from Aloware workflows
+export const CREATOR_TYPE_WORKFLOW = 3
+// Communication that initiated from Aloware broadcasts
+export const CREATOR_TYPE_BROADCAST = 4
+// Communication that initiated from Aloware power dialer
+export const CREATOR_TYPE_POWER_DIALER = 5
+// Communication that initiated from appointments sms reminders
+export const CREATOR_TYPE_SMS_REMINDER = 6
+// Communication that initiated from app notifications
+export const CREATOR_TYPE_APP_NOTIFICATIONS = 7
+// Communication that initiated from HubSpot
+export const CREATOR_TYPE_HUBSPOT = 8
+// Communication that initiated from Zapier
+export const CREATOR_TYPE_ZAPIER = 9
+
+export const ALL_CREATOR_TYPES = [
+  {
+    id: CREATOR_TYPE_MANUAL,
+    label: 'Manual'
+  },
+  {
+    id: CREATOR_TYPE_API,
+    label: 'API'
+  },
+  {
+    id: CREATOR_TYPE_WORKFLOW,
+    label: 'Workflow'
+  },
+  {
+    id: CREATOR_TYPE_BROADCAST,
+    label: 'Broadcast'
+  },
+  {
+    id: CREATOR_TYPE_POWER_DIALER,
+    label: 'Power Dialer'
+  },
+  {
+    id: CREATOR_TYPE_SMS_REMINDER,
+    label: 'SMS Reminder'
+  },
+  {
+    id: CREATOR_TYPE_APP_NOTIFICATIONS,
+    label: 'App Notifications'
+  },
+  {
+    id: CREATOR_TYPE_HUBSPOT,
+    label: 'Hubspot'
+  },
+  {
+    id: CREATOR_TYPE_ZAPIER,
+    label: 'Zapier'
+  }
+]
