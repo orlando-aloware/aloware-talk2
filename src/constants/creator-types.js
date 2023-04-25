22704765b0ef3ddb8a142218ef20d54af4735c64
Yhael S <yhaelopez@gmail.com>
2023-04-24 19:00:01 -0500

    added alohabot creator type

--- src/constants/creator-types.js
+++ src/constants/creator-types.js
@@ -16,6 +16,8 @@ export const CREATOR_TYPE_APP_NOTIFICATIONS = 7
 export const CREATOR_TYPE_HUBSPOT = 8
 // Communication that initiated from Zapier
 export const CREATOR_TYPE_ZAPIER = 9
+// Communication that initiated from AlohaBot
+export const CREATOR_TYPE_ALOHABOT = 10
 
 export const ALL_CREATOR_TYPES = [
   {
@@ -53,5 +55,9 @@ export const ALL_CREATOR_TYPES = [
   {
     id: CREATOR_TYPE_ZAPIER,
     label: 'Zapier'
+  },
+  {
+    id: CREATOR_TYPE_ALOHABOT,
+    label: 'AlohaBot'
   }
 ]
