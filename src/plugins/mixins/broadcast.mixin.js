db48e594a382da93f0bae094e4a1935405fb66de
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2023-04-25 21:07:35 -0700

    Prepare for the user and communication events move to presence channel

--- src/plugins/mixins/broadcast.mixin.js
+++ src/plugins/mixins/broadcast.mixin.js
@@ -592,6 +592,56 @@ export default {
         .listen('.app.newversion', (event) => {
           this.$VueEvent.fire('new_version', event.data.message)
         })
+        .listen('.communication.created', (event) => {
+          if (event.tags) {
+            event.communication.tags = event.tags
+            event.communication.tag_ids = event.communication.tags.map((a) => a.id)
+          }
+          if (event.contact) {
+            event.communication.contact = event.contact
+          }
+          if (event.owner) {
+            event.communication.owner = event.owner
+          }
+          const campaign = this.campaigns.find(campaign => campaign.id === event.communication.campaign_id)
+          if (campaign) {
+            event.communication.campaign = campaign
+          }
+          this.$VueEvent.fire('new_communication', event.communication)
+        })
+        .listen('.communication.updated', (event) => {
+          if (event.tags) {
+            event.communication.tags = event.tags
+            event.communication.tag_ids = event.communication.tags.map((a) => a.id)
+          }
+          if (event.contact) {
+            event.communication.contact = event.contact
+          }
+          if (event.owner) {
+            event.communication.owner = event.owner
+          }
+          const campaign = this.campaigns.find(campaign => campaign.id === event.communication.campaign_id)
+          if (campaign) {
+            event.communication.campaign = campaign
+          }
+          this.$VueEvent.fire('update_communication', event.communication)
+        })
+        .listen('.communication.deleted', (event) => {
+          this.$VueEvent.fire('delete_communication', event.communication)
+        })
+        .listen('.user.created', (event) => {
+          if (this.currentCompany && event.user.company_id && event.user.company_id === this.currentCompany.id) {
+            this.$VueEvent.fire('user_created', event.user)
+          }
+        })
+        .listen('.user.updated', (event) => {
+          this.$VueEvent.fire('user_updated', event.user)
+        })
+        .listen('.user.deleted', (event) => {
+          if (this.currentCompany && event.user.company_id && event.user.company_id === this.currentCompany.id) {
+            this.$VueEvent.fire('user_deleted', event.user)
+          }
+        })
 
       window.Echo.private('cache-agent-status-' + this.profile.company_id)
         .listen('.agent_status.updated', (event) => {
