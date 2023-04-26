8b15c4b43fc65ba94e156082950b3441f2fb6f6c
Jonathan M <jonathan@aloware.com>
2023-04-26 09:22:37 -0300

    Replaced contact.id by contact_id

--- src/components/communication-details.vue
+++ src/components/communication-details.vue
@@ -564,7 +564,7 @@
               </b-col>
               <b-col>
                 <div class="d-flex align-items-center">
-                  <router-link :to="{ name: 'Communication', params: {contactId: contactId, communicationId: communication.metadata.new_communication_id }}">
+                  <router-link :to="{ name: 'Communication', params: {contactId: communication.contact_id, communicationId: communication.metadata.new_communication_id }}">
                     More info
                   </router-link>
                 </div>
@@ -578,7 +578,7 @@
               </b-col>
               <b-col>
                 <div class="d-flex align-items-center">
-                  <router-link :to="{ name: 'Communication', params: {contactId: contactId, communicationId: communication.metadata.original_communication_id }}">
+                  <router-link :to="{ name: 'Communication', params: {contactId: communication.contact_id, communicationId: communication.metadata.original_communication_id }}">
                     More info
                   </router-link>
                 </div>
@@ -592,7 +592,7 @@
               </b-col>
               <b-col>
                 <div class="d-flex align-items-center">
-                  <router-link :to="{ name: 'Communication', params: {contactId: contactId, communicationId: communication.metadata.active_communication_id }}">
+                  <router-link :to="{ name: 'Communication', params: {contactId: communication.contact_id, communicationId: communication.metadata.active_communication_id }}">
                     More info
                   </router-link>
                 </div>
@@ -606,7 +606,7 @@
               </b-col>
               <b-col>
                 <div class="d-flex align-items-center">
-                  <router-link :to="{ name: 'Communication', params: {contactId: contactId, communicationId: communication.metadata.fake_communication_id }}">
+                  <router-link :to="{ name: 'Communication', params: {contactId: communication.contact_id, communicationId: communication.metadata.fake_communication_id }}">
                     More info
                   </router-link>
                 </div>
@@ -821,7 +821,11 @@ export default {
     DownloadButton
   },
 
-  mixins: [communicationInfoMixin, userMixin, aclMixin],
+  mixins: [
+    communicationInfoMixin,
+    userMixin,
+    aclMixin
+  ],
 
   data () {
     return {
@@ -840,11 +844,13 @@ export default {
     communication: {
       required: true
     },
+
     verbose: {
       required: false,
       default: false,
       type: Boolean
     },
+
     dialerMode: {
       required: false,
       default: false,
@@ -906,6 +912,7 @@ export default {
     attemptLabel () {
       return this.communication.attempt ? `attempt ${this.communication.attempt}` : 'no attempts'
     },
+
     callDescriptionText () {
       const text = 'This call '
 
@@ -919,9 +926,6 @@ export default {
         default:
           return text + 'was'
       }
-    },
-    contactId () {
-      return _.get(this.communication, 'contact.id', null)
     }
   },
 
