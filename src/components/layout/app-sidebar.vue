831ac38b42e17e8c6ee365a94680060fd2a92336
Alberto Mello <alberto@aloware.com>
2023-05-04 19:01:48 -0300

    Improve icon

--- src/components/layout/app-sidebar.vue
+++ src/components/layout/app-sidebar.vue
@@ -386,16 +386,28 @@
 
     <q-btn :to="{ name: 'Broadcasts' }"
            :ripple="false"
-           icon="img:app-icons/menu/broadcast.svg"
+           icon="img:app-icons/menu/broadcast_gray.svg"
+           align="center"
+           padding="none"
+           class="nav-icons w-100"
+           v-show="!isActive('Broadcasts')"
+           flat>
+      <q-tooltip anchor="center right"
+                 self="center left">
+        <span class="font-weight-bold text-sm">Broadcasts</span>
+      </q-tooltip>
+    </q-btn>
+    <q-btn :to="{ name: 'Broadcasts' }"
+           :ripple="false"
+           icon="img:app-icons/menu/broadcast_active.svg"
            align="left"
            padding="none"
            class="nav-icons w-100"
-           v-show="isActive('Broadcast')"
+           v-show="isActive('Broadcasts')"
            flat>
       <q-tooltip anchor="center right"
-                 self="center left"
-                 :offset="[-5, 0]">
-        <span class="font-weight-bold text-sm">Broadcast</span>
+                 self="center left">
+        <span class="font-weight-bold text-sm">Broadcasts</span>
       </q-tooltip>
     </q-btn>
 
