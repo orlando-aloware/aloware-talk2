6860421e1c9cb348796c07cc64c9703e6683e3d0
Eleonor Somosot <eleonor@aloware.com>
2023-05-24 07:45:22 +0800

    Bulk actions UI

---
+++ src/components/tags-bulk-action-menu.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="bulk-action-menu bulk-action-menu__tags">
+    <div class="menu-actions d-flex flex-row">
+
+      <div class="items">
+        <span>{{ getSelectedCount }} selected</span>
+      </div>
+
+      <div class="items">
+        <a href="#"
+           disabled
+           @click.prevent>
+          <i class="fa fa-minus-square"></i>
+          Unselect All
+        </a>
+      </div>
+
+      <div class="items">
+        <a href="#"
+           disabled
+           @click.prevent>
+          <i class="fa fa-layer-group"></i>
+          Assign Contacts
+        </a>
+      </div>
+
+      <div class="items">
+        <a href="#"
+           disabled
+           @click.prevent>
+          <i class="fa fa-layer-group"></i>
+          Add to Power Dialer
+        </a>
+      </div>
+
+      <div class="items">
+        <a href="#"
+           disabled
+           @click.prevent>
+          <i class="fa fa-layer-group"></i>
+          Enroll Contacts
+        </a>
+      </div>
+
+      <div class="items">
+        <a href="#"
+           disabled
+           @click.prevent>
+          <i class="fa fa-trash text-danger"></i>
+          Delete
+        </a>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import { tagsMixin } from 'src/plugins/mixins'
+import { mapState } from 'vuex'
+
+export default {
+  name: 'tags-bulk-action-menu',
+
+  mixins: [
+    tagsMixin
+  ],
+
+  computed: {
+    ...mapState('tagsModule', ['selectedTagIds']),
+
+    getSelectedCount () {
+      return [...this.selectedTagIds].length
+    }
+  }
+}
+</script>
