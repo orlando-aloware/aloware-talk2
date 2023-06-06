fa9c12a8fca56d3972b81f869e4f9cc470de655a
Eleonor Somosot <eleonor@aloware.com>
2023-06-07 07:12:13 +0800

    Fix condition

--- src/components/inbox/inbox-tasks/item.vue
+++ src/components/inbox/inbox-tasks/item.vue
@@ -40,7 +40,7 @@
         </div>
         <div class="comm-label text-grey-90 d-flex align-items-center">
           <div class="truncated-text"
-               v-if="!isNotInProgressCall">
+               v-if="isNotInProgressCall">
             {{ contact.last_communication.direction | fixCommDirection }} {{ contact.last_communication.type | fixCommType }}
           </div>
           <div class="truncated-text call-parked-label"
@@ -450,7 +450,7 @@ export default {
         CommunicationTypes.REMINDER
       ]
 
-      return notCallCommunicationType.includes(this.contact.last_communication.type) &&
+      return !notCallCommunicationType.includes(this.contact.last_communication.type) &&
         !this.isParkedCall && !this.isConnectedCall
     },
 
