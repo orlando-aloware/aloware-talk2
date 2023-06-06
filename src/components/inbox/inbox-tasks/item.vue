5797a3e96cca5aba29ecef8055790c6513120e43
Eleonor Somosot <eleonor@aloware.com>
2023-06-07 06:51:06 +0800

    Revert part of condition fix

--- src/components/inbox/inbox-tasks/item.vue
+++ src/components/inbox/inbox-tasks/item.vue
@@ -40,7 +40,7 @@
         </div>
         <div class="comm-label text-grey-90 d-flex align-items-center">
           <div class="truncated-text"
-               v-if="isNotInProgressCall">
+               v-if="!isNotInProgressCall">
             {{ contact.last_communication.direction | fixCommDirection }} {{ contact.last_communication.type | fixCommType }}
           </div>
           <div class="truncated-text call-parked-label"
