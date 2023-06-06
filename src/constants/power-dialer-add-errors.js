ce6c12eaed241b6ea8bf4fa5e7e74f8076f26fcc
Alberto Mello <alberto@aloware.com>
2023-06-06 14:03:02 -0300

    Adds constants to power dialer list add error

---
+++ src/constants/power-dialer-add-errors.js
@@ -0,0 +1,12 @@
+/* When the contact has a number that is duplicated */
+export const DUPLICATED = 1
+/* Contact with multiple numbers but not added */
+export const MULTIPLE = 2
+/* Contact with a disposition that cannot be added to pd list */
+export const UNAUTHORIZED_DISPOSITION = 3
+/* Contact with no primary phone number */
+export const NO_PRIMARY_PHONE_NUMBER = 4
+/* Invalid number */
+export const INVALID = 5
+/* International numbers not allowed */
+export const INTERNATIONAL_DISALLOWED = 6
