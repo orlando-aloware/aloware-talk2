6658fb5bf0da19552e39173ef2001d37829436e3
Alberto Mello <alberto@aloware.com>
2023-06-09 17:54:53 -0300

    Draggable columns

--- src/pages/broadcast/Broadcasts.vue
+++ src/pages/broadcast/Broadcasts.vue
@@ -125,7 +125,8 @@
                  :paginated="false"
                  :total-rows="visibleBroadcasts?.length ?? 0"
                  :loading="loading"
-                 @sort="onSortTable">
+                 @sort="onSortTable"
+                 @reordered="onColumnsReordered">
         <template slot="tbody">
           <tr v-for="(row, rowIndex) in visibleBroadcasts"
               v-bind:key="rowIndex">
@@ -301,54 +302,63 @@ const broadcastsColumns = [
     name: 'status',
     label: 'Status',
     field: 'status',
-    sortable: true
+    sortable: true,
+    draggable: true
   },
   {
     name: 'pending_tasks',
     label: 'Pending Tasks',
     field: 'pending_tasks',
-    sortable: true
+    sortable: true,
+    draggable: true
   },
   {
     name: 'total_failed',
     label: 'Failed Tasks',
     field: 'total_failed',
-    sortable: true
+    sortable: true,
+    draggable: true
   },
   {
     name: 'total_enrolled',
     label: 'Total Tasks',
     field: 'total_enrolled',
-    sortable: true
+    sortable: true,
+    draggable: true
   },
   {
     name: 'engagement_rate',
     label: 'Engagement',
     field: 'engagement_rate',
-    sortable: true
+    sortable: true,
+    draggable: true
   },
   {
     name: 'total_unsubscribed',
     label: 'Unsubscribed',
     field: 'total_unsubscribed',
-    sortable: true
+    sortable: true,
+    draggable: true
   },
   {
     name: 'target_group',
     label: 'Target Group',
-    field: 'target_group'
+    field: 'target_group',
+    draggable: true
   },
   {
     name: 'campaign_id',
     label: 'Line Used',
     field: 'campaign_id',
-    sortable: true
+    sortable: true,
+    draggable: true
   },
   {
     name: 'throttle_limit',
     label: 'Throttling',
     field: 'throttle_limit',
-    sortable: true
+    sortable: true,
+    draggable: true
   },
   {
     name: '',
@@ -786,6 +796,10 @@ export default {
 
     isSelectedRow (row) {
       return this.contextMenuTargetId === row.id
+    },
+
+    onColumnsReordered (columnOrder) {
+      this.broadcastsColumns = columnOrder
     }
   }
 }
