060ae783ca4b5ee753509fff23d2dd8ecd05cbfe
Eleonor Somosot <eleonor@aloware.com>
2023-06-14 00:22:53 +0800

    Display Fax comm type label

--- src/components/inbox/inbox-tasks/item.vue
+++ src/components/inbox/inbox-tasks/item.vue
@@ -41,7 +41,7 @@
         <div class="comm-label text-grey-90 d-flex align-items-center">
           <div class="truncated-text"
                :class="callStatusClass"
-               v-if="contact.last_communication.type === CommunicationTypes.CALL">
+               v-if="[CommunicationTypes.CALL, CommunicationTypes.FAX].includes(contact.last_communication.type)">
             <q-tooltip>
               {{ communicationLabel }}
             </q-tooltip>
