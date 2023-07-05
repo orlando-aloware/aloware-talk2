af3204b65c3502b7b321721c920021d8aedf7db1
Hamed Kamrava <hamed@aloware.com>
2023-07-05 18:22:56 +0100

    add log

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -635,6 +635,7 @@ export default {
       if (this.profile.sleep_mode) {
         return
       }
+      console.log('HAMED_LOG_8', incomingNumber, contact, direction)
       this.handleInAppHighSmsVolumeNotification(incomingNumber, contact, direction)
     }
 
@@ -2173,7 +2174,8 @@ export default {
     },
 
     handleInAppHighSmsVolumeNotification (incomingNumber, contact, direction) {
-      console.log('HAMED_LOG_10', incomingNumber, contact.name)
+      console.log('HAMED_LOG_10', incomingNumber)
+      console.log('HAMED_LOG_11', contact.name)
       if (window.Push.Permission.has()) {
         this.$generalNotification(`Received too many messages from a contacts.</br>Name: ${contact.name}</br>Incoming Number: ${incomingNumber.phone_number}`, 'error', 5000, true)
       }
