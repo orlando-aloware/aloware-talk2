f8c5f91b4173c70e88524e00a274bf4808d5ca99
Eleonor Somosot <eleonor@aloware.com>
2023-07-14 12:59:18 +0800

    Refresh button in Inbox page header

--- src/components/layout/app-header.vue
+++ src/components/layout/app-header.vue
@@ -23,8 +23,6 @@
       <inbox-list-navigation v-if="(['Inbox', 'Inbox Contact Task'].includes($route.name) || ['/channels/inbox/open', '/channels/inbox/pending', '/channels/inbox/closed'].includes($route.path)) && !titleOnly" />
       <inbox-channel-navigation v-if="(['Inbox Contact', 'Inbox Contact Communication', 'Inbox Channel'].includes($route.name) || ['/channels/mentions/received', '/channels/mentions/sent'].includes($route.path)) && !titleOnly" />
 
-      <inbox-my-contacts-filter v-if="!isMobile || !$q.screen.lt.md"/>
-
       <compact-btn class="bg-white border stats-refresh-btn border-half-rounded d-flex justify-content-center align-items-center"
                    :disabled="loading"
                    v-if="$route.name === 'Stats' && !titleOnly"
@@ -32,6 +30,7 @@
         <refresh-icon />
         Refresh
       </compact-btn>
+
       <compact-btn class="bg-white border stats-refresh-btn border-half-rounded d-flex justify-content-center align-items-center"
                    :disabled="loading || contactsRefreshIsDisabled"
                    v-if="$route.name === 'Contacts'"
@@ -39,6 +38,16 @@
         <refresh-icon />
         Refresh
       </compact-btn>
+
+      <compact-btn class="bg-white border stats-refresh-btn border-half-rounded d-flex justify-content-center align-items-center"
+                   :disabled="loading"
+                   v-if="isInInboxPage"
+                   @clicked="refreshInbox">
+        <refresh-icon />
+        Refresh
+      </compact-btn>
+
+      <inbox-my-contacts-filter v-if="!isMobile || !$q.screen.lt.md"/>
     </div>
     <!--div class="ml-auto d-none d-lg-block h-100"-->
     <div class="ml-auto d-block h-100">
@@ -313,6 +322,13 @@ export default {
 
     isDialerDisabled () {
       return (!this.isDialerReady && !this.dialer.error.code) || this.hasRole(Roles.COMPANY_REPORTER_ACCESS)
+    },
+
+    isInInboxPage () {
+      const path = this.$route.path
+      return this.$route.name === 'Inbox' ||
+          path.includes('inbox') ||
+          path.includes('channels')
     }
   },
 
@@ -327,6 +343,17 @@ export default {
   },
 
   methods: {
+    ...mapActions('stats', [
+      'setMetricGroups',
+      'setMetricLoader'
+    ]),
+
+    ...mapActions('contacts', [
+      'updateContactsListFilter'
+    ]),
+
+    ...mapActions(['setDialerFormStatus']),
+
     reconnectDialer () {
       this.$VueEvent.fire('reconnectDialer')
     },
@@ -412,16 +439,9 @@ export default {
       }
     },
 
-    ...mapActions('stats', [
-      'setMetricGroups',
-      'setMetricLoader'
-    ]),
-
-    ...mapActions('contacts', [
-      'updateContactsListFilter'
-    ]),
-
-    ...mapActions(['setDialerFormStatus'])
+    refreshInbox () {
+      this.$VueEvent.fire('fetchInbox')
+    }
   },
 
   watch: {
