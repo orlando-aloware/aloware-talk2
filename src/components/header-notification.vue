a5f367892610d1b7333c89af661ec185a5b3b16c
jason-aloware <jason@aloware.com>
2023-07-18 06:45:51 +0800

    Reverted changes

--- src/components/header-notification.vue
+++ src/components/header-notification.vue
@@ -58,7 +58,7 @@
 
       <compact-btn customClass="fs-14 _500 position-relative primary not-focusable ml-2 text-danger"
                    borderless
-                   v-if="diagnosis && diagnosis.closeable"
+                   v-if="diagnosis && diagnosis.closable"
                    @clicked="close">
         <i class="fa fa-times"/>
       </compact-btn>
