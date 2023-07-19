513b03ac75904aee91f80693db68ee10f501f7c3
Eleonor Somosot <eleonor@aloware.com>
2023-07-20 00:50:57 +0800

    Remove temporary

--- src/boot/bootstrap.js
+++ src/boot/bootstrap.js
@@ -27,8 +27,7 @@ storage.local.setItem('api_reporting_url', process.env.API_REPORTING_URL)
 storage.local.setItem('pusher_app_key', process.env.PUSHER_APP_KEY)
 storage.local.setItem('pusher_cluster', process.env.PUSHER_CLUSTER)
 storage.local.setItem('sentry_dsn_public', process.env.MIX_SENTRY_DSN_PUBLIC)
-// storage.local.setItem('aloware_demo_companies', Object.values(process.env.DEMO_COMPANY_IDS).join(','))
-storage.local.setItem('aloware_demo_companies', '7,47,1659') // temporary
+storage.local.setItem('aloware_demo_companies', Object.values(process.env.DEMO_COMPANY_IDS).join(','))
 
 Vue.use(infiniteScroll)
 Vue.use(BootstrapVue)
