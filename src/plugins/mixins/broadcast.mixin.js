a11ad7c962399579d015764a559e31ad1e91cc9b
Hamed Kamrava <hamed@aloware.com>
2023-07-24 17:51:28 +0100

    fix

--- src/plugins/mixins/broadcast.mixin.js
+++ src/plugins/mixins/broadcast.mixin.js
@@ -115,8 +115,8 @@ export default {
             this.$VueEvent.fire('new_in_app_call', event.communication)
           }
         })
-        .listen('.user.desktop.incoming_number.high_sms_volume', (event) => {
-          this.$VueEvent.fire('desktop_high_sms_volume', event)
+        .listen('.user.desktop.incoming_number.high_sms_volume', ({ incomingNumber, contact, direction }) => {
+          this.$VueEvent.fire('desktop_high_sms_volume', { incomingNumber, contact, direction })
         })
         .listen('.user.in-app.communication.answered_call', (event) => {
           const campaign = this.campaigns.find(campaign => campaign.id === event.communication.campaign_id)
