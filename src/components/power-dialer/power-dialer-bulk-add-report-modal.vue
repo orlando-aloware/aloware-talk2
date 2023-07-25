cdcb94d9ae5010805cc1351640c4c9ecd876d8be
jason-aloware <jason@aloware.com>
2023-07-26 02:54:11 +0800

    Added more summaries

--- src/components/power-dialer/power-dialer-bulk-add-report-modal.vue
+++ src/components/power-dialer/power-dialer-bulk-add-report-modal.vue
@@ -10,6 +10,9 @@
       <ul>
         <li>{{ addedFromContact }} tasks from contacts</li>
         <li v-if="addedFromMultipleNumbers > 0">{{ addedFromMultipleNumbers }} tasks from multiple numbers</li>
+        <li v-if="hasAddedDuplicates">{{ addedDuplicates }} duplicates</li>
+        <li v-if="hasAddedOwnContacts">{{ addedOwnContacts }} owned contacts</li>
+        <li v-if="hasAddedInternationalPhoneNumbers">{{ addedInternationalPhoneNumbers }} international phone numbers</li>
       </ul>
       <template v-if="skipped.length > 0">
         <p class="m-0">Contacts not added/skipped</p>
@@ -33,6 +36,7 @@
 </template>
 <script>
 import { PD_BULK_ADD_MESSAGES } from 'src/constants/power-dialer-add-errors'
+import { get } from 'lodash'
 
 export default {
   name: 'PowerDialerBulkAddReportModal',
@@ -66,6 +70,36 @@ export default {
 
     addedFromMultipleNumbers () {
       return this.statusReport?.success?.multiple_numbers ?? 0
+    },
+
+    hasAddedDuplicates () {
+      const flag = get(this.statusReport, 'success.duplicates', false)
+
+      return this.$isNumeric(flag) ? true : flag
+    },
+
+    addedDuplicates () {
+      return this.statusReport?.success?.duplicates ?? 0
+    },
+
+    hasAddedOwnContacts () {
+      const flag = get(this.statusReport, 'success.own_contacts', false)
+
+      return this.$isNumeric(flag) ? true : flag
+    },
+
+    addedOwnContacts () {
+      return this.statusReport?.success?.own_contacts ?? 0
+    },
+
+    hasAddedInternationalPhoneNumbers () {
+      const flag = get(this.statusReport, 'success.international_phone_numbers', false)
+
+      return this.$isNumeric(flag) ? true : flag
+    },
+
+    addedInternationalPhoneNumbers () {
+      return this.statusReport?.success?.international_phone_numbers ?? 0
     },
 
     skipped () {
