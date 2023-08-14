7dcbbbac14b9bd40692356fc3a8f32e39ee63245
Jason Behik <jason@aloware.com>
2023-08-15 07:57:30 +0800

    Disabled the reCAPTCHA for electron

--- src/plugins/mixins/recaptcha.mixin.js
+++ src/plugins/mixins/recaptcha.mixin.js
@@ -12,18 +12,23 @@ export default {
   },
 
   mounted () {
-    this.disabledSubmit = true
+    if (!this.$q.platform.is.electron) {
+      this.disabledSubmit = true
 
-    window.recaptchaOnloadCallback = () => {
-      if (document.querySelector('#recaptcha-element')) {
-        window.grecaptcha.render('recaptcha-element', {
-          sitekey: this.siteKey,
-          callback: this.onCaptchaVerified
-        })
+      window.recaptchaOnloadCallback = () => {
+        if (document.querySelector('#recaptcha-element')) {
+          window.grecaptcha.render('recaptcha-element', {
+            sitekey: this.siteKey,
+            callback: this.onCaptchaVerified
+          })
+        }
       }
+
+      this.loadRecaptchaScript()
+      return
     }
 
-    this.loadRecaptchaScript()
+    this.disabledSubmit = false
   },
 
   methods: {
