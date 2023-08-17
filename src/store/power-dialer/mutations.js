4946038643fe9865bfe9e3e430c0bf87b7f88984
Jason Behik <jason@aloware.com>
2023-08-17 16:54:47 +0800

    Debug

--- src/store/power-dialer/mutations.js
+++ src/store/power-dialer/mutations.js
@@ -246,6 +246,8 @@ export default {
   STORE_BULK_ACTION_NOTIFICATION (state, value) {
     if (isEmpty(state.bulkAddContactsNotification?.[value.contact_list_id])) {
       state.bulkAddContactsNotification[value.contact_list_id] = value
+      console.log('value: ', value)
+      console.log('state.bulkAddContactsNotification: ', JSON.parse(JSON.stringify(state.bulkAddContactsNotification)))
 
       return
     }
@@ -254,6 +256,8 @@ export default {
       state.bulkAddContactsNotification[value.contact_list_id],
       value
     )
+    console.log('value: ', value)
+    console.log('state.bulkAddContactsNotification: ', JSON.parse(JSON.stringify(state.bulkAddContactsNotification)))
   },
 
   CLEAR_BULK_ACTION_NOTIFICATION (state, contactListId) {
