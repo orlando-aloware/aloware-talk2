632e5cb701f5f25f08a6c5ba72ee17394c7d1be9
Jonathan M <jonathan@aloware.com>
2023-09-18 11:01:48 -0300

    Added constant file for inbound call recording modes

---
+++ src/constants/inbound-call-recording-modes.js
@@ -0,0 +1,6 @@
+// company follows the default inbound call recording logic of campaign
+export const INBOUND_CALL_RECORDING_MODE_DEFAULT = 1
+// all inbound calls will be recorded
+export const INBOUND_CALL_RECORDING_MODE_ALWAYS = 2
+// all inbound calls won't be recorded
+export const INBOUND_CALL_RECORDING_MODE_NEVER = 3
