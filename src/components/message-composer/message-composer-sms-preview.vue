2cbee2228ef06f1a6c5ea91eb4093749c844049f
Jonathan M <jonathan@aloware.com>
2023-09-21 14:40:12 -0300

    Added image preview in Message Composer preview component

--- src/components/message-composer/message-composer-sms-preview.vue
+++ src/components/message-composer/message-composer-sms-preview.vue
@@ -1,11 +1,16 @@
 <template>
-  <q-input class="q-input-composer"
-           borderless
-           autogrow
-           ref="smsMessageBody"
-           input-class="q-input-pl-0 q-input-pr-0 pt-0 pb-0"
-           type="textarea"
-           :value="text"/>
+  <div>
+    <img class="img-fluid"
+         :src="attachment"
+         v-if="attachment">
+    <q-input class="q-input-composer"
+             borderless
+             autogrow
+             ref="smsMessageBody"
+             input-class="q-input-pl-0 q-input-pr-0 pt-0 pb-0"
+             type="textarea"
+             :value="text"/>
+  </div>
 </template>
 
 <script>
@@ -42,6 +47,16 @@ export default {
 
     text () {
       return this.translateMessage(this.messageComposer.sms.body, this.contact, this.selectedLine, this.profile)
+    },
+
+    baseUrl () {
+      return window.axios.defaults.baseURL
+    },
+
+    attachment () {
+      return this.messageComposer.sms.gif_url ||
+        (this.messageComposer.sms.attachments.length ? `${this.baseUrl}/static/uploaded_file/${this.messageComposer.sms.attachments[0].uuid}` : null) ||
+        null
     }
   }
 }
