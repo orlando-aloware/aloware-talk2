c6ceb83c75f3395923a670da5000c094f991a4bf
nestor <nestor@xmlteam.com>
2023-10-03 09:42:03 -0600

    Remove unused action

--- src/components/contacts/contacts-filters.vue
+++ src/components/contacts/contacts-filters.vue
@@ -736,7 +736,6 @@ export default {
       'closeFilters',
       'setCurrentListFilters',
       'updateContactsListFilter',
-      'updateContactsExportListFilter',
       'setListContactsLoaded'
     ]),
 
