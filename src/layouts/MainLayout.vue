5e0701f86820a7e9a970f422a72af96214b78b30
Jason Behik <jason@aloware.com>
2023-10-05 02:19:56 +0800

    Fixed comment

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2625,7 +2625,7 @@ export default {
         this.mobilePhoneDrawer = true
       }
 
-      // fix the mobile screen top padding
+      // fixed the mobile screen top padding
       // excluding inbox default page in smaller screen
       const isPhonePage = from.name === 'Phone' || this.mobilePhoneDrawer
       const isSmallMobileInbox = this.$route.name.includes('Inbox') && this.$q.screen.lt.md
