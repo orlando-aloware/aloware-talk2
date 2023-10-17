d73554cc1cf2e879d75b4ffd617ae990fda0e8d1
Jorge A Ramirez <jorge@aloware.com>
2023-10-17 16:22:58 -0500

    CORE-3803 Adding Refer A Firend button

--- src/components/layout/app-header.vue
+++ src/components/layout/app-header.vue
@@ -55,6 +55,12 @@
 
       <inbox-my-contacts-filter v-if="(!isMobile || !$q.screen.lt.md) && isInInboxPage"/>
     </div>
+
+    <div class="ml-auto d-block h-100">
+      <div class="d-flex h-100 align-items-center justify-content-end ml-1">
+        <div id='referralhero-inline-button'></div>
+      </div>
+    </div>
     <!--div class="ml-auto d-none d-lg-block h-100"-->
     <div class="ml-auto d-block h-100">
       <div class="d-flex h-100 align-items-center justify-content-end ml-1">
