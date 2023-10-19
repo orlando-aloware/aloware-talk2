03d83b4595bdf5d8ded381d6a6ac13e0c3cc12ed
jefferson-aloware <jefferson@aloware.com>
2023-10-19 17:43:49 -0300

    feat: :sparkles: create business-information-form component

---
+++ src/components/account-registration/business-information-form.vue
@@ -0,0 +1,265 @@
+<template>
+  <div>
+    <step-header
+      title="Your Business"
+      description="Great! Now, we need important information to verify your business."
+    />
+
+    <div class="min-w-100">
+      <input-group>
+        <template v-slot:content>
+          <input-field
+            label="Business Name"
+            placeholder="Aloware Inc."
+            :paddingClasses="isLargeScreen ? 'q-pl-4' : ''"
+            v-model="form.company_name"
+          />
+
+          <div class="col-5 pr-0 pl-0">
+            <select-field
+              label="Business Type"
+              option-label="value"
+              option-value="value"
+              col-md="col-md-12"
+              :paddingClasses="isLargeScreen ? 'q-pr-0 q-pl-0' : ''"
+              :options="businessTypes"
+              v-model="form.business_type"
+            />
+            <div class="flex items-center">
+              <label class="flex mb-1 text-weight-bold pl-3 pr-4">
+                Company Status
+              </label>
+              <q-btn-toggle
+                class="toggle-border"
+                toggle-color="primary"
+                color="white"
+                text-color="#4F4F4F"
+                spread
+                no-caps
+                unelevated
+                dense
+                rounded
+                :options="[
+                  { label: 'Private', value: 1 },
+                  { label: 'Public', value: 2 }
+                ]"
+                v-model="form.company_status"
+              />
+            </div>
+          </div>
+        </template>
+      </input-group>
+
+      <input-group>
+        <template v-slot:content>
+          <select-field
+            label="Business ID Type"
+            option-label="label"
+            option-value="value"
+            :paddingClasses="isLargeScreen ? 'q-pr-4' : ''"
+            :options="businessIdTypes"
+            v-model="form.business_registration_identifier"
+          />
+
+          <input-field
+            label="Business Registration Number"
+            placeholder="Ex: C1234567"
+            :paddingClasses="isLargeScreen ? 'q-pl-4' : ''"
+            v-model="form.business_registration_number"
+          />
+        </template>
+      </input-group>
+
+      <input-group>
+        <template v-slot:content>
+          <select-field
+            label="Region of Operation"
+            option-label="label"
+            option-value="value"
+            :paddingClasses="isLargeScreen ? 'q-pr-4' : ''"
+            :options="regionsOfOperations"
+            v-model="form.business_regions_of_operation"
+          />
+
+          <input-field
+            label="Website URL"
+            placeholder="Ex: yourcompany.com"
+            :paddingClasses="isLargeScreen ? 'q-pl-4' : ''"
+            v-model="form.website_url"
+          />
+        </template>
+      </input-group>
+
+      <input-group>
+        <template v-slot:content>
+          <select-field
+            label="Business Industry"
+            option-label="name"
+            option-value="value"
+            :paddingClasses="isLargeScreen ? 'q-pr-4' : ''"
+            :options="businessIndustries"
+            v-model="form.business_industry"
+          />
+
+          <div
+            class="col-5"
+            v-if="isLargeScreen"/>
+        </template>
+      </input-group>
+
+      <div class="flex justify-center  pt-4 q-row">
+        <div class="col-xs-12 col-md-5 q-pl-none q-pr-none q-lg-pl-4">
+          <h4 class="text-h5 text-weight-bold">Business Address</h4>
+        </div>
+        <div class="col-5" />
+      </div>
+
+      <input-group>
+        <template v-slot:content>
+          <input-field
+            label="Street"
+            placeholder="Ex: Fifth Avenue"
+            col-md="col-md-10"
+            :paddingClasses="isLargeScreen ? 'q-pr-4' : ''"
+            v-model="form.street"
+          />
+        </template>
+      </input-group>
+
+      <input-group>
+        <template v-slot:content>
+          <input-field
+            label="State/Province/Region"
+            placeholder="Ex: California"
+            v-model="form.region"
+          />
+
+          <input-field
+            label="City"
+            placeholder="Ex: Los Angeles"
+            :paddingClasses="isLargeScreen ? 'q-pl-4' : ''"
+            v-model="form.city"
+          />
+        </template>
+      </input-group>
+
+      <input-group>
+        <template v-slot:content>
+          <select-field
+            label="Country"
+            placeholder="Ex: United States"
+            option-label="name"
+            option-value="id"
+            :paddingClasses="isLargeScreen ? 'q-pl-4' : ''"
+            :options="countries"
+            v-model="form.legal_country"
+          />
+
+          <input-field
+            label="Postal Code"
+            placeholder="Ex: 11223"
+            :paddingClasses="isLargeScreen ? 'q-pl-4' : ''"
+            bottom-slots
+            v-model="form.postal_code"
+          >
+            <template
+              v-slot:hint
+              v-if="form.postal_code.length">
+              <div class="zipcode-hint">
+                <q-icon
+                  class="q-mr-xs"
+                  :class="getZipCodeRuleClass(validateZipCode(form.postal_code))"
+                  :name="iconForValidation(validateZipCode(form.postal_code))"
+                >
+                </q-icon>
+                  {{
+                    validateZipCode(form.postal_code)
+                      ? 'The Postal Code is valid'
+                      : 'Please enter a valid Postal Code'
+                  }}
+              </div>
+            </template>
+          </input-field>
+        </template>
+      </input-group>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import StepHeader from 'src/components/account-registration/step-header.vue'
+import InputGroup from 'src/components/account-registration/input-group.vue'
+import InputField from 'src/components/account-registration/input-field.vue'
+import SelectField from 'src/components/account-registration/select-field.vue'
+import businessTypes from '../../constants/account-registration-business-types'
+import businessIdTypes from '../../constants/account-registration-business-registration-identifiers'
+import regionsOfOperations from '../../constants/account-registration-business-regions-of-operations'
+import businessIndustries from '../../constants/account-registration-business-industries'
+
+export default {
+  name: 'BusinessInformationForm',
+  components: {
+    StepHeader,
+    InputGroup,
+    InputField,
+    SelectField
+  },
+  props: {
+    shouldShowSubmitButton: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data () {
+    return {
+      businessTypes,
+      businessIdTypes,
+      regionsOfOperations,
+      businessIndustries
+    }
+  },
+  computed: {
+    ...mapState('accountRegistration', ['form']),
+    isLargeScreen () {
+      return this.$q.screen.width > 767
+    },
+    countries () {
+      const countries = window.CountriesAndTimezones.getAllCountries()
+
+      // first exclude US & CA, and then extract only id, name fields of the countries
+      const countriesArr = Object.values(countries)
+        .filter(({ id }) => id !== 'US' && id !== 'CA')
+        .map(({ id, name }) => {
+          return { id, name }
+        })
+
+      // add US & CA to the top and return
+      return [
+        { id: 'US', name: 'United States' },
+        { id: 'CA', name: 'Canada' },
+        ...countriesArr
+      ]
+    }
+  },
+  methods: {
+    getZipCodeRuleClass (rule) {
+      return rule ? 'text-green' : 'text-red'
+    },
+    validateZipCode (zip) {
+      const zipRegex = /^\d{5}(?:[-\s]\d{4})?$/
+      return zipRegex.test(zip)
+    },
+    iconForValidation (isValid) {
+      return isValid ? 'check' : 'close'
+    },
+    onSubmit () {
+      this.$emit('submit', this.form)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
