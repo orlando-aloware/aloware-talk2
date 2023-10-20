6f652d8048ff53c4f4ef4533181e2b7fcf4db3bf
jefferson-aloware <jefferson@aloware.com>
2023-10-20 11:42:57 -0300

    feat: :sparkles: add country

--- src/store/account-registration/account-registration.getters.js
+++ src/store/account-registration/account-registration.getters.js
@@ -5,6 +5,7 @@ export default {
       business_registration_identifier: state.form.business_registration_identifier?.value,
       business_regions_of_operation: state.form.business_regions_of_operation?.value,
       business_industry: state.form.business_industry?.value,
+      country: state.form.country?.id,
       legal_country: state.form.legal_country?.id
     }
   }
