168487560770966cd6662fb9c37f722c9b3940d4
Jason Behik <jason@aloware.com>
2023-10-24 00:40:02 +0800

    Removed debug

--- src/plugins/mixins/dialer-communication.mixin.js
+++ src/plugins/mixins/dialer-communication.mixin.js
@@ -14,10 +14,6 @@ export default {
     'dialer.communication': {
       deep: true,
       handler (value) {
-        console.log('value?.current_status2: ', value?.current_status2)
-        console.log('typeof value.current_status2: ', typeof value?.current_status2)
-        console.log('typeof CommunicationCurrentStatus.CURRENT_STATUS_INPROGRESS_NEW: ', typeof CommunicationCurrentStatus.CURRENT_STATUS_INPROGRESS_NEW)
-        console.log('value && value.current_status2 === CommunicationCurrentStatus.CURRENT_STATUS_INPROGRESS_NEW: ', (value && value.current_status2 === CommunicationCurrentStatus.CURRENT_STATUS_INPROGRESS_NEW))
         if (value && value.current_status2 === CommunicationCurrentStatus.CURRENT_STATUS_INPROGRESS_NEW) {
           this.isCallInProgressStatus = true
 
