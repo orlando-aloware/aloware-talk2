a7dcf72fc2d945f3b4014dfdc356b09d35fb6903
Jonathan M <jonathan@aloware.com>
2023-10-25 15:39:49 -0300

    Added broadcasts mixin

---
+++ src/plugins/mixins/broadcasts.mixin.js
@@ -0,0 +1,27 @@
+import { mapState } from 'vuex'
+import { aclMixin } from 'src/plugins/mixins'
+import * as Roles from '../../constants/roles'
+
+export default {
+  mixins: [
+    aclMixin
+  ],
+
+  computed: {
+    ...mapState('auth', [
+      'profile'
+    ]),
+
+    canUseBroadcast () {
+      const isCompanyEnabled = this.profile.bulk_rmv_enabled || this.profile.bulk_sms_enabled
+      const isUserEnabled = this.hasPermissionTo('create broadcast message') || this.hasPermissionTo('create broadcast rvm')
+      const isAgent = this.hasRole(Roles.COMPANY_AGENT)
+
+      if (isAgent && !isUserEnabled) {
+        return false
+      }
+
+      return isCompanyEnabled
+    }
+  }
+}
