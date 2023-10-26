ed98ff3a403c6c2367c5a68f728b425bc42b4c9d
Jonathan M <jonathan@aloware.com>
2023-10-26 08:28:19 -0300

    Fixed rmv attribute name

--- src/plugins/mixins/broadcasts.mixin.js
+++ src/plugins/mixins/broadcasts.mixin.js
@@ -13,7 +13,7 @@ export default {
     ]),
 
     canUseBroadcast () {
-      const isCompanyEnabled = this.profile.bulk_rmv_enabled || this.profile.bulk_sms_enabled
+      const isCompanyEnabled = this.profile.bulk_rvm_enabled || this.profile.bulk_sms_enabled
       const isUserEnabled = this.hasPermissionTo('create broadcast message') || this.hasPermissionTo('create broadcast rvm')
       const isAgent = this.hasRole(Roles.COMPANY_AGENT)
 
