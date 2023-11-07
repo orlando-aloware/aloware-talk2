a5ad70a422015d9969563b2e6e3763cac834f468
Tito Alvarez <augustoalvarez@gmail.com>
2023-11-07 14:34:03 -0600

    Adding the pre_signup_id to the KYC account creation

--- src/store/account-registration/account-registration.getters.js
+++ src/store/account-registration/account-registration.getters.js
@@ -1,6 +1,7 @@
 export default {
   getBusinessInformationFieldsValue (state) {
     return {
+      pre_signup_id: state.data.pre_signup_id,
       legal_name: state.form.legal_name,
       business_type: state.form.business_type?.value,
       company_status: state.form.company_status,
