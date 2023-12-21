845d1cf2c2cde5be878bbbad48695191623b52e0
jefferson-aloware <jefferson@aloware.com>
2023-12-21 15:17:52 -0300

    feat: :sparkles: use isSendTextDisabled

--- src/components/message-composer/message-composer-sms.vue
+++ src/components/message-composer/message-composer-sms.vue
@@ -122,7 +122,7 @@
                        type="textarea"
                        placeholder="Type your message"
                        v-model="messageComposer.sms.body"
-                       :disable="isDisabled || isTCPAApprovedTextNotAuthorized"
+                       :disable="isSendTextDisabled"
                        @input="imposeCharactersLimit"
                        @keydown="onKeyDown"
                        @blur="onBlur">
