881323c8d1a3b6e778e7731633b45b75bd26b917
nestor <nestor@xmlteam.com>
2023-12-28 12:21:21 -0600

    Cleanup

--- src/plugins/mixins/contact.mixin.js
+++ src/plugins/mixins/contact.mixin.js
@@ -626,27 +626,18 @@ export default {
 
     showContactInfo (contactId, forceClearLoading = false) {
       this.mapCommunicationsData()
-      console.log('showContactInfo 1',
-        this.communicationsAndAudits.length,
-        this.contact,
-        this.contact.initial_campaign_id)
+
       // 1. if contact has initial campaign and there were no communications select initial campaign
       if (!this.communicationsAndAudits.length && this.contact && this.contact.initial_campaign_id) {
         this.selectedCampaignId = this.contact.initial_campaign_id
       }
 
-      console.log('showContactInfo 2',
-        this.communicationsAndAudits.length,
-        this.selectedCampaignId,
-        this.selectedCampaign)
       // 2. if contact has communications select last communication campaign
       if (!this.selectedCampaignId && this.communicationsAndAudits.length) {
         const latestCommunication = _.find(_.orderBy(this.communicationsAndAudits, item => item.created_at, ['desc']), item => {
           return item.type === CommunicationTypes.SMS
         })
-        console.log('showContactInfo 2-1',
-          this.selectedCampaignId,
-          latestCommunication)
+
         if (latestCommunication) {
           this.selectedCampaignId = latestCommunication.campaign_id
         }
@@ -655,20 +646,13 @@ export default {
       // 3. if user has a personal line and contact does not have an initial line
       const userCampaignId = _.get(this.profile, 'campaign_id', null)
 
-      console.log('showContactInfo 3',
-        userCampaignId,
-        this.selectedCampaignId,
-        this.selectedCampaign)
       if (!this.selectedCampaignId && userCampaignId) {
         this.selectedCampaignId = userCampaignId
       }
 
       // 4. if contact doesn't have situation 1 and 2 and selected_contact_campaigns has one campaign select the campaign
       const selectedContactFirstCampaignId = _.get(this.selectedContactCampaigns, '[0].id', null)
-      console.log('showContactInfo 4',
-        selectedContactFirstCampaignId,
-        this.selectedCampaignId,
-        this.selectedCampaign)
+
       if (!this.selectedCampaignId && selectedContactFirstCampaignId) {
         this.selectedCampaignId = selectedContactFirstCampaignId
       }
@@ -676,18 +660,10 @@ export default {
       // 5. if contact doesn't have situation 1 and 2 and 3 and company has one campaign select that campaign
       const firstCampaignId = _.get(this.campaigns, '[0].id', null)
 
-      console.log('showContactInfo 5',
-        selectedContactFirstCampaignId,
-        this.selectedCampaignId,
-        firstCampaignId,
-        this.selectedCampaign)
       if (!this.selectedCampaignId && !selectedContactFirstCampaignId && firstCampaignId) {
         this.selectedCampaignId = firstCampaignId
       }
 
-      console.log('showContactInfo 6',
-        firstCampaignId,
-        this.selectedCampaign)
       // 6. if contact doesn't have situation 1 and 2 and 3 and 4 and company has more then one campaign select the first one
       if (!this.selectedCampaignId && firstCampaignId) {
         this.selectedCampaignId = firstCampaignId
@@ -753,9 +729,7 @@ export default {
         this.communicationsPage++
 
         if (!skipContactInfo) {
-          this.$nextTick(() => {
-            this.showContactInfo(this.contactId, true)
-          })
+          this.showContactInfo(this.contactId, true)
         }
 
         return res
@@ -1394,7 +1368,6 @@ export default {
 
   watch: {
     'selectedCampaign.id': _.debounce(function (value) {
-      console.log('selectedCampaign.id', value)
       this.updateMessageComposer()
       this.updateLineIncomingNumber()
     }, 1000),
