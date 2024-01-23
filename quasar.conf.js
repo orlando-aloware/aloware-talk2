4a37aa6de093bc3468a88e43f4549f36cb6b643e
Yhael S <yhaelopez@gmail.com>
2024-01-23 13:36:44 -0600

    cleanup

--- quasar.conf.js
+++ quasar.conf.js
@@ -107,7 +107,7 @@ module.exports = function (/* ctx */) {
         app.use(cors())
       },
 
-      https: false,
+      https: true,
       host: 'localhost',
       port: 9090,
       disableHostCheck: true,
