f097a2e39ff577256ae7b11eb4a37e843870fee7
Felipe Marques <contato@felipemarques.com.br>
2024-01-31 12:11:14 -0300

    initial pagination

--- src/components/contacts/contacts-shared.vue
+++ src/components/contacts/contacts-shared.vue
@@ -13,6 +13,26 @@
                             :key="item.id">
       </contacts-shared-item>
       <contacts-sidebar-loader v-if="isLoading"></contacts-sidebar-loader>
+
+      <div class="d-flex justify-content-center border-top flex-grow-0 overflow-x-hidden"
+         style="min-height: 56px; color:black"
+         v-if="paginated">
+        <q-pagination class="table-pagination"
+                      padding="0 5px"
+                      boundary-links
+                      direction-links
+                      dense
+                      :max="lastPage"
+
+                      :ellipses="false"
+                      :boundary-numbers="false"
+                      v-model="paginationPage"
+                      @input="loadFolders"
+                      >
+        </q-pagination>
+
+      </div>
+
     </div>
   </div>
 </template>
@@ -23,7 +43,24 @@ import talk2Api from 'src/plugins/api/api'
 import ContactsSharedItem from 'components/contacts/contacts-shared-item'
 import ContactsSidebarLoader from 'components/contacts/contacts-sidebar-loader'
 
+export const XL_SCREEN_PAGES = 11
+export const LG_SCREEN_PAGES = 7
+export const DEFAULT_PAGES = 3
+
 export default {
+
+  props: {
+    paginated: {
+      type: Boolean,
+      default: true
+    },
+
+    lastPage: {
+      type: Number,
+      default: 1
+    }
+  },
+
   components: {
     ContactsSidebarLoader,
     ContactsSharedItem
@@ -34,7 +71,9 @@ export default {
       isLoading: false,
       lists: [],
       layer: 1,
-      listeners: {}
+      listeners: {},
+      paginationPage: 1,
+      perPage: 5
     }
   },
   methods: {
@@ -51,11 +90,17 @@ export default {
         contact_folder_id: null
       })
     },
+
     loadFolders () {
       this.isLoading = true
       this.setPublicListsLoaded(false)
       this.lists = []
-      talk2Api.V2.contactList.public().then(response => {
+      talk2Api.V2.contactList.public({
+        page: this.paginationPage,
+        size: this.perPage
+      }).then(response => {
+        const total = response.data.total
+        this.lastPage = Math.ceil(total > this.perPage ? Math.ceil(total / this.perPage) : 1)
         this.lists = response.data.data
       }).catch((err) => {
         console.error(err)
@@ -69,6 +114,19 @@ export default {
     }
   },
   computed: {
+
+    maxPaginationPages () {
+      if (this.$q.screen.xl) {
+        return XL_SCREEN_PAGES
+      }
+
+      if (this.$q.screen.lg) {
+        return LG_SCREEN_PAGES
+      }
+
+      return DEFAULT_PAGES
+    },
+
     foldersWithoutRoot () {
       return this.lists.filter(folder => folder.name !== 'Root')
     },
@@ -84,6 +142,15 @@ export default {
     this.$VueEvent.listen('fetchContactsLists', this.listeners.fetchContactsLists)
   },
   watch: {
+
+    paginationPage: function () {
+      this.$emit('paginated', { page: this.paginationPage, per_page: this.perPage })
+    },
+
+    perPage: function () {
+      this.$emit('paginated', { page: this.paginationPage, per_page: this.perPage })
+    },
+
     $route (to) {
       if (to.name !== 'Contacts') {
         this.loadFolders()
