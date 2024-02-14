e032f55e3bdaf016c52fb3b4cd835f025916ee90
jefferson-aloware <jefferson@aloware.com>
2024-02-14 13:55:10 -0300

    feat: :sparkles: create conversation-section

---
+++ src/components/communication/transcription-components/conversation-section.vue
@@ -0,0 +1,59 @@
+<template>
+  <section class="transcription chat-area"
+           ref="chatArea">
+    <div v-if="!isEmpty(messages)">
+      <div :key="message_index"
+           v-for="(message, message_index) in formattedMessages">
+        <!--
+          Any agent message will prompt on the left side of conversation.
+          Any customer message will prompt on the right side of conversation.
+          Also, we need to change the background color based on the message sentiment.
+        -->
+        <p class="message-box break-word"
+           :class="message.classes.messageBoxClass"
+           :style="{ border: message.sentimentBorder }">
+          <strong>Speaker: {{ message.speaker }}</strong>
+          <br>
+          <span style="line-height: 1.6"
+                v-html="message.formattedText">
+          </span>
+        </p>
+
+        <!-- Show the speaker's sentiment below each message. -->
+        <span class="sentiment flex items-center justify-start"
+              :class="message.classes.sentimentClass">
+          <span class="sentiment-circle"
+                :style="{ background: message.sentimentBackgroundColor }" />
+          <span class="sentiment-description">{{ message.sentiment_possibility }}% {{ message.sentiment }}</span>
+        </span>
+      </div>
+    </div>
+
+    <!-- If no conversation was detected. -->
+    <div class="text-center"
+         v-else>
+      <span>No Conversation</span>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ConversationSection',
+
+  props: {
+    messages: {
+      type: Array,
+      required: true
+    },
+    isEmpty: {
+      type: Function,
+      required: true
+    },
+    formattedMessages: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
