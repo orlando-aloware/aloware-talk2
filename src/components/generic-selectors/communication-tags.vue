548a09639294e3d8563b034df41034aa89340874
nestor <nestor@xmlteam.com>
2024-02-27 12:30:54 -0600

    Apply search tags in communication-tags

--- src/components/generic-selectors/communication-tags.vue
+++ src/components/generic-selectors/communication-tags.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="w-100 tags-wrapper"
        v-if="hasPermissionTo('tag communication')">
-    <generic-multi-select :values="communication.tag_ids"
+    <tag-multi-select :values="communication.tag_ids"
                           :options="combinedTags"
+                          :current="currentTags"
                           :canEdit="hasPermissionTo(['list tag', 'view tag'])"
                           :optionsIsGrouped="true"
                           :height="height"
@@ -15,14 +16,14 @@
           {{ buttonText }}
         </span>
       </template>
-    </generic-multi-select>
+    </tag-multi-select>
   </div>
 </template>
 
 <script>
 import { aclMixin } from 'src/plugins/mixins'
 import { mapActions, mapState } from 'vuex'
-import GenericMultiSelect from 'components/generic-selectors/generic-multi-select'
+import TagMultiSelect from 'components/generic-selectors/tag-multi-select'
 import {
   TAG_CATEGORIES as TagCategories,
   TAG_CATEGORIES_VALUES as TagCategoriesValues
@@ -38,7 +39,7 @@ export default {
 
   components: {
     AddIconCircle,
-    GenericMultiSelect
+    TagMultiSelect
   },
 
   props: {
@@ -151,36 +152,10 @@ export default {
     if (this.tags) {
       this.options = this.tags
     }
-
-    if (!this.tags) {
-      this.getTags()
-    }
   },
 
   methods: {
-    getTags () {
-      if (!this.hasPermissionTo('list tag')) {
-        return
-      }
-
-      if (this.tagsFullyLoaded) {
-        this.options = this.tags
-        return
-      }
-
-      this.loadingTags = true
-      const params = {
-        full_load: true
-      }
-      return this.$axios.get('/api/v1/tag', { params }).then(res => {
-        this.options = res.data
-        this.loadingTags = false
-        this.setTagsFullyLoaded(true)
-      }).catch(err => {
-        console.log(err)
-        this.loadingTags = false
-      })
-    },
+    ...mapActions(['setTagsFullyLoaded']),
 
     saveTags (tags) {
       if (!this.hasPermissionTo('tag communication')) {
@@ -203,7 +178,10 @@ export default {
         }
       })
     },
-    ...mapActions(['setTagsFullyLoaded'])
+
+    currentTags () {
+      return this.communication?.tags ?? []
+    }
   },
   watch: {
     tags: {
