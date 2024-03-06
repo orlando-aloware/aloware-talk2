5a24c2481ca8ee2a96eecda9860937476e26619c
nestor <nestor@xmlteam.com>
2024-03-06 13:58:41 -0600

    Update inbox tab counter on contactAuditCreated

--- src/components/inbox/inbox-tab.vue
+++ src/components/inbox/inbox-tab.vue
@@ -1244,6 +1244,30 @@ export default {
             this.setSelectedContact(contacts[contactIndex])
           }
         }
+
+        console.log('contactAuditCreated data', data)
+        console.log('contactAuditCreated data.from === ContactTaskStatus.STATUS_PENDING', data.from === ContactTaskStatus.STATUS_PENDING)
+        console.log('contactAuditCreated data.from === ContactTaskStatus.STATUS_OPEN', data.from === ContactTaskStatus.STATUS_OPEN)
+
+        console.log('contactAuditCreated data.to === ContactTaskStatus.STATUS_OPEN', data.to === ContactTaskStatus.STATUS_OPEN)
+        console.log('contactAuditCreated data.to === ContactTaskStatus.STATUS_PENDING', data.to === ContactTaskStatus.STATUS_PENDING)
+
+        if (data.from === ContactTaskStatus.STATUS_PENDING) {
+          this.setPendingTaskCount(this.taskCounts.pending - 1)
+          this.setInboxPendingTaskCount(this.inboxTaskCounts.pending - 1)
+          if (data.to === ContactTaskStatus.STATUS_OPEN) {
+            this.setOpenTaskCount(this.taskCounts.open + 1)
+            this.setInboxOpenTaskCount(this.inboxTaskCounts.open + 1)
+          }
+        }
+        if (data.from === ContactTaskStatus.STATUS_OPEN) {
+          this.setOpenTaskCount(this.taskCounts.open - 1)
+          this.setInboxOpenTaskCount(this.inboxTaskCounts.open - 1)
+          if (data.to === ContactTaskStatus.STATUS_PENDING) {
+            this.setPendingTaskCount(this.taskCounts.pending + 1)
+            this.setInboxPendingTaskCount(this.inboxTaskCounts.pending + 1)
+          }
+        }
       }
     }
 
