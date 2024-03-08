3158633d57ee26166974047daf1eeddfd0277cd4
nestor <nestor@xmlteam.com>
2024-03-08 17:02:16 -0600

    Do not override total_queued

--- src/pages/power-dialer/PowerDialerSession.vue
+++ src/pages/power-dialer/PowerDialerSession.vue
@@ -221,15 +221,19 @@ export default {
         this.getTaskByFilter(params)
           .then(res => {
             console.log('tracking fetchTasks response: ', res)
-            this.powerDialerTaskFilters[taskType] = this.$jsonClone(res.data)
-            delete this.powerDialerTaskFilters[taskType].data
 
             if (status === AutoDialTaskStatus.STATUS_QUEUED && !refreshData) {
+              const currentTotalQueued = this.powerDialerTaskFilters[taskType].total_queued
+              this.powerDialerTaskFilters[taskType] = this.$jsonClone(res.data)
+              delete this.powerDialerTaskFilters[taskType].data
               this.pagesFetched += 1
+              this.powerDialerTaskFilters[taskType].total_queued = currentTotalQueued
               this.powerDialerTaskFilters[taskType].current_page = this.pagesFetched
               this.powerDialerTasks[taskType].push(...res.data.data)
             } else {
               this.powerDialerTasks[taskType].data = res.data.data
+              this.powerDialerTaskFilters[taskType] = this.$jsonClone(res.data)
+              delete this.powerDialerTaskFilters[taskType].data
             }
 
             // no more queued tasks
