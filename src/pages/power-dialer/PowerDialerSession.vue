c8a5839e7448988adf1002ee8fd34692fcfa5e28
nestor <nestor@xmlteam.com>
2024-03-11 07:39:24 -0600

    Undo updating total_queued

--- src/pages/power-dialer/PowerDialerSession.vue
+++ src/pages/power-dialer/PowerDialerSession.vue
@@ -221,18 +221,20 @@ export default {
         this.getTaskByFilter(params)
           .then(res => {
             console.log('tracking fetchTasks response: ', res)
+            this.powerDialerTaskFilters[taskType] = this.$jsonClone(res.data)
+            delete this.powerDialerTaskFilters[taskType].data
 
             if (status === AutoDialTaskStatus.STATUS_QUEUED && !refreshData) {
-              this.powerDialerTaskFilters[taskType] = this.$jsonClone(res.data)
-              delete this.powerDialerTaskFilters[taskType].data
+              // this.powerDialerTaskFilters[taskType] = this.$jsonClone(res.data)
+              // delete this.powerDialerTaskFilters[taskType].data
               this.pagesFetched += 1
-              this.powerDialerTaskFilters[taskType].total_queued = this.totalTasksInQueue + (res.data.to - res.data.from)
+              // this.powerDialerTaskFilters[taskType].total_queued = this.totalTasksInQueue + (res.data.to - res.data.from)
               this.powerDialerTaskFilters[taskType].current_page = this.pagesFetched
               this.powerDialerTasks[taskType].push(...res.data.data)
             } else {
               this.powerDialerTasks[taskType].data = res.data.data
-              this.powerDialerTaskFilters[taskType] = this.$jsonClone(res.data)
-              delete this.powerDialerTaskFilters[taskType].data
+              // this.powerDialerTaskFilters[taskType] = this.$jsonClone(res.data)
+              // delete this.powerDialerTaskFilters[taskType].data
             }
 
             // no more queued tasks
