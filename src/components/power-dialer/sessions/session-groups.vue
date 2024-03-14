57409eccf486a23dd86ed6644d8998858d1dc110
nestor <nestor@xmlteam.com>
2024-03-14 08:34:54 -0600

    Update getTotalItem for in queue

--- src/components/power-dialer/sessions/session-groups.vue
+++ src/components/power-dialer/sessions/session-groups.vue
@@ -529,8 +529,9 @@ export default {
 
       switch (key) {
         case 'in_queue':
-          const inQueue = get(this.powerDialerTasks, 'in_queue', null)
-          return inQueue ? inQueue.length : 0
+          // const inQueue = get(this.powerDialerTasks, 'in_queue', null)
+          // return inQueue ? inQueue.length : 0
+          return this.powerDialerTaskFilters[key] ? this.powerDialerTaskFilters[key].total_queued - 1 : 0 // -1 for the one in progress
         case 'called':
           return this.powerDialerTaskFilters[key] ? this.powerDialerTaskFilters[key].total_called : 0
         case 'failed':
