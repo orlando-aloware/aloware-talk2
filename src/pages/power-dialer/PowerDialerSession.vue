ae9c6f7f1409a0569b35d745afc241e9d6acb34f
nestor <nestor@xmlteam.com>
2024-03-18 16:46:18 -0600

    Add comments to a better understanding

--- src/pages/power-dialer/PowerDialerSession.vue
+++ src/pages/power-dialer/PowerDialerSession.vue
@@ -226,8 +226,9 @@ export default {
         const hasSkippedTasks = this.powerDialerTasks['skipped'].length > 0
         const remainingInQueueTasks = this.powerDialerTaskFilters['in_queue'] ? this.powerDialerTaskFilters['in_queue'].total_queued > this.inQueueFetchTasks.fetchedTasks : false
 
-        // Increment the pagination when the last items in the current list of IN QUEUE taks are reached.
-        // If we have skipped any calls, we need to fetch the next page of IN QUEUE tasks.
+        // Increment the pagination when the last items in the current list of IN QUEUE taks are reached
+        // AND we have skipped tasks, so we need to fetch the next page of IN QUEUE tasks.
+        // Otherwise we don't increment the page since the API response will change after a call is completed.
         if (lastItemsInCurrentQueue && hasSkippedTasks && remainingInQueueTasks) {
           params.page = this.inQueueFetchTasks.currentPage + 1
         }
