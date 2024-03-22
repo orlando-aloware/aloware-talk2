eb8748952b5a0677f66e902882956f1c4feda393
Juan Pablo Blariza <juanblariza@gmail.com>
2024-03-22 20:03:18 -0300

    Add data-testid to contacts-sidebar

--- src/components/contacts/contacts-sidebar.vue
+++ src/components/contacts/contacts-sidebar.vue
@@ -6,15 +6,17 @@
       expand-icon-toggle
       label="Public List"
       class="contact-sidebar-list-wrapper"
+      data-testid="contact-public-list-expansion-item"
     >
       <template v-slot:header>
         <q-item-section>
           <div class="pinned__header d-flex align-items-center list--header shared__header">
-            <div class="header__header__title font-weight-bold flex-grow-1">
+            <div class="header__header__title font-weight-bold flex-grow-1" data-testid="contact-public-list">
               Public Lists
               <q-icon name="info"
                       class="material-icons-outlined ml-2 cursor-pointer"
                       color="#62666E"
+                      data-testid="contact-public-list-tooltip"
                       size="14px">
                 <q-tooltip anchor="top middle"
                            self="center middle">
@@ -25,10 +27,10 @@
           </div>
         </q-item-section>
       </template>
-      <contacts-shared class="public-lists"></contacts-shared>
+      <contacts-shared class="public-lists" data-testid="contact-share-list"></contacts-shared>
     </q-expansion-item>
 
-    <contacts-folders></contacts-folders>
+    <contacts-folders data-testid="contact-share-folders"></contacts-folders>
   </card>
 </template>
 
