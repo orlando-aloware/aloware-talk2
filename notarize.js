d8c23bf076b421ed2b657537eba45d37ef885617
Tito Alvarez <tito@aloware.com>
2024-03-23 13:26:10 -0600

    Fixed file location

--- notarize.js
+++ notarize.js
@@ -14,7 +14,7 @@ exports.default = async function notarizing (context) {
   const notarizeParams = {
     appBundleId: 'com.aloware.talk2',
     appPath: `${appOutDir}/${appName}.app`,
-    appleApiKey: process.env.API_KEY, // Path to the .p8 API key file
+    appleApiKey: process.env.API_KEY_FILE, // Path to the .p8 API key file
     appleApiKeyId: process.env.API_KEY_ID, // Key ID
     appleApiIssuer: process.env.API_KEY_ISSUER_ID // Issuer ID
   }
