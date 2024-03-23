aa21aee08734eef3922d2206c9a012bb921db598
Tito Alvarez <tito@aloware.com>
2024-03-23 13:34:26 -0600

    Fixed macOS app path for notarization

--- notarize.js
+++ notarize.js
@@ -13,7 +13,7 @@ exports.default = async function notarizing (context) {
 
   const notarizeParams = {
     appBundleId: 'com.aloware.talk2',
-    appPath: `${appOutDir}/${appName}.app`,
+    appPath: process.env.FILE_LOCATION, // Path to the app
     appleApiKey: process.env.API_KEY_FILE, // Path to the .p8 API key file
     appleApiKeyId: process.env.API_KEY_ID, // Key ID
     appleApiIssuer: process.env.API_KEY_ISSUER_ID // Issuer ID
