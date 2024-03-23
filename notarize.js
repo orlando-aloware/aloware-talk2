ee8d84f4f01ea08ffdc15c415e7a08d49c66c479
Tito Alvarez <tito@aloware.com>
2024-03-23 12:54:22 -0600

    Debugging

--- notarize.js
+++ notarize.js
@@ -11,13 +11,17 @@ exports.default = async function notarizing (context) {
 
   const appName = context.packager.appInfo.productFilename
 
-  await notarize({
+  const notarizeParams = {
     appBundleId: 'com.aloware.talk2',
     appPath: `${appOutDir}/${appName}.app`,
     appleApiKey: process.env.API_KEY, // Path to the .p8 API key file
     appleApiKeyId: process.env.API_KEY_ID, // Key ID
     appleApiIssuer: process.env.API_KEY_ISSUER_ID // Issuer ID
-  })
+  }
+
+  console.log('Params:', notarizeParams)
+
+  await notarize(notarizeParams)
 
   console.log(`Notarized ${appName}`)
 }
