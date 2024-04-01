b54b5f6c5aa357fc5bee3cb1e919ca853a67942e
jefferson-aloware <jefferson@aloware.com>
2024-04-01 15:23:29 -0300

    feat: :sparkles: add if have oldAgentStatus and call resetAgentStatus

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -917,8 +917,13 @@ export default {
       if (this.profile && user.id === this.profile.id) {
         // this.setAgentStatus(user.agent_status)
         this.setProfile(user)
+
         console.log('Changed agent status [event]: ', this.getStatusLabel(user.agent_status))
         console.log('Old Agent Status: ', this.getStatusLabel(this.oldAgentStatus))
+
+        if (this.oldAgentStatus) {
+          this.resetAgentStatus()
+        }
       }
     }
 
