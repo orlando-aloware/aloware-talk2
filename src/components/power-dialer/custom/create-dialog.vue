62613f536e60c3c50c83f2d8f266b0d0edb0102f
nestor <nestor@xmlteam.com>
2024-04-08 11:18:07 -0600

    Implement search through API to get public contact lists

--- src/components/power-dialer/custom/create-dialog.vue
+++ src/components/power-dialer/custom/create-dialog.vue
@@ -15,7 +15,7 @@
                       :id="0"
                       :order="0"
                       :layer="0"
-                      :items="searchList(publicContactLists)" />
+                      :items="publicContactListItems" />
       <CreateListItem v-for="folder in contactFolders"
                       :name="folder.name"
                       :key="folder.id"
@@ -80,9 +80,11 @@ export default {
     return {
       searchValue: '',
       isCreating: false,
-      publicContactLists: null,
       contactFolders: null,
-      powerDialerParams: {}
+      powerDialerParams: {},
+      initialpublicContactListItems: [],
+      publicContactListItems: [],
+      folderListItems: []
     }
   },
   computed: {
@@ -175,10 +177,21 @@ export default {
     onHiddenPowerDialerModal () {
       this.powerDialerParams = {}
     },
-    searchList (lists) {
-      return this.searchedPdItem && this.searchedPdItem.length > 0
-        ? lists.filter(item => item.name.toLowerCase().includes(this.searchedPdItem.toLocaleLowerCase()))
-        : lists
+    searchList (list) {
+      if (!this.searchedPdItem.length) {
+        return list
+      }
+
+      const search = this.searchedPdItem.toLocaleLowerCase()
+      return list.filter(item => item.name.toLowerCase().includes(search))
+    },
+    searchPublicContactLists () {
+      this.getPublicContactLists({
+        search: this.searchedPdItem.toLocaleLowerCase()
+      })
+        .then(res => {
+          this.publicContactListItems = res.data.length ? res.data : []
+        })
     }
   },
   beforeDestroy () {
@@ -194,12 +207,27 @@ export default {
         document.body.removeEventListener('focus', this.handleClick)
         this.destroyDialogInstance(state)
       }
+    },
+    searchedPdItem: {
+      deep: true,
+      handler: function (newValue) {
+        console.log('WATCH searchedPdItem newValue', newValue)
+        // If no newValue has been set, set the initial list
+        if (!newValue) {
+          this.publicContactListItems = this.initialpublicContactListItems
+          return
+        }
+
+        // Search for the public contact lists through the API
+        this.searchPublicContactLists()
+      }
     }
   },
   mounted () {
     this.getPublicContactLists()
       .then(res => {
-        this.publicContactLists = res
+        this.initialpublicContactListItems = res.data
+        this.publicContactListItems = res.data
       })
     this.getContactFolders()
       .then(res => {
