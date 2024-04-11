219050759a95ec073fdb9618280ecdfde2edd1d9
Hamed Kamrava <hamed@aloware.com>
2024-04-11 17:30:33 +0100

    fix

--- src/components/trial-expired-modal.vue
+++ src/components/trial-expired-modal.vue
@@ -25,13 +25,23 @@
                   <div class="mb-3"
                        v-if="true">
                       <div class="row">
-                          <div class="col-12 d-flex align-items-center pl-0">
+                          <div class="col-12 d-flex align-items-center flex-center text-center pl-0">
                             <p>{{ profile.first_name }}, your trial period has ended, and what an incredible journey it's been! We trust you've discovered the wealth of features and benefits aimed at streamlining your connection with prospects and customers, making every interaction more impactful.</p>
                           </div>
                       </div>
                       <div class="row">
-                          <div class="col-12 d-flex align-items-center pl-0">
-                            <p>Though your trial has concluded, your path to success doesn't need to end. Dive into our variety of plans and flexible billing options, and upgrade today. Ensure you never miss a lead and your customers have uninterrupted access to you! Should you have any inquiries or require additional support, remember, our team is just a message away</p>
+                          <div class="col-12 d-flex align-items-center flex-center text-center pl-0">
+                            <p>Though your trial has concluded, your path to success doesn't need to end.</p>
+                          </div>
+                      </div>
+                      <div class="row">
+                          <div class="col-12 d-flex align-items-center flex-center text-center pl-0">
+                            <p>Dive into our variety of plans and flexible billing options, and upgrade today. Ensure you never miss a lead and your customers have uninterrupted access to you!</p>
+                          </div>
+                      </div>
+                      <div class="row">
+                          <div class="col-12 d-flex align-items-center flex-center text-center pl-0">
+                            <p>Should you have any inquiries or require additional support, remember, our team is just a message away</p>
                           </div>
                       </div>
                   </div>
@@ -45,20 +55,18 @@
               </div>
 
               <div class="d-flex align-items-center flex flex-column pt-3">
-                <p class="mb-0">Contact Support:</p>
-                <p class="mb-1">
-                    <strong class="text-success">
-                        <a href="tel:(855) 256-2001">
-                            <i class="fa fa-phone-alt"></i>
-                            (855) 256-2001
-                        </a>
-                        <span class="mx-2 text-dark">|</span>
-                        <a href="https://aloware.com"
-                           target="_blank">
-                            <i class="fa fa-globe"></i>
-                            Visit Website
-                        </a>
-                    </strong>
+                <p class="mt-3">Contact Support:</p>
+                <p class="my-2">
+                  <a href="tel:(855) 256-2001">
+                      <i class="fa fa-phone-alt"></i>
+                      (855) 256-2001
+                  </a>
+                  <span class="mx-2 text-dark">|</span>
+                  <a href="https://aloware.com"
+                      target="_blank">
+                      <i class="fa fa-globe"></i>
+                      Visit Website
+                  </a>
                 </p>
               </div>
           </div>
