3a6bb776c41ad4ebbf2a2b7ad970c64e6eb8f407
nestor <nestor@xmlteam.com>
2024-04-18 11:33:12 -0600

    pta-92: Add logs

--- src/plugins/mixins/sessions-call-status.mixin.js
+++ src/plugins/mixins/sessions-call-status.mixin.js
@@ -232,11 +232,15 @@ export default {
 
       // check if redial is true (this.redialedTask?.redialed_now) and if agent status is on call
       // then do not continue until agent status is not on call
+      console.log('*** this.redialedTask', this.redialedTask)
+      console.log('*** this.profile.agent_status', this.profile.agent_status)
       if (this.redialedTask?.redialed_now && this.profile.agent_status === AgentStatus.AGENT_STATUS_ON_CALL) {
+        console.log('*** esperando')
         await new Promise(resolve => {
           const checkAgentStatus = setInterval(() => {
             if (this.profile.agent_status !== AgentStatus.AGENT_STATUS_ON_CALL) {
               clearInterval(checkAgentStatus)
+              console.log('*** adelante')
               resolve()
             }
           }, 1000)
