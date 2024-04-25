1673eb5dae2ad792001ffdfd3e6e17c5b576fbc4
Jonathan M <jonathan@aloware.com>
2024-04-25 09:13:55 -0300

    fix(PTA-43): added condition for contacts / pd

--- src/components/contacts/contacts-folders.vue
+++ src/components/contacts/contacts-folders.vue
@@ -146,7 +146,7 @@
         <div class="item-empty"
               v-if="isFolderEmpty && !isLoading">
           <span class="fs-12 text-muted">
-            You don't have any power dialer list
+            You don't have any {{ isContactModuleType ? 'contact' : 'power dialer' }} list
           </span>
         </div>
         <contacts-sidebar-loader v-if="isLoading"/>
