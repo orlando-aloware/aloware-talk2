7c58d2bc0d708bf1c70b0367233ff6605b2a6b20
nestor <nestor@xmlteam.com>
2024-05-01 10:33:48 -0600

    fix(pta-11): Attempting to read properties of undefined

--- src/plugins/mixins/power-dialer.js
+++ src/plugins/mixins/power-dialer.js
@@ -93,8 +93,10 @@ export default {
         filters.filter_groups = currentFilters
       }
 
+      console.log('preparing filters listId', listId)
+      console.log('preparing filters this.currentSelectedContacts', this.currentSelectedContacts)
       // add selected contacts to the params, when they arent empty
-      if (Array.isArray(this.currentSelectedContacts[listId])) {
+      if (this.currentSelectedContacts && Array.isArray(this.currentSelectedContacts[listId]) && this.currentSelectedContacts[listId].length) {
         const contactIds = this.currentSelectedContacts[listId].map(contact => contact.id)
 
         if (contactIds.length) {
