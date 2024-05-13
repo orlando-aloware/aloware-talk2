71d8ea9b01b89c57425aa662f0addc87f4e0121f
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2024-05-13 14:21:35 -0700

    Better log errors

--- src/components/dialer/dialer.vue
+++ src/components/dialer/dialer.vue
@@ -298,6 +298,7 @@ export default {
 
       if (this.dialer.isReady) {
         this.$generalNotification('Whoops! You have lost connection with the server. Check your internet connection and try again.', 'error', 10000)
+        console.warn('[UNREGISTERED] Twilio token', this.dialer.token)
         this.setDialerIsReady(false)
         this.setDialerCurrentStatus('OFFLINE')
       }
@@ -1446,7 +1447,7 @@ export default {
       err.code = error.code
       error.token = this.dialer.token
 
-      console.log(error)
+      console.error(error)
 
       // 31000 => General Twilio Client error.
       // 31005 => WebSocket connection to Twilio's signaling servers were unexpectedly ended. If this is happening consistently,
