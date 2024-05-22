657989b15a3be289503287c72eb06dfb1a356a43
Volodymyr Shandak <volodymyr@aloware.com>
2024-05-22 11:08:25 -0400

    fix router-link

--- src/components/contacts/contact-phone-number-duplicates.vue
+++ src/components/contacts/contact-phone-number-duplicates.vue
@@ -16,7 +16,7 @@
                :fields="main_fields"
                :items="[phone_number.contact]">
         <template #cell(main)="data">
-          <router-link :to="{ name: 'Contact', params: { contact_id: data.item.id }}" >{{ data.item.id }}</router-link>
+          <router-link :to="{ name: 'Contact', params: { id: data.item.id }}" >{{ data.item.id }}</router-link>
         </template>
       </b-table>
       <b-table class='mb-0'
@@ -25,7 +25,7 @@
                :fields="conflicted_fields"
                :items="phone_number.conflicted_contacts">
         <template #cell(conflicted)="data">
-          <router-link :to="{ name: 'Contact', params: { contact_id: data.item.id }}" >{{ data.item.id }}</router-link>
+          <router-link :to="{ name: 'Contact', params: { id: data.item.id }}" >{{ data.item.id }}</router-link>
         </template>
       </b-table>
     </b-modal>
