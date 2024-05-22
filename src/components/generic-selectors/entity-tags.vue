62c752e1bd89ed0db9890405eba466ea1a44c907
jefferson-aloware <jefferson@aloware.com>
2024-05-22 10:34:48 -0300

    wat-502(feature): pr requested change

--- src/components/generic-selectors/entity-tags.vue
+++ src/components/generic-selectors/entity-tags.vue
@@ -11,7 +11,7 @@
                         :options="combinedTags"
                         :current="currentTags"
                         :canEdit="hasPermissionTo(['list tag', 'view tag'])"
-                        :optionsIsGrouped="true"
+                        :options-is-grouped="true"
                         :height="height"
                         :category="category"
                         :is-filter="isFilter"
