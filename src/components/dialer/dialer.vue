d6509906ed3a300b817b9637401b5778f254df40
Amir <amir@aloware.com>
2024-05-23 08:11:06 +0300

    Downgrade to 2.8 from 2.10

--- src/components/dialer/dialer.vue
+++ src/components/dialer/dialer.vue
@@ -1467,7 +1467,7 @@ export default {
       }
 
       // Request new token if error
-      if ([20101, 31102, 31203, 31204, 31205, 31207].includes(err.code)) {
+      if ([20101, 31102, 31204, 31205, 31207].includes(err.code)) {
         return this.getDesktopToken(true)
       }
 
