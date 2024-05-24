ea97d0f9e3d2e9c5422bee7ec29749f19eb7a883
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2024-05-23 18:46:15 -0700

    Update dialer.vue

--- src/components/dialer/dialer.vue
+++ src/components/dialer/dialer.vue
@@ -587,7 +587,7 @@ export default {
           options.enableImprovedSignalingErrorPrecision = true
         }
         if (this.isCompanyPartOfCustomEdgeLocations(this.currentCompany.id)) {
-          options.edge = ['ashburn', 'umatilla', 'roaming'],
+          options.edge = ['ashburn', 'umatilla', 'roaming']
         }
         this.device.initialize(this.dialer.token, options)
 
