68837f070f0cb71c4065a4ffe180007e69618b61
Keyner W. Tupac Yupanqui Caballero <keyner@aloware.com>
2024-05-28 14:42:06 -0500

    hotfix(TEL-337) remove unused other text

--- src/components/settings/personalization.vue
+++ src/components/settings/personalization.vue
@@ -24,10 +24,6 @@
                            self="center left"
                            :offset="[-20, 10]">
                   <div>
-                    <span class="d-flex mb-2"
-                      v-if="statics.whitelabel">
-                      Sets all users' statuses to 'Offline' by default after they login or have 5 minutes of app inactivity
-                    </span>
                     <span class="d-flex font-weight-bold">Turning on 'Force Users to Always Available' Setting:</span>
                     <ul class="mb-0">
                       <li>Users' statuses are set to 'Available' even after they login or have 5 minutes of app inactivity</li>
