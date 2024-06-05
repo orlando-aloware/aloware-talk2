96e5ab56b2a93479c7d90ce30ea0914712eec429
nestor <nestor@xmlteam.com>
2024-06-05 11:42:50 -0600

    logs

--- src/components/power-dialer/power-dialer-bulk-add-report-modal.vue
+++ src/components/power-dialer/power-dialer-bulk-add-report-modal.vue
@@ -230,6 +230,9 @@ export default {
       const isDncFromImport = this.fullReport?.extra?.is_dnc || 0
       const isBlockedFromImport = this.fullReport?.extra?.is_blocked || 0
 
+      console.log('buildReport this.fullReport', this.fullReport)
+      console.log('buildReport integrationReport', integrationReport)
+
       if (isDncFromImport > integrationReport.is_dnc) {
         integrationReport.is_dnc = isDncFromImport
       }
@@ -244,6 +247,10 @@ export default {
       const integrationDuplicates = integrationReport?.duplicates || 0
       const duplicatePhoneNumbers = this.fullReport?.success?.duplicates || 0
 
+      console.log('buildReport creationSettings', creationSettings)
+      console.log('buildReport integrationDuplicates', integrationDuplicates)
+      console.log('buildReport duplicatePhoneNumbers', duplicatePhoneNumbers)
+
       if (!creationSettings?.prevent_duplicates) {
         this.fullReport.fail.duplicates = integrationDuplicates > duplicatePhoneNumbers ? integrationDuplicates : duplicatePhoneNumbers
       } else if (creationSettings?.prevent_duplicates) {
@@ -255,6 +262,10 @@ export default {
       const updatedContactsCount = integrationReport?.updated_contacts_count || 0
       const selected = integrationReport?.total_selected || 0
 
+      console.log('buildReport createdContactsCount', createdContactsCount)
+      console.log('buildReport updatedContactsCount', updatedContactsCount)
+      console.log('buildReport selected', selected)
+
       let totalSelected = createdContactsCount + updatedContactsCount
       totalSelected = selected > totalSelected ? selected : totalSelected
 
@@ -265,6 +276,9 @@ export default {
       const duplicates = integrationReport?.duplicates ?? 0
       // the total of duplicates
       const duplicatedTasks = integrationReport?.duplicated_tasks ?? 0
+
+      console.log('buildReport duplicates', duplicates)
+      console.log('buildReport duplicatedTasks', duplicatedTasks)
 
       this.duplicatedTasks = duplicatedTasks ? duplicatedTasks - duplicates : 0
 
@@ -287,6 +301,9 @@ export default {
       /* if (!this.fullReport.extra.settings.prevent_duplicates && this.duplicatedTasks) {
         this.fullReport.fail[DUPLICATED] = this.duplicatedTasks
       } */
+
+      console.log('buildReport this.fullReport', this.fullReport)
+      console.log('buildReport integrationReport', integrationReport)
 
       // clean-up
       this.removeIntegrationPDImportSummary(id)
