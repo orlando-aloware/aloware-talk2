e0fd1710b92950c2d788a83bd141abbee1e06ee8
Amir <amir@aloware.com>
2024-06-18 15:30:31 +0300

    Bypass 728 area code

--- src/boot/bootstrap.js
+++ src/boot/bootstrap.js
@@ -129,6 +129,17 @@ window.getLocaleIfPhoneNumberIsFromNorthAmerica = function (phoneNumber) {
 
       if (isPossible && window.phoneUtil.isValidNumberForRegion(number, validCountry)) {
         return validCountry
+      } else {
+        // for US get the national number
+        if (validCountry === 'US') {
+          const nationalNumber = number.getNationalNumber()
+          // get the area code
+          const areaCode = nationalNumber.toString().substring(0, 3)
+          // if it's 728 then return US
+          if (areaCode === '728') {
+            return validCountry
+          }
+        }
       }
     }
 
