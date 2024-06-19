54cdda68c96b66e8343f295baae252d9d3b7ae4e
Keyner W. Tupac Yupanqui Caballero <keyner@aloware.com>
2024-06-19 18:03:50 -0500

    feature(TEL-403) get dispostions statuses, calling dispositions, etc

--- src/components/webrtc.vue
+++ src/components/webrtc.vue
@@ -8,9 +8,13 @@
 <script>
 import Phone from 'components/dialer/phone'
 import Dialer from 'components/dialer/dialer'
+import { mapActions } from 'vuex'
+import { aclMixin } from 'src/boot/mixins'
 
 export default {
   components: { Phone, Dialer },
+
+  mixins: [ aclMixin ],
 
   props: {
     carrierName: {
@@ -23,6 +27,108 @@ export default {
       type: Boolean,
       required: false
     }
+  },
+
+  data () {
+    return {
+      loadingDispositionStatuses: false,
+      loadingCallDispositionStatuses: false,
+      loadingActivityTypes: false,
+      loadingTemplates: false
+    }
+  },
+
+  methods: {
+    initAuth () {
+      this.getDispositionStatuses()
+      this.getCallDispositions()
+      this.getActivityTypes()
+      this.getTemplates()
+    },
+
+    getDispositionStatuses () {
+      if (this.hasPermissionTo('list disposition status')) {
+        this.loadingDispositionStatuses = true
+
+        return this.$axios
+          .get('/api/v1/disposition-status')
+          .then((res) => {
+            this.setDispositionStatuses(res.data)
+            this.loadingDispositionStatuses = false
+
+            return Promise.resolve()
+          })
+          .catch((err) => {
+            console.log(err)
+            this.loadingDispositionStatuses = false
+
+            return Promise.reject()
+          })
+      }
+    },
+
+    getCallDispositions () {
+      if (this.hasPermissionTo('list disposition status')) {
+        this.loadingCallDispositionStatuses = true
+
+        return this.$axios
+          .get('/api/v1/call-disposition')
+          .then((res) => {
+            this.setCallDispositions(res.data)
+            this.loadingCallDispositionStatuses = false
+
+            return Promise.resolve()
+          })
+          .catch((err) => {
+            console.log(err)
+            this.loadingCallDispositionStatuses = false
+
+            return Promise.reject()
+          })
+      }
+    },
+
+    getActivityTypes () {
+      this.loadingActivityTypes = true
+      return this.$axios
+        .get('/api/v1/activity-types').then(res => {
+          this.setActivityTypes(res.data)
+          this.loadingActivityTypes = false
+
+          return Promise.resolve()
+        }).catch(err => {
+          console.log(err)
+          this.loadingActivityTypes = false
+
+          return Promise.reject()
+        })
+    },
+
+    getTemplates () {
+      if (this.hasPermissionTo('list sms template')) {
+        this.loadingTemplates = true
+
+        return this.$axios.get('/api/v1/sms-template', {
+          mode: 'no-cors'
+        }).then(res => {
+          this.loadingTemplates = false
+          this.setTemplates(res.data)
+
+          return Promise.resolve()
+        }).catch(err => {
+          console.log(err)
+          this.loadingTemplates = false
+
+          return Promise.reject()
+        })
+      }
+    },
+
+    ...mapActions(['setDispositionStatuses', 'setCallDispositions', 'setActivityTypes', 'setTemplates'])
+  },
+
+  created () {
+    this.initAuth()
   },
 
   mounted () {
