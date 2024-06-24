335c23a6d715a7d4d5589f9e5276d186506e0849
Keyner W. Tupac Yupanqui Caballero <keyner.peru@gmail.com>
2024-06-24 17:44:55 -0500

    feature(TEL-403) fix showPlaceholder

--- src/plugins/mixins/selector.mixin.js
+++ src/plugins/mixins/selector.mixin.js
@@ -49,7 +49,6 @@ export default {
 
   mounted () {
     this.element = this.$el ? this.$el : document
-    this.showInputPlaceholder()
 
     if (this.referenceElement) {
       this.element = this.$refs[this.referenceElement].$el
