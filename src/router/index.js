7032bdb04507fa325dce7416b0305d5d8354e9f1
jefferson-aloware <jefferson@aloware.com>
2024-07-11 19:39:52 -0300

    feature(int-256): remove console

--- src/router/index.js
+++ src/router/index.js
@@ -42,7 +42,6 @@ export default function ({ store }) {
     next()
     for (const route of to.matched) {
       // Check if the route is a widget
-      console.log('beforeEach route -->', route)
       if (route?.meta?.isWidget) {
         store.commit('SET_IS_WIDGET', true)
         break // Stop the loop if isWidget is found
