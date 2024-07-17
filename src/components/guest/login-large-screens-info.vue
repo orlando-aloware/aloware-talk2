cfd2c1bf9760e60f1ce13b84ba8fe570f837c825
jefferson-aloware <jefferson@aloware.com>
2024-07-17 18:23:28 -0300

    feature(wat-583): show text only if is not simp social

--- src/components/guest/login-large-screens-info.vue
+++ src/components/guest/login-large-screens-info.vue
@@ -6,7 +6,7 @@
         <div class="row"
              style="max-width:550px">
           <div class="col-12"
-               v-if="xmasEnabled">
+               v-if="xmasEnabled && !isSimpSocial">
             <div>
               <img src="images/xmas-merry.png"
                      style="max-width: 550px;" />
@@ -24,14 +24,16 @@
           </div>
           <div class="col-12"
                v-else>
-            <h1 class="mb-4 text-white text-h4 text-weight-bold banner-font text-banner-title">
+            <h1 class="mb-4 text-white text-h4 text-weight-bold banner-font text-banner-title"
+                v-if="!isSimpSocial">
               Our AI-powered <br> call transcription feature <br> is now available
             </h1>
-            <p class="mb-4 text-white banner-font text-banner-subtitle">
+            <p class="mb-4 text-white banner-font text-banner-subtitle"
+               v-if="!isSimpSocial">
               <br>
               Get up to 5000 minutes for free
             </p>
-            <div>
+            <div v-if="!isSimpSocial">
               <a class="btn btn-primary btn-xl px-5"
                  target="_blank"
                  href="mailto:support@aloware.com?subject=Enable transcription for free">
@@ -46,8 +48,12 @@
 </template>
 
 <script>
+import { guestFormsMixin } from 'src/plugins/mixins'
+
 export default {
   name: 'login-large-screens-info',
+
+  mixins: [guestFormsMixin],
 
   props: {
     xmasEnabled: {
