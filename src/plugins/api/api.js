108eab28c7b765657f1d331b19b054da59486379
Gustavo Andrade <gustavo@aloware.com>
2024-07-25 12:17:30 -0300

    Remove MDE endpoint

--- src/plugins/api/api.js
+++ src/plugins/api/api.js
@@ -629,8 +629,7 @@ export default {
       counts (params, cancelTokenSource) {
         delete params.timezone
 
-        // return window.axios.get(`${process.env.API_REPORTING_URL}/api/v2/contacts/count`, { params, paramsSerializer: qs.stringify })
-        return window.axios.get(`https://pr-10098.mde.alodev.org/api/v2/contacts/count`, { params, paramsSerializer: qs.stringify })
+        return window.axios.get(`${process.env.API_REPORTING_URL}/api/v2/contacts/count`, { params, paramsSerializer: qs.stringify })
       },
 
       inboxCounts (params) {
