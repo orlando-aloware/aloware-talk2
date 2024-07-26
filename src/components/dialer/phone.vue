0b2df94b52b9a2a5f103c9fb1314cb31af31c451
Keyner W. Tupac Yupanqui Caballero <keyner@aloware.com>
2024-07-26 17:31:32 -0500

    remove logs

--- src/components/dialer/phone.vue
+++ src/components/dialer/phone.vue
@@ -1926,9 +1926,6 @@ export default {
     },
 
     isNotOnWrapUp () {
-      console.log('isNotOnWrapUp hasNoParkedAndInprogressCall', this.hasNoParkedAndInprogressCall)
-      console.log('isNotOnWrapUp hasParkedAndInprogressCall', this.hasParkedAndInprogressCall)
-      console.log('isNotOnWrapUp hasCallInProgressNoParkedCall', this.hasCallInProgressNoParkedCall)
       return this.profile.agent_status !== AgentStatus.AGENT_STATUS_ON_WRAP_UP && !(this.hasNoParkedAndInprogressCall || this.hasParkedAndInprogressCall || this.hasCallInProgressNoParkedCall)
     }
   },
