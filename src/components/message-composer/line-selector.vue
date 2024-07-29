a8380709e180fe196e6fb288b2e65d517bfdc1ee
Yhael S <yhael@aloware.com>
2024-07-29 16:22:17 -0600

    added text

--- src/components/message-composer/line-selector.vue
+++ src/components/message-composer/line-selector.vue
@@ -29,11 +29,11 @@
             <q-item-label v-html="scope.opt.name"></q-item-label>
           </q-item-section>
           <q-item-section v-if="isMessagingBlocked(scope.opt, checkBlockedMessaging, false, true)" side>
-            <q-tooltip :disabled="!isMessagingBlocked(scope.opt, checkBlockedMessaging, false, true)"
-                       anchor="top middle"
+            <q-tooltip anchor="top middle"
                        self="center middle">
-              <q-badge color="blue">i</q-badge>
+                To send messages to the US, A2P 10DLC Brand and Campaign are required.
             </q-tooltip>
+            <q-badge color="blue">i</q-badge>
           </q-item-section>
         </q-item>
         <q-item v-if="scope.opt.group"
