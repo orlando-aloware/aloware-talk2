55782f3b4459b6f14d4160f5063720adfbedf640
jefferson-aloware <jefferson@aloware.com>
2024-08-01 15:08:36 -0300

    feature(int-256): fix check login when hubspot widget

--- src/store/auth/auth.actions.js
+++ src/store/auth/auth.actions.js
@@ -2,9 +2,10 @@ import * as storage from 'src/plugins/helpers/storage'
 import userpilot from 'src/plugins/vendor/userpilot'
 import { get } from 'lodash'
 
-const check = async ({ commit }, payload, skipSetAuthenticated) => {
+const check = async ({ commit, state }, payload, skipSetAuthenticated) => {
   const preventLogout = get(payload, 'preventLogout', false)
   const preventRedirect = get(payload, 'preventRedirect', false)
+  const isHubspotWidget = window.location?.href?.includes('hubspot-call-extension') || false
 
   try {
     if (storage.local.getItem('api_token') === null) {
@@ -40,6 +41,7 @@ const check = async ({ commit }, payload, skipSetAuthenticated) => {
     }
 
     if (!preventRedirect &&
+      !isHubspotWidget &&
       response.data.user.enabled &&
       response.data.user.company.enabled &&
       (window.location.href.indexOf('/suspended') !== -1 ||
