118d8dead23c60c4e95e3ca65dd9d5d280a0b48f
Nestor Ruiz <nestor@aloware.com>
2024-08-02 21:14:14 -0600

    fix(wat-668): disable banner for other accounts

--- src/components/simpsocial-migration-banner.vue
+++ src/components/simpsocial-migration-banner.vue
@@ -73,7 +73,7 @@ export default {
   created () {
     this.$cookies = VueCookies
 
-    if (this.isSimpSocial && this.$cookies.get(this.parsedCookieName) && this.shouldShowInFirstVisit) {
+    if (!this.isSimpSocial || (this.isSimpSocial && this.$cookies.get(this.parsedCookieName) && this.shouldShowInFirstVisit)) {
       this.shouldShow = false
       this.setIsSimpsocialMigrationBannerVisible(false)
     }
