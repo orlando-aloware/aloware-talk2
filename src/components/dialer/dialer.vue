e155750b22c7dc958bbd4e5e211066039a0b184a
Adrian <adrian@aloware.com>
2024-08-05 16:51:45 -0500

    fix(INT-278): added code to show dialer when unparking a call

--- src/components/dialer/dialer.vue
+++ src/components/dialer/dialer.vue
@@ -1023,6 +1023,13 @@ export default {
       if (this.isMobile) {
         this.$VueEvent.fire('doneUnparkCall')
       }
+
+      console.log(this.$route.name)
+
+      if (this.$route.name !== 'Phone' && this.isWidget) {
+        this.$router.push({ name: 'Phone' })
+        this.setShowPhone(true)
+      }
     },
 
     parkCallCombo (shouldAnswer = false, shouldUnpark = false, data = null) {
@@ -1580,7 +1587,8 @@ export default {
       'setDialerError',
       'setDialerErrorDefault',
       'removeParkedCall',
-      'setIsCallBackButtonDisabled'
+      'setIsCallBackButtonDisabled',
+      'setShowPhone'
     ])
   },
 
