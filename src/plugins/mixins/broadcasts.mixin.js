0ebb3c9d01afe55e22dff75bc08d39cb5c890c7b
jefferson-aloware <jefferson@aloware.com>
2024-08-07 17:03:00 -0300

    feature(wat-637): add warning computeds

--- src/plugins/mixins/broadcasts.mixin.js
+++ src/plugins/mixins/broadcasts.mixin.js
@@ -1,4 +1,4 @@
-import { mapState } from 'vuex'
+import { mapGetters, mapState } from 'vuex'
 import { aclMixin } from 'src/plugins/mixins'
 
 export default {
@@ -11,6 +11,10 @@ export default {
       'profile'
     ]),
 
+    ...mapGetters('contacts', [
+      'messageComposer'
+    ]),
+
     canUseBroadcast () {
       const isCompanyEnabled = this.profile.bulk_rvm_enabled || this.profile.bulk_sms_enabled
       const isUserEnabled = this.hasPermissionTo('create broadcast message') || this.hasPermissionTo('create broadcast rvm')
@@ -20,6 +24,30 @@ export default {
       }
 
       return isCompanyEnabled
+    },
+
+    useMmsRate () {
+      return this.messageComposer.sms.attachments.length > 0 || this.messageComposer.sms.gif_url.length > 0
+    },
+
+    hasTollFreePhoneNumber () {
+      return this.campaign?.has_tollfree_pn
+    },
+
+    showMessageSentAsMmsWarning () {
+      return this.useMmsRate && !this.hasTollFreePhoneNumber
+    },
+
+    showMessageSentFromTollFreeNumberWarning () {
+      return !this.useMmsRate && this.hasTollFreePhoneNumber
+    },
+
+    showMessageSentFromTollFreeNumberAsMmsWarning () {
+      return this.useMmsRate && this.hasTollFreePhoneNumber
+    },
+
+    shouldShowWarning () {
+      return this.showMessageSentAsMmsWarning || this.showMessageSentFromTollFreeNumberWarning || this.showMessageSentFromTollFreeNumberAsMmsWarning
     }
   }
 }
