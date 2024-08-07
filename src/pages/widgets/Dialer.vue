e45b9344c0e1ea6a04c3968c0c74e1711dd5492c
Tito Alvarez <tito@aloware.com>
2024-08-07 12:45:35 -0600

    Revert "Merge pull request #1192 from aloware/INT-281"
    This reverts commit 7f6bd72f5e7ba70599157ac451defb4571e0ad7a, reversing
    changes made to 7312791af4b4db8eec6bcd32b5c00c9f3173b212.

--- src/pages/widgets/Dialer.vue
+++ src/pages/widgets/Dialer.vue
@@ -172,9 +172,7 @@ export default {
         console.log('Error: api key is not valid', err)
         this.$handleErrors(err.response)
         this.loading = false
-        if (this.$route.name !== 'Login') {
-          this.$router.push({ name: 'Login', query: { redirect: this.$route.fullPath } })
-        }
+        this.$router.push({ name: 'Login', query: { redirect: this.$route.fullPath } })
       })
     },
 
@@ -226,6 +224,11 @@ export default {
           this.$emit('change', this.$emit('change', this.getContactEmitPayload()))
           this.handleCall()
         }
+      } else if (!this.authProfile) {
+        this.timeout = setTimeout(async () => {
+          await this.init()
+          this.handleDialNumber(this.hubspotPhoneNumber)
+        }, 1000)
       } else if (!this.dialer?.isReady) {
         this.timeout = setTimeout(() => {
           this.handleDialNumber(this.hubspotPhoneNumber)
