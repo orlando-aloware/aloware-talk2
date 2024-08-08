8ed3b892f4877dc415a22d0c1df08cf708283fc4
Tito Alvarez <tito@aloware.com>
2024-08-07 23:05:47 -0600

    Revert "Merge pull request #1213 from aloware/int-106/temporary-reversion"
    This reverts commit 4672bc1e2a23caad37b1fc12ac4cad37bdb638cf, reversing
    changes made to 5c90b2ca9169e1f6e2f8f8d3c757a4b488ce5ab7.

--- src/pages/widgets/Dialer.vue
+++ src/pages/widgets/Dialer.vue
@@ -132,6 +132,7 @@ export default {
   async mounted () {
     await this.init()
 
+    this.$VueEvent.listen('userLoggedIn', this.handleUserLogin)
     this.$VueEvent.listen('agent_status_updated', this.handleAgentStatusUpdate)
     this.isFirstLoading = false
   },
@@ -172,7 +173,9 @@ export default {
         console.log('Error: api key is not valid', err)
         this.$handleErrors(err.response)
         this.loading = false
-        this.$router.push({ name: 'Login', query: { redirect: this.$route.fullPath } })
+        if (this.$route.name !== 'Login') {
+          this.$router.push({ name: 'Login', query: { redirect: this.$route.fullPath } })
+        }
       })
     },
 
@@ -194,6 +197,10 @@ export default {
     },
 
     async searchContact (phoneNumber) {
+      if (!phoneNumber) {
+        return null
+      }
+
       const url = '/api/v2/contacts/quick-search'
       const response = await this.$axios.get(url, {
         params: {
@@ -224,11 +231,6 @@ export default {
           this.$emit('change', this.$emit('change', this.getContactEmitPayload()))
           this.handleCall()
         }
-      } else if (!this.authProfile) {
-        this.timeout = setTimeout(async () => {
-          await this.init()
-          this.handleDialNumber(this.hubspotPhoneNumber)
-        }, 1000)
       } else if (!this.dialer?.isReady) {
         this.timeout = setTimeout(() => {
           this.handleDialNumber(this.hubspotPhoneNumber)
