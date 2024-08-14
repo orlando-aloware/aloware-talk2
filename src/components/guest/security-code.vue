822c04e856677c8cf907de1a011fe3c5c8991860
Adrian <adrian@aloware.com>
2024-08-14 10:21:24 -0500

    fix(INT-309): class name was modified

--- src/components/guest/security-code.vue
+++ src/components/guest/security-code.vue
@@ -2,7 +2,7 @@
     <div class="security-code clearfix">
         <div class="security-code-wrapper">
             <div class="security-code-field"
-                 :class="{ 'hubspot-widget': hubspotWidget }"
+                 :class="{ 'security-code-hs-widget-mode': hubspotWidget }"
                  v-for="n in securityCodeLength"
                  :key="n">
                 <input maxlength="1"
