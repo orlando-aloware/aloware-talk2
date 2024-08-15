083b4efdc7de73c46122116ea4971f3ea3eb6d98
jefferson-aloware <jefferson@aloware.com>
2024-08-15 14:39:53 -0300

    feature(wat-637): separate into multiple files

--- src/store/carrier-fee/index.js
+++ src/store/carrier-fee/index.js
@@ -1,35 +1,7 @@
-import API from 'src/plugins/api/api'
-
-const state = {
-  carrierFees: []
-}
-
-const getters = {
-  getCarrierFees: (state) => state.carrierFees
-}
-
-const actions = {
-  async fetchCarrierFees ({ state, commit }) {
-    if (state.carrierFees.length) {
-      return
-    }
-
-    try {
-      const res = await API.V1.carrierFees.get()
-      commit('SET_CARRIER_FEES', res.data)
-    } catch (err) {
-      commit('SET_CARRIER_FEES', [])
-      console.log(err)
-      this._vm.$handleErrors(err.response)
-    }
-  }
-}
-
-const mutations = {
-  SET_CARRIER_FEES (state, carrierFees) {
-    state.carrierFees = carrierFees
-  }
-}
+import state from './carrier-fee.state'
+import getters from './carrier-fee.getters'
+import actions from './carrier-fee.actions'
+import mutations from './carrier-fee.mutations'
 
 export default {
   namespaced: true,
