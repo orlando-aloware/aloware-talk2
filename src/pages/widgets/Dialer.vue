40fe755d2c5ad0b3043cdf110a107014f51461d5
Fernando Ortuno <fernando@aloware.com>
2024-08-20 09:43:39 -0400

    fix(TEL-496) added optional chaining operator

--- src/pages/widgets/Dialer.vue
+++ src/pages/widgets/Dialer.vue
@@ -478,9 +478,9 @@ export default {
       }
 
       const shouldForceContactDisposition = this.currentCompany.force_contact_disposition &&
-        !this.profile.last_call.contact.disposition_status_id
+        !this.profile.last_call?.contact?.disposition_status_id
       const shouldForceCallDisposition = this.currentCompany.force_call_disposition &&
-        !this.profile.last_call.call_disposition_id
+        !this.profile.last_call?.call_disposition_id
 
       const isCallInProgress = ['CALL_CONNECTED', 'WRAP_UP', 'MAKING_CALL']
       if (isCallInProgress?.includes(this.dialer?.currentStatus) &&
