2601e00ae4bee918a534a823cc867e6c27bf2bed
jefferson-aloware <jefferson@aloware.com>
2024-08-20 10:04:17 -0300

    feature(wat-637): improve sanity check and implementation

--- src/components/generic-selectors/throttle-selector.vue
+++ src/components/generic-selectors/throttle-selector.vue
@@ -99,13 +99,17 @@ export default {
 
       let mpsList = [0.05, 0.1, 0.25, 0.5, 1, 2, 3, 4, 6, 8, 10]
 
-      for (let index = 0; mpsList[index] <= this.campaign?.max_mps; index++) {
+      // Sanity check to avoid out-of-bounds index access
+      let maxMps = this.campaign?.max_mps || 0
+      let validMpsList = mpsList.filter(mps => mps <= maxMps)
+
+      validMpsList.forEach((mps, index) => {
         options.push({
           id: index + 1,
-          name: mpsList[index] * 60 * 60 + ' per hour',
-          value: mpsList[index] * 60
+          name: mps * 60 * 60 + ' per hour',
+          value: mps * 60
         })
-      }
+      })
 
       return options
     }
