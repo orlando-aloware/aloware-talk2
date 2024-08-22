84d65528cefdb5c13e0c54309cdb619aec16c924
jefferson-aloware <jefferson@aloware.com>
2024-08-22 12:33:43 -0300

    feature(wat-637): emit events when attachment is added or removed

--- src/components/message-composer/message-composer-sms.vue
+++ src/components/message-composer/message-composer-sms.vue
@@ -599,14 +599,25 @@ export default {
 
     gifSelected (gif) {
       this.setMessageComposerSmsGif(gif)
+      this.$emit('gif-selected', gif)
     },
 
     removeMessageGif () {
       this.setMessageComposerSmsGif('')
+      this.$emit('gif-removed', '')
+    },
+
+    attachmentUploaded (files) {
+      files.forEach((file) => {
+        this.appendMessageComposerSmsAttachments(file)
+      })
+
+      this.$emit('attachments-uploaded', files)
     },
 
     removeAttachment (attachment) {
       this.removeMessageComposerSmsAttachment(attachment)
+      this.$emit('attachment-removed', attachment)
     },
 
     getPreviewLink (uuid) {
@@ -619,12 +630,6 @@ export default {
 
     variableSelected (variable) {
       this.setMessageComposerSmsBody((this.messageComposer.sms.body ? this.messageComposer.sms.body + ' ' : '') + variable)
-    },
-
-    attachmentUploaded (files) {
-      files.forEach((file) => {
-        this.appendMessageComposerSmsAttachments(file)
-      })
     },
 
     showScheduleMessage () {
