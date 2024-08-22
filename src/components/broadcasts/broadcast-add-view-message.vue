f0f695c29ed85d7607a7d78793a9fd66404294e6
jefferson-aloware <jefferson@aloware.com>
2024-08-22 12:38:13 -0300

    feature(wat-637): add use groups false to not show empty option

--- src/components/broadcasts/broadcast-add-view-message.vue
+++ src/components/broadcasts/broadcast-add-view-message.vue
@@ -13,6 +13,7 @@
       </div>
       <div class="broadcast-add__schedule__row__fields">
         <contact-line-selector :value="propCampaign?.id"
+                               :use-groups="false"
                                @select="onCampaignSelected"/>
         <broadcast-warning-note :campaign="propCampaign" />
       </div>
