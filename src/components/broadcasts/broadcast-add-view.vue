cf06602e88413b475471834d240ff4f3550b128a
jefferson-aloware <jefferson@aloware.com>
2024-08-22 19:27:02 -0300

    feature(wat-637): fix default state

--- src/components/broadcasts/broadcast-add-view.vue
+++ src/components/broadcasts/broadcast-add-view.vue
@@ -560,7 +560,7 @@ export default {
     this.setMessageComposerSmsGif('')
     this.setMessageComposerAttachments([])
     this.setCurrentListFilters({})
-    this.SET_SELECTED_CAMPAIGN(null)
+    this.SET_SELECTED_CAMPAIGN({})
     this.rmv = null
   }
 }
