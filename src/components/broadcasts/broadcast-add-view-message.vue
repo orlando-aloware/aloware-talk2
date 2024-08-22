e2d4e559599701a36551a39dbd1b913246c8ef3e
Tito Alvarez <tito@aloware.com>
2024-08-22 17:56:15 -0600

    Revert "Merge pull request #1280 from aloware/wat-637/feature/improve-broadcast-calculator-in-talk"
    This reverts commit 6e24c7a2257ef3df0784d979ec0c64a5af9ac18b, reversing
    changes made to 408dcce134bac47e16bf490a75911ec46cd03ce7.

--- src/components/broadcasts/broadcast-add-view-message.vue
+++ src/components/broadcasts/broadcast-add-view-message.vue
@@ -13,7 +13,6 @@
       </div>
       <div class="broadcast-add__schedule__row__fields">
         <contact-line-selector :value="propCampaign?.id"
-                               :use-groups="false"
                                @select="onCampaignSelected"/>
         <broadcast-warning-note :campaign="propCampaign" />
       </div>
@@ -58,11 +57,7 @@
                               :reset-on-load="false"
                               :use-send-button="false"
                               :is-broadcast="true"
-                              @messageChanged="onMessageChanged"
-                              @attachments-uploaded="onAttachmentsUploaded"
-                              @attachment-removed="onAttachmentRemoved"
-                              @gif-selected="onGifSelected"
-                              @gif-removed="onGifRemoved" />
+                              @messageChanged="onMessageChanged"/>
       </div>
 
       <div class="broadcast-add__message__sms__composer-footer">
@@ -376,22 +371,6 @@ export default {
 
     onMessageChanged (message) {
       this.messageLength(message)
-      this.updatePrice()
-    },
-
-    onAttachmentsUploaded () {
-      this.updatePrice()
-    },
-
-    onAttachmentRemoved () {
-      this.updatePrice()
-    },
-
-    onGifSelected () {
-      this.updatePrice()
-    },
-
-    onGifRemoved () {
       this.updatePrice()
     }
   },
