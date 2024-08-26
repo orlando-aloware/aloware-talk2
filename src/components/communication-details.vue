05814952ef0a531366bc99976faa6d928b2785ca
Vinicius Alcantara <vinicius@aloware.com>
2024-08-26 10:13:40 -0300

    feat(INT-303): fix duplicated data-testids

--- src/components/communication-details.vue
+++ src/components/communication-details.vue
@@ -945,8 +945,8 @@
           <!-- COMMUNICATION CUSTOM FIELDS -->
           <q-card-section class="pt-0 pb-0"
                           v-if="communication.metadata?.custom_fields && Object.keys(communication.metadata.custom_fields).length > 0"
-                          data-testid="comm-details-call-disposition-card">
-            <b-form-row v-for="(custom_field, key) in communication.metadata?.custom_fields" data-testid="comm-details-call-disposition-row" :key="key">
+                          data-testid="comm-details-custom-fields">
+            <b-form-row v-for="(custom_field, key) in communication.metadata?.custom_fields" data-testid="comm-details-custom-field-row" :key="key">
               <b-col class="pl-0 pr-0">
                 <q-item-label> {{ convertToTitleCase(key) }}: </q-item-label>
               </b-col>
