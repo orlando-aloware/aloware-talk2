50992ac777a2fad34c5d3b09e9eb290e0fac5cde
Fernando Ortuno <fernando@aloware.com>
2024-08-27 10:38:10 -0400

    fix(TEL-504) added logic to show phone if banner exists.

--- src/components/dialer/phone.vue
+++ src/components/dialer/phone.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="phone d-flex flex-column"
        ref="phone"
-       :class="{ 'invisible': !isVisible, 'no-padding': loadingPhone, 'phone-widget': is_widget }"
+       :class="{'phone-with-notification': banner, 'invisible': !isVisible, 'no-padding': loadingPhone, 'phone-widget': is_widget }"
        v-if="loadingPhone || shouldShow">
     <mobile-live-call-bar :hide-live-call="true" />
     <div class="phone-header d-flex grabbable d-flex justify-content-between align-items-center flex-grow-0"
@@ -1242,6 +1242,13 @@
     </template>
   </div>
 </template>
+
+<style scoped>
+.phone-with-notification {
+  top: 150px;
+}
+</style>
+
 <script>
 import _ from 'lodash'
 import { mapActions, mapState } from 'vuex'
@@ -1406,6 +1413,7 @@ export default {
         right: 0,
         top: 0
       },
+      banner: false,
       isVisible: true,
       currentLocalTime: null,
       showLocalTime: true,
@@ -1990,9 +1998,17 @@ export default {
     if (this.dialer.currentStatus === 'WRAP_UP') {
       this.changeScreen('wrap-up')
     }
+
+    setTimeout(() => { this.checkNotification() }, 3000)
   },
 
   methods: {
+
+    checkNotification () {
+      const notification = document.querySelector('#notification-container')
+      this.banner = !!notification
+    },
+
     setupDraggable () {
       if (!this.is_widget && this.shouldShow) {
         this.openPhone()
