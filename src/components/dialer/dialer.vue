6a149123fb57d3cebc3582372f9149acd0256152
Keyner W. Tupac Yupanqui Caballero <keyner@aloware.com>
2024-08-27 17:44:04 -0500

    add logs4

--- src/components/dialer/dialer.vue
+++ src/components/dialer/dialer.vue
@@ -354,7 +354,10 @@ export default {
       this.removeUnownedLiveContactTask()
       console.log('Call invite canceled 1', call)
       this.setDialerCurrentStatus('INVITE_CANCELLED')
-      this.backToDial('Talk-Device.OnCancel')
+      if (call !== undefined) {
+        console.log('Call undefined 2')
+        this.backToDial('Talk-Device.OnCancel')
+      }
       this.connection = null
       this.$closeActionNotification('incomingCall')
     })
@@ -736,7 +739,11 @@ export default {
         console.log('Call invite canceled 2', call)
         this.connection = null
         this.setDialerCurrentStatus('INVITE_CANCELLED')
-        this.backToDial('Talk-Connection.OnCancel')
+        // this.backToDial('Talk-Connection.OnCancel')
+        if (call !== undefined) {
+          console.log('Call undefined 1')
+          this.backToDial('Talk-Device.OnCancel')
+        }
         this.$closeActionNotification('incomingCall')
       })
 
