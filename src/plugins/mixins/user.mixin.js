3b79ea32b2f672bb36e0113759f80b2c52883f83
Tito Alvarez <146394573+tito-aloware@users.noreply.github.com>
2024-08-27 08:55:02 -0600

    Merge pull request #1293 from aloware/WAT-722/feature/unassigned-filter

--- src/plugins/mixins/user.mixin.js
+++ src/plugins/mixins/user.mixin.js
@@ -81,6 +81,14 @@ export default {
       return storage.local.getItem('aloware_demo_companies') && storage.local.getItem('aloware_demo_companies').split(',').includes(String(companyId))
     },
 
+    // Unassigned filters should be available for beta and 568, 3459 companies
+    isCompanyPartOfNewUnassignedInboxFilters (companyId) {
+      const demoCompanies = storage.local.getItem('aloware_demo_companies') ? storage.local.getItem('aloware_demo_companies').split(',') : []
+      demoCompanies.push('568')
+      demoCompanies.push('3459')
+      return demoCompanies.includes(String(companyId))
+    },
+
     // Temporary function to check if company is part of new inbox filters
     // should be removed once all companies are migrated to new inbox filters
     isCompanyPartOfNewInboxFilters (companyId) {
