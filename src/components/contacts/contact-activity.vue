67addaf45644eae7f6686078cf2a22b231416452
Adrian <adrian@aloware.com>
2024-08-28 15:52:00 -0500

    refactor(INT-340): styles moved to app.scss

--- src/components/contacts/contact-activity.vue
+++ src/components/contacts/contact-activity.vue
@@ -922,19 +922,3 @@ export default {
   }
 }
 </script>
-
-<style scoped lang="scss">
-.activity-bottom-info-hs-widget {
-  min-height: 30px;
-  height: auto;
-  max-height: 100%;
-  overflow-x: auto;
-  overflow-y: auto;
-
-  span {
-    @media (max-width: 500px) {
-      white-space: normal;
-    }
-  }
-}
-</style>
