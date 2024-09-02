14dfca724c81ad9ac88456493b4ad9c647d5e328
Keyner W. Tupac Yupanqui Caballero <keyner@aloware.com>
2024-09-02 15:23:49 -0500

    clean-up 5

--- src/components/communication/device.js
+++ src/components/communication/device.js
@@ -51,7 +51,6 @@ export default class Device {
     this._device = this.carrier === Carriers.TWILIO ? new TwilioClientDevice(token, options) : null
     this._device.isEventsStarted = false
     this._is_initialized = true
-
     this._initEvents()
   }
 
