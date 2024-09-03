965e45a68cc7e257cab6c48afc2b2bbb5f63a568
Tito Alvarez <tito@aloware.com>
2024-09-03 17:30:32 -0600

    fix: adding missing properties

--- src/App.vue
+++ src/App.vue
@@ -57,9 +57,10 @@ export default {
   },
 
   computed: {
-    ...mapState('auth', ['profile', 'authenticated', 'loading']),
-
-    ...mapState(['statics', 'staticsLoaded', 'isWhiteLabel', 'isWidget']),
+    ...mapState('cache', ['currentCompany']),
+    ...mapState('auth', ['profile', 'authenticated, loading']),
+    ...mapState(['statics', 'staticsLoaded', 'isWhiteLabel', 'isWidget'])
+  },
 
     isFromClassic () {
       const urlParams = new URLSearchParams(window.location.search)
