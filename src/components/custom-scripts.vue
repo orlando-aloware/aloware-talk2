03208771f20fad1a16a57a4319eb2e1e970d87e1
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2024-09-03 15:00:24 -0700

    Fix

--- src/components/custom-scripts.vue
+++ src/components/custom-scripts.vue
@@ -80,7 +80,7 @@ export default {
       }
 
       // Disable Hubspot chat for Aloware and local environments
-      if (!this.isModGen || this.isLocal) {
+      if (this.isLocal) {
         return
       }
 
