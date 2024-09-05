3d0b5d9a26aaf191e34f250be396a7f92914419e
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2024-09-05 15:12:42 -0700

    More logs

--- src/components/webrtc.vue
+++ src/components/webrtc.vue
@@ -149,7 +149,7 @@ export default {
         this.profile.agent_status !== event.agent_status
       ) {
         this.setAgentStatus(event.agent_status)
-        console.log('Changed agent status [event]: ', event.agent_status)
+        console.log('Changed agent status from webrtc::agentStatusUpdated [event]: ', event.agent_status)
       }
     }
 
