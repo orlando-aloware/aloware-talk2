41cc3d72a07ccdf1649f36cdcf07da3eeb7508a2
Fernando Ortuno <fernando@aloware.com>
2024-09-10 13:20:37 -0400

    fix(int-346): revert change on conditional due logic equivalence

--- src/pages/widgets/Dialer.vue
+++ src/pages/widgets/Dialer.vue
@@ -221,7 +221,7 @@ export default {
       const shouldForceCallDisposition = this.currentCompany.force_call_disposition &&
         !this.profile.last_call?.call_disposition_id
 
-      if (!(shouldForceContactDisposition || shouldForceCallDisposition)) {
+      if (!shouldForceContactDisposition && !shouldForceCallDisposition) {
         this.$VueEvent.fire('resetCall')
       }
     },
