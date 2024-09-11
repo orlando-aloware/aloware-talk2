469a90e4e6eab4c597ae6866ada64601c7a7bfa4
paulo-aloware <paulo@aloware.com>
2024-09-10 23:35:58 -0400

    feat(pow-230): add aloai bulk contact enrollment

--- src/plugins/api/api.js
+++ src/plugins/api/api.js
@@ -787,6 +787,15 @@ export default {
       get (params) {
         return window.axios.get(`${suffixV2}users`, { params })
       }
+    },
+
+    aloAiBot: {
+      getBots (params) {
+        return window.axios.get(`${suffixV1}aloai/bots`, { params })
+      },
+      enrollContacts (botId, params) {
+        return window.axios.post(`${suffixV1}aloai/${botId}/enroll-contacts`, params)
+      }
     }
   }
 }
