008c401f48eabc98ee667ab5425cb5d92d10516d
Fernando Ortuno <fernando@aloware.com>
2024-09-16 16:59:44 -0400

    fix(tel-559): added logs to notification triggers

--- src/components/action-notification.vue
+++ src/components/action-notification.vue
@@ -563,12 +563,14 @@ export default {
     },
 
     onShow () {
+      console.log('Communication when event Show notification triggers: ', this.communication)
       this.isValidNotification = false
       this.stopNotificationListeners()
       this.startNotificationListeners()
     },
 
     autoClose () {
+      console.log('Communication when event Auto close notification triggers: ', this.communication)
       this.runDateTimeInterval()
 
       if ((this.id === 'callFishing' && document.getElementById('callFishing') &&
@@ -620,6 +622,7 @@ export default {
     },
 
     onHidden () {
+      console.log('Communication when event Hidden notification triggers: ', this.communication)
       if (this.id === 'call') {
         return
       }
