e4fa0123e51827fc0e5eb855d1777ade849177c7
jefferson-aloware <jefferson@aloware.com>
2024-09-17 15:30:00 -0300

    feature(wat-835): create data-table mixin

---
+++ src/plugins/mixins/data-table.mixin.js
@@ -0,0 +1,27 @@
+import { VIEWPORTS } from 'src/constants/viewport-sizes'
+
+export default {
+  computed: {
+    screenWidth () {
+      return this.$q.screen.width
+    }
+  },
+
+  methods: {
+    getResponsiveColumns (allColumns, columnsByViewport) {
+      // Find the current viewport
+      const currentViewport = VIEWPORTS.find((vp) => this.screenWidth >= vp.minWidth && this.screenWidth <= vp.maxWidth)
+
+      if (currentViewport) {
+        const columnNames = columnsByViewport[currentViewport.name]
+        return this.filterColumns(allColumns, columnNames)
+      }
+
+      return allColumns
+    },
+
+    filterColumns (allColumns, columnNames) {
+      return allColumns.filter((column) => columnNames.includes(column.name))
+    }
+  }
+}
