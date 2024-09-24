98f72df0bd8ae1abc0c5fffa595956604087a291
Gustavo Andrade <gustavo@aloware.com>
2024-09-24 13:48:12 -0300

    fix(wat-854): remove console logs

--- src/components/create-list-modal.vue
+++ src/components/create-list-modal.vue
@@ -273,10 +273,8 @@ export default {
 
         // Verify if filter_groups is already set and merge it with the contact_lists filter
         if (Array.isArray(params.filter_groups)) {
-          console.log('Option 1')
           params.filter_groups[0].filters.contact_lists = contactListFilter
         } else {
-          console.log('Option 2')
           params.filter_groups = [
             {
               filters: {
