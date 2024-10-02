171ae5fbe69cbb0e150046d71170db420b696723
Esteban <martiniroqueesteban@gmail.com>
2024-10-02 16:33:16 -0300

    feature(wat-699): save navbar status on localstorage

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -72,11 +72,11 @@
                   v-if="authenticated && !suspended && !isWidget">
           <q-list>
             <app-sidebar class="page-sidebar"
-                         :lightMode="lightMode"
-                         :xmasEnabled="isXmasEnabled"
+                         :is-sidebar-expanded="isSidebarExpanded"
+                         :light-mode="lightMode"
+                         :xmas-enabled="isXmasEnabled"
                          @toggleMode="toggleMode"
-                         @toggleSidebarExpansion="toggleSidebarExpansion">
-            </app-sidebar>
+                         @toggleSidebarExpansion="toggleSidebarExpansion" />
           </q-list>
         </q-drawer>
         <q-drawer class="mobile-phone-drawer position-relative h-100 overflow-hidden"
@@ -390,8 +390,7 @@ export default {
       MetricOptionGroups,
       AppDefaultLogin,
       isFirstLoading: true,
-      isSidebarExpanded: false,
-      sidebarWidth: 64
+      isSidebarExpanded: true
     }
   },
 
@@ -589,6 +588,14 @@ export default {
 
     isAuthenticated () {
       return !this.isGuest && this.authenticated
+    },
+
+    sidebarWidth () {
+      if (this.isSidebarExpanded) {
+        return 230
+      } else {
+        return 64
+      }
     }
   },
 
@@ -1149,6 +1156,13 @@ export default {
       this.sidebarVisible = true
     }
 
+    // using the negative because the default should be expanded, so whenever the value is falsy means expanded
+    const isSidebarCollapsed = localStorage.getItem('isSidebarCollapsed')
+
+    if (isSidebarCollapsed === 'true') {
+      this.isSidebarExpanded = false
+    }
+
     // check auth every 5 minutes
     const checkInterval = 5 * 60 * 1000
 
@@ -1413,11 +1427,7 @@ export default {
     toggleSidebarExpansion () {
       this.isSidebarExpanded = !this.isSidebarExpanded
 
-      if (this.isSidebarExpanded) {
-        this.sidebarWidth = 230
-      } else {
-        this.sidebarWidth = 64
-      }
+      localStorage.setItem('isSidebarCollapsed', !this.isSidebarExpanded)
     },
 
     toggleSidebar () {
