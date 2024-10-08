94145a6171984a170b07c2e9c28f5556d53f4248
Yhael S <yhael@aloware.com>
2024-10-08 12:46:12 -0500

    feature(COM-353): added AloAi menu and page

--- src/components/layout/app-sidebar.vue
+++ src/components/layout/app-sidebar.vue
@@ -674,6 +674,47 @@
       </span>
     </q-btn>
 
+    <q-btn icon="img:app-icons/menu/aloai_active.svg"
+           align="left"
+           padding="10px 20px"
+           class="nav-icons w-100"
+           :to="{ name: 'AloAi' }"
+           :ripple="false"
+           v-show="isActive('AloAi')"
+           flat>
+      <q-tooltip anchor="center right"
+                 self="center left"
+                 v-if="!isSidebarExpanded"
+                 :offset="[-5, 0]">
+        <span class="font-weight-bold text-sm">AloAi</span>
+      </q-tooltip>
+
+      <span class="text-size-lg font-weight-bold text-regular text-white ml-2"
+            v-if="isSidebarExpanded">
+        AloAi
+      </span>
+    </q-btn>
+    <q-btn icon="img:app-icons/menu/aloai_gray.svg"
+           align="left"
+           padding="10px 20px"
+           class="nav-icons w-100"
+           :to="{ name: 'AloAi' }"
+           :ripple="false"
+           v-show="!isActive('AloAi')"
+           flat>
+      <q-tooltip anchor="center right"
+                 self="center left"
+                 v-if="!isSidebarExpanded"
+                 :offset="[-5, 0]">
+        <span class="font-weight-bold text-sm">AloAi</span>
+      </q-tooltip>
+
+      <span class="text-size-lg font-weight-bold text-regular text-menu-purple ml-2"
+            v-if="isSidebarExpanded">
+        AloAi
+      </span>
+    </q-btn>
+
     <q-btn :to="{ name: 'Settings' }"
            :ripple="false"
            icon="img:app-icons/menu/settings_active.svg"
@@ -843,6 +884,10 @@ export default {
       }
 
       if (['Wallboard Agents', 'Wallboard Calls'].includes(this.$route.name) && name === 'Wallboard') {
+        return true
+      }
+
+      if (this.$route.name === 'AloAi' && name === 'AloAi') {
         return true
       }
 
---
+++ src/pages/AloAi.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header">
+              <h4 class="card-title">AloAi</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AloAi'
+}
+</script>
