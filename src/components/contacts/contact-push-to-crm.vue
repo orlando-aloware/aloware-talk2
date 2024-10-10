4b5c3dbb5973f02ba380a5ef055dfb9e539a1c05
jefferson-aloware <jefferson@aloware.com>
2024-10-10 12:51:18 -0300

    feature(wat-921): create contact-push-to-crm component

---
+++ src/components/contacts/contact-push-to-crm.vue
@@ -0,0 +1,80 @@
+<template>
+  <b-card class="border-0"
+          data-testid="contact-push-to-crm-wrapper">
+    <h4>Push contact to CRM</h4>
+    <b-button class="btn-block mt-2"
+              variant="outline-primary"
+              size="sm"
+              data-testid="contact-push-to-crm-button"
+              :disabled="pushed || isLoading"
+              @click="pushToCrm">
+      <i class="el-icon-loading"
+         v-if="isLoading"></i>
+      {{ isLoading ? 'Please wait..' : 'Push to CRM' }}
+      <q-tooltip anchor="top middle"
+                 self="center middle"
+                 data-testid="contact-push-to-crm-tooltip"
+                 v-if="pushed">
+          Contact has already been pushed to CRM.
+      </q-tooltip>
+    </b-button>
+  </b-card>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import talk2Api from 'src/plugins/api/api'
+
+export default {
+  name: 'contact-push-to-crm',
+
+  props: {
+    contact: {
+      required: true
+    },
+    noPadding: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+
+  data () {
+    return {
+      pushed: false,
+      isLoading: false
+    }
+  },
+
+  computed: {
+    ...mapState('auth', ['profile'])
+  },
+
+  methods: {
+    pushToCrm () {
+      this.isLoading = true
+      talk2Api.V1.contact.pushToCrm(this.contact.id).then(response => {
+        if (response.data.status === 'success') {
+          this.pushed = true
+          this.$generalNotification(
+            response.data?.message || 'Contact has been pushed to CRM successfully',
+            response.data?.status || 'success'
+          )
+        }
+      }).catch(err => {
+        this.$handleErrors(err.response)
+      }).finally(() => {
+        this.isLoading = false
+      })
+    }
+  },
+
+  watch: {
+    contact () {
+      if (this.contact) {
+        this.pushed = false
+      }
+    }
+  }
+}
+</script>
