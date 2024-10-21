c97dfb8989992c42227d47698afab44a675e044b
Gustavo Andrade <gustavo@aloware.com>
2024-10-21 10:32:37 -0300

    fix(pow-353): remove mde testing

--- src/components/power-dialer/power-dialer-add-modal.vue
+++ src/components/power-dialer/power-dialer-add-modal.vue
@@ -846,8 +846,7 @@ export default {
 
     getListCount (id) {
       return this.$axios
-        // .get(`${process.env.API_REPORTING_URL}/api/v2/contacts-list/${id}/count`)
-        .get(`https://pr-10767.mde.alodev.org/api/v2/contacts-list/${id}/count`)
+        .get(`${process.env.API_REPORTING_URL}/api/v2/contacts-list/${id}/count`)
     },
 
     checkIntegrationImport () {
