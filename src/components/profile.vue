5f34c7ac66e697a328930832c4b8647ed52ab35e
Fernando Ortuno <fernando@aloware.com>
2024-10-22 16:26:51 -0400

    fix(wat-705): created computed to wrap functionality

--- src/components/profile.vue
+++ src/components/profile.vue
@@ -93,7 +93,7 @@
                   v-close-popup
                   :class="[agentStatus === AgentStatus.AGENT_STATUS_OFFLINE ? 'cursor-inherit' : '']"
                   data-testid="profile-offline-time"
-                  :disable="isDisabled"
+                  :disable="shouldDisableProfileMenuActions"
                   @click="changeStatus(AgentStatus.AGENT_STATUS_OFFLINE)">
             <div class="d-flex align-items-center justify-content-between w-100">
               <div>
@@ -116,7 +116,7 @@
                   v-close-popup
                   :class="[agentStatus === AgentStatus.AGENT_STATUS_ACCEPTING_CALLS ? 'cursor-inherit' : '']"
                   data-testid="profile-available-calls"
-                  :disable="isDisabled"
+                  :disable="shouldDisableProfileMenuActions"
                   @click="changeStatus(AgentStatus.AGENT_STATUS_ACCEPTING_CALLS)">
             <div class="d-flex align-items-center justify-content-between w-100">
               <div>
@@ -136,7 +136,7 @@
           <q-item dense
                   clickable
                   v-close-popup
-                  :disable="(profile.company.force_users_always_available && !hasRole('Company Admin')) || isDisabled"
+                  :disable="forceUsersAlwaysAvailable || shouldDisableProfileMenuActions"
                   :class="[agentStatus === AgentStatus.AGENT_STATUS_NOT_ACCEPTING_CALLS ? 'cursor-inherit' : '']"
                   data-testid="profile-busy-calls"
                   @click="changeStatus(AgentStatus.AGENT_STATUS_NOT_ACCEPTING_CALLS)">
@@ -170,7 +170,7 @@
                   clickable
                   v-close-popup
                   :class="[agentStatus === AgentStatus.AGENT_STATUS_ON_BREAK ? 'cursor-inherit' : '']"
-                  :disable="(profile.company.force_users_always_available && !hasRole('Company Admin')) || isDisabled"
+                  :disable="(profile.company.force_users_always_available && !hasRole('Company Admin')) || shouldDisableProfileMenuActions"
                   data-testid="profile-on-break"
                   @click="changeStatus(AgentStatus.AGENT_STATUS_ON_BREAK)">
             <div class="d-flex align-items-center justify-content-between w-100">
@@ -201,7 +201,7 @@
           <q-item dense
                   clickable
                   data-testid="profile-turn-notifications-item"
-                  :disable="isDisabled"
+                  :disable="shouldDisableProfileMenuActions"
                   @click="toggleSleepMode">
             <q-item-section>
               <q-skeleton type="rect"
@@ -224,7 +224,7 @@
                   clickable
                   v-close-popup
                   data-testid="profile-select-account-item"
-                  :disable="isDisabled"
+                  :disable="shouldDisableProfileMenuActions"
                   v-if="profile?.has_multiple_access"
                   @click="showAccountSelector">
             <q-item-section>
@@ -343,10 +343,10 @@ export default {
 
     isProfileDropdownDisabled () {
       const isImpersonate = localStorage.getItem('impersonate')
-      return this.isDisabled && (!isImpersonate || (isImpersonate && this.dialer?.call?.state === 'open'))
+      return this.shouldDisableProfileMenuActions && (!isImpersonate || (isImpersonate && this.dialer?.call?.state === 'open'))
     },
 
-    isDisabled () {
+    shouldDisableProfileMenuActions () {
       const isForcedCallDisposition = this.currentCompany && this.currentCompany.force_call_disposition
       const isForcedContactDisposition = this.currentCompany && this.currentCompany.force_contact_disposition
       const isForcedDispositionOnWrapUp = (isForcedCallDisposition || isForcedContactDisposition) &&
@@ -359,6 +359,9 @@ export default {
 
     userPersonalLine () {
       return this.profile.campaign_id ? this.campaigns.find(campaign => campaign.id === this.profile.campaign_id) : null
+    },
+    forceUsersAlwaysAvailable () {
+      return this.profile.company.force_users_always_available && !this.hasRole('Company Admin')
     }
   },
 
