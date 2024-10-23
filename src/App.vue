44a3fd7735943b5097e002b3674cc36a94b3a3bb
Jonathan M <jonathan@aloware.com>
2024-10-23 13:59:38 -0300

    feature(pow-337): added agent status polling mixin

--- src/App.vue
+++ src/App.vue
@@ -40,12 +40,13 @@ import ActionNotification from 'components/action-notification'
 import { mapActions, mapState } from 'vuex'
 import Intercom from 'components/intercom'
 import HeaderNotification from 'components/header-notification'
-import { accessMixin } from 'src/plugins/mixins'
+import { accessMixin, pollingMixin } from 'src/plugins/mixins'
 export default {
   name: 'App',
 
   mixins: [
-    accessMixin
+    accessMixin,
+    pollingMixin
   ],
 
   components: {
@@ -115,6 +116,13 @@ export default {
         }
       })
     }
+
+    // runs after 'polling_interval' seconds the app is initiated, every 'polling_interval' seconds
+    setTimeout(() => {
+      setInterval(() => {
+        this.pollUsers(this.profile.company_id)
+      }, this.pollingInterval * 1000)
+    }, this.pollingInterval * 1000)
 
     this.$VueEvent.listen('make_new_call', (data) => {
       let fixedPhoneNumber = this.$options.filters.fixPhone(data.phone_number)
