54e01e3bd4d6827faab787989d625e0b99f482f2
nestor <nestor@xmlteam.com>
2024-10-23 12:05:50 -0600

    fix(POW-297): Fire contact_list_created event for the right user

--- src/plugins/mixins/broadcast.mixin.js
+++ src/plugins/mixins/broadcast.mixin.js
@@ -696,9 +696,7 @@ export default {
           window.VueEvent.fire('contact_list_import_failed', event)
         })
         .listen('.contact-list.created', (event) => {
-          const isTargetCompany = this.currentCompany && event.contact_list.company_id && event.contact_list.company_id === this.currentCompany.id
-          const isTargetUser = this.profile && event.user_id && event.user_id === this.profile.id
-          if (isTargetCompany && isTargetUser) {
+          if (this.profile && event.user_id && event.user_id === this.profile.id) {
             window.VueEvent.fire('contact_list_created', event)
           }
         })
