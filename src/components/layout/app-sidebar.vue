6a16e660ee76c417939b9b2075251b2596a87b98
Esteban <martiniroqueesteban@gmail.com>
2024-10-24 09:05:33 -0300

    feature(wat-000): fixed orange badge position in broadcast item

--- src/components/layout/app-sidebar.vue
+++ src/components/layout/app-sidebar.vue
@@ -617,10 +617,12 @@
            :ripple="false"
            v-show="!isActive('Broadcasts') && !canUseBroadcast"
            @click="toggleProFeatureDialog(true)">
-      <q-badge floating
-               rounded
-               color="orange">
-      </q-badge>
+      <q-badge rounded
+               floating
+               color="orange"
+               style="top:-5px; right: -5px"
+               v-if="!isSidebarExpanded"
+      />
       <q-tooltip anchor="center right"
                  self="center left"
                  v-if="!isSidebarExpanded"
@@ -631,6 +633,11 @@
       <span class="text-size-lg font-weight-bold text-regular text-menu-purple ml-2"
             v-if="isSidebarExpanded">
         Broadcasts
+        <q-badge rounded
+                 floating
+                 color="orange"
+                 style="top:-5px; right: -5px"
+        />
       </span>
     </q-btn>
     <q-btn icon="img:app-icons/menu/broadcast_active.svg"
