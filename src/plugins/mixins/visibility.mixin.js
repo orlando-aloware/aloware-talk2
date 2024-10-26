38aad02644089d9e83ff653b5e7225e58ff47130
Fernando Ortuno <fernando@aloware.com>
2024-10-26 01:25:35 -0400

    feature(wat-909): fixed conflict problem

--- src/plugins/mixins/visibility.mixin.js
+++ src/plugins/mixins/visibility.mixin.js
@@ -237,17 +237,16 @@ export default {
         communication.tags.length > 0) {
         return false
       }
-      // checks exclude automated communications filter and matches the communication
-      return !(filter.exclude_automated_communications !== undefined &&
-        filter.exclude_automated_communications &&
-        communication.workflow_id)
-
       // checks international filter is selected and matches the communication
       if (filter.has_international !== undefined &&
         filter.has_international &&
         !communication.is_international) {
         return false
       }
+      // checks exclude automated communications filter and matches the communication
+      return !(filter.exclude_automated_communications !== undefined &&
+        filter.exclude_automated_communications &&
+        communication.workflow_id)
     },
 
     checkCommunicationMatchesUserAccessibility (communication) {
