8f98749102c427da0fc6240348194ef0cea9f8b3
Marcos Grimm <marcos@aloware.com>
2024-11-05 18:22:14 -0300

    feature(pow-329): delete contact list item, and its async implications and feedback. Wip

--- src/components/bulk-action-menu.vue
+++ src/components/bulk-action-menu.vue
@@ -303,11 +303,9 @@ export default {
     showRemoveFromListButton () {
       // if is a default list, dont show
       if (this.currentList.is_default) {
-        console.log('is default list')
         return false
       }
 
-      console.log('is not default list')
       // if is only agent and not billing admin or admin or supervisor and list is public
       if (this.isAgent && !this.isBillingAdminOrAdminOrSupervisor && this.lists[this.id]?.show_in_public_folder) {
         return false
