076d3a6bcb12fbcf2c75d7e39822397979cf0dba
Marcos Grimm <marcos@aloware.com>
2024-11-04 21:16:09 -0300

    feature(pow-329): pagination, search, ellipsis lenght. Wip

--- src/components/contacts/contact-lists-card.vue
+++ src/components/contacts/contact-lists-card.vue
@@ -1,32 +1,51 @@
 <template>
   <b-card class="border-0 contact-lists-card"
           data-testid="contact-lists-card">
-    <div id="contact-lists-card-header" class="d-flex justify-content-between align-items-center">
-      <h4>{{ title }}</h4>
-      <b-link href="#"
-              class="custom-link text-decoration-none btn-tag-edit d-flex align-items-center"
-              @click="onSearch">
-        <slot name="button">
-          <q-tooltip anchor="top middle"
-                     self="center middle">
-            Search current lists
-          </q-tooltip>
-          <search-icon class="search-icon" :color="iconColor" />
-        </slot>
-      </b-link>
+    <div id="contact-lists-card-header" class="d-flex justify-content-between">
+      <h4 class="me-auto">{{ title }}</h4>
+      <span v-if="showSearchIcon">
+        <b-link href="#"
+                class="custom-link text-decoration-none btn-tag-edit d-flex align-items-center"
+                @click="onSearch">
+          <slot name="button">
+            <q-tooltip anchor="top middle"
+                       self="center middle">
+              Search current lists
+            </q-tooltip>
+            <search-icon class="search-icon" :color="iconColor" />
+          </slot>
+        </b-link>
+      </span>
+      <span v-else>
+        <q-input placeholder="Search List name"
+                 dense
+                 class="form-control form-control-search"
+                 v-model="searchQuery"
+                 borderless
+                 clearable
+                 data-testid="search-input">
+          <template v-slot:prepend>
+            <search-icon/>
+          </template>
+          <template v-if="searchQuery === ''" v-slot:append>
+           <q-icon name="cancel" size="14px" @click.stop.prevent="clearSearch" class="cursor-pointer"/>
+         </template>
+        </q-input>
+      </span>
     </div>
     <div id="contact-lists-card-body" class="my-3">
       <div class="d-flex align-items-start list-item"
            :key="list.id"
-           v-for="list in paginatedLists">
+           v-for="(list, index) in paginatedLists" >
         <list-icon class="mr-3 self-center"
-                   :key="'list_icon_' + list.id" />
+                   :key="'list_icon_' + list.id + index" />
         <div class="list-name pr-1">
-          <q-tooltip anchor="top middle"
-                     self="center middle">
-            {{ list.name }}
-          </q-tooltip>
-          {{ list.name }}
+            <q-tooltip anchor="top middle"
+                       self="center middle"
+            >
+              {{ list.name }}
+            </q-tooltip>
+          <span ref="listNamesRef" >{{ list.name }}</span>
         </div>
         <b-link href="#"
                 class="self-center trash-icon custom-link text-decoration-none btn-tag-edit d-flex align-items-center"
@@ -59,13 +78,22 @@
           </div>
         </div>
       </div>
-      <div class="contact-list-card-paginator">
-        <b-pagination
-          v-model="page"
-          :total-rows="lists.length"
-          :per-page="perPage"
-          aria-controls="contact-lists-card-body"
-        ></b-pagination>
+      <div class="contact-list-card-pagination">
+        <div class="paginated flex flex-center"
+             v-if="Math.ceil(lists.length / perPage) > 1">
+          <q-pagination class="table-pagination"
+                        data-testid="contacts-lists-pagination"
+                        padding="0 5px"
+                        direction-links
+                        dense
+                        :ellipses="false"
+                        :boundary-numbers="false"
+                        :max="Math.ceil(lists.length / perPage)"
+                        :max-pages="2"
+                        v-model="page">
+          </q-pagination>
+      </div>
+
       </div>
     </div>
 
@@ -109,10 +137,15 @@ export default {
     lists () {
       const isPublicFolder = this.$vnode.key === 'contact-public-lists-card'
 
-      const lists = this.contact.contact_lists.filter(list => list.show_in_public_folder === isPublicFolder)
+      let lists = this.contact.contact_lists.filter(list => list.show_in_public_folder === isPublicFolder)
+
       if (!this.isBillingAdminOrAdminOrSupervisor && this.isAgent) {
         // filter only lists that the agent has access to
-        return lists.filter(list => list.contact_folder_created_by === this.profile.id)
+        lists = lists.filter(list => list.contact_folder_created_by === this.profile.id)
+      }
+
+      if (this.searchQuery) {
+        lists = lists.filter(list => list.name.toLowerCase().includes(this.searchQuery.toLowerCase()))
       }
 
       return lists
@@ -131,6 +164,8 @@ export default {
       isSaving: false,
       prevValue: '',
       iconColor: '#256eff',
+      showSearchIcon: true,
+      searchQuery: '',
       page: 1,
       perPage: 5
     }
@@ -154,11 +189,30 @@ export default {
     },
 
     onSearch () {
+      this.showSearchIcon = false
+    },
+    searchFilter (filterName) {
+      this.searchQuery = filterName
+    },
+    clearSearch () {
+      this.searchQuery = ''
+      this.showSearchIcon = true
+    },
+    removeContactFromList (listId) {
 
     },
 
-    removeContactFromList (listId) {
+    isEllipsisActive (index) {
+      const test = this.$refs
+      console.log('isEllipsisActive', test, test.listNamesRef, test['listNamesRef'])
+      if (test.listNamesRef) {
+        const element = this.$refs.listNamesRef[index]
+        console.log('this.$refs.listNamesRef', element, element.offsetWidth < element.scrollWidth)
+        // return true
+        return element.offsetWidth < element.scrollWidth
+      }
 
+      return false
     }
   },
   watch: {
