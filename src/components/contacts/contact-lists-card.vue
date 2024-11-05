40ccb97b46f75dc0b641418bb3e1283dedca058c
Marcos Grimm <marcos@aloware.com>
2024-11-04 21:16:09 -0300

    feature(pow-329): pagination, search, ellipsis lenght. Wip

--- src/components/contacts/contact-lists-card.vue
+++ src/components/contacts/contact-lists-card.vue
@@ -1,115 +1,67 @@
 <template>
   <b-card class="border-0 contact-lists-card"
           data-testid="contact-lists-card">
-    <div id="contact-lists-card-header"
-         class="d-flex justify-content-between">
+    <div id="contact-lists-card-header" class="d-flex justify-content-between">
       <h4 class="me-auto">{{ title }}</h4>
-      <span>
+      <span v-if="showSearchIcon">
         <b-link href="#"
                 class="custom-link text-decoration-none btn-tag-edit d-flex align-items-center"
-                v-if="showSearchIcon"
                 @click="onSearch">
           <slot name="button">
             <q-tooltip anchor="top middle"
                        self="center middle">
               Search current lists
             </q-tooltip>
-            <search-icon class="search-icon"
-                         :color="iconColor" />
+            <search-icon class="search-icon" :color="iconColor" />
           </slot>
         </b-link>
-        <q-input class="form-control form-control-search"
-                 placeholder="Search List name"
+      </span>
+      <span v-else>
+        <q-input placeholder="Search List name"
                  dense
+                 class="form-control form-control-search"
+                 v-model="searchQuery"
                  borderless
                  clearable
-                 data-testid="search-input"
-                 ref="searchListNameInput"
-                 v-show="!showSearchIcon"
-                 v-model="searchQuery">
+                 data-testid="search-input">
           <template v-slot:prepend>
-            <search-icon />
+            <search-icon/>
           </template>
           <template v-if="searchQuery === ''" v-slot:append>
-           <q-icon class="cursor-pointer"
-                   name="cancel"
-                   size="14px"
-                   @click.stop.prevent="clearSearch" />
+           <q-icon name="cancel" size="14px" @click.stop.prevent="clearSearch" class="cursor-pointer"/>
          </template>
         </q-input>
       </span>
     </div>
-    <div id="contact-lists-card-body"
-         class="my-3">
-      <template v-for="(list, index) in paginatedLists">
-        <div class="d-flex align-items-start list-item"
-             :key="list.id">
-          <list-icon class="mr-3 self-center"
-                     :key="'list_icon_' + list.id + index"
-                     v-if="list.type === ContactListTypes.STATIC"/>
-          <folder-dynamic-icon class="mr-3 self-center"
-                               :key="'list_icon_' + list.id + index"
-                               v-else/>
-          <div class="list-name pr-1">
+    <div id="contact-lists-card-body" class="my-3">
+      <div class="d-flex align-items-start list-item"
+           :key="list.id"
+           v-for="(list, index) in paginatedLists" >
+        <list-icon class="mr-3 self-center"
+                   :key="'list_icon_' + list.id + index" />
+        <div class="list-name pr-1">
             <q-tooltip anchor="top middle"
-                       self="center middle"  v-if="list.name.length > 30 || (!list.show_in_public_folder && isBillingAdminOrAdminOrSupervisor)">
-              <template v-if="list.name.length > 30">
-                <strong>Name:</strong>{{ list.name }} <br/>
-              </template>
-              <template v-if="!list.show_in_public_folder && isBillingAdminOrAdminOrSupervisor">
-                <strong>Created by:</strong> {{ list.contact_folder_created_by_name }} <br/>
-              </template>
+                       self="center middle"
+            >
+              {{ list.name }}
             </q-tooltip>
-            <span ref="listNamesRef">{{ list.name }}</span>
-          </div>
-          <b-link class="self-center trash-icon custom-link text-decoration-none btn-tag-edit d-flex align-items-center"
-                  href="#"
-                  v-if="canEditList(list) && !isRemoving">
-            <slot name="button">
-              <span class='aloicons trash-icon'
-                    :style="'color:' + iconColor + '!important'"
-                    @click.prevent="removeContactFromList(list)">B
-              </span>
-            </slot>
-          </b-link>
-          <b-spinner class="self-center trash-icon custom-link text-decoration-none btn-tag-edit d-flex align-items-center"
-                     variant="warning"
-                     type="grow"
-                     label="Removing contact from list"
-                     small
-                     v-if="isRemoving && selectedList?.id === list.id">
-          </b-spinner>
+          <span ref="listNamesRef" >{{ list.name }}</span>
         </div>
-      </template>
-      <remove-contact-list-item-confirmation :dialogId="dialogId"
-                                             :contact="contact"
-                                             :list="selectedList"
-                                             v-if="contact && selectedList"
-                                             @deleting="onDeleting"
-                                             @deleted="onDeleted"
-                                             @finally="onFinally"/>
+        <b-link href="#"
+                class="self-center trash-icon custom-link text-decoration-none btn-tag-edit d-flex align-items-center"
+                @click="removeContactFromList(list.id)">
+          <slot name="button">
+            <span class='aloicons trash-icon' :style="'color:' + iconColor + '!important'">B</span>
+          </slot>
+        </b-link>
+      </div>
       <div v-if="lists.length === 0">
-        <div class="d-flex justify-content-center align-items-center mt-1"
-             style="height: 100%;">
+        <div class="d-flex justify-content-center align-items-center mt-1" style="height: 100%;">
           No available lists.
         </div>
       </div>
-    <div class="contact-lists-card-header">
-      <h4>{{ title }}</h4>
     </div>
-    <div class="contact-lists-card-body">
-      <div v-for="list in lists" :key="list.id" class="d-flex align-items-start">
-        <list-icon :key="'list_icon_' + list.id"/>
-        <div class="pl-6">{{ list.name }}</div>
-      </div>
-    </div>
-    <div class="notes mt-1"
-         style="min-height: 15px;"
-         data-testid="contact-update-lists"
-         @click="onUpdate">
-    </div>
-    <div class="contacts-list-card-footer">
-
+    <div id="contact-list-card-footer" class="d-flex justify-content-between">
       <div class="generic-multi-select">
         <div class="list-wrapper">
           <div class="w-100 mt-1">
@@ -140,58 +92,8 @@
                         :max-pages="2"
                         v-model="page">
           </q-pagination>
-        </div>
-
       </div>
-    </div>
-    <div id="contact-list-card-add-to-list"
-         class="d-flex justify-content-between">
-      <div class="w-100 mt-1"
-           v-if="showAvailableLists">
-        <q-select compact
-                  use-input
-                  input-debounce="0"
-                  behavior="menu"
-                  map-options
-                  emit-value
-                  multiple
-                  clearable
-                  option-value="id"
-                  option-label="name"
-                  style="width: 100%;"
-                  :options="addToListOptions"
-                  v-model="newSelectedListIds"
-                  @filter="filterNewLists">
-          <template v-slot:no-option>
-            <q-item>
-              <q-item-section class="text-grey">
-                No results
-              </q-item-section>
-            </q-item>
-          </template>
-        </q-select>
-        <div class="d-flex justify-content-between">
-          <b-button type="button"
-                    size="sm"
-                    variant="light"
-                    @click="cancelAddToList">
-            Cancel
-          </b-button>
-            <b-button type="button"
-                      size="sm"
-                      variant="primary"
-                      :disabled="isAdding"
-                      @click="addContactListItems">
-              <b-spinner class="pull-right self-center custom-link text-decoration-none"
-                         variant="warning"
-                         type="grow"
-                         label="Removing contact from list"
-                         small
-                         v-if="isAdding">
-            </b-spinner>
-            <span> Save </span>
-          </b-button>
-        </div>
+
       </div>
     </div>
 
@@ -203,20 +105,15 @@
 import PencilOIcon from 'components/icons/pencil-o-icon.vue'
 import ListIcon from 'components/icons/list-icon.vue'
 import SearchIcon from 'components/icons/search-icon.vue'
-import { aclMixin, contactLists } from 'src/plugins/mixins'
+
+import { aclMixin } from 'src/plugins/mixins'
 import { mapState } from 'vuex'
-import RemoveContactListItemConfirmation from 'components/remove-contact-list-item-confirmation.vue'
-import FolderDynamicIcon from 'components/icons/folder-dynamic-icon.vue'
-import * as ContactListTypes from 'src/constants/contacts-list-types'
 
 export default {
   name: 'contact-lists-card',
 
-  mixins: [aclMixin, contactLists],
-
+  mixins: [aclMixin],
   components: {
-    FolderDynamicIcon,
-    RemoveContactListItemConfirmation,
     PencilOIcon,
     ListIcon,
     SearchIcon
@@ -225,33 +122,6 @@ export default {
   props: {
     contact: {
       required: true
-    },
-    isPublicContactListCard: {
-      required: true,
-      type: Boolean
-    }
-  },
-
-  data () {
-    return {
-      isLoading: false,
-      isRemoving: false,
-      isAdding: false,
-      prevValue: '',
-      iconColor: '#256eff',
-      showSearchIcon: true,
-      searchQuery: '',
-      page: 1,
-      perPage: 5,
-      newSelectedListIds: null,
-      showAvailableLists: false,
-      availableLists: null,
-      selectedList: null,
-      contactLists: null,
-      loadedAllPublicLists: null,
-      loadedAllPrivateLists: null,
-      addToListOptions: null,
-      ContactListTypes
     }
   },
 
@@ -259,27 +129,17 @@ export default {
     ...mapState('auth', [
       'profile'
     ]),
+
     title () {
-      return this.isPublicContactListCard ? 'Public Lists' : 'Private Lists'
-    },
-
-    dialogId () {
-      if (!this.contact || !this.selectedList) {
-        return ''
-      }
-
-      return `remove-contact-list-item-confirmation-dialog_${this.contact.id}_${this.selectedList.id}`
+      return this.$vnode.key === 'contact-public-lists-card' ? 'Public Lists' : 'Private Lists'
     },
 
     lists () {
-      if (!this.contactLists) {
-        return []
-      }
+      const isPublicFolder = this.$vnode.key === 'contact-public-lists-card'
 
-      let lists = this.contactLists.filter(list => list.show_in_public_folder === this.isPublicContactListCard)
+      let lists = this.contact.contact_lists.filter(list => list.show_in_public_folder === isPublicFolder)
 
-      // agents can only view private lists owned by them and public lists
-      if (!this.isPublicContactListCard && !this.isBillingAdminOrAdminOrSupervisor && this.isAgent) {
+      if (!this.isBillingAdminOrAdminOrSupervisor && this.isAgent) {
         // filter only lists that the agent has access to
         lists = lists.filter(list => list.contact_folder_created_by === this.profile.id)
       }
@@ -288,29 +148,7 @@ export default {
         lists = lists.filter(list => list.name.toLowerCase().includes(this.searchQuery.toLowerCase()))
       }
 
-      // sort lists by contact_folder_created_by === this.profile.id first
-      lists.sort((a, b) => {
-        if (a.contact_folder_created_by === this.profile.id) {
-          return -1
-        }
-
-        if (b.contact_folder_created_by === this.profile.id) {
-          return 1
-        }
-
-        return 0
-      })
       return lists
-    },
-
-    computedAvailableLists () {
-      // remove deleted
-      const lists = this.isPublicContactListCard ? this.loadedAllPublicLists : this.loadedAllPrivateLists
-
-      if (!lists) {
-        return []
-      }
-      return lists.filter(list => !this.contactLists.some(contactList => contactList.id === list.id))
     },
 
     paginatedLists () {
@@ -320,120 +158,66 @@ export default {
     }
   },
 
-  beforeMount () {
-    this.contactLists = this.contact.contact_lists
-    this.loadAvailableLists()
+  data () {
+    return {
+      isLoading: false,
+      isSaving: false,
+      prevValue: '',
+      iconColor: '#256eff',
+      showSearchIcon: true,
+      searchQuery: '',
+      page: 1,
+      perPage: 5
+    }
+  },
+
+  mounted () {
+
   },
 
   methods: {
-    canEditList (list) {
-      // dynamic list
-      if (list.type === ContactListTypes.DYNAMIC) {
-        return false
-      }
+    onInput (value) {
 
-      if (this.isPublicContactListCard) {
-        return this.isBillingAdminOrAdminOrSupervisor
-      }
+    },
 
-      if (list.contact_folder_created_by === this.profile.id) {
-        return true
-      }
-    },
-    filterNewLists (val, update) {
-      if (val === '') {
-        update(() => {
-          this.addToListOptions = this.computedAvailableLists
-        })
-        return
-      }
+    onBlur () {
 
-      update(() => {
-        const needle = val.toLowerCase()
-        this.addToListOptions = this.computedAvailableLists.filter(list => list.name.toLowerCase().indexOf(needle) > -1)
-      })
     },
-    addContactListItems () {
-      this.isAdding = true
-      return this.$axios.post(`/api/v2/contact-list-items-bulk-lists`, {
-        contact_id: this.contact.id,
-        list_ids: this.newSelectedListIds
-      })
-        .then(() => {
-          this.$generalNotification('The Contact was added to the list successfully.')
-          this.addListToContactLists()
-        })
-        .catch((_err) => {
-          this.$generalNotification('Unable to add contact to list. Please try again.', 'error')
-        }).finally(() => {
-          this.isAdding = false
-        })
-    },
-    addListToContactLists () {
-      const addedLists = this.computedAvailableLists.filter(list => this.newSelectedListIds.includes(list.id))
-      for (let i = 0; i < addedLists.length; i++) {
-        this.contactLists.push(addedLists[i])
-      }
-      this.newSelectedListIds = null
-      this.showAvailableLists = false
-    },
-    addToList () {
-      this.showAvailableLists = true
-    },
-    cancelAddToList () {
-      this.showAvailableLists = false
+
+    onUpdate () {
+
     },
 
     onSearch () {
       this.showSearchIcon = false
-      // TODO: focus on input when showing it
-      // this.$refs.searchListNameInput.focus() did not work
-      // this.$refs.searchListNameInput.$el.lastChild.children[0].children[1].firstChild.focus() neither
+    },
+    searchFilter (filterName) {
+      this.searchQuery = filterName
     },
     clearSearch () {
       this.searchQuery = ''
       this.showSearchIcon = true
     },
-    removeContactFromList (list) {
-      // prevent multiple clicks
-      if (this.isRemoving) {
-        return
+    removeContactFromList (listId) {
+
+    },
+
+    isEllipsisActive (index) {
+      const test = this.$refs
+      console.log('isEllipsisActive', test, test.listNamesRef, test['listNamesRef'])
+      if (test.listNamesRef) {
+        const element = this.$refs.listNamesRef[index]
+        console.log('this.$refs.listNamesRef', element, element.offsetWidth < element.scrollWidth)
+        // return true
+        return element.offsetWidth < element.scrollWidth
       }
 
-      this.selectedList = list
+      return false
+    }
+  },
+  watch: {
+    'contact.id': function () {
 
-      // workaround to show modal after the list is selected.
-      // The modal was not being shown sometimes when the list was selected
-      // and the button had to be clicked twice to show the modal.
-      // Adding a small delay "fixes" the issue
-      setTimeout(() => {
-        this.$bvModal.show(this.dialogId)
-      }, 100)
-    },
-    onDeleting () {
-      this.isRemoving = true
-    },
-    onDeleted () {
-      // remove selected contact from list
-      this.contactLists = this.contactLists.filter(list => list.id !== this.selectedList.id)
-    },
-    onFinally () {
-      this.isRemoving = false
-      this.$bvModal.hide(this.dialogId)
-    },
-    async loadAvailableLists () {
-      let params = {
-        page: 1,
-        per_page: 99999,
-        list_type: ContactListTypes.STATIC
-      }
-      if (this.isPublicContactListCard) {
-        this.loadedAllPublicLists = await this.getPublicListsV2(params)
-      } else {
-        params.user_id = this.profile.id
-        params.private_only = true
-        this.loadedAllPrivateLists = await this.getListsV2(params)
-      }
     }
   }
 }
