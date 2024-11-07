11bc66262e281a4581845fe22fc247bedce6ec2f
Reza <reza@aloware.com>
2024-11-07 16:16:28 +0300

    fix(wat-914): fix issue in pd search

--- src/plugins/mixins/contacts.mixin.js
+++ src/plugins/mixins/contacts.mixin.js
@@ -230,9 +230,11 @@ export default {
     },
 
     onSearch (searchText) {
-      // ignore search if previous and current search are the same
-      if (this.previousSearch.trim() === searchText.trim()) {
-        return
+      // If both are non-null, compare trimmed values ignore search if previous and current search are the same
+      if (this.previousSearch !== null && searchText !== null) {
+        if (this.previousSearch.trim() === searchText.trim()) {
+          return
+        }
       }
 
       // requires at least 3 characters to allow the request or
