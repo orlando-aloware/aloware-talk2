e84f1237b42a2702b029dbce59e8bfaed9fd7ec7
Yhael S <yhael@aloware.com>
2024-11-06 18:35:28 -0600

    feature(PLA-407): added currently enrolled bot to UI

--- src/components/contacts/contact-aloai-enrollment-control.vue
+++ src/components/contacts/contact-aloai-enrollment-control.vue
@@ -6,6 +6,29 @@
       Enroll this contact to any of your Sales Bots and let them do the work for you!
     </b-card-text>
 
+    <b-card-text
+      v-if="isEnrolledToABot"
+      class="fs-14 mt-2"
+    >
+      Currently enrolled to:
+      <br />
+      <!-- Bot Use Case Label -->
+      <q-badge
+        :color="useCaseColor(firstEnrolledBot.use_case)"
+        class="mr-1"
+      >
+        <span>{{ formatUseCase(firstEnrolledBot.use_case) }}</span>
+      </q-badge>
+      <b>{{ firstEnrolledBot.name }}</b>
+      <br />
+      <q-badge
+        v-if="isEnrolledToMultipleBots"
+        color="blue-6"
+      >
+        <span>+ {{ extraEnrolledBotsCount }} bot(s)</span>
+      </q-badge>
+    </b-card-text>
+
     <div id="engage-control-popover">
       <b-button
         variant="outline-primary"
@@ -25,14 +48,18 @@
 </template>
 
 <script>
+import talk2Api from 'src/plugins/api/api'
 import AloaiIcon from 'components/icons/aloai-icon'
 import AloaiEnrollmentControlModal from 'components/aloai-enrollment-control-modal.vue'
 import { mapGetters } from 'vuex'
+import { aloaiMixin } from 'src/plugins/mixins'
 
 export default {
   name: 'contact-aloai-enrollment-control',
 
   components: { AloaiEnrollmentControlModal, AloaiIcon },
+
+  mixins: [aloaiMixin],
 
   props: {
     contact: {
@@ -41,14 +68,77 @@ export default {
     }
   },
 
+  data () {
+    return {
+      bots: [],
+      botEnrollments: []
+    }
+  },
+
   computed: {
-    ...mapGetters('auth', ['profile'])
+    ...mapGetters('auth', ['profile']),
+    isEnrolledToABot () {
+      return this.botEnrollments.length > 0
+    },
+    isEnrolledToMultipleBots () {
+      return this.botEnrollments.length > 1
+    },
+    extraEnrolledBotsCount () {
+      return this.botEnrollments.length - 1
+    },
+    firstEnrolledBot () {
+      if (!this.isEnrolledToABot) {
+        return null
+      }
+
+      let bot = this.bots.find((bot) => bot.id === this.botEnrollments[0].aloai_bot_id)
+
+      console.log('firstEnrolledBot', bot)
+
+      // Get the first enrolled bot with the id coming from the enrollments
+      return this.bots.find((bot) => bot.id === this.botEnrollments[0].aloai_bot_id)
+    }
+  },
+
+  mounted () {
+    this.fetchBots()
+      .then((bots) => {
+        this.bots = bots
+      })
+
+    this.fetchContactEnrolledBots()
+      .then((botEnrollments) => {
+        this.botEnrollments = botEnrollments
+      })
   },
 
   methods: {
     openEnrollmententControlModal () {
       if (this.$refs.aloaiEnrollmentControlModalRef) {
         this.$refs.aloaiEnrollmentControlModalRef.isOpen = true
+      }
+    },
+    async fetchBots () {
+      try {
+        if (this.bots.length > 0) {
+          return this.bots
+        }
+        const { data } = await talk2Api.V2.aloAiBot.getBots()
+        return data?.data ?? []
+      } catch (error) {
+        console.error('[fetchBots] error', error)
+        return []
+      }
+    },
+    async fetchContactEnrolledBots () {
+      try {
+        const { data } = await talk2Api.V2.aloAiBot.getContactEnrolledBots(
+          this.contact.id
+        )
+        return data
+      } catch (error) {
+        console.error('[fetchContactEnrolledBots] error', error)
+        return []
       }
     }
   }
