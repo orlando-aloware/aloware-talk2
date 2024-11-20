3099b6c65d0f2d162459830d56597680a8bbd6cf
Marcos Grimm <marcos@aloware.com>
2024-11-20 15:47:23 -0300

    fix(pow-403): fixed comment

--- src/components/bulk-action-menu.vue
+++ src/components/bulk-action-menu.vue
@@ -332,7 +332,7 @@ export default {
 
       // If user have been granted Delete permission, which is higher than Removing from list permission
       if (this.hasDeletePermission) {
-        // if is SimpSocial and user is just agent, dont show, event with specific permission
+        // if is SimpSocial and user is just agent don't show, even when user has specific permission to delete contacts
         if (this.isSimpSocial && this.isAgent && !this.isBillingAdminOrAdminOrSupervisor) {
           return false
         }
