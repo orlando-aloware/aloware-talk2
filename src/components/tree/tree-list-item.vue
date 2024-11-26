aa0fad75044dd23a47e286ecadd1d2d3a8c2b391
Tito Alvarez <tito@aloware.com>
2024-11-26 10:40:48 -0600

    Revert "Merge pull request #1614 from aloware/POW-296/feature/convert-list-to-public-contacts-page"
    This reverts commit 9a2f43365eca766a8176cdfc2a7eda69dbf4a3af, reversing
    changes made to dbc45320ccc395072b5fb1d95bcff3c946fb4e59.

--- src/components/tree/tree-list-item.vue
+++ src/components/tree/tree-list-item.vue
@@ -77,7 +77,6 @@
                         :contacts-count="contactsCount"
                         :has-edit="hasEdit"
                         :has-delete="hasDelete"
-                        :has-show-in-public-folder-permission="hasShowInPublicFolderPermission"
                         :is-pinned="isPinned"
                         @remove="onRemoveList"
                         @rename="onRenameList"
@@ -85,7 +84,6 @@
                         @move="onMove"
                         @duplicate="onDuplicate"
                         @split="onSplit"
-                        @showInPublicFolder="onShowInPublicFolder"
                         @clonestatic="onCloneStatic"/>
         </b-popover>
       </div>
@@ -144,9 +142,6 @@ export default {
     },
     hasDelete: {
       type: Number
-    },
-    hasShowInPublicFolderPermission: {
-      type: Boolean
     },
     showInPublicFolder: {
       type: Boolean,
@@ -282,22 +277,6 @@ export default {
         id: this.id,
         type: ContactListTypes.STATIC
       })
-    },
-    onShowInPublicFolder () {
-      this.$root.$emit('bv::hide::popover')
-      const params = { show_in_public_folder: true }
-      const id = this.id
-
-      this.updateContactList(id, params)
-        .then((response) => {
-          this.$generalNotification('Contact list has been successfully converted to public')
-          this.reloadFolders()
-          this.loadPublicLists()
-        })
-        .catch((error) => {
-          const { message } = extractErrorMessage(error)
-          this.$generalNotification(message, 'error')
-        })
     },
     createList (params) {
       this.$axios
--- src/plugins/mixins/contact-lists.mixin.js
+++ src/plugins/mixins/contact-lists.mixin.js
@@ -134,10 +134,6 @@ export default {
       })
     },
 
-    async updateContactList (id, params) {
-      await API.V2.contactList.update(id, params)
-    },
-
     loadPublicLists () {
       this.isLoading = true
       this.setPublicListsLoaded(false)
