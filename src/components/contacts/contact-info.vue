71e29359c21fabce3337d04335752f902c326d3a
paulo-aloware <paulo@aloware.com>
2024-12-11 15:52:31 -0400

    add log

--- src/components/contacts/contact-info.vue
+++ src/components/contacts/contact-info.vue
@@ -485,6 +485,11 @@ export default {
         return
       }
 
+      console.log('this.shouldShowLineSelector', this.shouldShowLineSelector)
+      console.log('this.this.profile.outbound_calling_mode', this.profile.outbound_calling_mode)
+      console.log('this.defaultOutboundLine', this.defaultOutboundLine)
+      console.log('this.this.currentCompany.default_outbound_campaign_id', this.currentCompany.default_outbound_campaign_id)
+
       if (this.shouldShowLineSelector && this.defaultOutboundLine) {
         data.outboundCampaignId = this.defaultOutboundLine
         this.$VueEvent.fire('makeCall', data)
