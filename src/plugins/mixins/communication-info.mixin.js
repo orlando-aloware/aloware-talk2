23d2c2866449ff28358058276316192e615e5631
Fernando Ortuno <fernando@aloware.com>
2024-12-13 18:58:24 -0400

    fix(pla-411): changed message

--- src/plugins/mixins/communication-info.mixin.js
+++ src/plugins/mixins/communication-info.mixin.js
@@ -270,7 +270,7 @@ export default {
         case CommunicationRejectionReasons.REJECTION_REASON_PROVIDER_ALLOWS_ONLY_ONE_ATTACHMENT:
           return 'Provider allows only one attachment.'
         case CommunicationRejectionReasons.REJECTION_REASON_NOT_MULTIMEDIA_MESSAGING_ENABLED:
-          return 'You cannot send an MMS from a non-MMS-capable number.'
+          return 'The selected number cannot send an MMS. Use an MMS-capable number or remove media attachments and try again.'
       }
     },
 
