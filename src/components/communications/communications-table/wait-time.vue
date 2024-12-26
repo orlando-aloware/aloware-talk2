4dc3b96a6f924696f3a28cc2a254011382b23839
Jonathan M <jonathan@aloware.com>
2024-12-26 14:03:42 -0300

    feature(wat-970): improved wait time column

--- src/components/communications/communications-table/wait-time.vue
+++ src/components/communications/communications-table/wait-time.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="row">
-    <div class="col-12 d-flex align-items-center justify-content-left"
-         data-testid="wait-time-div">
-      <span v-if="row.type === CommunicationTypes.CALL">{{ row.wait_time | fixDuration }}</span>
-      <span v-else>
-        -
-      </span>
-    </div>
+  <div class="d-flex align-items-center justify-content-left"
+       data-testid="wait-time-row">
+    <span v-if="row.type === CommunicationTypes.CALL">
+      {{ row.wait_time | fixDuration }}
+    </span>
+    <span v-else>
+      -
+    </span>
   </div>
 </template>
 
@@ -15,6 +15,7 @@ import * as CommunicationTypes from 'src/constants/communication-types'
 
 export default {
   name: 'WaitTime',
+
   props: {
     row: {
       type: Object,
