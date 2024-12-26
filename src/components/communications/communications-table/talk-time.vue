9d79680b8f712d16fe45a4a07050ab311c0de710
Jonathan M <jonathan@aloware.com>
2024-12-26 14:01:07 -0300

    feature(wat-970): improved talk time column

--- src/components/communications/communications-table/talk-time.vue
+++ src/components/communications/communications-table/talk-time.vue
@@ -1,33 +1,31 @@
 <template>
-  <div>
-    <div class="row"
-         data-testid="talk-time-row"
+  <div class="d-flex flex-column"
+       data-testid="talk-time-row">
+    <div class="d-flex align-items-center justify-content-left"
          v-if="row.type === CommunicationTypes.CALL">
-      <div class="col-12 d-flex align-items-center justify-content-left">
-        <span
-          v-if="row.disposition_status2 !== CommunicationDispositionStatus.DISPOSITION_STATUS_INPROGRESS_NEW && row.current_status2 === CommunicationCurrentStatus.CURRENT_STATUS_COMPLETED_NEW">
-          {{ row.talk_time | fixDuration }}
-        </span>
-        <relative-time class="text-muted"
-                       data-testid="comm-log-relative-time"
-                       :from_time="row.created_at"
-                       v-else-if="row.disposition_status2 === CommunicationDispositionStatus.DISPOSITION_STATUS_INPROGRESS_NEW && row.current_status2 === CommunicationCurrentStatus.CURRENT_STATUS_INPROGRESS_NEW" />
-      </div>
+      <span v-if="row.disposition_status2 !== CommunicationDispositionStatus.DISPOSITION_STATUS_INPROGRESS_NEW && row.current_status2 === CommunicationCurrentStatus.CURRENT_STATUS_COMPLETED_NEW">
+        {{ row.talk_time | fixDuration }}
+      </span>
+      <relative-time class="text-muted"
+                     data-testid="comm-log-relative-time"
+                     :from-time="row.created_at"
+                     v-else-if="row.disposition_status2 === CommunicationDispositionStatus.DISPOSITION_STATUS_INPROGRESS_NEW && row.current_status2 === CommunicationCurrentStatus.CURRENT_STATUS_INPROGRESS_NEW" />
     </div>
-    <div class="row">
-      <div class="col-12 d-flex align-items-center justify-content-left">
-        <span v-if="row.direction === CommunicationDirection.INBOUND
-          && row.disposition_status2 === CommunicationDispositionStatus.DISPOSITION_STATUS_ABANDONED_NEW
-          && [CallbackStatus.CALLBACK_STATUS_INITIATED, CallbackStatus.CALLBACK_STATUS_REQUESTED].includes(row.callback_status)"
-        >
-          Callback {{ getVisibleCallbackStatus(row) }}
-        </span>
-        <span v-else>{{ getVisibleStatus(row) }}</span>
-      </div>
+    <div class="d-flex align-items-center justify-content-left">
+      <span v-if="row.direction === CommunicationDirection.INBOUND
+        && row.disposition_status2 === CommunicationDispositionStatus.DISPOSITION_STATUS_ABANDONED_NEW
+        && [CallbackStatus.CALLBACK_STATUS_INITIATED, CallbackStatus.CALLBACK_STATUS_REQUESTED].includes(row.callback_status)">
+        Callback {{ getVisibleCallbackStatus(row) }}
+      </span>
+      <span v-else>
+        {{ getVisibleStatus(row) }}
+      </span>
     </div>
   </div>
 </template>
+
 <script>
+import RelativeTime from 'components/relative-time.vue'
 import * as CommunicationTypes from 'src/constants/communication-types'
 import * as CommunicationDispositionStatus from 'src/constants/communication-disposition-status'
 import * as CommunicationCurrentStatus from 'src/constants/communication-current-status'
@@ -36,12 +34,18 @@ import * as CallbackStatus from 'src/constants/callback-status'
 
 export default {
   name: 'TalkTime',
+
+  components: {
+    RelativeTime
+  },
+
   props: {
     row: {
       type: Object,
       required: true
     }
   },
+
   data () {
     return {
       CommunicationTypes,
@@ -51,6 +55,7 @@ export default {
       CallbackStatus
     }
   },
+
   methods: {
     getVisibleStatus (communication) {
       if (communication.current_status2 !== CommunicationCurrentStatus.CURRENT_STATUS_COMPLETED_NEW) {
