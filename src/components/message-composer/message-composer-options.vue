3fbe5c2093a56e58de0325d6e0cff40f6ed621bb
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2024-12-30 09:52:47 -0800

    Fix
    Signed-off-by: Sohrab Sheikhani <sheikhani.sohrab@gmail.com>

--- src/components/message-composer/message-composer-options.vue
+++ src/components/message-composer/message-composer-options.vue
@@ -98,7 +98,7 @@
       </q-tooltip>
     </b-link>
 
-    <b-link v-if="messageComposer.mode === 'sms' && isDemoCompany && contact"
+    <b-link v-if="messageComposer.mode === 'sms' && isDemoCompany && contact?.id"
             href="#"
             data-testid="add-suggested-text-messages-button"
             :disabled="isTextingDisabled">
