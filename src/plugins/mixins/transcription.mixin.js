cc6acaa74e1d6a30a3a704660a715449f348288a
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2024-12-30 10:20:55 -0800

    Fixed transcription issues
    Signed-off-by: Sohrab Sheikhani <sheikhani.sohrab@gmail.com>

--- src/plugins/mixins/transcription.mixin.js
+++ src/plugins/mixins/transcription.mixin.js
@@ -24,7 +24,7 @@ export default {
       return communication.is_eligible_for_transcribe &&
              this.showAudio(communication) &&
              !communication.metadata?.transcription_info &&
-             ![TranscriptionStatus.STATUS_PROCESSING, TranscriptionStatus.STATUS_COMPLETED, TranscriptionStatus.STATUS_PARSED].includes(communication.call_transcription_status)
+             ![TranscriptionStatus.STATUS_CREATED, TranscriptionStatus.STATUS_PROCESSING, TranscriptionStatus.STATUS_COMPLETED, TranscriptionStatus.STATUS_PARSED].includes(communication.call_transcription_status)
     },
 
     // check if the summarization is allowed for the communication
