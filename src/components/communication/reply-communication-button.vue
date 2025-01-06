097cc51a8f4b82a9d21485f5861d48333b29941b
Jonathan M <jonathan@aloware.com>
2025-01-06 11:04:59 -0300

    feature(wat-970): reply operation button added

---
+++ src/components/communication/reply-communication-button.vue
@@ -0,0 +1,55 @@
+<template>
+  <span class="cursor-pointer"
+        :class="customClass"
+        data-testid="comm-reply-button"
+        v-if="communication.type === CommunicationTypes.SMS && hasPermissionTo('send sms')"
+        @click="goToContactPage">
+    <reply-icon height="16"
+                width="16"
+                icon-color="grey"/>
+
+    <q-tooltip>
+      Reply
+    </q-tooltip>
+  </span>
+</template>
+
+<script>
+import ReplyIcon from 'components/icons/reply-icon.vue'
+import { aclMixin } from 'src/plugins/mixins'
+import * as CommunicationTypes from 'src/constants/communication-types'
+
+export default {
+  name: 'reply-communication-button',
+
+  mixins: [
+    aclMixin
+  ],
+
+  components: {
+    ReplyIcon
+  },
+
+  props: {
+    communication: {
+      type: Object,
+      required: true
+    },
+
+    customClass: {
+      type: String,
+      default: ''
+    }
+  },
+
+  data: () => ({
+    CommunicationTypes
+  }),
+
+  methods: {
+    goToContactPage () {
+      window.open(`/contacts/${this.communication.contact_id}`, '_blank')
+    }
+  }
+}
+</script>
