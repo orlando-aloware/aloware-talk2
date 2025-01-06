3d76cdd76c3346c66157b1a65e61489f12469a1e
Jonathan M <jonathan@aloware.com>
2025-01-06 11:04:37 -0300

    feature(wat-970): setup base for operation buttons

--- src/components/communications/communications-table/communications-operations.vue
+++ src/components/communications/communications-table/communications-operations.vue
@@ -1,29 +1,35 @@
 <template>
   <div class="d-flex justify-content-center context-menu">
-    <b-dropdown no-caret
-                size="sm"
-                container="body"
-                center>
-      <template #button-content>
-        <ellipse-icon />
-      </template>
-      <b-dropdown-item dense
-                       clickable>
-        Edit
-      </b-dropdown-item>
-    </b-dropdown>
+    <reply-communication-button custom-class="operation-button mx-1"
+                                :communication="row" />
+
+    <call-communication-button custom-class="operation-button mx-1"
+                               :communication="row" />
+
+<!--    <operation-info :communication="row" />-->
+
+<!--    <operation-archive :communication="row" />-->
+
+<!--    <operation-terminate :communication="row" />-->
+
+<!--    <operation-barge :communication="row" />-->
+
+<!--    <operation-whisper :communication="row" />-->
   </div>
 </template>
 
 <script>
-import EllipseIcon from 'components/icons/ellipse-icon'
+import ReplyCommunicationButton from 'components/communication/reply-communication-button.vue'
+import CallCommunicationButton from 'components/communication/call-communication-button.vue'
 
 export default {
   name: 'CommunicationsOperations',
-  components: {
-    EllipseIcon
 
+  components: {
+    ReplyCommunicationButton,
+    CallCommunicationButton
   },
+
   props: {
     row: {
       type: Object,
