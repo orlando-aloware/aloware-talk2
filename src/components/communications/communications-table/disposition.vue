5dc4fa0fe5f5624b06222353f7072ab5f0e703c1
Jonathan M <jonathan@aloware.com>
2025-01-07 15:46:11 -0300

    feature(wat-970): fixed disposition column icon and tooltip

--- src/components/communications/communications-table/disposition.vue
+++ src/components/communications/communications-table/disposition.vue
@@ -4,28 +4,27 @@
                  class="text-decoration-none"
                  :to="{ path: `/contacts/${row.contact_id}/communications/${row.id}` }"
                  v-if="row.id && row.contact_id">
-      <div class="status-icon centered-content"
-           v-html="stateToIcon(row.disposition_status2, row.direction, row.type, row.callback_status)" />
+      <component :is="stateToIcon(row.disposition_status2, row.type, row.direction, row.callback_status)"
+                 v-if="row.disposition_status2" />
 
       <q-tooltip>
-        Click to see the communication details
+        {{ dispositionTooltipData(row.disposition_status2, row.type, row.direction, row.callback_status) }}
       </q-tooltip>
     </router-link>
 
-    <div class="status-icon centered-content"
-         v-else
-         v-html="stateToIcon(row.disposition_status2, row.direction, row.type, row.callback_status)" />
+    <component :is="stateToIcon(row.disposition_status2, row.type, row.direction, row.callback_status)"
+               v-else />
   </div>
 </template>
 
 <script>
-import { communicationsMixin } from 'src/plugins/mixins'
+import { communicationInfoMixin } from 'src/plugins/mixins'
 
 export default {
   name: 'Disposition',
 
   mixins: [
-    communicationsMixin
+    communicationInfoMixin
   ],
 
   props: {
