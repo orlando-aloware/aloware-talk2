ef70fb3b86baf3d8950b9ab33da2fda5c7b9833e
Tito Alvarez <tito@aloware.com>
2025-01-08 14:03:07 -0600

    feature: adding both hs popup and hs view

--- src/components/power-dialer/sessions/pages/session-contact-page-crm.vue
+++ src/components/power-dialer/sessions/pages/session-contact-page-crm.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="row full-height">
     <div class="col-12 p-0">
-      <div class="hubspot-iframe-container">
+      <div class="hubspot-iframe-container" v-if="usePopup && hubspotLink">
         The CRM will display in a popup. <br />
         <a href="#" @click.prevent="openHubspotLink">Click here to manually open the CRM.</a>
+      </div>
+      <div class="hubspot-iframe-container" v-if="!usePopup && hubspotLink">
+        <iframe class="hubspot-crm-iframe"
+                :src="hubspotLink"
+                frameborder="0"
+                id="hubspot-crm">
+        </iframe>
       </div>
     </div>
   </div>
@@ -18,8 +25,15 @@ import { isEmpty } from 'lodash'
 export default {
   name: 'SessionContactPageCrm',
 
+  props: {
+    usePopup: {
+      type: Boolean,
+      default: true
+    }
+  },
+
   mounted () {
-    if (!isEmpty(this.hubspotLink)) {
+    if (this.usePopup && !isEmpty(this.hubspotLink)) {
       this.openHubspotLink()
     }
   },
@@ -33,7 +47,7 @@ export default {
     ...mapState('contacts', ['contact']),
 
     hubspotLink () {
-      return this.getHubspotContactLink(this.contact)
+      return this.getHubspotContactLink(this.contact, this.usePopup)
     },
 
     test () {
@@ -49,7 +63,10 @@ export default {
     ]),
 
     openHubspotLink () {
-      // Open it in a window.open popup.  Name the ppup so we can close it later and prevent multiple popups.
+      if (isEmpty(this.hubspotLink)) {
+        return
+      }
+
       window.open(this.hubspotLink, 'hubspot-crm', 'width=1200,height=800')
     },
 
@@ -129,14 +146,17 @@ export default {
     contact: {
       deep: true,
       handler (newValue, oldValue) {
-        const oldHubspotLink = this.getHubspotContactLink(oldValue)
-        const newHubspotLink = this.getHubspotContactLink(newValue)
+        const oldHubspotLink = this.getHubspotContactLink(oldValue, this.usePopup)
+        const newHubspotLink = this.getHubspotContactLink(newValue, this.usePopup)
 
         // if it's the same contact (contact was immediately redialed)
         // but hubspot link is gone, reuse the old contact.
         if (oldValue?.id === newValue?.id && isEmpty(newHubspotLink) && !isEmpty(oldHubspotLink)) {
           this.setContact(oldValue)
           this.setContactClone(oldValue)
+        }
+
+        if (this.usePopup) {
           this.openHubspotLink()
         }
       }
