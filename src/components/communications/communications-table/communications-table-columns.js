b7fd95b2268b0ae2c3eac4a159e3e63b0dba2187
Jonathan M <jonathan@aloware.com>
2025-01-13 14:00:27 -0300

    feature(wat-1146): moved columns to a single file

---
+++ src/components/communications/communications-table/communications-table-columns.js
@@ -0,0 +1,241 @@
+export const ALL_COLUMNS = [
+  {
+    label: '',
+    name: 'disposition_status2',
+    align: 'center',
+    headerStyle: 'min-width: 105px'
+  },
+  {
+    label: 'Number',
+    name: 'incoming_number',
+    align: 'left',
+    headerStyle: 'min-width: 150px'
+  },
+  {
+    label: 'Team',
+    name: 'teams',
+    align: 'left',
+    headerStyle: 'min-width: 180px'
+  },
+  {
+    label: 'Ring Group',
+    name: 'ring_group',
+    align: 'left',
+    headerStyle: 'min-width: 100px'
+  },
+  {
+    label: 'Sequence',
+    name: 'workflow',
+    align: 'left',
+    headerStyle: 'min-width: 400px'
+  },
+  {
+    label: 'Broadcast',
+    name: 'broadcast',
+    align: 'left',
+    headerStyle: 'min-width: 400px'
+  },
+  {
+    label: 'Start Time',
+    name: 'created_at',
+    align: 'left',
+    headerStyle: 'min-width: 100px'
+  },
+  {
+    label: 'Talk Time',
+    name: 'talk_time',
+    align: 'left',
+    headerStyle: 'min-width: 100px'
+  },
+  {
+    label: 'Wait Time',
+    name: 'wait_time',
+    align: 'left',
+    headerStyle: 'min-width: 100px'
+  },
+  {
+    label: 'Hold Time',
+    name: 'hold_time',
+    align: 'left',
+    headerStyle: 'min-width: 100px'
+  },
+  {
+    label: 'Duration',
+    name: 'duration',
+    align: 'left',
+    headerStyle: 'min-width: 100px'
+  },
+  {
+    label: 'Resolution',
+    name: 'resolution2',
+    align: 'left',
+    headerStyle: 'min-width: 100px'
+  },
+  {
+    label: 'Contact',
+    name: 'contact',
+    align: 'left',
+    headerStyle: 'min-width: 110px'
+  },
+  {
+    label: 'Location',
+    name: 'lead_location',
+    align: 'left',
+    headerStyle: 'min-width: 100px'
+  },
+  {
+    label: 'Lines',
+    name: 'line',
+    align: 'left',
+    headerStyle: 'min-width: 100px'
+  },
+  {
+    label: 'User',
+    name: 'user_id',
+    align: 'left',
+    headerStyle: 'min-width: 200px'
+  },
+  {
+    label: 'Attempting',
+    name: 'attempting_users',
+    align: 'left',
+    headerStyle: 'min-width: 250px'
+  },
+  {
+    label: 'Transferred From',
+    name: 'transfer_prior_user_ids',
+    align: 'left',
+    headerStyle: 'min-width: 200px'
+  },
+  {
+    label: 'Transferred To',
+    name: 'transfer_target_user_ids',
+    align: 'left',
+    headerStyle: 'min-width: 200px'
+  },
+  {
+    label: 'Cold Transferred?',
+    name: 'in_cold_transfer',
+    align: 'left',
+    headerStyle: 'min-width: 100px'
+  },
+  {
+    label: 'Transfer Type',
+    name: 'transfer_type',
+    align: 'left',
+    headerStyle: 'min-width: 100px'
+  },
+  {
+    label: 'Callback Status',
+    name: 'callback_status',
+    align: 'left',
+    headerStyle: 'min-width: 100px'
+  },
+  {
+    label: 'Queue Resolution',
+    name: 'queue_resolution2',
+    align: 'left',
+    headerStyle: 'min-width: 100px'
+  },
+  {
+    label: 'Email',
+    name: 'email',
+    align: 'left',
+    headerStyle: 'min-width: 100px'
+  },
+  {
+    label: 'Creator Type',
+    name: 'creator_type',
+    align: 'left',
+    headerStyle: 'min-width: 100px'
+  },
+  {
+    label: 'Tags',
+    name: 'tags',
+    align: 'left',
+    headerStyle: 'min-width: 100px'
+  },
+  {
+    label: 'Notes',
+    name: 'notes',
+    align: 'left',
+    headerStyle: 'min-width: 200px'
+  },
+  {
+    label: 'CSAT Score',
+    name: 'csat_score',
+    align: 'left',
+    headerStyle: 'min-width: 100px'
+  },
+  {
+    label: 'Operations',
+    name: 'operations',
+    align: 'center',
+    headerStyle: 'min-width: 100px'
+  }
+]
+
+export const DEFAULT_COLUMNS = [
+  {
+    name: 'disposition_status2',
+    field: 'disposition_status2',
+    label: '',
+    align: 'center',
+    headerStyle: 'min-width: 105px'
+  },
+  {
+    label: 'Number',
+    name: 'incoming_number',
+    field: 'incoming_number',
+    align: 'left',
+    headerStyle: 'min-width: 100px'
+  },
+  {
+    label: 'Ring Group',
+    name: 'ring_group',
+    field: 'ring_group',
+    align: 'left',
+    headerStyle: 'min-width: 100px'
+  },
+  {
+    label: 'Start Time',
+    name: 'created_at',
+    field: 'created_at',
+    align: 'left',
+    headerStyle: 'min-width: 100px'
+  },
+  {
+    label: 'Talk Time',
+    name: 'talk_time',
+    field: 'talk_time',
+    align: 'left',
+    headerStyle: 'min-width: 100px'
+  },
+  {
+    label: 'Duration',
+    name: 'duration',
+    field: 'duration',
+    align: 'left',
+    headerStyle: 'min-width: 100px'
+  },
+  {
+    label: 'Contact',
+    name: 'contact',
+    field: 'lead_number',
+    align: 'left',
+    headerStyle: 'min-width: 200px'
+  },
+  {
+    label: 'User',
+    name: 'user_id',
+    field: 'user_id',
+    align: 'left',
+    headerStyle: 'min-width: 200px'
+  },
+  {
+    label: 'Operations',
+    name: 'operations',
+    align: 'center',
+    headerStyle: 'min-width: 100px'
+  }
+]
