cd03691073fccff69054c80ad90ca590c9b20713
Jonathan M <jonathan@aloware.com>
2025-01-14 09:18:49 -0300

    fix(wat-1146): updated the size of several columns

--- src/components/communications/communications-table/communications-table-columns.js
+++ src/components/communications/communications-table/communications-table-columns.js
@@ -9,157 +9,183 @@ export const ALL_COLUMNS = [
     label: 'Number',
     name: 'incoming_number',
     align: 'left',
-    headerStyle: 'min-width: 150px'
+    headerStyle: 'min-width: 150px',
+    columnStyle: 'width: 150px'
   },
   {
     label: 'Team',
     name: 'teams',
     align: 'left',
-    headerStyle: 'min-width: 180px'
+    headerStyle: 'min-width: 180px',
+    columnStyle: 'width: 180px'
   },
   {
     label: 'Ring Group',
     name: 'ring_group',
     align: 'left',
-    headerStyle: 'min-width: 100px'
+    headerStyle: 'min-width: 200px',
+    columnStyle: 'width: 200px'
   },
   {
     label: 'Sequence',
     name: 'workflow',
     align: 'left',
-    headerStyle: 'min-width: 400px'
+    headerStyle: 'min-width: 250px',
+    columnStyle: 'width: 250px'
   },
   {
     label: 'Broadcast',
     name: 'broadcast',
     align: 'left',
-    headerStyle: 'min-width: 400px'
+    headerStyle: 'min-width: 250px',
+    columnStyle: 'width: 250px'
   },
   {
     label: 'Start Time',
     name: 'created_at',
     align: 'left',
-    headerStyle: 'min-width: 100px'
+    headerStyle: 'min-width: 100px',
+    columnStyle: 'width: 100px'
   },
   {
     label: 'Talk Time',
     name: 'talk_time',
     align: 'left',
-    headerStyle: 'min-width: 100px'
+    headerStyle: 'min-width: 100px',
+    columnStyle: 'width: 100px'
   },
   {
     label: 'Wait Time',
     name: 'wait_time',
     align: 'left',
-    headerStyle: 'min-width: 100px'
+    headerStyle: 'min-width: 100px',
+    columnStyle: 'width: 100px'
   },
   {
     label: 'Hold Time',
     name: 'hold_time',
     align: 'left',
-    headerStyle: 'min-width: 100px'
+    headerStyle: 'min-width: 100px',
+    columnStyle: 'width: 100px'
   },
   {
     label: 'Duration',
     name: 'duration',
     align: 'left',
-    headerStyle: 'min-width: 100px'
+    headerStyle: 'min-width: 100px',
+    columnStyle: 'width: 100px'
   },
   {
     label: 'Resolution',
     name: 'resolution2',
     align: 'left',
-    headerStyle: 'min-width: 100px'
+    headerStyle: 'min-width: 100px',
+    columnStyle: 'width: 100px'
   },
   {
     label: 'Contact',
     name: 'contact',
     align: 'left',
-    headerStyle: 'min-width: 200px'
+    headerStyle: 'min-width: 250px',
+    columnStyle: 'width: 250px'
   },
   {
     label: 'Location',
     name: 'lead_location',
     align: 'left',
-    headerStyle: 'min-width: 150px'
+    headerStyle: 'min-width: 150px',
+    columnStyle: 'width: 150px'
   },
   {
     label: 'Lines',
     name: 'line',
     align: 'left',
-    headerStyle: 'min-width: 150px'
+    headerStyle: 'min-width: 150px',
+    columnStyle: 'width: 150px'
   },
   {
     label: 'User',
     name: 'user_id',
     align: 'left',
-    headerStyle: 'min-width: 200px'
+    headerStyle: 'min-width: 250px',
+    columnStyle: 'width: 250px'
   },
   {
     label: 'Attempting',
     name: 'attempting_users',
     align: 'left',
-    headerStyle: 'min-width: 250px'
+    headerStyle: 'min-width: 250px',
+    columnStyle: 'width: 250px'
   },
   {
     label: 'Transferred From',
     name: 'transfer_prior_user_ids',
     align: 'left',
-    headerStyle: 'min-width: 200px'
+    headerStyle: 'min-width: 200px',
+    columnStyle: 'width: 200px'
   },
   {
     label: 'Transferred To',
     name: 'transfer_target_user_ids',
     align: 'left',
-    headerStyle: 'min-width: 200px'
+    headerStyle: 'min-width: 200px',
+    columnStyle: 'width: 200px'
   },
   {
     label: 'Cold Transferred?',
     name: 'in_cold_transfer',
     align: 'left',
-    headerStyle: 'min-width: 100px'
+    headerStyle: 'min-width: 100px',
+    columnStyle: 'width: 100px'
   },
   {
     label: 'Transfer Type',
     name: 'transfer_type',
     align: 'left',
-    headerStyle: 'min-width: 100px'
+    headerStyle: 'min-width: 100px',
+    columnStyle: 'width: 100px'
   },
   {
     label: 'Callback Status',
     name: 'callback_status',
     align: 'left',
-    headerStyle: 'min-width: 100px'
+    headerStyle: 'min-width: 100px',
+    columnStyle: 'width: 100px'
   },
   {
     label: 'Queue Resolution',
     name: 'queue_resolution2',
     align: 'left',
-    headerStyle: 'min-width: 100px'
+    headerStyle: 'min-width: 100px',
+    columnStyle: 'width: 100px'
   },
   {
     label: 'Creator Type',
     name: 'creator_type',
     align: 'left',
-    headerStyle: 'min-width: 100px'
+    headerStyle: 'min-width: 100px',
+    columnStyle: 'width: 100px'
   },
   {
     label: 'Tags',
     name: 'tags',
     align: 'left',
-    headerStyle: 'min-width: 100px'
+    headerStyle: 'min-width: 200px',
+    columnStyle: 'width: 200px'
   },
   {
     label: 'Notes',
     name: 'notes',
     align: 'left',
-    headerStyle: 'min-width: 200px'
+    headerStyle: 'min-width: 400px',
+    columnStyle: 'width: 400px'
   },
   {
     label: 'CSAT Score',
     name: 'csat_score',
     align: 'left',
-    headerStyle: 'min-width: 100px'
+    headerStyle: 'min-width: 100px',
+    columnStyle: 'width: 100px'
   },
   {
     label: 'Operations',
@@ -175,56 +201,64 @@ export const DEFAULT_COLUMNS = [
     field: 'disposition_status2',
     label: '',
     align: 'center',
-    headerStyle: 'min-width: 105px'
+    headerStyle: 'min-width: 105px',
+    columnStyle: 'width: 105px'
   },
   {
     label: 'Number',
     name: 'incoming_number',
     field: 'incoming_number',
     align: 'left',
-    headerStyle: 'min-width: 100px'
+    headerStyle: 'min-width: 150px',
+    columnStyle: 'width: 150px'
   },
   {
     label: 'Ring Group',
     name: 'ring_group',
     field: 'ring_group',
     align: 'left',
-    headerStyle: 'min-width: 100px'
+    headerStyle: 'min-width: 200px',
+    columnStyle: 'width: 200px'
   },
   {
     label: 'Start Time',
     name: 'created_at',
     field: 'created_at',
     align: 'left',
-    headerStyle: 'min-width: 100px'
+    headerStyle: 'min-width: 100px',
+    columnStyle: 'width: 100px'
   },
   {
     label: 'Talk Time',
     name: 'talk_time',
     field: 'talk_time',
     align: 'left',
-    headerStyle: 'min-width: 100px'
+    headerStyle: 'min-width: 100px',
+    columnStyle: 'width: 100px'
   },
   {
     label: 'Duration',
     name: 'duration',
     field: 'duration',
     align: 'left',
-    headerStyle: 'min-width: 100px'
+    headerStyle: 'min-width: 100px',
+    columnStyle: 'width: 100px'
   },
   {
     label: 'Contact',
     name: 'contact',
     field: 'lead_number',
     align: 'left',
-    headerStyle: 'min-width: 200px'
+    headerStyle: 'min-width: 250px',
+    columnStyle: 'width: 250px'
   },
   {
     label: 'User',
     name: 'user_id',
     field: 'user_id',
     align: 'left',
-    headerStyle: 'min-width: 200px'
+    headerStyle: 'min-width: 250px',
+    columnStyle: 'width: 250px'
   },
   {
     label: 'Operations',
