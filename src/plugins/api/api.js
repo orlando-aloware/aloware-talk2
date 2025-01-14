05daf26978a4c9a402b1b67bad098c1800bd0d33
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2025-01-14 11:07:45 -0800

    Cleanup summary and transcription generation
    Signed-off-by: Sohrab Sheikhani <sheikhani.sohrab@gmail.com>

--- src/plugins/api/api.js
+++ src/plugins/api/api.js
@@ -504,19 +504,11 @@ export default {
 
       // Generate transcription for the communication
       generateTranscription (communicationId) {
-        if (!communicationId) {
-          return null
-        }
-
         return window.axios.post(`${suffixV1}transcription/communication/${communicationId}`)
       },
 
       // Generate summary for the transcription
       generateSummary (communicationId) {
-        if (!communicationId) {
-          return null
-        }
-
         return window.axios.post(
           `${suffixV1}transcription/communication/${communicationId}/generate-summary`
         )
