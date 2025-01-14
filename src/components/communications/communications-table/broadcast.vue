c9106d26f2da0ceece5009e328f3c3185673ee3e
Jonathan M <jonathan@aloware.com>
2025-01-14 16:01:12 -0300

    fix(wat-1008): removed link for agents and non-allowed users

--- src/components/communications/communications-table/broadcast.vue
+++ src/components/communications/communications-table/broadcast.vue
@@ -4,12 +4,15 @@
     <router-link class="ellipse"
                  target='_blank'
                  :to="broadcastActivityParams"
-                 v-if="broadcast.id">
+                 v-if="isBroadcastAllowed && broadcast.id">
       {{ broadcast.name || '-' }}
       <q-tooltip>
         Click to see Broadcast activity's page
       </q-tooltip>
     </router-link>
+    <span v-else-if="broadcast.id">
+      {{ broadcast.name || '-' }}
+    </span>
     <span v-else>
       -
     </span>
@@ -18,9 +21,14 @@
 
 <script>
 import { mapState } from 'vuex'
+import { aclMixin } from 'src/plugins/mixins'
 
 export default {
   name: 'Broadcast',
+
+  mixins: [
+    aclMixin
+  ],
 
   props: {
     value: {
@@ -32,8 +40,19 @@ export default {
   computed: {
     ...mapState(['broadcasts']),
 
+    ...mapState('auth', [
+      'profile'
+    ]),
+
     broadcast () {
       return this.broadcasts.find(broadcast => broadcast.id === this.value) || {}
+    },
+
+    isBroadcastAllowed () {
+      const isCompanyEnabled = this.profile.bulk_rvm_enabled || this.profile.bulk_sms_enabled
+      const isUserEnabled = this.hasPermissionTo('create broadcast message') || this.hasPermissionTo('create broadcast rvm')
+
+      return !this.isAgent && isUserEnabled && isCompanyEnabled
     },
 
     broadcastActivityParams () {
