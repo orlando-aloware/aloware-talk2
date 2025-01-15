da42062523a50c96dd86e0140b99c3701a491575
Jonathan M <jonathan@aloware.com>
2025-01-15 09:42:15 -0300

    fix(wat-1143): removed lines column

--- src/components/communications/communications-table/communications-table-columns.js
+++ src/components/communications/communications-table/communications-table-columns.js
@@ -90,13 +90,6 @@ export const ALL_COLUMNS = [
     columnStyle: 'width: 250px'
   },
   {
-    label: 'Lines',
-    name: 'line',
-    align: 'left',
-    headerStyle: 'min-width: 150px',
-    columnStyle: 'width: 150px'
-  },
-  {
     label: 'User',
     name: 'user_id',
     align: 'left',
