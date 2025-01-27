28808d9e858cab40eeb8025ffadc4ecfd23ec713
Reza <reza@aloware.com>
2025-01-28 01:05:21 +0300

    feature(pla-522): hide call recording when has voicemail

--- src/components/communication-audio.vue
+++ src/components/communication-audio.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-100" data-testid="comm-audio-wrapper">
-    <div v-if="!isDeleted && hasAudio">
+    <div v-if="!isDeleted && hasAudio && (type !== UploadedFileTypes.TYPE_CALL_RECORDING || !communication?.has_voicemail)">
       <div class="audio-player p-2">
         <div class="d-flex flex-row align-items-center w-100"
              v-if="remoteUrl">
