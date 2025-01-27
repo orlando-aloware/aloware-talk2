78667141618ee2ed8195fba07d1332282e05182e
Reza <reza@aloware.com>
2025-01-27 17:50:15 +0300

    feature(pla-522): fix transcription button issue

--- src/components/communication-info.vue
+++ src/components/communication-info.vue
@@ -873,11 +873,11 @@
             </h3>
           </div>
           <div class="transcription-summary-container">
-            <a class="transcription-link text-decoration-none"
-              @click.prevent="fetchSmartTranscriptionData()"
+            <span class="transcription-link text-decoration-none"
+              @click="fetchSmartTranscriptionData()"
               v-if="communication.has_transcription">
               Show transcription
-            </a>
+            </span>
             <span class="transcription-message text-decoration-none"
                   v-else>
               <span v-if="communication.call_transcription_status === TranscriptionStatus.STATUS_PROCESSING">Transcription in progress</span>
