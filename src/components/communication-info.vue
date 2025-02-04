514d1769a1f959e5d7bda265345dfd34add19142
Reza <reza@aloware.com>
2025-02-04 18:07:49 +0300

    feature(pla-522): fix comments

--- src/components/communication-info.vue
+++ src/components/communication-info.vue
@@ -902,13 +902,12 @@
         </div>
         <div class="text-left-align text-15"
              v-else-if="currentCompany?.transcription_settings?.call_transcription_enabled && !communication?.call_transcription_status">
-          <div>Click on the button to generate a transcription of this call. </div>
+          <div class="mr-2">Click on the button to generate a transcription of this call.</div>
           <generate-transcription-button class="mr-2"
                                          variant="button"
                                          data-testid="comm-details-generate-transcription-button"
                                          :communication="communication"
-                                         v-if="fileUuid && isMigrated">
-          </generate-transcription-button>
+                                         v-if="fileUuid && isMigrated"/>
         </div>
         <div class="text-left-align text-13 relative"
              v-if="communication.call_summary">
