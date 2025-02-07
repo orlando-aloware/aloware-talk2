503540aecad0538b098bad566c50f6326f45a00d
Jonathan M <jonathan@aloware.com>
2025-02-07 15:40:12 -0300

    feature(wat-1178): added contact name component

---
+++ src/components/einbox/communication-items/contact-name.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="contact-name"
+       :id="`contact-name-${_uid}`">
+    <div class="ellipse">
+      {{ name }}
+    </div>
+
+    <b-popover triggers="hover"
+               custom-class="talk-table__popover"
+               placement="bottom"
+               :target="`contact-name-${_uid}`">
+      {{ name }}
+    </b-popover>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: false,
+      default: 'No Name'
+    }
+  }
+}
+</script>
+
+<style>
+.contact-name {
+  font-size: 14px;
+  font-weight: 500;
+  width: 195px;
+}
+</style>
