2b4a3364e292d545bcc94eb70c941062a2c99f05
Jonathan M <jonathan@aloware.com>
2025-02-11 15:45:05 -0300

    fix(wat-1178): dry code

--- src/plugins/mixins/einbox.mixin.js
+++ src/plugins/mixins/einbox.mixin.js
@@ -1,3 +1,4 @@
+import { THREADED } from 'src/store/einbox/einbox.store'
 import { mapActions, mapState } from 'vuex'
 import talk2Api from 'src/plugins/api/api'
 
@@ -77,13 +78,8 @@ export default {
     async fetchItems (inboxId) {
       try {
         this.setIsLoadingItems(true)
-        const nextPage = 1
 
-        const response = await talk2Api.V2.inbox.contacts.get({
-          inboxId,
-          page: nextPage,
-          view_mode: this.viewMode
-        })
+        const response = await this.getItemsRequest(inboxId, 1)
 
         this.setItems(response.data)
       } catch (error) {
@@ -98,13 +94,9 @@ export default {
         if (this.isLoadingMoreItems || !this.hasMoreItems) return
 
         this.setIsLoadingMoreItems(true)
-        const nextPage = this.currentItemsPage + 1
 
-        const response = await talk2Api.V2.inbox.contacts.get({
-          inboxId,
-          page: nextPage,
-          view_mode: this.viewMode
-        })
+        const nextPage = this.currentItemsPage + 1
+        const response = await this.getItemsRequest(inboxId, nextPage)
 
         this.appendItems(response.data)
       } catch (error) {
@@ -112,6 +104,24 @@ export default {
       } finally {
         this.setIsLoadingMoreItems(false)
       }
+    },
+
+    getItemsRequest (inboxId, nextPage) {
+      // get the request based on the view mode (threaded or unthreaded)
+      if (this.viewMode === THREADED) {
+        return talk2Api.V2.communications.threaded({
+          inboxId,
+          page: nextPage
+        })
+      }
+
+      return talk2Api.V1.reports.communications.get({
+        params: {
+          inbox_id: inboxId,
+          page: nextPage
+        },
+        headers: { 'requested-from': 'api' }
+      })
     }
   },
 
