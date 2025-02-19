6a57819467cdaff30ae66be8eb6a13038391c9fd
Jonathan M <jonathan@aloware.com>
2025-02-19 10:48:25 -0300

    fix(wat-1178): page cleanup

--- src/pages/EInbox.vue
+++ src/pages/EInbox.vue
@@ -2,12 +2,10 @@
   <div class="h-100"
        v-if="authenticated">
     <div class="inbox animate__animated animate__fadeIn position-relative">
-      <e-inbox-side ref="inbox-side"
-                    :class="inboxSideClasses"
+      <e-inbox-side :class="inboxSideClasses"
                     @itemSelected="onItemSelected" />
 
-      <div class="inbox-details d-flex flex-grow-1"
-           :class="{ 'mobile-contact-active' : isMobileContactActive }"
+      <div :class="['inbox-details', 'd-flex', 'flex-grow-1', { 'mobile-contact-active' : isMobileContactActive }]"
            v-if="isContactShow">
         <Contact />
       </div>
@@ -16,29 +14,12 @@
 </template>
 
 <script>
-import EInboxSide from 'components/einbox/einbox-side'
-import { mapActions, mapGetters, mapState } from 'vuex'
-import {
-  contactMixin,
-  contactV2AttributesMixin,
-  inboxMixin,
-  aclMixin,
-  visibilityMixin,
-  userMixin
-} from 'src/plugins/mixins'
 import Contact from 'pages/contacts/Contact'
+import EInboxSide from 'components/einbox/einbox-side'
+import { mapGetters } from 'vuex'
 
 export default {
   name: 'EInbox',
-
-  mixins: [
-    contactMixin,
-    contactV2AttributesMixin,
-    inboxMixin,
-    aclMixin,
-    visibilityMixin,
-    userMixin
-  ],
 
   components: {
     Contact,
@@ -51,10 +32,6 @@ export default {
       'profile'
     ]),
 
-    ...mapState('inbox', [
-      'navListItems'
-    ]),
-
     isMobileContactActive () {
       return this.mobileContactScreenRoutes.includes(this.$route.name)
     },
@@ -65,136 +42,22 @@ export default {
 
     inboxSideClasses () {
       return {
-        'mobile-contact-active': this.isMobileContactActive,
-        'inbox-side border-top-0 flex-shrink-0 h-100': this.$route.name === 'Inbox Channel'
+        'mobile-contact-active': this.isMobileContactActive
       }
     }
   },
 
   data () {
     return {
-      contactInfoOpen: false,
-      title: 'Inbox',
-      contactId: null,
-      miniState: true,
       mobileContactScreenRoutes: [
-        'Inbox Contact',
-        'Inbox Contact Task',
-        'Inbox View Contact Task',
-        'Inbox Contact Communication',
         'EInboxCommunicationDetail'
       ]
     }
   },
 
   methods: {
-    ...mapActions('inbox', [
-      'setActiveChannel',
-      'setTaskCount',
-      'setShowViewsList',
-      'setIsInboxRefreshBtnLoading'
-    ]),
-
-    // ...mapActions('Einbox', [
-    //   'toggleNewInbox',
-    //   'initNewInbox'
-    // ]),
-
-    setChannel (routeChanged = false) {
-      if (this.inboxChannelRoutes.includes(this.$route.name)) {
-        let channel = null
-
-        if (this.inboxViewsRoutes.includes(this.$route.name) && this.isLoadedPinnedViews) {
-          channel = this.getPinnedViewChannel(this.$route.params.viewId)
-        } else {
-          const routeChannel = this.$route.params?.channel ?? 'inbox' // default fallback
-          channel = this.navListItems.find(item => item.value === routeChannel)
-        }
-
-        this.setActiveChannel(channel)
-        return
-      }
-
-      if (this.$route.name === 'Inbox' && !this.activeChannel) {
-        const channel = this.navListItems.find(item => item.value === 'inbox')
-        this.setActiveChannel(channel)
-      }
-
-      if (routeChanged && this.$refs['inbox-side']) {
-        this.$refs['inbox-side'].navigateToInbox()
-      }
-    },
-
     onItemSelected (routeData) {
-      this.contactId = routeData.params.id
       this.$router.push(routeData)
-    },
-
-    onWindowResize () {
-      this.setShowViewsList(false)
-    }
-  },
-
-  created () {
-    if (this.isCompanyPartOfAlowareDemoCompanies(this.profile.company_id) || this.isInboxViewsEnabledCompany) {
-      this.getPinnedViews()
-    }
-
-    if (this.$route.query && this.$route.query.add_contact) {
-      this.$VueEvent.fire('add_contact', {
-        phone_number: this.$options.filters.fixPhone(this.$route.query.add_contact)
-      })
-    }
-
-    // this.initNewInbox(true)
-  },
-
-  mounted () {
-    // when the user tries to access the channel directly but without a personal line
-    if (this.$route.params?.channel === 'my-personal-line' && !this.profile.campaign_id) {
-      this.$router.push({ name: 'Inbox' })
-      return
-    }
-
-    if (this.authenticated && this.$route.name !== 'Inbox View') {
-      this.setChannel()
-      this.fetchTaskCounts()
-    }
-
-    this.$VueEvent.listen('fetchInbox', () => {
-      if (this.$route.params?.status) {
-        this.currentTask = this.$options.filters.getTaskStatusIdByName(this.$route.params.status)
-      }
-
-      this.loadContactTasks(false)
-      this.fetchTaskCounts()
-      this.setIsInboxRefreshBtnLoading(false)
-    })
-
-    window.addEventListener('resize', this.onWindowResize)
-  },
-
-  unmounted () {
-    window.removeEventListener('resize', this.onWindowResize)
-  },
-
-  beforeDestroy () {
-    window.removeEventListener('resize', this.onWindowResize)
-  },
-
-  watch: {
-    '$route.name': function (value) {
-      const isNotInboxRouteName = !value.includes('Inbox')
-      this.setChannel(isNotInboxRouteName)
-    },
-
-    isLoadedPinnedViews (value) {
-      if (value && this.$route.name === 'Inbox View') {
-        this.setChannel()
-      }
-
-      // get counts for inbox
-      this.fetchInboxTaskCounts()
     }
   }
 }
