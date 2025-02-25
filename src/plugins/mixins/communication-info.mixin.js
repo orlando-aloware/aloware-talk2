4672607bd8ebba296ec614ce1a93f7eca96fa082
Reza <reza@aloware.com>
2025-02-25 21:27:26 +0300

    fix: safely access transcription metadata with optional chaining

--- src/plugins/mixins/communication-info.mixin.js
+++ src/plugins/mixins/communication-info.mixin.js
@@ -342,7 +342,7 @@ export default {
         communication.type === CommunicationTypes.CALL &&
         communication.is_eligible_for_transcribe &&
         // Don't show generate transcription button if the transcription is already parsed and is empty
-        !(communication.call_transcription_status === TranscriptionStatus.STATUS_PARSED && communication.metadata['transcription_info']['summary'] === '') &&
+        !(communication.call_transcription_status === TranscriptionStatus.STATUS_PARSED && communication?.metadata?.transcription_info?.summary === '') &&
         (communication.has_voicemail || this.showAudio(communication)) &&
         (
           (!communication?.call_transcription_status && this.currentCompany?.transcription_settings?.call_transcription_enabled) ||
