22742a38d49d434d0e09f75fb3f9cf178504ed6b
Henrique melo <henrique@aloware.com>
2025-03-11 18:19:51 -0300

    cleanup

--- quasar.conf.js
+++ quasar.conf.js
@@ -62,6 +62,8 @@ module.exports = function (ctx) {
       // Options below are automatically set depending on the env, set them if you want to override
       // extractCSS: false,
 
+      distDir: `${process.env.NODE_ENV || 'dist'}/spa`,
+
       // https://quasar.dev/quasar-cli/handling-webpack
       extendWebpack (cfg) {
         cfg.module.rules.push({
