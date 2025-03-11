18acf44700dae57818faddb4a9e4fbb769bb6e3f
Henrique melo <henrique@aloware.com>
2025-03-11 18:10:24 -0300

    cleanup

--- quasar.conf.js
+++ quasar.conf.js
@@ -8,21 +8,6 @@
 /* eslint-env node */
 
 module.exports = function (ctx) {
-  const DotEnv = require('dotenv')
-  const envFile = (() => {
-    switch (process.env.NODE_ENV) {
-      case 'production':
-        return '.env.prod'
-      case 'staging':
-        return '.env.staging'
-      case 'dev1':
-        return '.env.dev1'
-      case 'dev2':
-        return '.env.dev2'
-      default:
-        return '.env'
-    }
-  })()
 
   const noHttps = process.env.APP_SECURE === 'false'
 
@@ -76,8 +61,6 @@ module.exports = function (ctx) {
 
       // Options below are automatically set depending on the env, set them if you want to override
       // extractCSS: false,
-
-      distDir: `${process.env.NODE_ENV || 'dist'}/spa`,
 
       // https://quasar.dev/quasar-cli/handling-webpack
       extendWebpack (cfg) {
