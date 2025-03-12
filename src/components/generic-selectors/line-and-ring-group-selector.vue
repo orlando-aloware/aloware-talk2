e5e763a1d7ab53dcd0bc1c39e1a31860445e04cf
Keyner W. Tupac Yupanqui Caballero <keyner@aloware.com>
2025-03-12 10:39:52 -0500

    feature(WAT-1285) back to rename Ring Group

--- src/components/generic-selectors/line-and-ring-group-selector.vue
+++ src/components/generic-selectors/line-and-ring-group-selector.vue
@@ -173,9 +173,9 @@ export default {
 
       switch (true) {
         case this.multiple && this.selected.length < 1:
-          return this.customPlaceholder || 'Select Lines or Einbox'
+          return this.customPlaceholder || 'Select Lines or Ring Groups'
         case !this.multiple && !this.selected:
-          return this.customPlaceholder || 'Select Line or Einbox'
+          return this.customPlaceholder || 'Select Line or Ring Group'
         case this.multiple && this.selected.length > 0:
         case !this.multiple && this.selected:
         default:
@@ -197,7 +197,7 @@ export default {
 
       if (ringGroupOptions && ringGroupOptions.length > 0) {
         ringGroupOptions.unshift({
-          group: 'Filter by Einbox',
+          group: 'Filter by Ring Group',
           disable: true
         })
       }
