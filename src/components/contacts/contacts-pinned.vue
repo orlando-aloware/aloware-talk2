da8899bc7c7140138ca5bab4fe3edb3f12ad178c
Paulo <paulo@aloware.com>
2025-03-21 11:59:07 -0400

    fix(wat-1349): deprecate loading count and listeners on pinned lists

--- src/components/contacts/contacts-pinned.vue
+++ src/components/contacts/contacts-pinned.vue
@@ -26,11 +26,9 @@
 </template>
 
 <script>
-import qs from 'qs'
 import { mapActions, mapGetters } from 'vuex'
 import * as ContactListTypes from 'src/constants/contacts-list-types'
 import { DEFAULT_PINNED_LIST } from 'src/constants/contacts-list-default-pinned-list'
-import { OPERATORS } from 'src/constants/contacts-filter-operators'
 import ContactsPinnedItem from 'components/contacts/contacts-pinned-item'
 import ContactsSidebarLoader from 'components/contacts/contacts-sidebar-loader'
 
@@ -71,36 +69,8 @@ export default {
     this.listeners.fetchContactsLists = () => {
       this.init()
     }
-    this.listeners.getListCount = (list) => {
-      if (list.type === this.ContactListTypes.DYNAMIC && list.id !== 'my-contacts') {
-        this.loadDynamicListPinnedCount(list, true)
-      }
-
-      if (list.type === this.ContactListTypes.STATIC) {
-        this.loadPinnedCount(list.id)
-      }
-    }
-    this.listeners.listCountUpdated = (data) => {
-      const isPinned = this.pinnedLists.find(item => item.id.toString() === data.list.id.toString())
-
-      if (!isPinned) {
-        return
-      }
-
-      this.pinnedCountLoaded({
-        id: data.list.id,
-        count: data.count
-      })
-    }
-
-    this.listeners.updateLoadingPinned = (value) => {
-      this.loadingPinned = value
-    }
 
     this.$VueEvent.listen('fetchContactsLists', this.listeners.fetchContactsLists)
-    this.$VueEvent.listen('getListCount', this.listeners.getListCount)
-    this.$VueEvent.listen('listCountUpdated', this.listeners.listCountUpdated)
-    this.$VueEvent.listen('update-loading-pinned', this.listeners.updateLoadingPinned)
   },
 
   methods: {
@@ -115,120 +85,6 @@ export default {
       if (this.authenticated) {
         this.loadPinned()
       }
-    },
-
-    loadDefaultCounts () {
-      this.loadingDefaultCounts = true
-      return Promise.all([
-        // this.loadAllCount(),
-        // this.loadMyContactsCount(),
-        this.loadStatusCounts()
-      ]).then((
-        [
-          // allContacts,
-          // myContacts,
-          statusCounts
-        ]) => {
-        this.pinnedCountLoaded({
-          id: DEFAULT_PINNED_LIST.ALL_CONTACTS.id,
-          count: statusCounts.all_contacts_count
-        })
-        this.pinnedCountLoaded({
-          id: DEFAULT_PINNED_LIST.MY_CONTACTS.id,
-          count: statusCounts.my_contacts_count
-        })
-        this.pinnedCountLoaded({
-          id: DEFAULT_PINNED_LIST.NEWLEADS.id,
-          count: statusCounts.new_contacts_count
-        })
-        this.pinnedCountLoaded({
-          id: DEFAULT_PINNED_LIST.UNANSWERED.id,
-          count: statusCounts.unanswered_contacts_count
-        })
-        this.pinnedCountLoaded({
-          id: DEFAULT_PINNED_LIST.UNASSIGNED.id,
-          count: statusCounts.unassigned_contacts_count
-        })
-      }).catch((err) => {
-        console.error(err)
-        this.$generalNotification('Unable to load pinned list counts, please try again.', 'error')
-        this.setPinnedListsLoaded(true)
-        this.loadingDefaultCounts = false
-      }).finally(() => {
-        this.loadingDefaultCounts = false
-      })
-    },
-
-    loadAllCount () {
-      return this.$axios.get(`${process.env.API_REPORTING_URL}/api/v2/contacts/count`).then((response) => response.data.count)
-    },
-
-    loadMyContactsCount () {
-      return this.$axios.get(`${process.env.API_REPORTING_URL}/api/v2/contacts/count`, {
-        params: {
-          filter_groups: [{
-            filters: {
-              contact_owner: [
-                {
-                  operator: OPERATORS.IS_ANY_OF,
-                  value: [this.profile.id]
-                }
-              ]
-            },
-            is_conjunction: true
-          }]
-        },
-        paramsSerializer: qs.stringify
-      }).then((response) => {
-        this.pinnedCountLoaded({
-          id: 'my-contacts',
-          count: response.data.count
-        })
-      })
-    },
-
-    loadStatusCounts () {
-      return this.$axios.get(`${process.env.API_REPORTING_URL}/api/v2/contacts/status-counts`).then((response) => response.data)
-        .catch((err) => {
-          console.error(err)
-          this.$generalNotification('Unable to load status counts, please try again.', 'error')
-          this.setPinnedListsLoaded(true)
-          this.loadingPinned = false
-        })
-    },
-
-    async loadPinnedCount (id) {
-      await this.$axios.get(`api/v2/contacts-list/${id}/items?per_page=1`)
-        .then((response) => {
-          this.pinnedCountLoaded({
-            id: id,
-            count: response.data.total
-          })
-        }).catch((err) => {
-          console.error(err)
-          this.$generalNotification('Unable to load pinned count, please try again.', 'error')
-          this.setPinnedListsLoaded(true)
-          this.loadingPinned = false
-        })
-    },
-
-    async loadDynamicListPinnedCount (data, skipCancelToken = false) {
-      this.$VueEvent.fire('get-list-count', {
-        data: data,
-        skipCancelToken: skipCancelToken,
-        thenFunctions: {
-          'pinnedCountLoaded': {
-            id: data.contact_list_id || data.id,
-            count: 'response.data.count'
-          }
-        },
-        catchFunctions: {
-          'setPinnedListsLoaded': true
-        },
-        catchEventFires: {
-          'update-loading-pinned': true
-        }
-      })
     },
 
     loadPinned () {
@@ -273,9 +129,6 @@ export default {
   },
   beforeDestroy () {
     this.$VueEvent.stop('fetchContactsLists', this.listeners.fetchContactsLists)
-    this.$VueEvent.stop('getListCount', this.listeners.getListCount)
-    this.$VueEvent.stop('listCountUpdated', this.listeners.listCountUpdated)
-    this.$VueEvent.stop('update-loading-pinned', this.listeners.updateLoadingPinned)
   }
 }
 </script>
