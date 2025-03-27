6cf57339c62925e20e2a14bcaafc87adb195b351
Keyner W. Tupac Yupanqui Caballero <keyner@aloware.com>
2025-03-27 13:36:35 -0500

    bug(WAT-1359) fix to show call in a new empty ring_group

--- src/components/einbox/einbox-tab.vue
+++ src/components/einbox/einbox-tab.vue
@@ -76,7 +76,7 @@
       </div>
 
       <!-- items list -->
-      <template v-else-if="items.length">
+      <template v-else-if="itemsData.length">
         <div :key="item.id"
              v-for="item in itemsData"
              @click="onItemClick(item)">
