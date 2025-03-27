8f83a7ffe4e9e19cf96b878d69dde7c0f886e516
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2025-03-27 00:40:40 -0500

    Cleanup
    Signed-off-by: Sohrab Sheikhani <sheikhani.sohrab@gmail.com>

--- src/components/integrations/integration-pipedrive.vue
+++ src/components/integrations/integration-pipedrive.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="integration-wrapper integration-wrapper-generic" data-testid="integration-pipedrive-wrapper">
-    <q-card class="integration-card"
-            data-testid="integration-pipedrive-card"
-            flat>
+  <div
+    class="integration-wrapper integration-wrapper-generic"
+    data-testid="integration-pipedrive-wrapper"
+  >
+    <q-card
+      class="integration-card"
+      data-testid="integration-pipedrive-card"
+      flat
+    >
       <q-item class="p-0">
         <span class='integration-jit-card-header'>
           <i class="pipedrive-btn integration-icon-in-header"></i>
@@ -10,41 +15,59 @@
         </span>
       </q-item>
 
-      <q-separator data-testid="integration-pipedrive-separator"/>
+      <q-separator data-testid="integration-pipedrive-separator" />
 
-      <q-card-section v-if="integrationData && integrationData.contact_details" data-testid="integration-pipedrive-card-section-1">
-        <a class="external-contact-integration-link-icon color-primary"
-           target="_blank"
-           :href="contactLink">
-          <i class="fa fa-external-link" aria-hidden="true"/>
+      <q-card-section
+        v-if="integrationData && integrationData.contact_details"
+        data-testid="integration-pipedrive-card-section-1"
+      >
+        <a
+          class="external-contact-integration-link-icon color-primary"
+          target="_blank"
+          :href="contactLink"
+        >
+          <i
+            class="fa fa-external-link"
+            aria-hidden="true"
+          />
         </a>
-        <p class="mb-0"
-           data-testid="integration-pipedrive-name"
-           v-if="integrationData.contact_details.name !== undefined">
+        <p
+          class="mb-0"
+          data-testid="integration-pipedrive-name"
+          v-if="integrationData.contact_details.name !== undefined"
+        >
           <span class="data-icon-label">Name: </span>
           <span class="data-value">
-             <q-tooltip anchor="top middle"
-                        self="center middle">
+            <q-tooltip
+              anchor="top middle"
+              self="center middle"
+            >
               {{ integrationData.contact_details.name }}
             </q-tooltip>
             {{ integrationData.contact_details.name }}
           </span>
         </p>
-        <p class="mb-0"
-           data-testid="integration-pipedrive-email"
-           v-if="integrationData.contact_details.email && integrationData.contact_details.email.length > 0 && integrationData.contact_details.email[0].value">
+        <p
+          class="mb-0"
+          data-testid="integration-pipedrive-email"
+          v-if="integrationData.contact_details.email && integrationData.contact_details.email.length > 0 && integrationData.contact_details.email[0].value"
+        >
           <span class="data-icon-label">Email: </span>
           <span class="data-value">{{ integrationData.contact_details.email[0].value }}</span>
         </p>
-        <p class="mb-0"
-           data-testid="integration-pipedrive-company"
-           v-if="integrationData.contact_details.org_name">
+        <p
+          class="mb-0"
+          data-testid="integration-pipedrive-company"
+          v-if="integrationData.contact_details.org_name"
+        >
           <span class="data-icon-label">Company: </span>
           <span class="data-value">{{ integrationData.contact_details.org_name }}</span>
         </p>
-        <p class="mb-0"
-           data-testid="integration-pipedrive-owner"
-           v-if="integrationData.contact_details.owner_name">
+        <p
+          class="mb-0"
+          data-testid="integration-pipedrive-owner"
+          v-if="integrationData.contact_details.owner_name"
+        >
           <span class="data-icon-label">Owner: </span>
           <span class="data-value">{{ integrationData.contact_details.owner_name }}</span>
         </p>
@@ -52,27 +75,37 @@
 
       <q-card-section data-testid="integration-pipedrive-card-section-2">
         <b-row>
-          <b-button class="text-white"
-                    size="sm"
-                    variant="primary"
-                    tabindex="0"
-                    block
-                    data-testid="integration-pipedrive-sync-button"
-                    @click="syncPipedrive">
-            <i class="fa fa-sync-alt" v-if="!isSyncing"></i>
-            <q-spinner-bars v-if="isSyncing"
-                            data-testid="integration-pipedrive-sync-spinner"
-                            color="white">
+          <b-button
+            class="text-white"
+            size="sm"
+            variant="primary"
+            tabindex="0"
+            block
+            data-testid="integration-pipedrive-sync-button"
+            @click="syncPipedrive"
+          >
+            <i
+              class="fa fa-sync-alt"
+              v-if="!isSyncing"
+            ></i>
+            <q-spinner-bars
+              v-if="isSyncing"
+              data-testid="integration-pipedrive-sync-spinner"
+              color="white"
+            >
             </q-spinner-bars>
             {{ isSyncing ? 'Syncing...' : 'Sync with Pipedrive' }}
-            <q-tooltip anchor="center start"
-                       self="center left"
-                       data-testid="integration-pipedrive-sync-tooltip"
-                       :offset="[-220, 10]">
+            <q-tooltip
+              anchor="top middle"
+              self="center middle"
+              data-testid="integration-pipedrive-sync-tooltip"
+              :offset="[-220, 10]"
+            >
               <p class="font-weight-bold mb-0">Click on this button to sync the data for this contact between {{ whiteLabelName }} and Pipedrive.</p>
               <p class="font-weight-bold">You'll want to click on this button if:</p>
               <p class="mt-1 mb-0">- The contact was recently merged in Pipedrive with another contact.</p>
-              <p class="mt-0 mb-0">- You notice any inconsistencies between {{ whiteLabelName }} and Pipedrive data on this contact.</p>
+              <p class="mt-0 mb-0">- You notice any inconsistencies between {{ whiteLabelName }} and Pipedrive data on
+                this contact.</p>
             </q-tooltip>
           </b-button>
         </b-row>
@@ -84,14 +117,14 @@
 </template>
 
 <script>
-import { mapActions, mapState } from 'vuex'
-import talk2Api from 'src/plugins/api/api'
 import _ from 'lodash'
+import talk2Api from 'src/plugins/api/api'
 import {
-  pipedriveIntegrationMixin,
   integrationMixin,
+  pipedriveIntegrationMixin,
   simpsocialMixin
 } from 'src/plugins/mixins'
+import { mapActions, mapState } from 'vuex'
 
 export default {
   name: 'integration-pipedrive',
