abd2230aec4f4e254875d8c0a9b117c07280c254
Paulo <paulo@aloware.com>
2025-03-27 11:50:30 -0400

    chore: remove unnecessary boolean check

--- src/plugins/mixins/dispositions.mixin.js
+++ src/plugins/mixins/dispositions.mixin.js
@@ -118,10 +118,10 @@ export default {
       // if force redial is enabled, force sms sending if the
       // selected call disposition is not one of the successful call dispositions
       // and the sms template has not been sent yet
-      return Boolean(this.isForcedRedialEnabled &&
+      return this.isForcedRedialEnabled &&
         this.sessionSettings?.force_sms &&
         !successfulCallDispositionSelected &&
-        !this.tasksSentSmsTemplates[this.activeTask.id])
+        !this.tasksSentSmsTemplates[this.activeTask.id]
     }
   },
 
