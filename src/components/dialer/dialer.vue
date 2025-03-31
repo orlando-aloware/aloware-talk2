e4ddcde3b960f94132ec66f392c82fd43bb2e1bb
Fernando Ortuno <fernando@aloware.com>
2025-03-31 16:19:04 -0400

    fix(wat-1120): added logs

--- src/components/dialer/dialer.vue
+++ src/components/dialer/dialer.vue
@@ -435,9 +435,10 @@ export default {
     },
 
     callParkedFromAnotherTab () {
-      console.log(this.dialer.communication.current_status2, this.dialer.communication.disposition_status2, this.dialer.conference_status2, this.dialer.communication)
+      console.log(this.dialer.communication.current_status2, this.dialer.communication.disposition_status2, this.dialer.communication.conference_status2, this.dialer.communication)
       const found = this.parkedCalls.find(parkedCall => parkedCall.id === this.dialer.communication.id)
       console.log(found)
+      console.log(this.dialer)
       return found || (this.dialer.communication.current_status2 === CommunicationCurrentStatus.CURRENT_STATUS_HOLD_NEW && this.dialer.communication.conference_status2 === 4)
     },
 
@@ -884,7 +885,7 @@ export default {
             this.setDialerParkedCall(this.dialer.communication)
           }
           this.backToDial('Talk-Connection.OnDisconnect')
-        }, 1000)
+        }, 1500)
       })
     },
 
