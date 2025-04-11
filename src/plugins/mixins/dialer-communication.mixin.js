af4f0a715fc136ddfdbc856facf415de7a3e7802
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2025-04-11 15:46:42 -0700

    Revert "Fix"
    This reverts commit eeb7bbf2a843bea043dbbab4e70c558309a4270f.

--- src/plugins/mixins/dialer-communication.mixin.js
+++ src/plugins/mixins/dialer-communication.mixin.js
@@ -2,11 +2,26 @@ import * as CommunicationCurrentStatus from 'src/constants/communication-current
 import { mapState } from 'vuex'
 
 export default {
-  computed: {
-    ...mapState(['dialer']),
+  data: () => ({
+    isCallInProgressStatus: false
+  }),
 
-    isCallInProgressStatus () {
-      return this.dialer?.communication?.current_status2 === CommunicationCurrentStatus.CURRENT_STATUS_INPROGRESS_NEW
+  computed: {
+    ...mapState(['dialer'])
+  },
+
+  watch: {
+    'dialer.communication': {
+      deep: true,
+      handler (value) {
+        if (value && value.current_status2 === CommunicationCurrentStatus.CURRENT_STATUS_INPROGRESS_NEW) {
+          this.isCallInProgressStatus = true
+
+          return
+        }
+
+        this.isCallInProgressStatus = false
+      }
     }
   }
 }
