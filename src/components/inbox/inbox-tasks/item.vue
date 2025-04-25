f187a49f514f2fe7a2c3c059a715feed0212eec8
Fernando Ortuno <fernando@aloware.com>
2025-04-25 14:09:30 -0400

    fix(wat-1120): small refactor

--- src/components/inbox/inbox-tasks/item.vue
+++ src/components/inbox/inbox-tasks/item.vue
@@ -138,7 +138,7 @@
                       :offset="[5, 9]"
                       data-testid="item-answer-menu"
                       v-model="showIncomingCallMenu"
-                      v-if="isDialerConnected || isAgentOnCall"
+                      v-if="isDialerOrAgentOnCall"
                       @hide="showIncomingCallMenu = false">
                 <q-list>
                   <q-item clickable
@@ -212,7 +212,7 @@
                       fit
                       :offset="[5, 9]"
                       v-model="showParkedCallMenu"
-                      v-if="isDialerConnected || isAgentOnCall"
+                      v-if="isDialerOrAgentOnCall"
                       data-testid="item-parked-menu"
                       @hide="showParkedCallMenu = false">
                 <q-list>
