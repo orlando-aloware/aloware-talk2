a6f1bbd7afe502a4f5937377cdb38827db6fff3d
Fernando Ortuno <fernando@aloware.com>
2025-04-25 14:06:09 -0400

    fix(wat-1120): small refactor

--- src/components/communication-info.vue
+++ src/components/communication-info.vue
@@ -995,7 +995,7 @@ import { marked } from 'marked'
 import UserDisplay from 'src/components/user-display.vue'
 import { TAG_CATEGORIES as TagCategories } from 'src/constants/tag-categories'
 import API from 'src/plugins/api/api'
-import { aclMixin, agentMixin, avatarMixin, classicMixin, communicationInfoMixin, dateMixin, liveCallsMixin, mentionsMixin, notificationMixin, userMixin } from 'src/plugins/mixins'
+import { aclMixin, avatarMixin, classicMixin, communicationInfoMixin, dateMixin, liveCallsMixin, mentionsMixin, notificationMixin, userMixin } from 'src/plugins/mixins'
 import { mapState } from 'vuex'
 import * as AnswerTypes from '../constants/answer-types'
 import * as CommunicationCallbackStatus from '../constants/callback-status'
@@ -1015,7 +1015,6 @@ export default {
 
   mixins: [
     aclMixin,
-    agentMixin,
     avatarMixin,
     communicationInfoMixin,
     dateMixin,
