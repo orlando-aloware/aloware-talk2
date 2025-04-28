b296e9fe83cecd4a7b15c9dfcde11a128ce0b6c2
Tito Alvarez <tito@aloware.com>
2025-04-28 09:19:08 -0600

    chore: improved filtering and sorting styling

--- src/components/teaminbox/teaminbox-filter-sort.vue
+++ src/components/teaminbox/teaminbox-filter-sort.vue
@@ -1,23 +1,52 @@
 <template>
   <div class="teaminbox-filter-sort d-flex justify-content-between align-items-center">
     <div class="teaminbox-filter">
-      <b-dropdown :text="filterOption" variant="outline-primary" size="sm" class="filter-dropdown">
+      <b-dropdown variant="outline-primary" size="sm" class="filter-dropdown">
+        <template #button-content>
+          <span v-if="filterOption === 'All'">All</span>
+          <span v-else-if="filterOption === 'Unread'">
+            <unread-mail-icon class="mr-1" /> Unread
+          </span>
+        </template>
         <b-dropdown-item :active="filterOption === 'All'" @click="setFilterOption('All')">All</b-dropdown-item>
-        <b-dropdown-item :active="filterOption === 'Unread'" @click="setFilterOption('Unread')">Unread</b-dropdown-item>
+        <b-dropdown-item :active="filterOption === 'Unread'" @click="setFilterOption('Unread')">
+          <unread-mail-icon class="mr-2" /> Unread
+        </b-dropdown-item>
       </b-dropdown>
     </div>
     <div class="teaminbox-sort">
-      <b-dropdown :text="sortOption" variant="outline-primary" size="sm" class="sort-dropdown" right>
-        <b-dropdown-item :active="sortOption === 'Newest'" @click="setSortOption('Newest')">Newest</b-dropdown-item>
-        <b-dropdown-item :active="sortOption === 'Oldest'" @click="setSortOption('Oldest')">Oldest</b-dropdown-item>
+      <b-dropdown variant="outline-primary" size="sm" class="sort-dropdown" right>
+        <template #button-content>
+          <span v-if="sortOption === 'Newest'">
+            <sort-up-icon class="mr-1" /> Newest
+          </span>
+          <span v-else-if="sortOption === 'Oldest'">
+            <sort-down-icon class="mr-1" /> Oldest
+          </span>
+        </template>
+        <b-dropdown-item :active="sortOption === 'Newest'" @click="setSortOption('Newest')">
+          <sort-up-icon class="mr-2" /> Newest
+        </b-dropdown-item>
+        <b-dropdown-item :active="sortOption === 'Oldest'" @click="setSortOption('Oldest')">
+          <sort-down-icon class="mr-2" /> Oldest
+        </b-dropdown-item>
       </b-dropdown>
     </div>
   </div>
 </template>
 
 <script>
+import UnreadMailIcon from '../../components/icons/inbox/unread-mail-icon.vue'
+import SortUpIcon from '../../components/icons/inbox/sort-up-icon.vue'
+import SortDownIcon from '../../components/icons/inbox/sort-down-icon.vue'
+
 export default {
   name: 'TeamInboxFilterSort',
+  components: {
+    UnreadMailIcon,
+    SortUpIcon,
+    SortDownIcon
+  },
   data () {
     return {
       filterOption: 'All',
