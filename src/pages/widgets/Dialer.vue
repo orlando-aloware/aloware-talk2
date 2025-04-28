8c686de4a952e8fd9a13815c0e5b3d2d5d179dca
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2025-04-28 12:57:50 -0700

    Update Dialer.vue

--- src/pages/widgets/Dialer.vue
+++ src/pages/widgets/Dialer.vue
@@ -228,7 +228,14 @@ export default {
       this.showAlertCallFinished = false
 
       do {
-        console.log('waiting for generated token', this.dialer.currentStatus, this.agentStatus)
+        if (this.dialer.currentStatus !== 'GENERATING_TOKEN') {
+          console.log('waiting for dialer token to be generated', this.dialer.currentStatus)
+        }
+
+        if (this.agentStatus !== AgentStatus.AGENT_STATUS_ON_CALL) {
+          console.log('waiting for agent to become available to make the call', this.agentStatus)
+        }
+        
         await new Promise(resolve => setTimeout(resolve, 500)) // Check every 0.5sec
       } while (this.dialer.currentStatus === 'GENERATING_TOKEN' || this.agentStatus === AgentStatus.AGENT_STATUS_ON_CALL)
 
