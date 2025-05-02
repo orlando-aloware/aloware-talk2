b131d3ce8fc6a17d7402b62288bf958d47d80522
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2025-05-01 20:03:04 -0700

    Cleanup
    Signed-off-by: Sohrab Sheikhani <sheikhani.sohrab@gmail.com>

--- src/boot/posthog.js
+++ src/boot/posthog.js
@@ -2,14 +2,16 @@ import posthog from 'posthog-js'
 
 export default {
   install (Vue) {
-    posthog.init(
-      '',
-      {
-        api_host: 'https://us.i.posthog.com',
-        capture_pageview: false,
-        capture_pageleave: true
-      }
-    )
+    if (process.env.POSTHOG_API_KEY) {
+      posthog.init(
+        process.env.POSTHOG_API_KEY,
+        {
+          api_host: 'https://us.i.posthog.com',
+          capture_pageview: false,
+          capture_pageleave: true
+        }
+      )
+    }
 
     Vue.prototype.$posthog = posthog
   }
