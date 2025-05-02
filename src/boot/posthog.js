7a60306e72b1800536016a3fc74786b1f3810ee8
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2025-05-01 19:52:37 -0700

    Cleanup
    Signed-off-by: Sohrab Sheikhani <sheikhani.sohrab@gmail.com>

--- src/boot/posthog.js
+++ src/boot/posthog.js
@@ -3,7 +3,7 @@ import posthog from 'posthog-js'
 export default {
   install (Vue) {
     posthog.init(
-      'phc_Qvfr0aYv5uZP7iZbEvy7ylcRalLTNeuTTJ6Gs2sFmyj',
+      '',
       {
         api_host: 'https://us.i.posthog.com',
         capture_pageview: false,
