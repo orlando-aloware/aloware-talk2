4a1b3963c14da728b431a14a2c69283b2ae35f68
Tito Alvarez <tito@aloware.com>
2025-05-09 13:49:10 -0600

    fix(wat-1478): navigation and store issues

--- src/components/teaminbox/teaminbox-nav-list.vue
+++ src/components/teaminbox/teaminbox-nav-list.vue
@@ -231,15 +231,26 @@ export default {
       const queryString = getQueryString(this.$route.query)
       const route = `/team-inboxes/${inboxId}` + (contactId ? `/contacts/${contactId}/communications` : '') + queryString
 
-      // avoid redundant navigation
-      if (this.$route.path !== route) {
+      // avoid redundant navigation (including query)
+      if (this.$route.fullPath !== route) {
         if (force) {
           // force redirect to the first inbox to prevent the user from navigating back to the Team Inboxes page without any inboxId
-          this.$router.replace(route)
+          this.$router.replace(route).catch(err => {
+            console.log('error when force redirecting to the first inbox', err)
+            if (err.name !== 'NavigationDuplicated' && err.name !== 'NavigationCancelled') {
+              console.error(err)
+            }
+          })
           return
         }
 
-        this.$router.push(route)
+        // Catch added since we are only adding a query string
+        this.$router.push(route).catch(err => {
+          console.log('error when navigating to the inbox', err)
+          if (err.name !== 'NavigationDuplicated' && err.name !== 'NavigationCancelled') {
+            console.error(err)
+          }
+        })
       }
     },
 
