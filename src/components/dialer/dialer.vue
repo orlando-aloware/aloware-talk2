34040bf039a7684c6cc8d8b891f3518d6c28f727
Reza Aleyasin <reza@aloware.com>
2025-05-13 02:01:22 +0300

    feature(ai-151): fix comments

--- src/components/dialer/dialer.vue
+++ src/components/dialer/dialer.vue
@@ -1046,10 +1046,6 @@ export default {
           })
             .then(response => {
               console.log('Successfully dropped other agents from call')
-              // Change agent status from SENTRY to ON_CALL after successful takeover
-              if (this.agentStatus === this.AgentStatus.AGENT_STATUS_SENTRY) {
-                this.changeAgentStatus(this.AgentStatus.AGENT_STATUS_ACCEPTING_CALLS, false, 1, 'Talk-DropAloAiAgent')
-              }
             })
             .catch(err => {
               console.error('Error dropping other agents from call:', err)
