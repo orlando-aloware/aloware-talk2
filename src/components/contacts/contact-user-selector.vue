9b6954e0be297979be7f258218ee37b73d031223
Yhael S <yhael@aloware.com>
2025-05-14 14:50:48 -0600

    cleanup because component is not used anywhere

--- src/components/contacts/contact-user-selector.vue
+++ src/components/contacts/contact-user-selector.vue
@@ -110,17 +110,11 @@ export default {
       const usersArray = { data: nUsers }
 
       if (!this.hideExtensions && this.extensionUsers && this.extensionUsers.length > 0) {
-        let eUsers = [...this.extensionUsers]
+        const eUsers = [...this.extensionUsers]
         eUsers.unshift({
           group: 'Extensions',
           disable: true
         })
-
-        // If AloAi users should not be included, filter them out
-        if (!this.includeAloAiUsers) {
-          eUsers = eUsers.filter((user) => user.type !== User.TYPE_AI_AGENT)
-        }
-
         usersArray.data = [...nUsers, ...eUsers]
       }
 
