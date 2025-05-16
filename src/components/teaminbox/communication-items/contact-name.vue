8429e6ec3c4ccd332ae2cc077bc4287882cbc038
Tito Alvarez <tito@aloware.com>
2025-05-16 17:58:01 -0600

    chore: unthreadig all the unthreaded items (no repeats)

--- src/components/teaminbox/communication-items/contact-name.vue
+++ src/components/teaminbox/communication-items/contact-name.vue
@@ -3,6 +3,9 @@
        :id="`contact-name-${_uid}`">
     <div class="contact-name ellipse">
       {{ name }}
+      <span class="contact-name__repeat-counter">
+        {{ repeats ? `(${repeats + 1})` : '' }}
+      </span>
     </div>
 
     <b-popover triggers="hover"
