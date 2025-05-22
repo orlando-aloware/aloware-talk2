38957f9729ee5e519e339997d773a1efbf164ab2
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2025-05-22 09:16:14 -0700

    Hard code soketi
    Signed-off-by: Sohrab Sheikhani <sheikhani.sohrab@gmail.com>

--- src/plugins/mixins/broadcast.mixin.js
+++ src/plugins/mixins/broadcast.mixin.js
@@ -54,7 +54,7 @@ export default {
        * for events that are broadcast by Laravel. Echo and event broadcasting
        * allows your team to easily build robust real-time web applications.
        */
-      const broadcastDriver = this.currentCompany?.broadcast_driver || 'soketi'
+      const broadcastDriver = 'soketi'
 
       window.Echo = this.initEcho(broadcastDriver)
       this.broadcastListen()
