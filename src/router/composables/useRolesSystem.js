e7469b3e4df200d416b9958002736043bf1482c9
Miroslav Borisov <miroslav@aloware.com>
2025-05-23 03:21:10 +0800

    WAT-1544 | Fix | Urgent fix

--- src/router/composables/useRolesSystem.js
+++ src/router/composables/useRolesSystem.js
@@ -1,5 +1,5 @@
-const focusedPowerDialerUserIds = []
-const focusedPowerDialerCompanyIds = []
+const focusedPowerDialerUserIds = process.env.NODE_ENV === 'production' ? [69563] : [69563, 76585]
+const focusedPowerDialerCompanyIds = process.env.NODE_ENV === 'production' ? [] : []
 
 export const allowedRoutes = {
   focusedPowerDialer: [
@@ -27,7 +27,7 @@ export const allowedRoutes = {
 export const setRouterType = (authModule) => {
   // TODO - MOVE TO BACKEND FLAG IF PoC IS SUCCESSFUL
 
-  if (!focusedPowerDialerUserIds.includes(authModule.state.profile.id) || !focusedPowerDialerCompanyIds.includes(authModule.state.profile.company_id)) {
+  if (focusedPowerDialerUserIds.includes(authModule.state.profile.id) || focusedPowerDialerCompanyIds.includes(authModule.state.profile.company_id)) {
     authModule.commit('SET_FOCUSED_POWER_DIALER', true)
   }
 }
