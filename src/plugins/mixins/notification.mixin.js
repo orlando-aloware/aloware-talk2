1c3259e0e68a4df3c3e593817b5d1485013c1747
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2025-05-30 14:04:55 -0700

    Fixed Sentry issue
    https://aloware.sentry.io/issues/6432432644/?project=5867343&query=is%3Aunresolved&referrer=issue-stream&stream_index=19
    Signed-off-by: Sohrab Sheikhani <sheikhani.sohrab@gmail.com>

--- src/plugins/mixins/notification.mixin.js
+++ src/plugins/mixins/notification.mixin.js
@@ -349,7 +349,7 @@ export default {
         if (['callFishing', 'incomingCall'].includes(params.data.type)) {
           console.log('processActionNotification - params.data', params.data)
 
-          if (this.isOnPowerDialerSessionRoute && ringGroup.experimental_fishing_mode_repeat_call_routing) {
+          if (this.isOnPowerDialerSessionRoute && ringGroup?.experimental_fishing_mode_repeat_call_routing) {
             const contactWithCommunication = {
               ...params.data.contact,
               'communication_id': params.data.communicationId
