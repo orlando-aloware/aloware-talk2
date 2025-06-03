5c8789521877d13e8d7ff0cfded9752e9162364f
Paulo <paulo@aloware.com>
2025-06-03 11:20:01 -0400

    fix css

--- src/components/action-notification.vue
+++ src/components/action-notification.vue
@@ -21,7 +21,7 @@
          @click="onNotificationClick">
       <div class="d-flex flex-row align-items-center">
         <b-badge v-if="id === 'callFishing' && queueCount > 1"
-                 class="call-fishing-queue-badge d-flex justify-center align-items-center position-absolute ml-4"
+                 class="call-fishing-queue-badge d-flex justify-center align-items-center position-absolute"
                  variant="danger"
                  pill>
           {{ queueCount }}
