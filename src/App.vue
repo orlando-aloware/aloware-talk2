d68dfceff52eed097440f85eb3f4f1d391119e5a
Tito Alvarez <tito@aloware.com>
2025-06-05 20:45:15 -0600

    Revert "Merge pull request #2206 from aloware/WAT-1409/fix/inbound-calls-notification-sound"
    This reverts commit 203539662aa2579d2916f5156605143ee57046e1.

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,6 @@
                  v-if="cookieValidated" />
     <portal-target name="app"
                    multiple />
-    <notification-sound-user-action-toast />
     <action-notification id="system"
                          v-if="!isWidget" />
     <action-notification id="sms"
@@ -42,7 +41,6 @@ import { mapActions, mapState } from 'vuex'
 import Intercom from 'components/intercom'
 import HeaderNotification from 'components/header-notification'
 import { accessMixin } from 'src/plugins/mixins'
-import NotificationSoundUserActionToast from 'components/notification-sound-user-action-toast'
 
 export default {
   name: 'App',
@@ -54,8 +52,7 @@ export default {
   components: {
     HeaderNotification,
     Intercom,
-    ActionNotification,
-    NotificationSoundUserActionToast
+    ActionNotification
   },
 
   data () {
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1248,7 +1248,13 @@ export default {
       }, checkInterval)
     }
 
-    this.checkMediaPlaybackRequiresUserGesture()
+    if (this.mediaPlaybackRequiresUserGesture()) {
+      window.addEventListener('keydown', this.removeBehaviorsRestrictions)
+      window.addEventListener('mousedown', this.removeBehaviorsRestrictions)
+      window.addEventListener('touchstart', this.removeBehaviorsRestrictions)
+    } else {
+      this.setEnableAudio(true)
+    }
 
     console.log('Push permission: ' + window.Push.Permission.get())
 
@@ -1271,16 +1277,6 @@ export default {
   },
 
   methods: {
-    async checkMediaPlaybackRequiresUserGesture () {
-      if (await this.mediaPlaybackRequiresUserGesture()) {
-        window.addEventListener('keydown', this.removeBehaviorsRestrictions)
-        window.addEventListener('mousedown', this.removeBehaviorsRestrictions)
-        window.addEventListener('touchstart', this.removeBehaviorsRestrictions)
-      } else {
-        this.setEnableAudio(true)
-      }
-    },
-
     processUrl (url) {
       // New format: expected url like "contact-<phoneNumber>" or "contact-<phoneNumber>?first=...&last=...&isCompany=..."
       let cleaned = url.replace(/contact[:-]/, '')
@@ -1601,21 +1597,22 @@ export default {
       this.setEnableAudio(true)
     },
 
-    async mediaPlaybackRequiresUserGesture () {
+    mediaPlaybackRequiresUserGesture () {
       // test if play() is ignored when not called from an input event handler
       const audio = document.createElement('audio')
+      const promise = audio.play()
 
-      try {
-        await audio.play()
-        // Auto-play started successfully
-        // No user gesture required
-        return audio.paused
-      } catch (error) {
-        // Auto-play was prevented
-        // User gesture is required
-        return true
-      } finally {
-        audio.remove()
+      if (promise !== undefined) {
+        promise
+          .catch(() => {
+            // Auto-play was prevented
+            // Show a UI element to let the user manually start playback
+            return true
+          })
+          .then(() => {
+            // Auto-play started
+            return audio.paused
+          })
       }
     },
 
