55305b38c1bdcba7edc20306e5fde3e6009082a7
Fernando Ortuno <fernando@aloware.com>
2025-06-06 17:44:09 -0400

    fix(wat-1148): added logic to prevent redirect loop when profile is already open

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1222,6 +1222,17 @@ export default {
   mounted () {
     if (this.authenticated) {
       this.sidebarVisible = true
+
+      // Check if profile and company_id exist before making the request
+      if (this.profile?.company_id && !this.hasCompanyLegacyInboxEnabled && !this.hasCompanyTeamInboxEnabled) {
+        this.$axios.get('/api/v1/company/' + this.profile.company_id)
+          .then((res) => {
+            this.setCurrentCompany(res.data)
+          })
+          .catch(err => {
+            console.error('Error fetching company info:', err)
+          })
+      }
     }
 
     // using the negative because the default should be expanded, so whenever the value is falsy means expanded
@@ -2831,6 +2842,10 @@ export default {
       }
 
       this.checkDebounce()
+
+      if (this.profile?.company && !this.hasCompanyLegacyInboxEnabled && !this.hasCompanyTeamInboxEnabled) {
+        this.setCurrentCompany(this.profile.company)
+      }
 
       const toDepth = to.path.split('/').length
       const fromDepth = from.path.split('/').length
