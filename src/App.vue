cebc7a1784013d64e9393bfaa79c1fa91df7bcae
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2025-06-12 14:20:11 -0700

    Added manual session capture control to FullStory
    Signed-off-by: Sohrab Sheikhani <sheikhani.sohrab@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -286,8 +286,9 @@ export default {
           ...data,
           timezone_str: window.timezone
         })
+        window.FS('start')
       }).catch(() => {
-        console.log('Error while retrieving fullstory metadata from server. Using local variables.')
+        console.log('Error while retrieving fullstory metadata from server')
       })
     },
 
