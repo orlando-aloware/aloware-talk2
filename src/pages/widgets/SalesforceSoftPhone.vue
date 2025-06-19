a484ffa815d50db7e4f8852fb94e651f4318583a
shandak <shandakvm@gmail.com>
2025-06-19 11:21:34 -0400

    fix a problem with a broken select line

--- src/pages/widgets/SalesforceSoftPhone.vue
+++ src/pages/widgets/SalesforceSoftPhone.vue
@@ -421,8 +421,8 @@ export default {
       }
 
       const params = {
-        timezone: this.contact.timezone,
-        name: this.contact.name,
+        timezone: this.contactTimezone,
+        name: this.contactName,
         calls_notifications_open_time: this.currentCompany.calls_notifications_open_time,
         calls_notifications_close_time: this.currentCompany.calls_notifications_close_time
       }
