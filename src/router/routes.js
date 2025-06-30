11c671f9a5eeac34893a196ee9b38fe4f35eb62d
shandak <shandakvm@gmail.com>
2025-06-30 15:21:15 -0400

    INT-561 Salesforce Errors for Messenger

--- src/router/routes.js
+++ src/router/routes.js
@@ -32,6 +32,7 @@ const Broadcasts = () => import('pages/broadcast/Broadcasts.vue')
 const BroadcastAdd = () => import('pages/broadcast/BroadcastAdd.vue')
 const AccountRegistration = () => import('pages/account-registration/AccountRegistration.vue')
 const HubSpotMessageWidgetError = () => import('pages/widgets/HubSpotMessageWidgetError.vue')
+const SalesforceMessageWidgetError = () => import('pages/widgets/SalesforceMessageWidgetError.vue')
 const SalesforceSoftPhone = () => import('pages/widgets/SalesforceSoftPhone.vue')
 const AloAi = () => import('pages/AloAi.vue')
 const Apps = () => import('pages/Apps.vue')
@@ -685,6 +686,15 @@ const routes = [
           isWidget: true
         },
         component: HubSpotMessageWidgetError
+      },
+      {
+        name: 'Salesforce Widget Error',
+        path: '/errors/salesforce',
+        props: true,
+        meta: {
+          isWidget: true
+        },
+        component: SalesforceMessageWidgetError
       }
     ]
   },
