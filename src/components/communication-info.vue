714d135499ac40c3ec0896bc35cb75e305753c7b
marcelo-aloware <marcelo@aloware.com>
2025-07-03 14:32:39 -0300

    fix(WAT-1160): add method to get the communication appointment campaign id

--- src/components/communication-info.vue
+++ src/components/communication-info.vue
@@ -643,7 +643,8 @@
                                class="d-flex flex-row justify-content-center w-100"
                                data-testid="communication-info-sms-reminders"
                                :communicationId="communication.id"
-                               :campaignId="campaignId"
+                               :campaignId="getAppointmentCampaignId(communication)"
+                               :contactId="contact.id"
                                :appointmentDatetime="communication.engagement_data.appointment_datetime"
                                v-if="communication.type === CommunicationTypes.APPOINTMENT && campaignId">
                 </sms-reminders>
@@ -1323,6 +1324,10 @@ export default {
       }
 
       return false
+    },
+
+    getAppointmentCampaignId (communication) {
+      return communication.engagement_data?.appointment_campaign_id ?? this.campaignId
     }
   },
 
