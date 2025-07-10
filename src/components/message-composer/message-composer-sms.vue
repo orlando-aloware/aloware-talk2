df6fdd8ff7e92bbd7e236de2b98c9ad696e96045
marcelo-aloware <marcelo@aloware.com>
2025-07-10 17:23:36 -0300

    feat(WAT-1654): include ring_group_id in outbound communications from dead end lines

--- src/components/message-composer/message-composer-sms.vue
+++ src/components/message-composer/message-composer-sms.vue
@@ -278,6 +278,7 @@ import AudioPlaceholder from 'components/message-composer/file-placeholders/audi
 import MessageComposerOptions from 'components/message-composer/message-composer-options'
 import * as CommunicationTypes from 'src/constants/communication-types'
 import { kycMixin, selectorMixin } from 'src/plugins/mixins'
+import { CALL_ROUTER_BEHAVIOR_MODE_DEAD_END, CALL_ROUTER_BEHAVIOR_MODE_IVR } from 'src/constants/campaign-call-router-behaviors'
 export default {
   name: 'message-composer-sms',
 
@@ -566,9 +567,12 @@ export default {
         gif: this.messageComposer.sms.gif_url
       }
 
-      if (this.activeInboxId && this.selectedLine?.ivr_id) {
+      if (
+        this.activeInboxId &&
+        [CALL_ROUTER_BEHAVIOR_MODE_DEAD_END, CALL_ROUTER_BEHAVIOR_MODE_IVR].includes(this.selectedLine?.call_router_behavior)
+      ) {
         // Ring Group ID is used to identify the current inbox
-        // when sending a message from an IVR line
+        // when sending a message from an IVR or Dead End line
         data.ring_group_id = this.activeInboxId
       }
 
