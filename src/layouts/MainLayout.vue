9c2fba17477b7a1f5acc2a00e866c7d9f3201eca
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2025-07-17 12:26:42 -0700

    Disable some logic to test
    Signed-off-by: Sohrab Sheikhani <sheikhani.sohrab@gmail.com>

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1271,6 +1271,10 @@ export default {
         this.closeCallNotifications(this.getNotificationType(communication.ring_group_id), communication.id)
       }
 
+      if (this.currentCompany?.id === 7113) {
+        return
+      }
+
       if (!this.checkCommunicationMatchesUserAccessibility(communication) && !isCommunicationHasUnownedContact) {
         return
       }
