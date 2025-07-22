ffb6d90bf577dc4d0b8f642183638b4974ed104e
Fernando Ortuno <fernando@aloware.com>
2025-07-22 17:35:37 -0400

    fix(pla-772): added comment

--- src/pages/CommunicationsView.vue
+++ src/pages/CommunicationsView.vue
@@ -112,6 +112,8 @@ export default {
           const routeChannel = this.$route.params?.channel ?? DEFAULT_COMMUNICATIONS_CHANNEL // default fallback
           channel = this.navListItems.find(item => item.value === routeChannel)
 
+          // If channel is not found, redirect to default communications route
+
           if (!channel && routeChannel !== DEFAULT_COMMUNICATIONS_CHANNEL) {
             this.$router.push({ name: DEFAULT_COMMUNICATIONS_ROUTE_NAME })
             return
