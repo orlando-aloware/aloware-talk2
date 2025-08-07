4c5a631accdf4a371c2552635611089db69b35c7
Paulo <paulo@aloware.com>
2025-08-07 14:25:53 -0400

    fix suggestion

--- src/components/dialer/active-call.vue
+++ src/components/dialer/active-call.vue
@@ -206,7 +206,8 @@ export default {
     },
 
     isCallCompleted () {
-      return ((this.dialer.communication && this.dialer.communication.disposition_status2 !== CommunicationDispositionStatus.DISPOSITION_STATUS_INPROGRESS_NEW) || ['HANGING_UP_CALL', 'CALL_DISCONNECTED', 'WRAP_UP'].includes(this.dialer.currentStatus))
+      return (this.dialer.communication && this.dialer.communication.disposition_status2 !== CommunicationDispositionStatus.DISPOSITION_STATUS_INPROGRESS_NEW) ||
+        ['HANGING_UP_CALL', 'CALL_DISCONNECTED', 'WRAP_UP'].includes(this.dialer.currentStatus)
     }
   },
 
