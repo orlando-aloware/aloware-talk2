bfd1137470da27cc84b34f48dc8d537045720451
marcelo-aloware <marcelo@aloware.com>
2025-08-13 14:18:46 -0300

    refactor(WAT-1699): handle 403 error for contact access and notify user

--- src/plugins/mixins/contact.mixin.js
+++ src/plugins/mixins/contact.mixin.js
@@ -793,6 +793,12 @@ export default {
 
         return res
       }).catch(err => {
+        if (err.response.status === 403) {
+          // No access to contact
+          this.$router.replace({ name: TEAMINBOXES_MENU_TITLE })
+          this.$generalNotification(err.response.data?.error || 'You do not have access to this contact', 'error')
+        }
+
         if (!useDefaultCatch) {
           return
         }
