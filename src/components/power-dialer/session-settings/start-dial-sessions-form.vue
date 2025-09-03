d3a98acfe676ca6e810adf7a902d57bc952c58ed
Paulo <paulo@aloware.com>
2025-09-03 13:20:39 -0400

    fix(pow-620): saved dial sessions fields enablement

--- src/components/power-dialer/session-settings/start-dial-sessions-form.vue
+++ src/components/power-dialer/session-settings/start-dial-sessions-form.vue
@@ -448,22 +448,12 @@ export default {
     },
 
     disableField (field, ignoreDisabledFlag = false) {
-      // if not disabled, it is not needed to perform any other checks
-      if (!this.disabled) {
-        return false
-      }
-
       if (this.disabled && !ignoreDisabledFlag) {
         return true
       }
 
       // account power dialer settings
-      let powerDialerSettings = this.currentCompany?.power_dialer_settings ?? {}
-
-      // if making changes to a saved session, the settings should come from it
-      if (this.selectedSettings?.id) {
-        powerDialerSettings = this.selectedSettings
-      }
+      const powerDialerSettings = this.currentCompany?.power_dialer_settings ?? {}
 
       const redialRequired = powerDialerSettings.min_redials > 0
 
@@ -471,8 +461,13 @@ export default {
         case 'min_redials': return redialRequired
         case 'force_immediate_redial': return redialRequired && powerDialerSettings.force_immediate_redial
         case 'force_sms': return redialRequired && powerDialerSettings.force_sms
-        case 'successful_call_disposition_ids': return redialRequired && (powerDialerSettings.successful_call_disposition_ids?.length > 0)
-        case 'call_disposition_ids': return powerDialerSettings.call_disposition_ids?.length > 0
+        case 'successful_call_disposition_ids':
+          return redialRequired &&
+            (powerDialerSettings.successful_call_disposition_ids?.length > 0 &&
+            // in case of editing a setting, it should enable the field if there are no items pre-selected
+            (this.selectedSettings?.id ? this.selectedSettings?.successful_call_disposition_ids?.length > 0 : true))
+        case 'call_disposition_ids':
+          return this.disabled && this.selectedSettings?.call_disposition_ids?.length > 0
       }
 
       return false
