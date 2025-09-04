d2eaafeae6ca11cca558ebcc8f938313174b1970
marcelo-aloware <marcelo@aloware.com>
2025-09-04 14:16:48 -0300

    refactor(WAT-1716): change personal inbox verification to the new is_personal_inbox flag

--- src/plugins/mixins/live-calls.mixin.js
+++ src/plugins/mixins/live-calls.mixin.js
@@ -225,7 +225,7 @@ export default {
     },
 
     isPersonalInbox () {
-      return this.communication.campaign?.call_waiting_ring_group_id && this.hasCompanyTeamInboxEnabled
+      return this.communication.campaign?.is_personal_inbox && this.hasCompanyTeamInboxEnabled
     }
   },
 
