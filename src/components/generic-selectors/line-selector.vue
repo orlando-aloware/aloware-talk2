90315c33427abb7d156b1c52f69592ff6f76cbce
marcelo-aloware <marcelo@aloware.com>
2025-09-08 13:23:05 -0300

    Revert "Merge pull request #2531 from aloware/revert-2526-WAT-1710/fix/all-inboxes-visibility-issues-hotfix"
    This reverts commit 1e213dc585f875172d7f6fe87bda5cf9bd10d06f, reversing
    changes made to c1e8e432803810989496a96876788b42fd0a2479.

--- src/components/generic-selectors/line-selector.vue
+++ src/components/generic-selectors/line-selector.vue
@@ -108,6 +108,7 @@ import userMixin from 'src/plugins/mixins/user.mixin'
 import { COMPANY_AGENT } from 'src/constants/roles'
 import { agentAvailableCampaignsCallback, isIvrOrDeadEndCampaign } from 'src/plugins/helpers/campaigns'
 import { TEAMINBOXES_MENU_TITLE } from 'src/router/routes'
+import { ALL_INBOXES_ID } from 'src/store/teaminbox/teaminbox.store'
 
 export default {
   name: 'line-selector',
@@ -283,7 +284,7 @@ export default {
     ...mapState(['campaigns', 'campaignsIsLoading']),
     ...mapState('cache', ['currentCompany']),
     ...mapState('auth', ['profile']),
-    ...mapState('TeamInbox', ['contactsLastUsedLines', 'activeInbox', 'activeInboxId', 'teamInboxCampaigns', 'loadingTeamInboxCampaigns']),
+    ...mapState('TeamInbox', ['contactsLastUsedLines', 'activeInbox', 'activeInboxId', 'teamInboxCampaigns', 'loadingTeamInboxCampaigns', 'inboxes']),
     ...mapGetters('TeamInbox', ['activeInboxCampaignIds']),
 
     useTeamInboxCampaigns () {
@@ -352,11 +353,15 @@ export default {
         )
       }
 
+      const inboxCampaigns = this.$route.params.inboxId === ALL_INBOXES_ID
+        ? this.getCampaignIdsByInboxId(+this.$route.query.inboxId)
+        : this.activeInboxCampaignIds
+
       return !this.preSelectedTeamInboxLineId
         ? activeCampaigns
         : activeCampaigns.filter(
           campaign =>
-            this.activeInboxCampaignIds.includes(campaign.id) ||
+            inboxCampaigns.includes(campaign.id) ||
             isIvrOrDeadEndCampaign(campaign)
         )
     },
@@ -520,6 +525,13 @@ export default {
         this.$emit('change', null)
         this.$emit('invalid-line-selection', this.allCampaigns.find(({ id }) => id === lineId))
       }
+    },
+
+    getCampaignIdsByInboxId (inboxId) {
+      const inbox = this.inboxes.find(inbox => inbox.id === inboxId)
+      const inboxCampaignIds = inbox?.campaign_ids || []
+      const inboxCallWaitingCampaignIds = inbox?.campaign_ids_as_call_waiting_ring_group || []
+      return [...inboxCampaignIds, ...inboxCallWaitingCampaignIds]
     }
   },
 
