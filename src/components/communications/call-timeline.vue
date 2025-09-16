620e3c3b163b249b11faedde77b53abdb01e5d1d
Amir <amir@aloware.com>
2025-09-16 15:36:24 +0300

    feature(tel-608): applied feedback

--- src/components/communications/call-timeline.vue
+++ src/components/communications/call-timeline.vue
@@ -1,38 +1,31 @@
 <template>
-  <div data-testid="call-timeline-wrapper">
-    <q-card
-      class="ring-group-snapshot-card"
-      flat
-      bordered
-      data-testid="call-timeline-card"
-    >
-    <q-card-section data-testid="call-timeline-card-section">
+  <div data-testid="call-timeline-wrapper" class="call-timeline-wrapper">
+    <q-card class="ring-group-snapshot-card" flat bordered data-testid="call-timeline-card">
+      <q-card-section data-testid="call-timeline-card-section">
         <div class="text-h6">Call Quality</div>
       </q-card-section>
       <q-separator inset />
-    <q-timeline color="primary" class="ml-3 mt-3">
-      <q-timeline-entry v-for="item in timeline" :key="item.id"
-        :title="item.title"
-        :subtitle="item.datetime | fixTimelineDateTime"
-        icon="phone"
-      >
-        <div>
-          {{ item.description }}
-        </div>
-      </q-timeline-entry>
-    </q-timeline>
-  </q-card>
+      <q-timeline color="primary" class="ml-3 mt-3">
+        <q-timeline-entry v-for="item in timeline" :key="item.id" :title="item.title" :subtitle="subtitle(item)"
+          :icon="icon(item)">
+          <div>
+            {{ item.description }}
+          </div>
+        </q-timeline-entry>
+      </q-timeline>
+    </q-card>
   </div>
 </template>
 
 <script>
-import { fixTimelineDateTime } from 'src/plugins/filters/datetime.filters'
+import { fixTimelineDateTime, fixDuration } from 'src/plugins/filters/datetime.filters'
 
 export default {
   name: 'call-timeline',
 
   filters: {
-    fixTimelineDateTime
+    fixTimelineDateTime,
+    fixDuration
   },
 
   props: {
@@ -43,10 +36,41 @@ export default {
     }
   },
 
-  data () {
-    return {
-
+  methods: {
+    subtitle (item) {
+      const duration = item.duration
+      const datetime = item.datetime
+      let label = ''
+      if (duration) {
+        label += this.$options.filters.fixDuration(duration)
+      }
+      if (datetime) {
+        if (label) {
+          label += ' | '
+        }
+        label += this.$options.filters.fixTimelineDateTime(datetime)
+      }
+      return label
+    },
+    icon (item) {
+      if (/^https?:\/\//.test(item.icon)) {
+        return `img:${item.icon}`
+      }
+      return item.icon
     }
   }
 }
 </script>
+
+<style>
+.call-timeline-wrapper .q-timeline__dot .q-icon>img {
+  width: unset !important;
+  height: unset !important;
+}
+
+.call-timeline-wrapper .q-icon>svg,
+.call-timeline-wrapper .q-icon>img {
+  width: unset !important;
+  height: unset !important;
+}
+</style>
