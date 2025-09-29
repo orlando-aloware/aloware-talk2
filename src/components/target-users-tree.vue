356401f02bc606ee7062e1bf10f0861d36cf762d
Fernando Ortuno <fernando@aloware.com>
2025-09-29 17:53:42 -0400

    fix(wat-1750): added logs

--- src/components/target-users-tree.vue
+++ src/components/target-users-tree.vue
@@ -163,7 +163,7 @@ export default {
           label: this.getUserName(user.data)
         })
       }
-
+      console.log('generateAttemptingUsersTree', data)
       // return the tree
       return data
     }
