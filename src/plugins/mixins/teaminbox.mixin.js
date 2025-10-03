1d113feeef3a3743808b67432edc7c27cbadc316
Sohrab Sheikhani <sheikhani.sohrab@gmail.com>
2025-10-03 09:42:24 -0700

    Update teaminbox.mixin.js

--- src/plugins/mixins/teaminbox.mixin.js
+++ src/plugins/mixins/teaminbox.mixin.js
@@ -44,7 +44,7 @@ export default {
     },
 
     companyHasAccessToAllInboxes () {
-      // All Inboxes Whitelisted companies (WAT-1772)
+      // @custom All Inboxes Whitelisted companies (WAT-1772)
       const ALL_INBOXES_WHITELISTED_COMPANIES = [135, 497, 1362, 1413, 1990, 2382, 2521, 3493, 4152, 5018, 5597, 6224, 6554, 7113, 1728, 1979, 6663, 7666, 7756, 7757]
 
       return this.isCompanyPartOfAlowareDemoCompanies(this.profile.company_id) || ALL_INBOXES_WHITELISTED_COMPANIES.includes(this.profile.company_id)
