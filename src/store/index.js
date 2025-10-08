16dbecd3410f78cfca98c4386ac57f6bc5f6f906
Amir <amir@the-h.me>
2025-10-08 16:27:45 +0300

    fix(tel-618): better format

--- src/store/index.js
+++ src/store/index.js
@@ -1033,7 +1033,7 @@ export default function (/* { ssrContext } */) {
 
         // if the current communication.call_wrap_up_options.user_id is not empty but the new communication.call_wrap_up_options.user_id is empty, use the current communication.call_wrap_up_options
         // this is to prevent overwriting the current communication.call_wrap_up_options when API call gets the response after the communciation.updated event is received.
-        if (communication?.call_wrap_up_options?.user_id === null && state.dialer?.communication?.call_wrap_up_options?.user_id !== null) {
+        if (communication?.call_wrap_up_options?.user_id === null && ![null, undefined].includes(state.dialer?.communication?.call_wrap_up_options?.user_id)) {
           communication.call_wrap_up_options = state.dialer.communication.call_wrap_up_options
         }
 
