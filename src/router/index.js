238f0928d835f90087b9c468992bf863f54bcbab
Jego Carlo Ramos <jego@aloware.com>
2025-10-17 01:52:09 +0800

    refactor: removed unused backup code

--- src/router/index.js
+++ src/router/index.js
@@ -113,18 +113,6 @@ export default function ({ store }) {
       name: to.name,
       path: to.path
     })
-
-    // Clear HubSpot widget flag when navigating away from widget to non-widget pages
-    // But don't clear during Login flow (auth pages) or root (common intermediate step)
-    const wasOnWidget = from.matched.some(route => route?.meta?.isHubSpotWidget)
-    const isNowOnWidget = to.matched.some(route => route?.meta?.isHubSpotWidget)
-    const isNowOnAuth = to.matched.some(route => route?.meta?.isGuest)
-    const isNowOnRoot = to.path === '/' || to.name === 'Inbox'
-
-    if (wasOnWidget && !isNowOnWidget && !isNowOnAuth && !isNowOnRoot) {
-      console.log('[Router] Left HubSpot widget, clearing flag')
-      store.commit('SET_IS_HUBSPOT_WIDGET', false)
-    }
   })
 
   if (process.env.APP_ENV === 'production' && process.env.GA_TRACKING_ID) {
