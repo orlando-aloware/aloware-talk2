5e717d00f9e87cf582f9ce36f0a642f7fa6d4be4
Yhael S <yhael@aloware.com>
2025-10-22 14:21:38 -0600

    enhacement(PLA-831): cleanup

--- src/constants/rejection-reason-messages.js
+++ src/constants/rejection-reason-messages.js
@@ -55,7 +55,7 @@ export const REJECTION_REASONS = [
   },
   {
     type: CommunicationRejectionReasons.REJECTION_REASON_NUMBER_IS_INTERNATIONAL,
-    message: 'The Traffic to this country is not allowed. Contact Support to get access to this country.'
+    message: 'The traffic to this country is not allowed. Contact Support to get access to this country.'
   },
   {
     type: CommunicationRejectionReasons.REJECTION_REASON_INVALID_OR_WRONG_PHONE_NUMBER,
@@ -147,6 +147,6 @@ export const REJECTION_REASONS = [
   },
   {
     type: CommunicationRejectionReasons.REJECTION_REASON_BANNED_COUNTRY,
-    message: 'The traffic to this country is not allowed'
+    message: 'The traffic to this country is not allowed.'
   }
 ]
