c0446b21155dfd893141d720f92f7a8b834c319d
Jego Carlo Ramos <jego@aloware.com>
2025-10-25 07:30:43 +0800

    test: reactivity fix

--- src/components/dialer/dialer.vue
+++ src/components/dialer/dialer.vue
@@ -62,7 +62,7 @@ export default {
       loadingUnhold: false,
       loadingPark: false,
       loadingUnpark: false,
-      device: new TwilioDevice(null),
+      // device is now non-reactive (created in beforeCreate) // TODO: Test
       connection: null,
       warnings: [],
       hangupInterval: null,
@@ -127,6 +127,13 @@ export default {
     isOnPowerDialerSessionRoute () {
       return this.$route?.meta?.id === 'power-dialer-session'
     }
+  },
+
+  // TODO: Remove beforeCreate if not working
+  beforeCreate () {
+    // Create Twilio device as a non-reactive property
+    // This prevents Vue's Observer from wrapping it and breaking its internal event system
+    this.device = new TwilioDevice(null)
   },
 
   created () {
