22d7b4e5cb760e03fbbf94c41b24cc7f41ec48f0
Jego Carlo Ramos <jego@aloware.com>
2025-10-25 06:15:09 +0800

    test: possible fix

--- src/plugins/mixins/notification.mixin.js
+++ src/plugins/mixins/notification.mixin.js
@@ -216,7 +216,7 @@ export default {
     },
 
     processActionNotification (communication, type) {
-      if (this.isWidget && !this.isSalesforceWidget && !this.isHubSpotWidget) { // TODO: maybe just show this instead
+      if (this.isWidget && !this.isSalesforceWidget) {
         return
       }
       const name = { data: '' }
