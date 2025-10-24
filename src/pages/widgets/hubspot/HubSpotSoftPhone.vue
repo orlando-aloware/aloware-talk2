740be6d80ad58046b0dd1d1e20130317d100eb0c
Jego Carlo Ramos <jego@aloware.com>
2025-10-25 04:41:17 +0800

    chore: cleanup

--- src/pages/widgets/hubspot/HubSpotSoftPhone.vue
+++ src/pages/widgets/hubspot/HubSpotSoftPhone.vue
@@ -596,7 +596,6 @@ export default {
       'setIsHubSpotWidget',
       'setHubspotDialNumber',
       'setDialerCommunication',
-      'setDialerCall',
       'setDialerContact',
       'setDialerCurrentStatus'
     ]),
@@ -1205,16 +1204,6 @@ export default {
       // Apply the returned state
       if (result.success) {
         this.setDialerCommunication(result.communication)
-        
-        // Set dialer.call with direction so the phone UI shows accept/reject buttons
-        // TODO: Maybe not needed
-        const callWithDirection = {
-          ...result.communication,
-          direction: 'INCOMING'
-        }
-        
-        this.setDialerCall(callWithDirection)
-        
         this.setDialerContact(result.contact)
         this.setDialerCurrentStatus(result.dialerStatus)
         this.displayState = result.displayState
