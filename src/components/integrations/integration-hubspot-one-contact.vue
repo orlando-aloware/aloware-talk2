f0dd3ada6e6ae78f576eabb6460f4f5c727c0dff
Jego Carlo Ramos <jego@aloware.com>
2025-11-05 03:01:21 +0800

    feature: jit card deal icon

--- src/components/integrations/integration-hubspot-one-contact.vue
+++ src/components/integrations/integration-hubspot-one-contact.vue
@@ -193,14 +193,17 @@
                 flat bordered>
           <q-card-section>
             <q-card-section class='p-0'>
-              <h6 class='mb-2'>
+              <h6 class='mb-0'>
                 <b-link class='deals-title ml-0'
-                        :href="deal.link"
-                        data-testid='integration-hubspot-deal-link'
-                        target='_blank'>
-                  {{ deal.properties.dealname }}
+                          :href="deal.link"
+                          data-testid='integration-hubspot-deal-link'
+                          target='_blank'>
+                  <div class='d-flex justify-content-between align-items-center mb-2'>
+                      <span>{{ deal.properties.dealname }}</span>
+                      <span class='font-weight-bold'>$</span>
+                  </div>
                 </b-link>
-              </h6>
+                </h6>
               <p class='mb-1 d-flex' data-testid='integration-hubspot-amount'>
                 <span class='data-icon-label'>Amount: </span>
                 <span class='data-value ml-1'
