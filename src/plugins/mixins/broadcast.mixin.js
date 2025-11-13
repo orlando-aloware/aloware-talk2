2029dfbc6f1bfbab991e6d3359812d7b007605ef
Reza Aleyasin <reza@aloware.com>
2025-11-13 15:40:25 +0400

    feature: enhance file migrated event to support broadcasting and update related components

--- src/plugins/mixins/broadcast.mixin.js
+++ src/plugins/mixins/broadcast.mixin.js
@@ -263,6 +263,10 @@ export default {
           this.dispatchToMemoryMonitor('recording_status.updated', event)
           this.$VueEvent.fire('updated_recording_status', event)
         })
+        .listen('.communication.file.migrated', (event) => {
+          this.dispatchToMemoryMonitor('.communication.file.migrated', event)
+          this.$VueEvent.fire('communication.file.migrated', event)
+        })
 
         /**
          * ------------------------------------
