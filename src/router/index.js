88059bdc2344baca8d3f320e281d2ef9a90cf82b
Jego Carlo Ramos <jego@aloware.com>
2025-12-10 00:23:29 +0800

    Revert "Merge pull request #2627 from aloware/int-671/feature/inbound-calling-hs"
    This reverts commit 89a8e3b7f407f6c596cfffcd2338807b514c1121, reversing
    changes made to 722dd3ceaf4ea70dc79274b54a3ced63e9e29404.

--- src/router/index.js
+++ src/router/index.js
@@ -53,12 +53,6 @@ export default function ({ store }) {
   })
 
   Router.beforeEach((to, from, next) => {
-    // HubSpot widget restoration: if the widget was active, and we're navigating to root, redirect to the widget
-    if (store.state.isHubSpotWidget && (to.path === '/' || to.name === 'Inbox')) {
-      console.log('[Router] Restoring HubSpot widget route')
-      return next('/widgets/hubspot-call-extension')
-    }
-
     // Redirect old legacy inbox paths
     // Note: This handles old URLs that might still be accessed via direct links
     const isLegacyInboxRoute = isLegacyInboxPath(to.path)
@@ -91,12 +85,6 @@ export default function ({ store }) {
 
     if (isSalesforceWidget) {
       store.commit('SET_IS_SALESFORCE_WIDGET', true)
-    }
-
-    const isHubSpotWidget = to.matched.some(route => route?.meta?.isHubSpotWidget)
-
-    if (isHubSpotWidget) {
-      store.commit('SET_IS_HUBSPOT_WIDGET', true)
     }
 
     const record = to.matched.find(record => record.meta.title)
--- src/store/index.js
+++ src/store/index.js
@@ -58,7 +58,6 @@ export default function (/* { ssrContext } */) {
     state: {
       isWidget: false,
       isSalesforceWidget: false,
-      isHubSpotWidget: false,
       showMenu: false,
       filter: {},
       tags: [],
@@ -346,10 +345,6 @@ export default function (/* { ssrContext } */) {
 
       setIsSalesforceWidget ({ commit }, value) {
         commit('SET_IS_SALESFORCE_WIDGET', value)
-      },
-
-      setIsHubSpotWidget ({ commit }, value) {
-        commit('SET_IS_HUBSPOT_WIDGET', value)
       },
 
       setDialerToken ({ commit }, token) {
@@ -1008,10 +1003,6 @@ export default function (/* { ssrContext } */) {
 
       SET_IS_SALESFORCE_WIDGET (state, value) {
         state.isSalesforceWidget = value
-      },
-
-      SET_IS_HUBSPOT_WIDGET (state, value) {
-        state.isHubSpotWidget = value
       },
 
       SET_DIALER_TOKEN (state, token) {
